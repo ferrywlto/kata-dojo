572d6870886138ae3a46b170fd786db5bb0f2282
Ferry To <ferrywlto@gmail.com>
2023-12-21 12:27:50 +0800

    test: add roman only constraints tests

--- csharp/leetcode/easy/Q13_RomanToInteger.cs
+++ csharp/leetcode/easy/Q13_RomanToInteger.cs
@@ -16,6 +16,20 @@ public class Q13_RomainToIntegerTests {
         var sut = new Q13_RomainToInteger();
         Assert.False(sut.ValidateInput(input));
     }
+    [Theory]
+    [InlineData("I")]
+    [InlineData("IVXLCDM")]
+    public void ValidateInput_ShouldReturnTrue_ContainsRomanOnly(string input) {
+        var sut = new Q13_RomainToInteger();
+        Assert.True(sut.ValidateInput(input));
+    }
+    [Theory]
+    [InlineData("A")]
+    [InlineData("IVXLBCDM")]
+    public void ValidateInput_ShouldReturnFalse_ContainsNonRoman(string input) {
+        var sut = new Q13_RomainToInteger();
+        Assert.False(sut.ValidateInput(input));
+    }
 }
 
 public class Q13_RomainToInteger {
@@ -23,7 +37,11 @@ public class Q13_RomainToInteger {
         return 0;
     }
 
+    const string roman = "IVXLCDM";
     public bool ValidateInput(string input) {
+        foreach(var c in input) {
+            if (!roman.Contains(c)) return false;
+        }
         return input.Length > 0 && input.Length <= 15;
     }
 }

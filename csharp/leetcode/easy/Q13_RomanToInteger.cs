2474f5743d232da9bf13087e6ab2daa4308b7d77
Ferry To <ferrywlto@gmail.com>
2023-12-21 12:23:15 +0800

    test: add length constraints tests

--- csharp/leetcode/easy/Q13_RomanToInteger.cs
+++ csharp/leetcode/easy/Q13_RomanToInteger.cs
@@ -1,9 +1,29 @@
 namespace dojo.leetcode;
 
 public class Q13_RomainToIntegerTests {
-
+    [Theory]
+    [InlineData("I")]
+    [InlineData("III")]
+    [InlineData("IIIIVIIIIVIIIIV")]
+    public void ValidateInput_ShouldReturnTrue_LengthWithinRnage(string input) {
+        var sut = new Q13_RomainToInteger();
+        Assert.True(sut.ValidateInput(input));
+    }
+    [Theory]
+    [InlineData("")]
+    [InlineData("IIIIVIIIIVIIIIVI")]
+    public void ValidateInput_ShouldReturnFalse_LengthOutOfRange(string input) {
+        var sut = new Q13_RomainToInteger();
+        Assert.False(sut.ValidateInput(input));
+    }
 }
 
 public class Q13_RomainToInteger {
+    public int RomanToInt(string s) {
+        return 0;
+    }
 
+    public bool ValidateInput(string input) {
+        return input.Length > 0 && input.Length <= 15;
+    }
 }

764c154060b92792cd0931f134091506018746b4
Ferry To <ferrywlto@gmail.com>
2023-12-23 15:21:51 +0800

    feat: Add util method to create ListNodes from integer array

--- csharp/leetcode/ListNode.cs
+++ csharp/leetcode/ListNode.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace dojo.leetcode;
 
 public class ListNode 
@@ -8,5 +10,20 @@ public class ListNode
     {
         this.val = val;
         this.next = next;
+    }
+
+    public static ListNode? FromArray(int[] arr) 
+    {
+        if (arr.Length == 0)
+            return null;
+
+        var head = new ListNode(arr[0]);
+        var current = head;
+        for (int i = 1; i < arr.Length; i++) 
+        {
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+        return head;
     }
 }

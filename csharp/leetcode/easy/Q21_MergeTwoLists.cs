ac2d080b9d8ef5179aa63ef343a8bd8ee5b8c508
Ferry To <ferrywlto@gmail.com>
2023-12-23 15:58:24 +0800

    test: All tests passed

--- csharp/leetcode/easy/Q21_MergeTwoLists.cs
+++ csharp/leetcode/easy/Q21_MergeTwoLists.cs
@@ -8,14 +8,16 @@ public class Q21_MergeTwoListsTests(ITestOutputHelper output) : ListNodeTest(out
     public void OfficialTestCases(int[] l1, int[] l2, int[] expected) {
         var list1 = ListNode.FromArray(l1);
         var list2 = ListNode.FromArray(l2);
-        var actual = Q21_MergeTwoLists.MergeTwoLists(list1, list2);
         var expectedList = ListNode.FromArray(expected);
+        var actual = Q21_MergeTwoLists.MergeTwoLists(list1, list2);
         AssertListNodeEquals(expectedList, actual);
     }
 }
 
 public class Q21_MergeTwoLists {
     
+    // Speed: 45ms (100%), Memory: 41.4MB (8.83%)
+    // Don't think there is room to use less memory as there is only references to the nodes, no data/node created. 
     public static ListNode? MergeTwoLists(ListNode? l1, ListNode? l2) {
         if (l1 == null && l2 == null)
             return null;
@@ -24,7 +26,32 @@ public class Q21_MergeTwoLists {
         else if(l2 == null)
             return l1;
         
+        ListNode current;
+        if(l1.val >= l2.val) {
+            current = l2;
+            l2 = l2.next;
+        } else {
+            current = l1;
+            l1 = l1.next;
+        }
+        var head = current;
 
-        return new ListNode(0);
+        while (l1 != null && l2 != null) {
+            if (l1.val >= l2.val) {
+                current.next = l2;
+                l2 = l2.next;
+            } else {
+                current.next = l1;
+                l1 = l1.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null)
+            current.next = l1;
+        else if (l2 != null)
+            current.next = l2;
+
+        return head;
     }
 }

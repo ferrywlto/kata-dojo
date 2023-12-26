54eab0c016785fb102042205f1f7052a77258d0a
Ferry To <ferrywlto@gmail.com>
2023-12-26 18:16:19 +0800

    test: All test cases passed

--- csharp/leetcode/easy/Q67_AddBinary.cs
+++ csharp/leetcode/easy/Q67_AddBinary.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 public class Q67_AddBinaryTests {
     [Theory]
     [InlineData("11", "1", "100")]
@@ -28,6 +26,7 @@ a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
 */
 public class Q67_AddBinary {
+    // Speed: 51ms (99.39%), Memory: 40.08MB (44.57%)
     public string AddBinary(string a, string b) {
         string shorterStr;
         string longerStr; 
@@ -39,17 +38,18 @@ public class Q67_AddBinary {
             longerStr = b;
             shorterStr = a;
         }
-        
-        var result = new StringBuilder();
+        var result = new char[longerStr.Length];
 
         bool carryOver = false;
-        for (int i=1; i<shorterStr.Length; i++) {
+        
+        for (int i=1; i<=shorterStr.Length; i++) {
+            
             if (carryOver) {
                 if (shorterStr[^i] == '0' && longerStr[^i] == '0') {
                     result[^i] = '1';
                     carryOver = false;
                 } 
-                else if (shorterStr[^i] == '1' && a[^i] == '1') {
+                else if (shorterStr[^i] == '1' && longerStr[^i] == '1') {
                     result[^i] = '1';
                     carryOver = true;
                 }
@@ -59,11 +59,11 @@ public class Q67_AddBinary {
                 }
             }
             else {
-                if (a[^i] == '0' && b[^i] == '0') {
+                if (shorterStr[^i] == '0' && longerStr[^i] == '0') {
                     result[^i] = '0';
                     carryOver = false;
                 } 
-                else if (a[^i] == '1' && a[^i] == '1') {
+                else if (shorterStr[^i] == '1' && longerStr[^i] == '1') {
                     result[^i] = '0';
                     carryOver = true;
                 }
@@ -73,6 +73,33 @@ public class Q67_AddBinary {
                 }
             }
         }
-        return result.ToString();
+
+        var diff = longerStr.Length - shorterStr.Length;
+        if (diff > 0) {
+            for (int j= diff-1; j>=0; j--) {
+                if(carryOver) {
+                    if (longerStr[j]=='0')
+                    {
+                        result[j] = '1';
+                        carryOver = false;
+                    }
+                    else {
+                        result[j] = '0';
+                        carryOver = true;
+                    }
+                } else {
+                    result[j] = longerStr[j];
+                }
+            }        
+        }
+
+        if (!carryOver)
+            return new string(result);
+        else {
+            char[] x = new char[result.Length+1];
+            Array.Copy(result, 0, x, 1, result.Length);
+            x[0] = '1';
+            return new string(x);
+        }
     }
 }

4518539ce5abee9ac30e4b9c5e230e8dd4fc71d4
Ferry To <ferrywlto@gmail.com>
2023-12-26 17:03:35 +0800

    test: Add extra test cases

--- csharp/leetcode/easy/Q67_AddBinary.cs
+++ csharp/leetcode/easy/Q67_AddBinary.cs
@@ -1,8 +1,20 @@
+using System.Text;
+
 public class Q67_AddBinaryTests {
     [Theory]
     [InlineData("11", "1", "100")]
     [InlineData("1010", "1011", "10101")]
     public void OfficialTestCases(string a, string b, string expected) {
+        var sut = new Q67_AddBinary();
+        Assert.Equal(expected, sut.AddBinary(a, b));
+    }
+
+    [Theory]
+    [InlineData("0", "0", "0")]
+    [InlineData("0", "1", "1")]
+    [InlineData("1", "0", "1")]
+    [InlineData("1", "1", "10")]
+    public void ExtraTestCases(string a, string b, string expected) {
         var sut = new Q67_AddBinary();
         Assert.Equal(expected, sut.AddBinary(a, b));
     }
@@ -17,6 +29,50 @@ Each string does not contain leading zeros except for the zero itself.
 */
 public class Q67_AddBinary {
     public string AddBinary(string a, string b) {
-        return string.Empty;
+        string shorterStr;
+        string longerStr; 
+        if (a.Length >= b.Length) {
+            longerStr = a;
+            shorterStr = b;
+        }
+        else {
+            longerStr = b;
+            shorterStr = a;
+        }
+        
+        var result = new StringBuilder();
+
+        bool carryOver = false;
+        for (int i=1; i<shorterStr.Length; i++) {
+            if (carryOver) {
+                if (shorterStr[^i] == '0' && longerStr[^i] == '0') {
+                    result[^i] = '1';
+                    carryOver = false;
+                } 
+                else if (shorterStr[^i] == '1' && a[^i] == '1') {
+                    result[^i] = '1';
+                    carryOver = true;
+                }
+                else {
+                    result[^i] = '0';
+                    carryOver = true;
+                }
+            }
+            else {
+                if (a[^i] == '0' && b[^i] == '0') {
+                    result[^i] = '0';
+                    carryOver = false;
+                } 
+                else if (a[^i] == '1' && a[^i] == '1') {
+                    result[^i] = '0';
+                    carryOver = true;
+                }
+                else {
+                    result[^i] = '1';
+                    carryOver = false;
+                }
+            }
+        }
+        return result.ToString();
     }
 }

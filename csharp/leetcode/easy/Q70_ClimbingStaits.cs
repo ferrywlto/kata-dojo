8074f24bb8b3be3d0ccca094dbbb2dcacdf0cceb
Ferry To <ferrywlto@gmail.com>
2023-12-28 18:15:50 +0800

    test: All test cases passed

--- csharp/leetcode/easy/Q70_ClimbingStaits.cs
+++ csharp/leetcode/easy/Q70_ClimbingStaits.cs
@@ -1,7 +1,12 @@
+using System.Data;
+
 public class Q70_ClimbingStairsTests {
     [Theory]
     [InlineData(2,2)]
     [InlineData(3,3)]
+    [InlineData(4,5)]
+    [InlineData(5,8)]
+    [InlineData(45,1836311903)]
     public void OfficialTestCases(int input, int expected) {
         var sut = new Q70_ClimbingStairs();
         Assert.Equal(expected, sut.ClimbStairs(input));
@@ -9,7 +14,16 @@ public class Q70_ClimbingStairsTests {
 }
 
 public class Q70_ClimbingStairs {
+    
+    Dictionary<int, int> dict = new() {{1,1}, {2,2}, {3,3} };
+    // Speed:26ms (19.89%), Memory: 27.1MB (12.73%)
     public int ClimbStairs(int n) {
-        return 0;
+
+        if (dict.TryGetValue(n, out int value)) {
+            return value;
+        }
+        var sum = ClimbStairs(n-1) + ClimbStairs(n-2);
+        dict.Add(n, sum);
+        return sum;
     }
 }

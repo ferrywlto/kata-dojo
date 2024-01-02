8462532aa1ea6a7f51bb3488d5a81b73faf275c4
Ferry To <ferrywlto@gmail.com>
2024-01-02 14:27:03 +0800

    test: All test cases passed

--- csharp/leetcode/easy/Q119_PascelTriangle2.cs
+++ csharp/leetcode/easy/Q119_PascelTriangle2.cs
@@ -3,11 +3,11 @@ public class Q119_PascelTriangle2TestData : IEnumerable<object[]>
 {
     private readonly List<object[]> _data =
     [
-        [0, new List<IList<int>>{new List<int>() {1}}],
-        [1, new List<IList<int>>{new List<int>() {1, 1}}],
-        [2, new List<IList<int>>{new List<int>() {1, 2, 1}}],
-        [3, new List<IList<int>>{new List<int>() {1, 3, 3, 1}}],
-        [4, new List<IList<int>>{new List<int>() {1, 4, 6, 4, 1}}],
+        [0, new List<int>() {1}],
+        [1, new List<int>() {1, 1}],
+        [2, new List<int>() {1, 2, 1}],
+        [3, new List<int>() {1, 3, 3, 1}],
+        [4, new List<int>() {1, 4, 6, 4, 1}],
     ];
 
     public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
@@ -32,8 +32,30 @@ Constraints:
 */
 public class Q119_PascelTriangle2
 {
+    // input must start and end with 1
     public IList<int> GetRow(int rowIndex)
     {
-        return new List<int>();
+        // because the tests are zero based
+        rowIndex++;
+        var input = new List<int>() { 1 };
+        if (rowIndex == 1) return input;
+        else if(rowIndex == 2) {
+            input.Insert(0, 1);
+            return input;
+        }
+
+        for (var j = 1; j < rowIndex; j++)
+        {
+            // add generated from input 
+            for (var i = 0; i < input.Count - 1; i++)
+            {
+                input[i] = input[i] + input[i + 1];
+            }
+            // set the last to 1;
+            input[input.Count - 1] = 1;
+            // append 1 at the beginning
+            input.Insert(0, 1);
+        }
+        return input;
     }
 }

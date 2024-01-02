b6db98e55b1f8d4e5e538e461b599b9783420917
Ferry To <ferrywlto@gmail.com>
2024-01-02 14:39:15 +0800

    perf: Improve SC from O(n^2) to O(n)

--- csharp/leetcode/easy/Q119_PascelTriangle2.cs
+++ csharp/leetcode/easy/Q119_PascelTriangle2.cs
@@ -32,7 +32,7 @@ Constraints:
 */
 public class Q119_PascelTriangle2
 {
-    // input must start and end with 1
+    // TC: still O(n^2), SC: Improved from O(n^2) to O(n)
     public IList<int> GetRow(int rowIndex)
     {
         // because the tests are zero based
@@ -40,21 +40,23 @@ public class Q119_PascelTriangle2
         var input = new List<int>() { 1 };
         if (rowIndex == 1) return input;
         else if(rowIndex == 2) {
-            input.Insert(0, 1);
+            input.Add(1);
             return input;
         }
 
+        // The main idea is, the next row is always having one more element than the previous row.
+        // therefore we can either doing right to left by updating each element by adding the next element and then insert 1 at the beginning
+        // or we can do it in reverse, update each elemeng by adding the previous element and then append 1 at the end
+        // doing the reverse way is more efficient because .Add() is O(1) while .Insert() is O(n)
         for (var j = 1; j < rowIndex; j++)
         {
             // add generated from input 
-            for (var i = 0; i < input.Count - 1; i++)
+            for (var i = input.Count - 1; i >= 1; i--)
             {
-                input[i] = input[i] + input[i + 1];
+                input[i] = input[i] + input[i- 1];
             }
-            // set the last to 1;
-            input[input.Count - 1] = 1;
-            // append 1 at the beginning
-            input.Insert(0, 1);
+            // append 1 at the end
+            input.Add(1);
         }
         return input;
     }

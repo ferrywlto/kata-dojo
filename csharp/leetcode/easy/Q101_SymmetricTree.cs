0f4564437e88b1bd0fa13469dffcfb530083032f
Ferry To <ferrywlto@gmail.com>
2024-01-03 21:06:06 +0800

    perf: Add iteration version

--- csharp/leetcode/easy/Q101_SymmetricTree.cs
+++ csharp/leetcode/easy/Q101_SymmetricTree.cs
@@ -19,7 +19,8 @@ public class Q101_SymmetricTreeTests : TreeNodeTests
     {
         var sut = new Q101_SymmetricTree();
         var node = TreeNode.FromLevelOrderingIntArray(input);
-        var actual = sut.IsSymmetric_Recursion(node!);
+        // var actual = sut.IsSymmetric_Recursion(node!);
+        var actual = sut.IsSymmetric_Loop(node!);
         Assert.Equal(expected, actual);
     }
 }
@@ -45,5 +46,34 @@ public class Q101_SymmetricTree
                 && IsSymmetric_Recursion(leftTree?.left, rightTree?.right) 
                 && IsSymmetric_Recursion(leftTree?.right, rightTree?.left);
         }
+    }
+
+    // Demonstration purpose of not using recursion.
+    public bool IsSymmetric_Loop(TreeNode root)
+    {
+        if (root == null || (root.left == null && root.right == null ) )
+            return true;
+
+        var leftQueue = new Queue<TreeNode?>();
+        var rightQueue = new Queue<TreeNode?>();
+
+        leftQueue.Enqueue(root.left);
+        rightQueue.Enqueue(root.right);
+
+        while(leftQueue.Count > 0 && rightQueue.Count > 0) 
+        {
+            var leftCurrent = leftQueue.Dequeue();
+            var rightCurrent = rightQueue.Dequeue();
+
+            if (leftCurrent == null && rightCurrent == null) continue;
+            else if (leftCurrent?.val != rightCurrent?.val) return false;
+
+            leftQueue.Enqueue(leftCurrent?.left);
+            rightQueue.Enqueue(rightCurrent?.right);
+
+            leftQueue.Enqueue(leftCurrent?.right);
+            rightQueue.Enqueue(rightCurrent?.left);
+        }
+        return true;
     }
 }

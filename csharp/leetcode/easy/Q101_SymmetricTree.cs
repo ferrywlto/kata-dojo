e3d6cc4b6f55cb8bf9e0c7dcb6185386a13a9e14
Ferry To <ferrywlto@gmail.com>
2024-01-03 18:27:29 +0800

    test: Add test cases passed

---
+++ csharp/leetcode/easy/Q101_SymmetricTree.cs
@@ -0,0 +1,49 @@
+public class Q101_SymmetricTreeTestData : IEnumerable<object[]>
+{
+    private readonly List<object[]> _data =
+    [
+        [new int?[]{1, 2, 2, 3, 4, 4, 3}, true],
+        [new int?[]{1, 2, 2, null, 3, null, 3}, false],
+    ];
+
+    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class Q101_SymmetricTreeTests : TreeNodeTests
+{
+    [Theory]
+    [ClassData(typeof(Q101_SymmetricTreeTestData))]
+    public void OfficialTestCases(int?[] input, bool expected)
+    {
+        var sut = new Q101_SymmetricTree();
+        var node = TreeNode.FromLevelOrderingIntArray(input);
+        var actual = sut.IsSymmetric_Recursion(node!);
+        Assert.Equal(expected, actual);
+    }
+}
+
+public class Q101_SymmetricTree
+{
+    // the idea is, given two trees, A.left should equals to B.right and vice versa.
+    // Linear complexity using recursion. TC: O(nodes_of_tree), SC: O(height_of_tree)
+    public bool IsSymmetric_Recursion(TreeNode root) 
+    {
+        if (root == null || (root.left == null && root.right == null ) )
+            return true;
+
+        return IsSymmetric_Recursion(root.left, root.right);    
+    }
+
+    public bool IsSymmetric_Recursion(TreeNode? leftTree, TreeNode? rightTree) 
+    {
+        if (leftTree == null && rightTree == null) return true;
+        else 
+        {
+            return leftTree?.val == rightTree?.val 
+                && IsSymmetric_Recursion(leftTree?.left, rightTree?.right) 
+                && IsSymmetric_Recursion(leftTree?.right, rightTree?.left);
+        }
+    }
+}

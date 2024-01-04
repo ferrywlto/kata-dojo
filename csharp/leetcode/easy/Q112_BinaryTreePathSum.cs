308fb386d75f11336657c8d81b57c259d8dfe308
Ferry To <ferrywlto@gmail.com>
2024-01-05 02:12:09 +0800

    test: All test cases passed

--- csharp/leetcode/easy/Q112_BinaryTreePathSum.cs
+++ csharp/leetcode/easy/Q112_BinaryTreePathSum.cs
@@ -6,8 +6,8 @@ public class Q112_BinaryTreePathSumTestData :LeetCodeTestData
     [
         [new int?[]{5,4,8,11,null,13,4,7,2,null,null,null,1}, 22, true],
         [new int?[]{1,2,3}, 5, false],
+        [new int?[]{1,2}, 1, false],
         [Array.Empty<int?>(), 0, false],
-    
     ];
 }
 public class Q112_BinaryTreePathSumTests 
@@ -24,8 +24,18 @@ public class Q112_BinaryTreePathSumTests
 
 public class Q112_BinaryTreePathSum 
 {
+    // TC: O(n), SC: O(n)
     public bool HasPathSum(TreeNode root, int targetSum) 
     {
-        return false;    
+        return SumRecursive(root, 0, targetSum); 
     } 
+
+    public bool SumRecursive(TreeNode? node, int sum, int targetSum)
+    {
+        if (node == null) return false;
+        if (sum + node.val == targetSum && IsLeaf(node)) return true;
+
+        return SumRecursive(node.left, sum + node.val, targetSum) || SumRecursive(node.right, sum + node.val, targetSum);
+    }
+    public bool IsLeaf(TreeNode node) => node.left == null && node.right == null;
 }

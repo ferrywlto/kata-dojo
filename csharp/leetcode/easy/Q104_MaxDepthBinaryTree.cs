97f59b8f7bf2d3efd29af1e33e951d2db49e60da
Ferry To <ferrywlto@gmail.com>
2024-01-04 22:20:56 +0800

    perf: Add iterative solution

--- csharp/leetcode/easy/Q104_MaxDepthBinaryTree.cs
+++ csharp/leetcode/easy/Q104_MaxDepthBinaryTree.cs
@@ -19,7 +19,7 @@ public class Q104_MaxDepthBinaryTreeTests
     {
         var sut = new Q104_MaxDepthBinaryTree();
         var tree = TreeNode.FromLevelOrderingIntArray(input);
-        var actual = sut.MaxDepth(tree!);
+        var actual = sut.MaxDepth_Iterative(tree!);
         Assert.Equal(expected, actual);
     }
 }
@@ -30,5 +30,28 @@ public class Q104_MaxDepthBinaryTree
     public int MaxDepth(TreeNode root)
     {
         return root == null? 0 : 1 + Math.Max(MaxDepth(root.left!), MaxDepth(root?.right!));
+    }
+
+    // Demonstrate iterative solution in addition to recursive solution
+    public int MaxDepth_Iterative(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        var stack = new Stack<(TreeNode, int)>();
+        stack.Push((root, 1));
+        var max = 1;
+
+        while (stack.Count > 0) 
+        {
+            var (node, depth) = stack.Pop();
+
+            if (node != null) 
+            {
+                max = Math.Max(max, depth);
+                stack.Push((node.left!, depth + 1));
+                stack.Push((node.right!, depth + 1));
+            }
+        }
+        return max;
     }
 }

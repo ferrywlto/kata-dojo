94197046b5aaaf4a5434bf3e2c4bf2efd0e21dc9
Ferry To <ferrywlto@gmail.com>
2024-01-09 12:36:21 +0800

    perf: Add SC: O(1) approach

--- csharp/leetcode/easy/Q169_MajorityElement.cs
+++ csharp/leetcode/easy/Q169_MajorityElement.cs
@@ -8,14 +8,14 @@ public class Q169_MajorityElementTestData : TestDataBase
         [new int[] { 2, 2, 1, 1, 1, 2, 2}, 2]
     ];
 }
-public class Q169_MajorityElementTests(ITestOutputHelper output) : TestBase(output) 
+public class Q169_MajorityElementTests(ITestOutputHelper output) : TestBase(output)
 {
     [Theory]
     [ClassData(typeof(Q169_MajorityElementTestData))]
-    public void OfficialTestCases(int[] nums, int expected) 
+    public void OfficialTestCases(int[] nums, int expected)
     {
         var sut = new Q169_MajorityElement();
-        var res = sut.MajorityElement(nums);
+        var res = sut.MajorityElementEfficient(nums);
 
         Assert.Equal(expected, res);
     }
@@ -23,22 +23,50 @@ public class Q169_MajorityElementTests(ITestOutputHelper output) : TestBase(outp
 
 public class Q169_MajorityElement
 {
+    // TC: O(n), SC: O(n)
     public int MajorityElement(int[] nums)
     {
         var dict = new Dictionary<int, int>();
         var half = nums.Length / 2;
-        for(var i=0; i<nums.Length; i++) 
+        for (var i = 0; i < nums.Length; i++)
         {
-            if (dict.TryGetValue(nums[i], out int value)) 
+            if (dict.TryGetValue(nums[i], out int value))
             {
                 dict[nums[i]] = ++value;
             }
-            else 
+            else
             {
                 dict.Add(nums[i], 1);
             }
             if (dict[nums[i]] > half) return nums[i];
         }
         return 0;
+    }
+
+    // Demonstration of using Boyer-Moore Voting Algorithm suggested from Copilot
+    // TC: O(n), SC: O(1)
+    public int MajorityElementEfficient(int[] nums)
+    {
+        int count = 0;
+        int mostOccur = int.MaxValue;
+
+        foreach (int num in nums)
+        {
+            if (count == 0)
+            {
+                mostOccur = num;
+            }
+
+            if (num == mostOccur)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+
+        return mostOccur;
     }
 }

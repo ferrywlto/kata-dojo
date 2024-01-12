0397fc8e7da3293a33fbc7df3f700d007c295171
Ferry To <ferrywlto@gmail.com>
2024-01-13 00:22:58 +0800

    test: Add offical test cases for Q195 and all test cases passed

---
+++ csharp/leetcode/easy/Q195_TenthLine.cs
@@ -0,0 +1,70 @@
+
+namespace dojo.leetcode;
+
+public class Q195_TenthLineTestData : TestData
+{
+    protected override List<object[]> Data => 
+    [
+        [
+            """
+            Line 1
+            Line 2
+            Line 3
+            Line 4
+            Line 5
+            Line 6
+            Line 7
+            Line 8
+            Line 9
+            Line 10
+            """,
+            "Line 10"
+        ],
+        [
+            """
+            Line 1
+            Line 2
+            Line 3
+            Line 4
+            Line 5
+            Line 6
+            Line 7
+            Line 8
+            Line 9
+            """,
+            ""
+        ]
+    ];
+}
+
+public class Q195_TenthLineTests(ITestOutputHelper output) : ShellTest(output)
+{
+    [Theory]
+    [ClassData(typeof(Q195_TenthLineTestData))]
+    public void Test(string input, string expected) 
+    {
+        File.WriteAllText("./file.txt", input.Trim());
+
+        var sut = new Q195_TenthLine();
+        var actual = Run(sut.Command);
+        
+        File.Delete("./file.txt");
+
+        var expectedLines = expected.Trim().Split(Environment.NewLine);
+        var actualLines = actual.Trim().Split(Environment.NewLine);
+
+        foreach(var line in actualLines)
+        {
+            output.WriteLine(line);
+            Assert.Contains(line, expectedLines);
+        }
+    }
+}
+
+public class Q195_TenthLine
+{
+    public string Command =>
+    """
+    awk 'NR == 10' file.txt
+    """;
+}

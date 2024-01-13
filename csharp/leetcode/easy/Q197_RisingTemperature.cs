934c1218ce552b90ee4e4609f9f3d4ff385eb56b
Ferry To <ferrywlto@gmail.com>
2024-01-13 17:59:29 +0800

    test: Add offiical test case for Q197 and all test case passed

---
+++ csharp/leetcode/easy/Q197_RisingTemperature.cs
@@ -0,0 +1,51 @@
+namespace dojo.leetcode;
+
+public class Q197_RisingTemperatureTestData : TestData
+{
+    protected override List<object[]> Data =>
+    [
+        [
+            """
+            INSERT INTO Weather VALUES (1, '2015-01-01', 10);
+            INSERT INTO Weather VALUES (2, '2015-01-02', 25);
+            INSERT INTO Weather VALUES (3, '2015-01-03', 20);
+            INSERT INTO Weather VALUES (4, '2015-01-04', 30);
+            """
+        ]
+    ];
+}
+
+public class Q197_RisingTemperatureTests(ITestOutputHelper output): DatabaseTest(output)
+{
+    protected override string TestSchema =>
+    """
+    CREATE TABLE IF NOT EXISTS Weather (id INT, recordDate DATE, temperature INT);
+    """;
+
+    [Theory]
+    [ClassData(typeof(Q197_RisingTemperatureTestData))]
+    public void OfficialTestCases(string input)
+    {
+        InputTestData(input);
+
+        var sut = new Q197_RisingTemperature();
+        var result = ExecuteQuery(sut.Command);
+
+        Assert.True(result.Read());
+        Assert.Equal(2, result.GetInt32(0));
+
+        Assert.True(result.Read());
+        Assert.Equal(4, result.GetInt32(0));
+    }
+}
+
+
+public class Q197_RisingTemperature
+{
+    public string Command =>
+    """
+    SELECT w1.id FROM Weather w1, Weather w2 
+    WHERE w1.recordDate = Date(w2.recordDate, '+1 days') 
+    AND w1.temperature > w2.temperature; 
+    """;
+}

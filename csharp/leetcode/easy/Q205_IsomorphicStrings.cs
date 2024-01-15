be23074f0b544d9bdc5c6ddae236c3f7932fe19e
Ferry To <ferrywlto@gmail.com>
2024-01-15 23:58:33 +0800

    test: All test cases for Q205 passed

--- csharp/leetcode/easy/Q205_IsomorphicStrings.cs
+++ csharp/leetcode/easy/Q205_IsomorphicStrings.cs
@@ -1,11 +1,34 @@
-
 namespace dojo.leetcode;
 
 public class Q205_IsomorphicStrings
 {
     public bool IsIsomorphic(string s, string t)
     {
-        return false;
+        // 1. Two strings must in same length
+        if (s.Length != t.Length) return false;
+
+        // 2. If one char map to multiple chars, it must be false
+        // 3. There should be no two keys map to the same character 
+        var dict = new Dictionary<char, char>();
+        for(var i = 0; i<s.Length; i++) 
+        {
+            if (!dict.TryGetValue(s[i], out char value))
+            {
+                // Key exist but mapped to a value already exist
+                if (dict.ContainsValue(t[i]))
+                {
+                    return false;
+                }
+                // Key not exist and not duplicates
+                dict.Add(s[i], t[i]);
+            }
+            // Same key mapped to multiple characters
+            else if (value != t[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
@@ -15,7 +38,9 @@ public class Q205_IsomorphicStringsTestData : TestData
     [
         ["egg", "add", true],
         ["foo", "bar", false],
-        ["paper", "title", true]
+        ["paper", "title", true],
+        ["bbbaaaba", "aaabbbba", false],
+        ["badc", "baba", false],
     ];
 }
 

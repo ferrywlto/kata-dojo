4011638d7f8e940ba3cd50de62282d53c34355a3
Ferry To <ferrywlto@gmail.com>
2024-01-19 11:52:07 +0800

    test: Add test cases of Q228 passed

--- csharp/leetcode/easy/Q228_SummaryRanges.cs
+++ csharp/leetcode/easy/Q228_SummaryRanges.cs
@@ -1,11 +1,46 @@
-using dojo.leetcode;
+namespace dojo.leetcode;
 
 public class Q228_SummaryRanges
 {
     public IList<string> SummaryRanges(int[] nums)
     {
+        if (nums.Length == 1) return [nums[0].ToString()];
+
         // use 2 int pointers
-        return new List<string>();
+        int start = 0;
+        int end = 0;
+        var dict = new Dictionary<int, int>();
+
+        for(var i=0; i<nums.Length-1; i++)
+        {
+            if(nums[i]+1 != nums[i+1])
+            {
+                dict.Add(start, end);
+                start = i+1;
+                end = i+1;
+
+                if (i==nums.Length-2) 
+                {
+                    dict.Add(start, end);
+                }
+                continue;
+            }
+
+            end++;
+            if (i==nums.Length-2) 
+            {
+                dict.Add(start, end);
+            }
+        }
+
+        var result = dict
+            .Select(x => x.Key == x.Value 
+                ? nums[x.Key].ToString() 
+                : $"{nums[x.Key]}->{nums[x.Value]}")
+            .ToList();
+
+        foreach (var range in result) Console.WriteLine(range);
+        return result;
     }
 }
 
@@ -15,6 +50,7 @@ public class Q228_SummaryRangesTestData: TestData
     [
         [new int[]{0, 1, 2, 4, 5, 7}, new string[]{"0->2", "4->5", "7"}],
         [new int[]{0, 2, 3, 4, 6, 8, 9}, new string[]{"0", "2->4", "6", "8->9"}],
+        [new int[]{-1}, new string[]{"-1"}],
     ];
 }
 

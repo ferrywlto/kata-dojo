84a6ce72f473b1f5f61722cd6d4037b7233b722b
Ferry To <ferrywlto@gmail.com>
2024-01-19 14:21:05 +0800

    feat: Add unit test and add service

---
+++ csharp/challenges/fullstack-add-two/backend/AddService.cs
@@ -0,0 +1,20 @@
+using Xunit;
+public class AddServiceTest
+{
+    [Theory]
+    [InlineData(1,2,3)]
+    [InlineData(3,4,7)]
+    public void AddServiceShouldReturnCorrectSum(int num1, int num2, int expected)
+    {
+        var sut = new AddService();
+        Assert.Equal(expected, sut.Add(num1, num2));
+    }
+}
+
+public class AddService
+{
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}

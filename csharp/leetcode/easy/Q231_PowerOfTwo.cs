d8dcff9a37895051b553635ee6507eb7bc0cef8d
Ferry To <ferrywlto@gmail.com>
2024-01-19 11:22:44 +0800

    test: All test cases of Q231 passed and added loop based bitwise solution for fun

--- csharp/leetcode/easy/Q231_PowerOfTwo.cs
+++ csharp/leetcode/easy/Q231_PowerOfTwo.cs
@@ -2,9 +2,27 @@ namespace dojo.leetcode;
 
 public class Q231_PowerOfTwo
 {
+    // TC:O(1), SC:O(1)
     public bool IsPowerOfTwo(int n) 
     {
         return Math.Log2(n) % 1 == 0;
+    }
+
+    // Add a loop based bitwise solution for fun
+    // Still O(1) as it only runs a constant number of times (32)
+    public bool IsPowerOfTwoLoop(int n)
+    {
+        // invert to positive representation by 2s complement
+        // if (n < 0) n = ~n+1;
+
+        // actually just need to return false if less than zero
+        if (n < 0) return false;
+
+        for(byte i=0; i<32; i++)
+        {
+            if (1 << i == n) return true;
+        }
+        return false;
     }
 }
 
@@ -13,8 +31,21 @@ public class Q231_PowerOfTwoTestData: TestData
     protected override List<object[]> Data => 
     [
         [1, true],
+        [2, true],
+        [4, true],
+        [8, true],
         [16, true],
+        [256, true],
+        [1024, true],
+        [65536, true],
         [3, false],
+        [5, false],
+        [9, false],
+        [-9, false],
+        [17, false],
+        [101, false],
+        [int.MaxValue, false],
+        [int.MinValue, false],
     ];
 }
 

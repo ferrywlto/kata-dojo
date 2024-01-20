80cf4f1ffb77436092b3e3969e9358d3afda1005
Ferry To <ferrywlto@gmail.com>
2024-01-20 23:39:33 +0800

    test: All test cases of Q242 passed

--- csharp/leetcode/easy/Q242_ValidAnagram.cs
+++ csharp/leetcode/easy/Q242_ValidAnagram.cs
@@ -5,7 +5,51 @@ public class Q242_ValidAnagram
 {
     public bool IsAnagram(string s, string t) 
     {
-        return false;
+        // First analysis s
+        // Then analysis t
+
+        var dict_s = new Dictionary<string, int>();
+        var dict_t = new Dictionary<string, int>();
+
+        foreach(var str in s)
+        {
+            if(dict_s.TryGetValue(str.ToString(), out var value)) 
+            {
+                dict_s[str.ToString()]++;
+            }
+            else 
+            {
+                dict_s.Add(str.ToString(), 1);
+            }
+        }
+
+        foreach(var str in t)
+        {
+            if(dict_t.TryGetValue(str.ToString(), out var value)) 
+            {
+                dict_t[str.ToString()]++;
+            }
+            else 
+            {
+                dict_t.Add(str.ToString(), 1);
+            }
+        }
+
+        foreach(var pair in dict_t)
+        {
+            if (!dict_s.TryGetValue(pair.Key, out int value)) return false;
+
+            if (!(value == pair.Value)) return false;
+        }
+
+        foreach(var pair in dict_s)
+        {
+            if (!dict_t.TryGetValue(pair.Key, out int value)) return false;
+
+            if (!(value == pair.Value)) return false;
+        }
+
+        return true;
     }
 }
 
@@ -16,6 +60,7 @@ public class Q242_ValidAnagramTestData: TestData
         ["anagram", "nagaram", true],
         ["anagram😀", "😀nagaram", true],
         ["rat", "car", false],
+        ["ab", "a", false],
     ];
 }
 

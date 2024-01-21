d327e6c5ec6e18bea5673be1eb0820bf04225f27
Ferry To <ferrywlto@gmail.com>
2024-01-21 08:45:01 +0800

    doc: Add solution idea

--- csharp/leetcode/easy/Q232_ImplementQueueUsingStacks.cs
+++ csharp/leetcode/easy/Q232_ImplementQueueUsingStacks.cs
@@ -2,7 +2,10 @@ using dojo.leetcode;
 
 public class Q232_ImplementQueueUsingStacks
 {
-
+    // need 2 stacks
+    // whenever push, pop all stack 1 to stack 2
+    // push new to stack 1
+    // push all stack2 back to stack 1  
     public void Push(int x)
     {
 

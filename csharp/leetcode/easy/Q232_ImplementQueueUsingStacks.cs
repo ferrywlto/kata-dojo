c0ec1f2a58e675e396c148c4faf8f97ac23285bb
Ferry To <ferrywlto@gmail.com>
2024-01-21 08:34:49 +0800

    test: Add official test case of Q232

---
+++ csharp/leetcode/easy/Q232_ImplementQueueUsingStacks.cs
@@ -0,0 +1,39 @@
+using dojo.leetcode;
+
+public class Q232_ImplementQueueUsingStacks
+{
+
+    public void Push(int x)
+    {
+
+    }
+
+    public int Pop()
+    {
+        return 0;
+    }
+
+    public int Peek()
+    {
+        return 0;
+    }
+
+    public bool Empty()
+    {
+        return false;
+    }
+}
+
+public class Q232_ImplementQueueUsingStacksTests(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public void OfficialTestCases()
+    {
+        var sut = new Q232_ImplementQueueUsingStacks();
+        sut.Push(1);
+        sut.Push(2);
+        Assert.Equal(1, sut.Peek());
+        Assert.Equal(1, sut.Pop());
+        Assert.False(sut.Empty());
+    }
+}

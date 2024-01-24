caaf547febd8981a441afdae86cf9a4c31fb71f8
Ferry To <ferrywlto@gmail.com>
2024-01-24 22:28:27 +0800

    test: Add more edge test cases and all test cases of Q263 passed

--- csharp/leetcode/easy/Q263_UglyNumber.cs
+++ csharp/leetcode/easy/Q263_UglyNumber.cs
@@ -4,7 +4,32 @@ public class Q263_UglyNumber
 {
     public bool IsUgly(int n)
     {
-        return false;
+        if (n <= 0) return false;
+        if (n == 1 || n == 2 || n == 3 || n == 5)
+            return true;
+
+        var temp = n;
+        while(temp != 1)
+        {
+            if (temp % 5 == 0)
+            {
+                temp /= 5;
+                continue;
+            }
+            if (temp % 3 == 0)
+            {
+                temp /= 3;
+                continue;
+            }
+            if (temp % 2 == 0)
+            {
+                temp /= 2;
+                continue;
+            }
+            return false;
+        }
+
+        return true;
     }
 }
 
@@ -15,6 +40,12 @@ public class Q263_UglyNumberTestData: TestData
         [6, true],
         [1, true],
         [14, false],
+        [-6, false],
+        [-1, false],
+        [-14, false],
+        [int.MaxValue, false],
+        [int.MinValue, false],
+        [0, false]
     ];
 }
 

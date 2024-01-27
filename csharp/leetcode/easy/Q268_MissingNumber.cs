afc0eaaa0a9865502e787651b2e715c26ecad8ed
Ferry To <ferrywlto@gmail.com>
2024-01-27 13:01:58 +0800

    perf: Add O(1) space arithmetic version

--- csharp/leetcode/easy/Q268_MissingNumber.cs
+++ csharp/leetcode/easy/Q268_MissingNumber.cs
@@ -3,27 +3,33 @@ namespace dojo.leetcode;
 public class Q268_MissingNumber
 {
     // TC: O(n), SC:O(1)
+    // This version make use of the properties of arithmetic series
     public int MissingNumber(int[] nums)
     {
-        for(var i=0; i<=nums.Length; i++)
+        int n = nums.Length;
+        int totalSum = n * (n + 1) / 2; // sum of 0 to n
+
+        int arraySum = 0;
+        for (int i = 0; i < n; i++)
         {
-            if (!nums.Contains(i)) return i;
+            arraySum += nums[i];
         }
-        return 0;
+
+        return totalSum - arraySum; // the missing number
     }
 }
 
-public class Q268_MissingNumberTestData: TestData
+public class Q268_MissingNumberTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
-        [new int[]{3,0,1}, 2],    
-        [new int[]{0,1}, 2],    
+        [new int[]{3,0,1}, 2],
+        [new int[]{0,1}, 2],
         [new int[]{9,6,4,2,3,5,7,0,1}, 8],
     ];
 }
 
-public class Q268_MissingNumberTests(ITestOutputHelper output): BaseTest(output)
+public class Q268_MissingNumberTests(ITestOutputHelper output) : BaseTest(output)
 {
     [Theory]
     [ClassData(typeof(Q268_MissingNumberTestData))]

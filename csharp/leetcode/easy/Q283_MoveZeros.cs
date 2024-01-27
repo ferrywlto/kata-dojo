504f2a0ed764d1eede6cf2f53fe246cb7cfb8e8d
Ferry To <ferrywlto@gmail.com>
2024-01-27 17:59:12 +0800

    test: All test cases of Q283 passed

--- csharp/leetcode/easy/Q283_MoveZeros.cs
+++ csharp/leetcode/easy/Q283_MoveZeros.cs
@@ -5,7 +5,31 @@ public class Q283_MoveZeros
 {
     public void MoveZeroes(int[] nums)
     {
-
+        if (nums.Length <= 1) return;
+        var haveZero = false;
+        var zeroIdx = 0;
+        var zeroPassed = 0;
+        for(var i=0; i<nums.Length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                if(haveZero) 
+                {
+                    zeroPassed++;
+                }
+                else 
+                {
+                    zeroIdx = i;
+                    haveZero = true;
+                }
+            }
+            else if(haveZero)
+            {
+                nums[zeroIdx] = nums[i];
+                nums[i] = 0;
+                zeroIdx = i-zeroPassed;
+            }
+        }
     }
 }
 
@@ -15,6 +39,9 @@ public class Q283_MoveZerosTestData: TestData
     [
         [new int[]{0,1,0,3,12}, new int[]{1,3,12,0,0}],
         [new int[]{0}, new int[]{0}],
+        [new int[]{2,1}, new int[]{2,1}],
+        [new int[]{1,0,1}, new int[]{1,1,0}],
+        [new int[]{4,2,4,0,0,3,0,5,1,0}, new int[]{4,2,4,3,5,1,0,0,0,0}],
     ];
 }
 

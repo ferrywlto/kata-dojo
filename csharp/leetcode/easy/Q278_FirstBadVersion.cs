2e277aa081bbaac8fdce03ae498aaa627a02dd75
Ferry To <ferrywlto@gmail.com>
2024-01-27 13:41:11 +0800

    test: All test cases of Q278 passed

--- csharp/leetcode/easy/Q278_FirstBadVersion.cs
+++ csharp/leetcode/easy/Q278_FirstBadVersion.cs
@@ -1,4 +1,4 @@
-using dojo.leetcode;
+namespace dojo.leetcode;
 
 public class VersionControl
 {
@@ -9,15 +9,35 @@ public class VersionControl
 
 public class Q278_FirstBadVersion(int bad) : VersionControl(bad)
 {
-
     public int FirstBadVersion(int n)
     {
-        for (var i = 1; i <= n; i++)
+        if (n == 1 || IsBadVersion(1)) return 1;
+        if (IsBadVersion(n) && !IsBadVersion(n-1)) return n;
+
+        var start = 1;
+        var end = n;
+        var middle = Middle(start, end);
+
+        while (middle != start && middle != end)
         {
-            if (IsBadVersion(i)) return i;
+            if (IsBadVersion(middle) && !IsBadVersion(middle - 1)) return middle;
+            else 
+            {
+                if (IsBadVersion(middle - 1))
+                    end = middle;
+                else
+                    start = middle;
+                middle = Middle(start, end);
+            }
         }
         return int.MaxValue;
-    }  
+    }
+
+    public int Middle(int start, int end) 
+    {
+        var temp = start + (long)end;
+        return (int)(temp / 2);
+    } 
 }
 
 public class Q278_FirstBadVersionTestData: TestData
@@ -26,6 +46,9 @@ public class Q278_FirstBadVersionTestData: TestData
     [
         [5, 4, 4],
         [1, 1, 1],
+        [int.MaxValue, int.MaxValue, int.MaxValue],
+        [int.MaxValue, 1, 1],
+        [int.MaxValue, int.MaxValue-1, int.MaxValue-1],
     ];
 }
 

7147d6f758845b5473616f3a8004c963dfbb07c8
Ferry To <ferrywlto@gmail.com>
2024-01-28 13:14:52 +0800

    test: All test cases of Q292 passed

--- csharp/leetcode/easy/Q292_NimGame.cs
+++ csharp/leetcode/easy/Q292_NimGame.cs
@@ -4,13 +4,16 @@ namespace dojo.leetcode;
 public class Q292_NimGame
 {
     public bool CanWinNim(int n) {
-        // do something
         // if reminder <= 3, I win, else I cannot win
         // combinations: [1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3]
         // outcome: 2, 3, 3, 4, 4, 4, 5, 5, 6 
         // count: 1, 2, 3, 2, 1
-        //   
-        return false;
+        // if minus the combination the reminder is 5, it is sure I will win 
+        // therefore it becomes a recursion of minus 2,3,4,5,6 from input, 5^n times  
+
+        // actually the above is not important because it is an exhaustive search
+        // only one case friend will definitely win, n % 4 == 0
+        return n % 4 != 0;
     }
 }
 

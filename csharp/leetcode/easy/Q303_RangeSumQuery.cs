c22ab8f89a0d2ada3bc33f2afdb5b73b0437121d
Ferry To <ferrywlto@gmail.com>
2024-01-29 12:16:37 +0800

    test: All test cases of Q303 passed

--- csharp/leetcode/easy/Q303_RangeSumQuery.cs
+++ csharp/leetcode/easy/Q303_RangeSumQuery.cs
@@ -1,13 +1,24 @@
 
 namespace dojo.leetcode;
 
+/* Constraints
+1 <= nums.length <= 10^4
+-10^5 <= nums[i] <= 10^5
+0 <= left <= right < nums.length
+At most 10^4 calls will be made to sumRange
+*/
 public class Q303_RangeSumQuery(int[] nums)
 {
     protected readonly int[] Nums = nums;
 
     public int SumRange(int left, int right)
     {
-        return 0;
+        var sum = 0;
+        for(var i=left; i<=right; i++)
+        {
+            sum += Nums[i];
+        }
+        return sum;
     }
 }
 

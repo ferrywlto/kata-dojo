090b29733cac43438f2f24a0e4f1698aa4994619
Ferry To <ferrywlto@gmail.com>
2024-01-29 15:25:29 +0800

    test: All test cases of Q326 passed

--- csharp/leetcode/easy/Q326_PowerOfThree.cs
+++ csharp/leetcode/easy/Q326_PowerOfThree.cs
@@ -3,9 +3,21 @@ namespace dojo.leetcode;
 
 public class Q326_PowerOfThree
 {
+    /*
+    The notation 1E-14 is a way of representing numbers using scientific notation in programming languages. 
+    It stands for 1 times 10 to the power of -14, which is a very small number.
+
+    In decimal form, 1E-14 is 0.00000000000001. 
+    This is a very small number close to zero, often used as a tolerance level in numerical comparisons to account for precision errors in floating-point calculations.    
+    */
     public bool IsPowerOfThree(int n)
     {
-        return false;
+        if (n <= 0) return false;
+        // the return type is double, so for n=243 it returns 4.999999999 instead of 5
+        // therefore need to check if it is very close to an integer
+        var logResult = Math.Log(n, 3); 
+        
+        return Math.Abs(logResult - Math.Round(logResult)) < 1E-14;
     }
 }
 
@@ -16,6 +28,7 @@ public class Q326_PowerOfThreeTestData: TestData
         [27, true],
         [0, false],
         [-1, false],
+        [243, true],
     ];
 }
 

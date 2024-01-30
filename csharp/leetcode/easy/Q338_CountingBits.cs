a90ebb8aebb3dd16f4f72c618d8aa224a86dd5de
Ferry To <ferrywlto@gmail.com>
2024-01-30 15:39:14 +0800

    test: All test cases of Q338 passed

--- csharp/leetcode/easy/Q338_CountingBits.cs
+++ csharp/leetcode/easy/Q338_CountingBits.cs
@@ -5,9 +5,36 @@ public class Q338_CountingBits
 {
     public int[] CountBits(int n)
     {
-        return [];
+        var num = n;
+        var head = new List<int> { 0, 1 };
+        
+        List<int> temp = [1];
+        while(num > 0)
+        {
+            temp = ReplicateThenExpand(temp);
+            head.AddRange(temp);
+            num /= 2;
+        }
+        var result = head[..(n+1)];
+        return [.. result];
+    }
+
+    public List<int> ReplicateThenExpand(List<int> nums)
+    {
+        var output = new List<int>();
+        output.AddRange(nums);
+        output.AddRange(nums.Select(x => x + 1));
+        return output;
     }
 }
+    // This method is to get the bit sequence, it is observed that the binary bit sequence have the pattern below
+    // For each power of 2 the bit sequence of 1s will repeat the sequence of previous power of 2, then repeat again with each plus 1
+    // Thus: number of ones in bits (actual number) 
+    // 2^0 = 1 (1)
+    // 2^1 = 1 (2), 2 (3) -> repeat 1, then repeat with plus one get 2 -> 1,2
+    // 2^2 = 1 (4), 2 (5), 2 (6), 3 (7) -> repeat last power of 2, 1,2, then repeat again with each plus one, get 2,3 -> 1, 2, 2, 3
+    // 2^3 = 1 (8), 2 (9), 2 (10), 3 (11), 2 (12), 3 (13), 3 (14), 4 (15) -> repeat 1,2,2,3 then repeat with each plus one get 2,3,3,4 -> 1,2,2,3,2,3,3,4
+    // and so on.  
 
 public class Q338_CountingBitsTestData: TestData
 {
@@ -15,10 +42,11 @@ public class Q338_CountingBitsTestData: TestData
     [
         [2, new int[] {0,1,1}],
         [5, new int[] {0,1,1,2,1,2}],
+        [8, new int[] {0,1,1,2,1,2,2,3,1}],
     ];
 }
 
-public class Q338_CountingBitsTests: BaseTest
+public class Q338_CountingBitsTests(ITestOutputHelper output): BaseTest(output)
 {
     [Theory]
     [ClassData(typeof(Q338_CountingBitsTestData))]
@@ -26,6 +54,26 @@ public class Q338_CountingBitsTests: BaseTest
     {
         var sut = new Q338_CountingBits();
         var actual = sut.CountBits(input);
+        Output?.WriteLine($"{string.Join(",", actual)}");
         Assert.True(Enumerable.SequenceEqual(expected, actual));
     }
+
+    [Theory]
+    [ClassData(typeof(Q338_CountingBitsExtraTestData))]
+    public void ReplicateThenExpand_ShouldCopyInput_AndCopyAgainWithPlusOne(List<int> input, List<int> expected)
+    {
+        var sut = new Q338_CountingBits();
+        var actual = sut.ReplicateThenExpand(input);
+        Assert.True(Enumerable.SequenceEqual(expected, actual));
+    }
+}
+
+public class Q338_CountingBitsExtraTestData: TestData
+{
+    protected override List<object[]> Data => 
+    [
+        [new List<int>{1}, new List<int>{1,2}],
+        [new List<int>{1,2}, new List<int>{1,2,2,3}],
+        [new List<int>{1,2,2,3}, new List<int>{1,2,2,3,2,3,3,4}],
+    ];
 }

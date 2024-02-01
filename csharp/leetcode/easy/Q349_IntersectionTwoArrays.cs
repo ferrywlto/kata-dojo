a7a962cba1fed01e8f5076441b262514b719861e
Ferry To <ferrywlto@gmail.com>
2024-02-01 23:43:20 +0800

    test: All test cases of Q349 passed

--- csharp/leetcode/easy/Q349_IntersectionTwoArrays.cs
+++ csharp/leetcode/easy/Q349_IntersectionTwoArrays.cs
@@ -2,21 +2,24 @@ namespace dojo.leetcode;
 
 public class Q349_IntersectionOfTwoArrays
 {
-        public int[] Intersection(int[] nums1, int[] nums2) {
-        return [];
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var set3 = nums1.Intersect(nums2);
+        return [.. set3];
     }
 }
 
-public class Q349_IntersectionOfTwoArraysTestData: TestData
+public class Q349_IntersectionOfTwoArraysTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
-        [new int[]{1,2,2,1}, new int[]{2,2}, new int[]{1}],
+        [new int[]{1,2,2,1}, new int[]{2,2}, new int[]{2}],
         [new int[]{4,9,5}, new int[]{9,4,9,8,4}, new int[]{4,9}],
+        [new int[]{1,2}, new int[]{1,1}, new int[]{1}],
     ];
 }
 
-public class Q349_IntersectionOfTwoArraysTests
+public class Q349_IntersectionOfTwoArraysTests(ITestOutputHelper output)
 {
     [Theory]
     [ClassData(typeof(Q349_IntersectionOfTwoArraysTestData))]
@@ -24,6 +27,7 @@ public class Q349_IntersectionOfTwoArraysTests
     {
         var sut = new Q349_IntersectionOfTwoArrays();
         var actual = sut.Intersection(input1, input2);
+        output.WriteLine($"{string.Join(",", actual)}");
         Assert.True(Enumerable.SequenceEqual(expected, actual));
     }
 }

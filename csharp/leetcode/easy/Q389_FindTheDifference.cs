98ac8bbc3171adaa32710aea6cb560a730569c9a
Ferry To <ferrywlto@gmail.com>
2024-02-05 14:03:20 +0800

    test: All test cases of Q389 passed

--- csharp/leetcode/easy/Q389_FindTheDifference.cs
+++ csharp/leetcode/easy/Q389_FindTheDifference.cs
@@ -2,22 +2,37 @@ namespace dojo.leetcode;
 
 public class Q389_FindTheDifference
 {
+    // Constaints
+    // t.length == s.length + 1
+
+    // TC: O(n), SC: O(n)
     public char FindTheDifference(string s, string t)
     {
-        return 'c';
+        var dict = s.Analyze();
+        foreach(var c in t)
+        {
+            if (!dict.TryGetValue(c, out var count) || count == 0)
+            {
+                return c;
+            } 
+            else 
+            {
+                dict[c]--;
+            }
+        }
+        throw new ArgumentException("Invalid input: t must have one more character than s");
     }
 }
 
 public class Q389_FindTheDifferenceTestData: TestData
 {
-    protected override List<object[]> Data => new List<object[]>
-    {
-        new object[] {"abcd", "abcde", 'e'},
-        new object[] {"", "y", 'y'},
-        new object[] {"a", "aa", 'a'},
-        new object[] {"ae", "aea", 'a'},
-        new object[] {"a", "a", 'a'},
-    };
+    protected override List<object[]> Data =>
+    [
+        ["abcd", "abcde", 'e'],
+        ["", "y", 'y'],
+        ["a", "aa", 'a'],
+        ["ae", "aea", 'a'],
+    ];
 }
 
 public class Q389_FindTheDifferenceTests

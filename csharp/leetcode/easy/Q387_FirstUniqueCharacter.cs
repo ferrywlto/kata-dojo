072ee6c5aeca8b2947fe16983c26bcee905ea8da
Ferry To <ferrywlto@gmail.com>
2024-02-05 13:46:03 +0800

    test: All test cases of Q387 passed

--- csharp/leetcode/easy/Q387_FirstUniqueCharacter.cs
+++ csharp/leetcode/easy/Q387_FirstUniqueCharacter.cs
@@ -2,10 +2,34 @@ namespace dojo.leetcode;
 
 public class Q387_FirstUniqueCharacter
 {
+    // TC: O(n), SC: O(n)
     public int FirstUniqChar(string s) 
     {
-        return -1;
+        var dict = new Dictionary<char, CharInfo>();
+     
+        for (var i=0; i<s.Length; i++)
+        {
+            if (dict.TryGetValue(s[i], out CharInfo? info))
+            {
+                dict[s[i]] = info with { Count = info.Count+1 };
+            }
+            else 
+            {
+                dict.Add(s[i], new CharInfo(i,1));
+            }
+        }
+
+        var minIdx = int.MaxValue;
+        foreach (var kvp in dict)
+        {
+            if (kvp.Value.Count == 1 && kvp.Value.FisrtSeenIdx < minIdx)
+            {
+                minIdx = kvp.Value.FisrtSeenIdx;
+            }
+        }
+        return minIdx == int.MaxValue ? -1 : minIdx;
     }
+    record CharInfo(int FisrtSeenIdx,int Count);
 }
 
 public class Q387_FirstUniqueCharacterTestData: TestData
@@ -15,6 +39,8 @@ public class Q387_FirstUniqueCharacterTestData: TestData
         ["leetcode", 0],
         ["loveleetcode", 2],
         ["aabb", -1],
+        ["aadadaad", -1],
+        ["dddccdbba", 8],
     ];
 }
 

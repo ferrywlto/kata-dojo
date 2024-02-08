e4e52ae09058aa301f9e589e2dca3c7e3424a1a1
Ferry To <ferrywlto@gmail.com>
2024-02-08 14:07:56 +0800

    test: All test cases of Q409 passed

--- csharp/leetcode/easy/Q409_LongestPalindrome.cs
+++ csharp/leetcode/easy/Q409_LongestPalindrome.cs
@@ -4,7 +4,27 @@ public class Q409_LongestPalindrome
 {
     public int LongestPalindrome(string s)
     {
-        return 1;
+        // 1. analyze
+        // 2. if s.length is odd, check dict have only 1 odd char and all others are even
+        // 3. if s.length is even, check if all chars are even 
+        var dict = s.Analyze();
+        List<KeyValuePair<char, int>> odds = [];
+        List<KeyValuePair<char, int>> evens = [];
+
+        foreach(var kv in dict)
+        {
+            if (kv.Value % 2 == 0) evens.Add(kv);
+            else odds.Add(kv);
+        }
+
+        int sumOfEven = evens.Sum(x => x.Value);
+
+        if (odds.Count > 0)
+        {
+            var sumOfOdd = odds.Sum(x => x.Value - 1);
+            return sumOfEven + sumOfOdd + 1;
+        }
+        return sumOfEven;
     }
 }
 
@@ -15,6 +35,9 @@ public class Q409_LongestPalindromeTestData : TestData
         ["abccccdd", 7],
         ["a", 1],
         ["bb", 2],
+        ["ccc", 3],
+        ["aaaCCC", 5], // aCCCa, not 3 
+        ["civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth", 983],
     ];
 }
 

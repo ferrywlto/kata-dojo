18b1cac885a004930ac1dd93e0193dcde228174e
Ferry To <ferrywlto@gmail.com>
2024-02-12 12:49:09 +0800

    test: All test cases of Q459 passed

--- csharp/leetcode/easy/Q459_RepeatedSubstring.cs
+++ csharp/leetcode/easy/Q459_RepeatedSubstring.cs
@@ -1,10 +1,32 @@
-
 namespace dojo.leetcode;
 
 public class Q459_RepeatedSubstring
 {
+    // TC: O(n^2), SC: O(n)
     public bool RepeatedSubstringPattern(string s)
     {
+        if (s.Length <= 1) return false;
+        if (s.Length == 2) return s[0] == s[1];
+        // from window size 1 to s.Length/2
+        // if s % substring.Length != 0 skip
+        // n = s / substring .Length
+        // repeat temp += substring n times
+        // if temp == s return true 
+
+        var windowSize = s.Length / 2;
+        // Console.WriteLine($"windowSize: {windowSize}");
+        for (var i=windowSize; i>0; i--)
+        {
+            if (s.Length % i != 0) continue;
+
+            var substr = s[..i];
+            for (var j=i; j<s.Length; j++) 
+            {
+                // Console.WriteLine($"subStr: {string.Join(string.Empty, substr)}, subStr[{j%i}]: {substr[j % i]}, s[{j}: {s[j]}] ");
+                if (!substr[j % i].Equals(s[j])) break;
+                if (j == s.Length - 1) return true;
+            }
+        }
         return false;
     }
 }
@@ -16,11 +38,16 @@ public class Q459_RepeatedSubstringTestData: TestData
         ["abab", true],
         ["aba", false],
         ["abcabcabcabc", true],
+        ["abcdabcdabcd", true],
         ["abcabcabc", true],
         ["abcabcab", false],
         ["abcabc", true],
         ["abcab", false],
         ["abc", false],
+        ["abcdadcba", false],
+        ["abcdaabcd", false],
+        ["abcdefghiabcdefghabcdefghabcdefghiabcdefghabcdefghabcdefghiabcdefghabcdefghabcdefghiabcdefghabcdefghi", false],
+        ["abcdefghiabcdefghabcdefghiabcdefghabcdefghiabcdefghiabcdefghiabcdefghabcdefghiabcdefghabcdefghiabcdefghi", true],
         ["ab", false],
         ["a", false],
         ["", false],

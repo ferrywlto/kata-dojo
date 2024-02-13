ca81615ac8eb870a4752cb08b0bb19a15e507d32
Ferry To <ferrywlto@gmail.com>
2024-02-13 11:48:34 +0800

    test: All test cases of Q476 passed

--- csharp/leetcode/easy/Q476_NumberComplement.cs
+++ csharp/leetcode/easy/Q476_NumberComplement.cs
@@ -1,11 +1,20 @@
-
 namespace dojo.leetcode;
 
 public class Q476_NumberComplement
 {
+    // Constraints
+    // 1 <= num < 2^31
+    // TC: O(1), SC: O(1)
     public int FindComplement(int num)
     {
-        return ~num; ;
+        // Can't simply use ~ operator because of leading zeros
+        var bitsOfNum = (int)Math.Log(num, 2) + 1;
+        var complementedStr = Convert.ToString(~num, 2);
+        var trimmed = complementedStr[^bitsOfNum..];
+        var result = int.Parse(trimmed, System.Globalization.NumberStyles.BinaryNumber);
+        // Console.WriteLine($"num: {num}, binary:{Convert.ToString(num, 2)}, ~num: {~num}, binary: {complementedStr}, shift: {shiftRequired}, trimed: {trimmed}, result: {result}");
+
+        return result;
     }
 }
 
@@ -14,7 +23,8 @@ public class Q476_NumberComplementTestData: TestData
     protected override List<object[]> Data => 
     [
         [5, 2],
-        [1, 0]
+        [1, 0],
+        [1024, 1023],
     ];
 }
 

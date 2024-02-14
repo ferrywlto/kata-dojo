1ab959a5c584317e3124361d33b52bfe0329f35e
Ferry To <ferrywlto@gmail.com>
2024-02-14 17:34:54 +0800

    test: All test cases of Q482 passed

--- csharp/leetcode/easy/Q482_LicenseKeyFormatting.cs
+++ csharp/leetcode/easy/Q482_LicenseKeyFormatting.cs
@@ -5,23 +5,46 @@ namespace dojo.leetcode;
 
 public class Q482_LicenseKeyFormatting
 {
+    // TC: O(n), SC: O(n)
     public string LicenseKeyFormatting(string s, int k)
     {
         var sb = new StringBuilder(s);
         sb.Replace("-", string.Empty);
+        Console.WriteLine($"after replace: {sb}");
+        for(var i=0; i<sb.Length; i++)
+        {
+            sb[i] = ToUpper(sb[i]);
+        }
+        if (k >= sb.Length) return sb.ToString();
+
         Console.WriteLine(sb);
-        sb.ToString().ToUpperInvariant();
+
+        for (var j=sb.Length-k; j>=1; j-=k) 
+        {
+            sb.Insert(j, "-");
+            Console.WriteLine(sb);
+        }
+        
         Console.WriteLine(sb);
-        return string.Empty;
+        return sb.ToString();
     }
-}
+
+    public char ToUpper(char input) 
+        => (input >= 97 && input <= 122) 
+        ? (char)(input - 32) 
+        : input;
+ }
 
 public class Q482_LicenseKeyFormattingTestData : TestData
 {
     protected override List<object[]> Data =>
     [
         ["5F3Z-2e-9-w", 4, "5F3Z-2E9W"],
-        ["2-5g-3-J", 2, "2-5G-3J"]
+        ["5F3Z-2e-9-w", 7, "5-F3Z2E9W"],
+        ["2-5g-3-J", 2, "2-5G-3J"],
+        ["r", 1, "R"],
+        ["a-----a", 2, "AA"],
+        ["---", 3, ""],
     ];
 }
 

024f1e3c801edfd2546315d4316e61986bb29ab3
Ferry To <ferrywlto@gmail.com>
2024-02-15 16:20:22 +0800

    test: All test cases of Q492 passed

--- csharp/leetcode/easy/Q492_ConstructTheRectangle.cs
+++ csharp/leetcode/easy/Q492_ConstructTheRectangle.cs
@@ -4,7 +4,18 @@ public class Q492_ConstructTheRectangle
 {
     public int[] ConstructRectangle(int area)
     {
-        return [];
+        var sqrt = Math.Sqrt(area);
+        var sqrt_int = (int)sqrt;
+
+        if (sqrt_int * sqrt_int == area) 
+            return [sqrt_int, sqrt_int];
+        
+        for(var i=sqrt_int; i>0; i--)
+        {
+            if(area % i == 0) return [area / i, i];
+        }
+
+        return [area, 1];
     }
 }
 
@@ -12,7 +23,9 @@ public class Q492_ConstructTheRectangleTestData : TestData
 {
     protected override List<object[]> Data =>
     [
+        [2, new int[] { 2, 1 }],
         [4, new int[] { 2, 2 }],
+        [6, new int[] { 3, 2 }],
         [37, new int[] { 37, 1 }],
         [122122, new int[] { 427, 286 }],
     ];

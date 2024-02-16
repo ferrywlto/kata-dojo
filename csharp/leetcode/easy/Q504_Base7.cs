dfbdcc7e8c54539943bc8b5f246976a642421582
Ferry To <ferrywlto@gmail.com>
2024-02-16 16:10:20 +0800

    test: All test cases of Q504 passed

--- csharp/leetcode/easy/Q504_Base7.cs
+++ csharp/leetcode/easy/Q504_Base7.cs
@@ -1,10 +1,36 @@
+using System.Text;
+
 namespace dojo.leetcode;
 
 public class Q504_Base7
 {
     public string ConvertToBase7(int num)
     {
-        return string.Empty;
+        return ConvertToBase(num, 7);
+    }
+
+    // TC: O(log n), SC: O(n)
+    public string ConvertToBase(int input, int numBase)
+    {
+        var sb = new StringBuilder();
+        bool negative = false;
+        if (input < 0)
+        {
+            input *= -1;
+            negative = true;
+        }
+
+        do
+        {
+            sb.Append(input % numBase);
+            input /= numBase;
+        } while(input > 0);
+
+        if (negative) sb.Append('-');
+
+        var charArray = sb.ToString().ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
     }
 }
 
@@ -14,10 +40,19 @@ public class Q504_Base7TestData : TestData
     [
         [100, "202"],
         [-7, "-10"],
+        [6, "6"],
+        [7, "10"],
+
+        // [2, "10"],
+        // [3, "11"],
+        // [4, "100"],
+        // [7, "111"],
+        // [10, "1010"],
+        // [-10, "-1010"],
     ];
 }
 
-public class Q504_Base7Tests
+public class Q504_Base7Tests(ITestOutputHelper output)
 {
     [Theory]
     [ClassData(typeof(Q504_Base7TestData))]
@@ -25,6 +60,7 @@ public class Q504_Base7Tests
     {
         var sut = new Q504_Base7();
         var actual = sut.ConvertToBase7(num);
+        output.WriteLine(actual);
         Assert.Equal(expected, actual);
     }
 }

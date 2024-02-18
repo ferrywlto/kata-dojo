a486ce9a4649934e416215b4ecce724add7d6916
Ferry To <ferrywlto@gmail.com>
2024-02-18 12:09:33 +0800

    test: All test cases of Q501 passed

--- csharp/leetcode/easy/Q501_FindModeInBST.cs
+++ csharp/leetcode/easy/Q501_FindModeInBST.cs
@@ -3,15 +3,44 @@ namespace dojo.leetcode;
 
 public class Q501_FindModeInBST
 {
+    // TC: O(n) as have to traverse each node at least once
+    // SC: O(n) as dictionary size = number of nodes in worst case 
     public int[] FindMode(TreeNode root)
     {
-        return [];
+        if (root.left == null && root.right == null) 
+            return [root.val];
+
+        var demography = new Dictionary<int, int>();
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while(queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if(demography.ContainsKey(node.val))
+            {
+                demography[node.val]++;
+            }
+            else
+            {
+                demography.Add(node.val, 1);
+            }
+
+            if (node.left != null) queue.Enqueue(node.left);
+            if (node.right != null) queue.Enqueue(node.right);
+        }
+
+        var mode = demography.Max(x => x.Value);
+        
+        return demography
+            .Where(x => x.Value == mode)
+            .Select(x => x.Key)
+            .ToArray();
     }
 }
 
-public class Q501_FindModeInBSTTestData: TestData
+public class Q501_FindModeInBSTTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new TreeNode(1, null, new TreeNode(2, new TreeNode(2), null)), new int[] {2}],
         [new TreeNode(1, null, new TreeNode(2, new TreeNode(2), new TreeNode(3))), new int[] {2}],

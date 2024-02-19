71dda3d3ae714e35d428cfda5fb74d1edb2ea0a8
Ferry To <ferrywlto@gmail.com>
2024-02-19 11:58:05 +0800

    test: All test cases of Q520 passed

--- csharp/leetcode/easy/Q520_DetectCaptial.cs
+++ csharp/leetcode/easy/Q520_DetectCaptial.cs
@@ -2,9 +2,43 @@ namespace dojo.leetcode;
 
 public class Q520_DetectCaptial
 {
+    // TC: O(n)
+    // SC: O(1)
     public bool DetectCapitalUse(string word)
     {
-        return false;
+        // 3 rules
+        // 1. all lower
+        // 2. all Upper
+        // 3. only the first is upper
+        if (word.Length == 1) return true;
+        
+        // a
+        var firstIsLower = char.IsLower(word[0]);
+        if(firstIsLower)
+        {
+            return RestAllLower(word, 1);
+        }
+        // Aa
+        var secondIsLower = char.IsLower(word[1]);
+        if(secondIsLower)
+        {
+            return RestAllLower(word, 2);
+        }
+        // AA
+        for (var i=2; i<word.Length; i++)
+        {
+            if (!char.IsUpper(word[i])) return false;
+        }
+        return true;        
+    }
+
+    private bool RestAllLower(string word, int startIdx)
+    {
+        for (var i=startIdx; i<word.Length; i++)
+        {
+            if (!char.IsLower(word[i])) return false;
+        }
+        return true;
     }
 }
 

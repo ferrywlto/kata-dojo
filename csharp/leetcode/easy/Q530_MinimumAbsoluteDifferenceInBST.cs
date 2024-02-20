f5396e548c8504cf3058dd2c6e0b3fa4aa25352b
Ferry To <ferrywlto@gmail.com>
2024-02-20 21:08:57 +0800

    test: All test cases of Q530 passed

--- csharp/leetcode/easy/Q530_MinimumAbsoluteDifferenceInBST.cs
+++ csharp/leetcode/easy/Q530_MinimumAbsoluteDifferenceInBST.cs
@@ -4,7 +4,37 @@ public class Q530_MinimumAbsoluteDifferenceInBST
 {
     public int GetMinimumDifference(TreeNode root)
     {
-        return 0;
+        if (root.IsLeaf) return 0;
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        var hashset = new HashSet<int>();
+
+        while(queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            hashset.Add(node.val);
+
+            if (node.left != null)
+                queue.Enqueue(node.left);
+
+            if (node.right != null)
+                queue.Enqueue(node.right);
+        }
+
+        var temp = hashset.ToArray();
+        Array.Sort(temp);
+
+        var smallest = int.MaxValue;
+        for(var i=0; i<temp.Length - 1; i++)
+        {
+            var diff = Math.Abs(temp[i] - temp[i + 1]);
+            if (diff < smallest)
+                smallest = diff;
+        }
+
+        return smallest;
     }
 }
 
@@ -12,8 +42,9 @@ public class Q530_MinimumAbsoluteDifferenceInBSTTestData: TestData
 {
     protected override List<object[]> Data => 
     [
-        [new TreeNode(1, null, new TreeNode(3, new TreeNode(2), null)), 1],
-        [new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6)), 1]
+        [new int?[]{1,0,48,null,null,12,49}, 1],
+        [new int?[]{4,2,6,1,3}, 1],
+        [new int?[]{236,104,701,null,227,null,911}, 9],
     ];
 }
 
@@ -21,10 +52,11 @@ public class Q530_MinimumAbsoluteDifferenceInBSTTests
 {
     [Theory]
     [ClassData(typeof(Q530_MinimumAbsoluteDifferenceInBSTTestData))]
-    public void OfficialTestCases(TreeNode root, int expected)
+    public void OfficialTestCases(int?[] input, int expected)
     {
         var sut = new Q530_MinimumAbsoluteDifferenceInBST();
-        var actual = sut.GetMinimumDifference(root);
+        var tree = TreeNode.FromLevelOrderingIntArray(input);
+        var actual = sut.GetMinimumDifference(tree!);
         Assert.Equal(expected, actual);
     }
 }

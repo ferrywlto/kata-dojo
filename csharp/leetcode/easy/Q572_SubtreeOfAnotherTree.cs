19114793450e0e0c87bd7e0786a4f569c9fa560d
Ferry To <ferrywlto@gmail.com>
2024-02-25 17:00:13 +0900

    test: Add solution of Q572 that pass local test cases

--- csharp/leetcode/easy/Q572_SubtreeOfAnotherTree.cs
+++ csharp/leetcode/easy/Q572_SubtreeOfAnotherTree.cs
@@ -5,9 +5,72 @@ public class Q572_SubtreeOfAnotherTree
     // find the node with in root tree that have val == subroot.val
     // do a double tree traverse in same node, if in any step the node value is not equal, return false;
     // return true if all nodes values are the same
+    // TC: O(n)
+    // SC: O(n)
     public bool IsSubtree(TreeNode? root, TreeNode? subRoot)
     {
-        return false;
+        if (root == null || subRoot == null) return false;
+        //get the node that is the root of subroot
+        var stack = new Stack<TreeNode>();
+        stack.Push(root!);
+
+        TreeNode? nodeOfSubRoot = null;
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+            if (node.val == subRoot.val)
+            {
+                nodeOfSubRoot = node;
+                Console.WriteLine($"node found: root:{node.val}, sub:{subRoot.val}");
+                break;
+            }
+            if (node.left != null)
+            {
+                stack.Push(node.left);
+            }
+            if (node.right != null)
+            {
+                stack.Push(node.right);
+            }
+        }
+
+        if (nodeOfSubRoot == null)
+        {
+            return false;
+        }
+
+        var parallelStack = new Stack<(TreeNode nodeFromRoot, TreeNode nodeFromSubRoot)>();
+        parallelStack.Push((nodeOfSubRoot, subRoot));
+
+        while (parallelStack.Count > 0)
+        {
+            var (nodeFromRoot, nodeFromSubRoot) = parallelStack.Pop();
+            Console.WriteLine($"parallel: root:{nodeFromRoot.val}, sub:{nodeFromSubRoot.val}");
+            if(nodeFromRoot.val != nodeFromSubRoot.val)
+            {
+                return false;
+            }
+
+            if(nodeFromRoot.left != null && nodeFromSubRoot.left != null)
+            {
+                parallelStack.Push((nodeFromRoot.left, nodeFromSubRoot.left));
+            }
+            else if (nodeFromRoot.left != null || nodeFromSubRoot.left != null)
+            {
+                return false;
+            }
+
+            if (nodeFromRoot.right != null && nodeFromSubRoot.right != null)
+            {
+                parallelStack.Push((nodeFromRoot.right, nodeFromSubRoot.right));
+            }
+            else if(nodeFromRoot.right != null || nodeFromSubRoot.right != null)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 

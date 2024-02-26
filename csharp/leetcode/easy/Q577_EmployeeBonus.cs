99238b7376eab16b4ab9f07f15801ce52b051643
Ferry To <ferrywlto@gmail.com>
2024-02-26 15:24:36 +0800

    test: Add official test caes of Q577 and all test cases passed

---
+++ csharp/leetcode/easy/Q577_EmployeeBonus.cs
@@ -0,0 +1,67 @@
+
+namespace dojo.leetcode;
+
+public class Q577_EmployeeBonus: SqlQuestion
+{
+    public override string Query => 
+    """
+    SELECT name, bonus FROM Employee emp 
+    LEFT JOIN 
+    Bonus b on emp.empId = b.empId
+    WHERE bonus is NULL or bonus < 1000;
+    """;
+}
+
+public class Q577_EmployeeBonusTestData: TestData
+{
+    protected override List<object[]> Data => 
+    [
+        [
+            """
+            INSERT INTO Employee VALUES 
+            (3, 'Brad', null, 4000), 
+            (1, 'John', 3, 1000),
+            (2, 'Dan', 3, 2000),
+            (4, 'Thomas', 3, 4000);
+
+            INSERT INTO Bonus VALUES
+            (2, 500),
+            (4, 2000);
+            """
+        ]
+    ];
+}
+
+public class Q577_EmployeeBonusTests(ITestOutputHelper output): DatabaseTest(output)
+{
+    protected override string TestSchema =>
+    """
+    CREATE TABLE IF NOT EXISTS Employee (empId INT, name VARCHAR, supervisor INT, salary INT);
+    CREATE TABLE IF NOT EXISTS Bonus (empId INT, bonus INT);
+    """;
+
+    [Theory]
+    [ClassData(typeof(Q577_EmployeeBonusTestData))]
+    public void OfficialTestCases(string testDataSql)
+    {
+        InputTestData(testDataSql);
+
+        var sut = new Q577_EmployeeBonus();
+        var reader = ExecuteQuery(sut.Query);
+
+        Assert.True(reader.HasRows);
+        Assert.Equal(2, reader.FieldCount);
+
+        Assert.True(reader.Read());
+        Assert.Equal("Brad", reader.GetString(0));
+        Assert.True(reader.IsDBNull(1));
+
+        Assert.True(reader.Read());
+        Assert.Equal("John", reader.GetString(0));
+        Assert.True(reader.IsDBNull(1));
+
+        Assert.True(reader.Read());
+        Assert.Equal("Dan", reader.GetString(0));
+        Assert.Equal(500, reader.GetInt32(1));
+    }
+}

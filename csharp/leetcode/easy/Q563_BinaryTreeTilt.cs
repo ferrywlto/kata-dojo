ee1d79e9b8790ab40714570d7cc49537aa284c0a
Ferry To <ferrywlto@gmail.com>
2024-02-26 14:39:29 +0800

    test: All test cases of Q563 passed

--- csharp/leetcode/easy/Q563_BinaryTreeTilt.cs
+++ csharp/leetcode/easy/Q563_BinaryTreeTilt.cs
@@ -2,9 +2,20 @@ namespace dojo.leetcode;
 
 public class Q563_BinaryTreeTilt
 {
-    public int FindTilt(TreeNode root)
+    public int FindTilt(TreeNode? root)
     {
-        return 0;
+        if (root == null) return 0;
+
+        var leftSum = SumOfTree(root.left);
+        var rightSum = SumOfTree(root.right);
+        var diff = Math.Abs(leftSum - rightSum);
+        // Console.WriteLine($"node: {root.val}, diff: {diff}, leftSum: {leftSum}, rightSum: {rightSum}");
+        return diff + FindTilt(root.left) + FindTilt(root.right);
+    }
+
+    public int SumOfTree(TreeNode? tree)
+    {
+        return tree == null ? 0 : tree.val + SumOfTree(tree.left) + SumOfTree(tree.right);
     }
 }
 
@@ -14,7 +25,8 @@ public class Q563_BinaryTreeTiltTestData : TestData
     [
         [new int?[] {1, 2, 3}, 1],
         [new int?[] {4, 2, 9, 3, 5, null, 7}, 15],
-    ];   
+        [new int?[] {21, 7, 14, 1, 1, 2, 2, 3, 3}, 9],
+    ];
 }
 
 public class Q563_BinaryTreeTiltTests

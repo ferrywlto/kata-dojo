f71f85b7fe0a1c77e1f6eac66bce99c71a6da282
Ferry To <ferrywlto@gmail.com>
2024-02-29 15:30:43 +0800

    test: All test cases of Q605 passed

--- csharp/leetcode/easy/Q605_CanPlaceFlowers.cs
+++ csharp/leetcode/easy/Q605_CanPlaceFlowers.cs
@@ -3,9 +3,48 @@ namespace dojo.leetcode;
 
 public class Q605_CanPlaceFlowers
 {
+    // Siliding window, 1 pass
     public bool CanPlaceFlowers(int[] flowerbed, int n) 
     {
-        return false;   
+        if (n == 0) return true;
+        if (flowerbed.Length == 1)
+        {
+            if (n > 1) return false;
+            else if (n == 1) return flowerbed[0] == 0;
+        }
+        else if (flowerbed.Length == 2)
+        {
+            if (n > 1) return false;
+            else if (n == 1) return flowerbed[0] == 0 && flowerbed[1] == 0;
+        }
+
+        for(var i=0; i<flowerbed.Length; i++)
+        {
+            // special case for start and end
+            if(i == 0)
+            {
+                if (flowerbed[0] == 0 && flowerbed[1] == 0)
+                {
+                    flowerbed[0] = 1;
+                    n--;
+                }
+            }
+            else if(i == flowerbed.Length - 1)
+            {
+                if (flowerbed[^1] == 0 && flowerbed[^2] == 0)
+                {
+                    flowerbed[^1] = 1;
+                    n--;
+                }
+            }
+            else if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+            if (n == 0) return true;
+        }
+        return n == 0;   
     }
 }
 
@@ -13,8 +52,21 @@ public class Q605_CanPlaceFlowersTestData: TestData
 {
     protected override List<object[]> Data => 
     [
+        [new int[]{0}, 1, true],
+        [new int[]{0}, 2, false],
+        [new int[]{1}, 1, false],
+        [new int[]{0,0}, 1, true],
+        [new int[]{0,0}, 2, false],
+        [new int[]{1,0}, 1, false],
+        [new int[]{0,1}, 1, false],
+        [new int[]{0,0,0}, 1, true],
+        [new int[]{0,0,0}, 2, true],
+        [new int[]{0,1,0}, 1, false],
+        [new int[]{1,0,0}, 1, true],
+        [new int[]{1,0,0}, 2, false],
         [new int[]{1,0,0,0,1}, 1, true],
         [new int[]{1,0,0,0,1}, 2, false],
+        [new int[]{0,0,0,0,0,1,0,0}, 0, true],
     ];
 }
 

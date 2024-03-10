3035cbf8aabca446bc1fd171e01d6be296deead1
Ferry To <ferrywlto@gmail.com>
2024-03-10 17:10:18 +0800

    test: All test cases of Q559 passed

--- csharp/leetcode/easy/Q559_MaxDepthNaryTree.cs
+++ csharp/leetcode/easy/Q559_MaxDepthNaryTree.cs
@@ -1,15 +1,36 @@
 
 using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
 
 namespace dojo.leetcode;
 
 public class Q559_MaxDepthNaryTree
 {
+    // TC: O(n)
+    // SC: O(n)
     public int MaxDepth(NaryTreeNode root) 
     {
-        // use depth first search
-        // update max depth whenever stack.count > max depth when push
-        return 0;
+        if (root == null) return 0;
+        if (root.children.Count == 0) return 1;
+
+        var maxDepth = 1;
+        var stack = new Stack<(NaryTreeNode node, int depth)>();
+        stack.Push((root, 1));
+        while(stack.Count > 0)
+        {
+            var (node, depth) = stack.Pop();
+            if (depth > maxDepth) 
+                maxDepth = depth;
+
+            if(node.children.Count > 0)
+            {
+                for(var i=0; i<node.children.Count; i++)
+                {
+                    stack.Push((node.children[i], depth + 1));
+                }
+            }
+        }
+        return maxDepth;
     }
 }
 
@@ -17,7 +38,7 @@ public class Q559_MaxDepthNaryTreeTestData : TestData
 {
     protected override List<object[]> Data => 
     [
-        // [new int?[]{1,null,3,2,4,null,5,6}, 3],
+        [new int?[]{1,null,3,2,4,null,5,6}, 3],
         [new int?[]{1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14}, 5],
     ];
 }

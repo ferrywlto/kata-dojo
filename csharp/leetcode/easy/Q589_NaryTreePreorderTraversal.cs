5278a4cad5c3f53ebc7927f57ef0d4146a621d5c
Ferry To <ferrywlto@gmail.com>
2024-03-11 13:36:02 +0800

    test: Add official test cases of Q589

---
+++ csharp/leetcode/easy/Q589_NaryTreePreorderTraversal.cs
@@ -0,0 +1,38 @@
+
+namespace dojo.leetcode;
+
+public class Q589_NaryTreePreorderTraversal
+{
+    public IList<int> Preorder(NaryTreeNode root) 
+    {
+        return [];    
+    }
+}
+
+public class Q589_NaryTreePreorderTraversalTestData : TestData
+{
+    protected override List<object[]> Data => 
+    [
+        [
+            new int?[]{1, null, 3, 2, 4, null, 5, 6}, 
+            new int[] {1,3,5,6,2,4}
+            ],
+        [
+            new int?[]{1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14}, 
+            new int[] {1,2,3,6,7,11,14,4,8,12,5,9,13,10}
+        ],
+    ];
+}
+
+public class Q589_NaryTreePreorderTraversalTests : NaryTreeNodeTest
+{
+    [Theory]
+    [ClassData(typeof(Q589_NaryTreePreorderTraversalTestData))]
+    public void OfficialTestCases(int?[] input, int[] expected)
+    {
+        var sut = new Q589_NaryTreePreorderTraversal();
+        var tree = NaryTreeNode.FromLevelOrderIntArray(input);
+        var actual = sut.Preorder(tree!);
+        Assert.True(Enumerable.SequenceEqual(expected, actual));
+    }
+}

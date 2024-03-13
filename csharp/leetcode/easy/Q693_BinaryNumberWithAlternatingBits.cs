b92fb766c1185715e3100e857a231ab9dff78e84
Ferry To <ferrywlto@gmail.com>
2024-03-13 12:24:19 +0800

    perf: Add space O(1) approach with bitwise operation

--- csharp/leetcode/easy/Q693_BinaryNumberWithAlternatingBits.cs
+++ csharp/leetcode/easy/Q693_BinaryNumberWithAlternatingBits.cs
@@ -5,21 +5,40 @@ public class Q693_BinaryNumberWithAlternatingBits
 {
     // TC: O(n)
     // SC: O(n)
-    public bool HasAlternatingBits(int n) 
+    public bool HasAlternatingBits(int n)
     {
-        var binaryStr = Convert.ToString(n,2);
+        var binaryStr = Convert.ToString(n, 2);
         Console.WriteLine(binaryStr);
-        for(var i=0; i<binaryStr.Length - 1; i++)
+        for (var i = 0; i < binaryStr.Length - 1; i++)
         {
             if (binaryStr[i] == binaryStr[i + 1]) return false;
         }
-        return true;       
-    }    
+        return true;
+    }
+
+    // TC: O(n)
+    // SC: O(1)
+    public bool HasAlternatingBits_Bitwise(int n)
+    {
+        int lastBit = n & 1;
+        n >>= 1;
+
+        while (n > 0)
+        {
+            int currentBit = n & 1;
+            if (currentBit == lastBit) return false;
+
+            lastBit = currentBit;
+            n >>= 1;
+        }
+
+        return true;
+    }
 }
 
 public class Q693_BinaryNumberWithAlternatingBitsTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [5, true],
         [7, false],

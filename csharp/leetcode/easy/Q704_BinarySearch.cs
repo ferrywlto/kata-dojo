ee4ef6668c8c77eff9312da9db331b018899d3b8
Ferry To <ferrywlto@gmail.com>
2024-03-15 22:41:37 +0800

    test: All test cases of Q704 passed

--- csharp/leetcode/easy/Q704_BinarySearch.cs
+++ csharp/leetcode/easy/Q704_BinarySearch.cs
@@ -3,8 +3,29 @@ namespace dojo.leetcode;
 
 public class Q704_BinarySearch
 {
+    // TC: O(n)
+    // SC: O(1)
     public int Search(int[] nums, int target) 
     {
+        if (nums.Length == 1) return nums[0] == target ? 0 : -1;
+
+        int left = 0, right = nums.Length - 1;
+
+        while(right-left > 1)
+        {
+            var middle = (right + left)/2;
+            if (target > nums[middle])
+            {
+                left = middle;
+            }
+            else if (target < nums[middle])
+            {
+                right = middle;
+            }
+            else return middle;
+        }
+        if (nums[left] == target) return left;
+        if (nums[right] == target) return right;
         return -1;    
     }
 }
@@ -13,6 +34,8 @@ public class Q704_BinarySearchTestData : TestData
 {
     protected override List<object[]> Data => 
     [
+        [new int[]{5}, 5, 0],
+        [new int[]{2,5}, 5, 1],
         [new int[]{-1,0,3,5,9,12}, 9, 4],
         [new int[]{-1,0,3,5,9,12}, 2, -1],
     ];

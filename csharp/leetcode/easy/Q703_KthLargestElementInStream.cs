ffab22bc9b34c89fabfbc4480e487db66a1de232
Ferry To <ferrywlto@gmail.com>
2024-03-17 14:17:54 +0800

    perf: Improve time from O(n^2) to O(log n) with binary insertion sort

--- csharp/leetcode/easy/Q703_KthLargestElementInStream.cs
+++ csharp/leetcode/easy/Q703_KthLargestElementInStream.cs
@@ -11,19 +11,55 @@ public abstract class Kthlargest
 public class Q703_KthLargestElementInStream : Kthlargest
 {
     protected readonly int targetRank;
-    protected readonly List<int> _nums = [];
+    protected List<int> list = [];
+    // TC: O(log n)
+    // SC: O(n)
     public Q703_KthLargestElementInStream(int k, int[] nums) : base(k, nums)
     {
         targetRank = k;
-        _nums.AddRange(nums);
+        Array.Sort(nums);
+        list.AddRange(nums);
     }
 
     public override int Add(int val)
     {
-        _nums.Add(val);
-        _nums.Sort();
-        var largestK = _nums.TakeLast(targetRank);
-        return largestK.First();
+        BinaryAdd(val);
+        return list[^targetRank];
+    }
+
+    public void BinaryAdd(int val)
+    {
+        if (list.Count == 0 || val > list[^1])
+        {
+            list.Add(val);
+            return;
+        } 
+        else if (val < list[0])
+        {
+            list.Insert(0, val);
+            return;
+        }
+
+        var left = 0;
+        var right = list.Count - 1;
+        while(right - left > 1)
+        {
+            var middle = (right + left) / 2;
+            if(val > list[middle])
+            {
+                left = middle;
+            }
+            else if (val < list[middle])
+            {
+                right = middle;
+            }
+            else 
+            {
+                list.Insert(middle + 1, val);
+                return;
+            }
+        }
+        list.Insert(left + 1, val);
     }
 }
 
@@ -32,6 +68,7 @@ public class Q703_KthLargestElementInStreamTestData : TestData
     protected override List<object[]> Data =>
     [
         [3, new int[] {4, 5, 8, 2}, new int[]{3, 5, 10, 9, 4}, new int?[] {null, 4, 5, 5, 8, 8}],
+        [1, Array.Empty<int>(), new int[]{-3, -2, -4, 0, 4}, new int?[] {null, -3, -2, -2, 0, 4}],
     ];
 }
 

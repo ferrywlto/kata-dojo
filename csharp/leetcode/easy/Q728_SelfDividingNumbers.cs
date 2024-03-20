088b56848e4574bc6f0dcf0bc90a8332591c8de8
Ferry To <ferrywlto@gmail.com>
2024-03-20 14:41:16 +0800

    pref: Avoid int parse and string creation

--- csharp/leetcode/easy/Q728_SelfDividingNumbers.cs
+++ csharp/leetcode/easy/Q728_SelfDividingNumbers.cs
@@ -3,36 +3,34 @@ namespace dojo.leetcode;
 
 public class Q728_SelfDividingNumbers
 {
-    public IList<int> SelfDividingNumbers(int left, int right) 
+    public IList<int> SelfDividingNumbers(int left, int right)
     {
         var result = new List<int>();
-        
-        for(var i=left; i<=right; i++)
+
+        for (var i = left; i <= right; i++)
         {
             if (i < 10) result.Add(i);
             else
             {
-                var str = i.ToString();
+                var n = i;
                 var selfDividing = true;
-                foreach(var c in str) 
+                while (n > 0 && selfDividing)
                 {
-                    var x = c - 48;
-                    if (x == 0 || i % x != 0)
-                    {
-                        selfDividing = false;
-                        break;
-                    }
+                    var digit = n % 10;
+                    if (digit == 0 || i % digit != 0) selfDividing = false;
+                    else n /= 10;
                 }
+                
                 if (selfDividing) result.Add(i);
             }
         }
-        return result;    
+        return result;
     }
 }
 
 public class Q728_SelfDividingNumbersTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [1,22, new int[]{1,2,3,4,5,6,7,8,9,11,12,15,22}],
         [47,85, new int[]{48,55,66,77}],

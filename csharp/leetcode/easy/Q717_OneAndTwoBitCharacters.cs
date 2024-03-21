266f05cd8b3b1a89da3447378aeb02f7fa3a4e78
Ferry To <ferrywlto@gmail.com>
2024-03-21 15:04:01 +0800

    test: All test cases of Q717 passed

--- csharp/leetcode/easy/Q717_OneAndTwoBitCharacters.cs
+++ csharp/leetcode/easy/Q717_OneAndTwoBitCharacters.cs
@@ -3,15 +3,31 @@ namespace dojo.leetcode;
 
 public class Q717_OneAndTwoBitCharacters
 {
-    public bool IsOneBitCharacter(int[] bits) 
+    public bool IsOneBitCharacter(int[] bits)
     {
-        return false;    
+        return Scan(bits, 0);
+    }
+
+    // TC: O(n)
+    // SC: O(1)
+    public bool Scan(int[] bits, int idx)
+    {
+        if (bits[idx] == 0)
+        {
+            if (idx == bits.Length - 1) return true;
+            else return Scan(bits, idx + 1);
+        }
+        else
+        {
+            if (idx == bits.Length - 2) return false;
+            else return Scan(bits, idx + 2);
+        }
     }
 }
 
 public class Q717_OneAndTwoBitCharactersTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[]{1,0,0}, true],
         [new int[]{1,1,1,0}, false],

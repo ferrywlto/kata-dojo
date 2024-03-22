078ed7bdfc2d9b877038c36caa13b5343b04f02f
Ferry To <ferrywlto@gmail.com>
2024-03-22 11:32:55 +0800

    test: Add official test cases of Q724

---
+++ csharp/leetcode/easy/Q724_FindPivotIndex.cs
@@ -0,0 +1,32 @@
+
+namespace dojo.leetcode;
+
+public class Q724_FindPivotIndex
+{
+    public int PivotIndex(int[] nums) 
+    {
+        return -1;    
+    }   
+}
+
+public class Q724_FindPivotIndexTestData : TestData
+{
+    protected override List<object[]> Data => 
+    [
+        [new int[] {1,7,3,6,5,6}, 3],
+        [new int[] {1,2,3}, -1],
+        [new int[] {2,1,-1}, 0],
+    ];
+}
+
+public class Q724_FindPivotIndexTests
+{
+    [Theory]
+    [ClassData(typeof(Q724_FindPivotIndexTestData))]
+    public void OfficialTestCases(int[] input, int expected)
+    {
+        var sut = new Q724_FindPivotIndex();
+        var actual = sut.PivotIndex(input);
+        Assert.Equal(expected, actual);
+    }
+}

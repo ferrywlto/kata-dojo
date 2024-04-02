be753bae6c65f26f8800bf831238aea6e35bf12c
Ferry To <ferrywlto@gmail.com>
2024-04-02 11:32:05 +0800

    test: All test cases of Q804 passed

--- csharp/leetcode/easy/Q804_UniqueMorseCodeWords.cs
+++ csharp/leetcode/easy/Q804_UniqueMorseCodeWords.cs
@@ -1,17 +1,38 @@
+using System.Text;
 
 namespace dojo.leetcode;
 
 public class Q804_UniqueMoreseCodeWords
 {
-    public int UniqueMorseRepresentations(string[] words) 
+    public readonly string[] MorseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+    public string GetMorseCodeFromChar(char c)
     {
-        return 0;    
-    }    
+        if (c < 65 || c > 122 || (c > 90 && c < 97)) 
+            return string.Empty;
+        if (c > 64 && c < 91) 
+            return MorseCode[c - 65];
+        return MorseCode[c - 97];
+    }
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        var hashset = new HashSet<string>();
+        var sb = new StringBuilder();
+        foreach (var word in words)
+        {
+            sb.Clear();
+            foreach (var c in word)
+            {
+                sb.Append(GetMorseCodeFromChar(c));
+            }
+            hashset.Add(sb.ToString());
+        }
+        return hashset.Count;
+    }
 }
 
 public class Q804_UniqueMoreseCodeWordsTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new string[] {"gin","zen","gig","msg"}, 2],
         [new string[] {"a"}, 1],
@@ -26,6 +47,46 @@ public class Q804_UniqueMoreseCodeWordsTests
     {
         var sut = new Q804_UniqueMoreseCodeWords();
         var actual = sut.UniqueMorseRepresentations(input);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData('a', ".-")]
+    [InlineData('z', "--..")]
+    [InlineData('h', "....")]
+    public void GetMoreCodeFromChar_LowerCaseInput_ReturnCorrectMorseCode(char input, string expected)
+    {
+        var sut = new Q804_UniqueMoreseCodeWords();
+        var actual = sut.GetMorseCodeFromChar(input);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData('A', ".-")]
+    [InlineData('Z', "--..")]
+    [InlineData('H', "....")]
+    public void GetMoreCodeFromChar_UpperCaseInput_ReturnCorrectMorseCode(char input, string expected)
+    {
+        var sut = new Q804_UniqueMoreseCodeWords();
+        var actual = sut.GetMorseCodeFromChar(input);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(']')]
+    [InlineData('`')]
+    [InlineData('{')]
+    [InlineData('~')]
+    [InlineData(' ')]
+    [InlineData('!')]
+    [InlineData('@')]
+    [InlineData('0')]
+    [InlineData('9')]
+    public void GetMoreCodeFromChar_InvalidInput_ReturnEmptyString(char input)
+    {
+        var expected = string.Empty;
+        var sut = new Q804_UniqueMoreseCodeWords();
+        var actual = sut.GetMorseCodeFromChar(input);
         Assert.Equal(expected, actual);
     }
 }

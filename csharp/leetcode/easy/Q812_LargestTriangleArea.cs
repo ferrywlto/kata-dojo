65afaa16eb248246ee53a960ec78354176c053f0
Ferry To <ferrywlto@gmail.com>
2024-04-04 11:55:27 +0800

    test: All test cases of Q812 passed

--- csharp/leetcode/easy/Q812_LargestTriangleArea.cs
+++ csharp/leetcode/easy/Q812_LargestTriangleArea.cs
@@ -3,15 +3,38 @@ namespace dojo.leetcode;
 
 public class Q812_LargestTriangleArea
 {
-    public double LargestTriangleArea(int[][] points) 
+    // This can be achieved O(n log n) + O(m) time with Convex Hull algorithm.
+    // However this is not the purpose of kata dojo thus not implementing it. 
+    // TC: O(n^3)
+    // SC: O(1)
+    public double LargestTriangleArea(int[][] points)
     {
-        return 0;    
+        var largestArea = double.MinValue;
+
+        for (var i = 0; i < points.Length - 2; i++)
+        {
+            for (var j = i + 1; j < points.Length - 1; j++)
+            {
+                for (var k = j + 1; k < points.Length; k++)
+                {
+                    var area = 0.5 * Math.Abs
+                    (
+                        points[i][0] * (points[j][1] - points[k][1]) +
+                        points[j][0] * (points[k][1] - points[i][1]) +
+                        points[k][0] * (points[i][1] - points[j][1])
+                    );
+                    if (area > largestArea) largestArea = area;
+                }
+            }
+        }
+
+        return largestArea;
     }
 }
 
 public class Q812_LargestTriangleAreaTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [
             new int[][] { [0,0],[0,1],[1,0],[0,2],[2,0] }, 2.00000

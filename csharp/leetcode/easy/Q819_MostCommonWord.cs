00a856e9afffbc92d3899f4f40362e88db60ef0b
Ferry To <ferrywlto@gmail.com>
2024-04-05 14:45:35 +0800

    test: All test cases of Q819 passsed

--- csharp/leetcode/easy/Q819_MostCommonWord.cs
+++ csharp/leetcode/easy/Q819_MostCommonWord.cs
@@ -1,19 +1,51 @@
-
 namespace dojo.leetcode;
 
 public class Q819_MostCommonWord
 {
-    public string MostCommonWord(string paragraph, string[] banned) 
+    // TC: O(n), n is length of paragraph
+    // SC: O(n), n is number of distinct words found 
+    const string symbols = "!?',;. ";
+    public string MostCommonWord(string paragraph, string[] banned)
     {
-        return string.Empty;    
+        var tokens = paragraph.ToLower().Split(symbols.ToCharArray());
+        var bannedSet = banned.ToHashSet();
+        var dict = new Dictionary<string, int>();
+        var mostFrequent = 0;
+        var mostFrequentWord = string.Empty;
+        foreach (var token in tokens)
+        {
+            if (token == string.Empty || bannedSet.Contains(token)) continue;
+
+            if (!dict.TryGetValue(token, out var value))
+            {
+                dict.Add(token, 1);
+                if (mostFrequent == 0)
+                {
+                    mostFrequent = 1;
+                    mostFrequentWord = token;
+                }
+            }
+            else
+            {
+                dict[token] = ++value;
+                if (value > mostFrequent)
+                {
+                    mostFrequent = value;
+                    mostFrequentWord = token;
+                }
+            }
+        }
+        return mostFrequentWord;
     }
 }
 
 public class Q819_MostCommonWordTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["Bob hit a ball, the hit BALL flew far after it was hit.", new string[] {"hit"}, "ball"],
+        ["a, a, a, a, b,b,b,c, c", new string[] {"a"}, "b"],
+        ["a.", Array.Empty<string>(), "a"],
         ["a", Array.Empty<string>(), "a"],
     ];
 }

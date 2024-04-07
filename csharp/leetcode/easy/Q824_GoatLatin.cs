a3a2c59df53f6553e797871c0c8415426007bad9
Ferry To <ferrywlto@gmail.com>
2024-04-07 11:57:14 +0800

    test: All test cases of Q824 passed

--- csharp/leetcode/easy/Q824_GoatLatin.cs
+++ csharp/leetcode/easy/Q824_GoatLatin.cs
@@ -1,11 +1,33 @@
+using System.Text;
 
 namespace dojo.leetcode;
 
 public class Q824_GoatLatin
 {
+    // TC: O(n), n is total words in sentence
+    // SC: O(n+m), n is average word length, m is number of words
+    const string vowels = "aeiouAEIOU";
     public string ToGoatLatin(string sentence) 
     {
-        return string.Empty;    
+        var endA = new StringBuilder("a");
+        var words = sentence.Split(" ");
+        var stringFactory = new StringBuilder();
+        for(var i=0; i<words.Length; i++)
+        {
+            stringFactory.Clear();
+            if(vowels.IndexOf(words[i][0]) > -1)
+            {
+                stringFactory.Append(words[i]);
+            }
+            else
+            {
+                stringFactory.Append(words[i][1..]).Append(words[i][0]);
+            }
+            stringFactory.Append("ma").Append(endA);
+            words[i] = stringFactory.ToString();
+            endA.Append('a');
+        }
+        return string.Join(' ', words);    
     }
 }
 
@@ -15,6 +37,7 @@ public class Q824_GoatLatinTestData : TestData
     [
         ["I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"],
         ["The quick brown fox jumped over the lazy dog", "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"],
+        ["Each word consists of lowercase and uppercase letters only", "Eachmaa ordwmaaa onsistscmaaaa ofmaaaaa owercaselmaaaaaa andmaaaaaaa uppercasemaaaaaaaa etterslmaaaaaaaaa onlymaaaaaaaaaa"],
     ];
 }
 

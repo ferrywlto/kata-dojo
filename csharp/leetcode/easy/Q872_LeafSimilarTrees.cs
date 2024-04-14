01d2aa8cd140760a0401ede585a5c222e84675e4
Ferry To <ferrywlto@gmail.com>
2024-04-14 13:53:09 +0800

    test: All test cases of Q872 passed

--- csharp/leetcode/easy/Q872_LeafSimilarTrees.cs
+++ csharp/leetcode/easy/Q872_LeafSimilarTrees.cs
@@ -1,10 +1,43 @@
 namespace dojo.leetcode;
 
+// TC: O(n), all nodes need to traverse once
+// SC: O(tree1.depth + tree2.depth)
 public class Q872_LeafSimilarTrees
 {
+    readonly Stack<TreeNode> ParallelStack1 = new();
+    readonly Stack<TreeNode> ParallelStack2 = new();
+    int? Leaf1Value = null;
+    int? Leaf2Value = null;
     public bool LeafSimilar(TreeNode root1, TreeNode root2) 
     {
-        return false;    
+        ParallelStack1.Push(root1);
+        ParallelStack2.Push(root2);
+        
+        while(ParallelStack1.Count > 0 || ParallelStack2.Count > 0)
+        {
+            Leaf1Value = GetNextLeaf(ParallelStack1);
+            Leaf2Value = GetNextLeaf(ParallelStack2);
+            if (Leaf1Value != Leaf2Value) return false;
+        }
+        return true;
+    }
+
+    public int? GetNextLeaf(Stack<TreeNode> stack)
+    {
+        while(stack.Count > 0)
+        {
+            var node = stack.Pop();
+            if(node.IsLeaf)
+            {
+                return node.val;
+            }
+            else 
+            {
+                if (node.right != null) stack.Push(node.right);
+                if (node.left != null) stack.Push(node.left);
+            }
+        }
+        return null;
     }
 }
 
@@ -21,11 +54,21 @@ public class Q872_LeafSimilarTreesTestData : TestData
             new int?[]{1,2,3},
             new int?[]{1,3,2},
             false,
-        ]
+        ],
+        [
+            new int?[]{4,2,6,1,3,5,7},
+            new int?[]{4,2,6,null,3,5,7},
+            false,
+        ],
+        [
+            new int?[]{3,5,1,6,7,4,2,null,null,null,null,null,null,9,11,null,null,8,10},
+            new int?[]{3,5,1,6,2,9,8,null,null,7,4},
+            false,
+        ],
     ];
 }
 
-public class Q872_LeafSimilarTreesTests
+public class Q872_LeafSimilarTreesTests(ITestOutputHelper output) : TreeNodeTest(output)
 {
     [Theory]
     [ClassData(typeof(Q872_LeafSimilarTreesTestData))]

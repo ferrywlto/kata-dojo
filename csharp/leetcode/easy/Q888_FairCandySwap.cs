5a658714809540707b445cc8724d55e100f3e841
Ferry To <ferrywlto@gmail.com>
2024-04-15 13:52:12 +0800

    test: All test cases of Q888 passed

--- csharp/leetcode/easy/Q888_FairCandySwap.cs
+++ csharp/leetcode/easy/Q888_FairCandySwap.cs
@@ -1,8 +1,26 @@
 public class Q888_FairCandySwap
 {
+    // This use one pass instead of nested n^2 approach
+    // TC: O(n)
+    // SC: O(n), n is distinct values from both Alice and Bob 
     public int[] FairCandySwap(int[] aliceSizes, int[] bobSizes) 
     {
-        return [0,0];    
+        var aliceSum = aliceSizes.Sum();
+        var bobSum = bobSizes.Sum();
+        var avg = (bobSum + aliceSum) / 2;
+        var aliceDiffFromAvg = avg - aliceSum;
+
+        var bobSet = new HashSet<int>(bobSizes);
+        var aliceSet = new HashSet<int>(aliceSizes);
+        foreach (var size in aliceSet)
+        {
+            if (bobSet.Contains(size + aliceDiffFromAvg))
+            {
+                return [size, size + aliceDiffFromAvg];
+            }
+        }
+
+        return [0, 0];
     }
 }
 
@@ -13,6 +31,7 @@ public class Q888_FairCandySwapTestData : TestData
         [new int[]{1,1}, new int[] {2,2}, new int[] {1,2}],
         [new int[]{1,2}, new int[] {2,3}, new int[] {1,2}],
         [new int[]{2}, new int[] {1,3}, new int[] {2,3}],
+        [new int[]{1,3}, new int[] {2}, new int[] {3,2}],
     ];
 }
 

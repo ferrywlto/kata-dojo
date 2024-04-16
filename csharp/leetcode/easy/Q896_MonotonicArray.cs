da14f50abf167f3a3481583ea444c92019d149be
Ferry To <ferrywlto@gmail.com>
2024-04-16 12:52:13 +0800

    test: All test cases of Q896 passed

--- csharp/leetcode/easy/Q896_MonotonicArray.cs
+++ csharp/leetcode/easy/Q896_MonotonicArray.cs
@@ -1,14 +1,35 @@
 public class Q896_MonotonicArray
 {
-    public bool IsMonotonic(int[] nums) 
+    // TC: O(n), n is length of nums, it have to iterate all items except the first once
+    // SC: O(1), no extra memory used
+    public bool IsMonotonic(int[] nums)
     {
-        return false;    
-    }    
+        if (nums.Length <= 1) return true;
+
+        // 0 is init
+        // 1 is upward
+        // -1 is downward
+        int direction = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                if (direction == 0) direction = 1;
+                else if (direction == -1) return false;
+            }
+            else if (nums[i] < nums[i - 1])
+            {
+                if (direction == 0) direction = -1;
+                else if (direction == 1) return false;
+            }
+        }
+        return true;
+    }
 }
 
 public class Q896_MonotonicArrayTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[]{1,2,2,3}, true],
         [new int[]{6,5,4,4}, true],
@@ -18,6 +39,8 @@ public class Q896_MonotonicArrayTestData : TestData
         [new int[]{-1,-3,-2}, false],
         [new int[]{-1,0,1}, true],
         [new int[]{0,0,0}, true],
+        [new int[]{0}, true],
+        [Array.Empty<int>(), true],
     ];
 }
 

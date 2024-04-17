0d620a72486fb0dcd1ec3f0779909f5a128d2787
Ferry To <ferrywlto@gmail.com>
2024-04-17 11:57:47 +0800

    test: All test cases of Q884 passed

--- csharp/leetcode/leetcode/Q884_UncommonWordsInTwoSentences.cs
+++ csharp/leetcode/leetcode/Q884_UncommonWordsInTwoSentences.cs
@@ -1,15 +1,38 @@
 
 public class Q884_UncommonWordsInTwoSentences
 {
+    // TC: O(n), n is total length of s1 + s2
+    // SC: O(n), n is total words from both string
     public string[] UncommonFromSentences(string s1, string s2)
     {
-        return [];
+        var dict = new Dictionary<string, int>();
+        var words1 = s1.Split(' ');
+        var words2 = s2.Split(' ');
+
+        GroupWords(words1);
+        GroupWords(words2);
+
+        void GroupWords(string[] words)
+        {
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (!dict.TryGetValue(words[i], out var value)) 
+                    dict.Add(words[i], 1);
+                else 
+                    dict[words[i]] = ++value;
+            }
+        }
+
+        return dict
+            .Where(p => p.Value == 1)
+            .Select(p => p.Key)
+            .ToArray();
     }
 }
 
 public class Q884_UncommonWordsInTwoSentencesTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["this apple is sweet", "this apple is sour", new string[] {"sweet","sour"}],
         ["apple apple", "banana", new string[] {"banana"}],

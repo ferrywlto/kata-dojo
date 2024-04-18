30e94d1ef7706aef9a1bc5c1b3836a49281d8d40
Ferry To <ferrywlto@gmail.com>
2024-04-18 12:07:29 +0800

    test: All test cases of Q883 passed

--- csharp/leetcode/easy/Q883_ProjectionAreaOf3DShape.cs
+++ csharp/leetcode/easy/Q883_ProjectionAreaOf3DShape.cs
@@ -1,15 +1,37 @@
-
 class Q883_ProjectionAreaOf3DShape
 {
-    public int ProjectionArea(int[][] grid) 
+    /*
+        [1,2]
+        [3,4]
+        1. top -> sum of non-zero
+        2. x -> keep largest across rows
+        3. y -> keep largest across cols
+    */
+    // TC: O(n), n is the total cells in grid
+    // SC: O(n), n is the length the edge of the grid * 2
+    public int ProjectionArea(int[][] grid)
     {
-        return 0;
+        var nonZero = 0;
+        var largestAcrossRow = new int[grid.Length];
+        var largestAcrossCol = new int[grid.Length];
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] > 0) nonZero++;
+                if (grid[i][j] > largestAcrossRow[i]) largestAcrossRow[i] = grid[i][j];
+                if (grid[i][j] > largestAcrossCol[j]) largestAcrossCol[j] = grid[i][j];
+            }
+
+        }
+        return nonZero + largestAcrossRow.Sum() + largestAcrossCol.Sum();
     }
 }
 
 class Q883_ProjectionAreaOf3DShapeTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[][] {[1,2],[3,4]}, 17],
         [new int[][] {[2]}, 5],

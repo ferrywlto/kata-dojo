b1a8a2f82cd03b85cce3960d24359b53693fd9ba
Ferry To <ferrywlto@gmail.com>
2024-04-21 13:16:09 +0800

    pref: Improve time from O(2n) to O(n/2) using two pointers approach

--- csharp/leetcode/easy/Q905_SortArrayByParity.cs
+++ csharp/leetcode/easy/Q905_SortArrayByParity.cs
@@ -1,5 +1,24 @@
 class Q905_SortArrayByParity
 {
+    // TC: O(n/2) -> O(n), which improves time from original O(2n) 
+    public int[] SortArrayByParity_TwoPointers(int[] nums)
+    {
+        var startIdx = 0;
+        var endIdx = nums.Length - 1;
+        while (startIdx < endIdx)
+        {
+            // Means an odd at the beginning and an even at the end, which is the swap case
+            if (nums[startIdx] % 2 > nums[endIdx] % 2)
+            {
+                (nums[startIdx], nums[endIdx]) = (nums[endIdx], nums[startIdx]);
+            }
+
+            if(nums[startIdx] % 2 == 0) startIdx++;
+            if(nums[endIdx] % 2 == 1) endIdx--;
+        }
+        return nums;
+    }
+
     // TC: O(2n) -> O(n), n is the length of nums
     // SC: O(1), no extra data structure used
     public int[] SortArrayByParity(int[] nums)
@@ -35,7 +54,7 @@ class Q905_SortArrayByParityTestData : TestData
 {
     protected override List<object[]> Data =>
     [
-        [new int[]{3,1,2,4}, new int[]{2,4,3,1}],
+        [new int[]{3,1,2,4}, new int[]{4,2,1,3}],
         [new int[]{2,1,3,4}, new int[]{2,4,3,1}],
         [new int[]{2,4,3,1}, new int[]{2,4,3,1}],
         [new int[]{0}, new int[]{0}],
@@ -49,37 +68,7 @@ public class Q905_SortArrayByParityTests
     public void OfficialTestCases(int[] input, int[] expected)
     {
         var sut = new Q905_SortArrayByParity();
-        var actual = sut.SortArrayByParity(input);
+        var actual = sut.SortArrayByParity_TwoPointers(input);
         Assert.Equal(expected, actual);
     }
-
-    // [Theory]
-    // [InlineData(new int[] { 2 } , 0, 0)]
-    // [InlineData(new int[] { 1, 2, 3 } , 0, 1)]
-    // [InlineData(new int[] { 1, 2, 3 } , 1, 1)]
-    // [InlineData(new int[] { 1, 2, 4, 3 } , 0, 1)]
-    // [InlineData(new int[] { 1, 2, 4, 3 } , 1, 1)]
-    // public void NextEvenIdx_StartFromFirstEvenOrBefore_ReturnCorrectIndex(int[] input, int startIdx, int expected) 
-    // => Assert_NextEvenIdx(input, startIdx, expected);
-
-    // [Theory]
-    // [InlineData(new int[] { 1, 2, 4, 3 } , 2, 2)]
-    // public void NextEvenIdx_StartFromSecondEven_ReturnCorrectIndex(int[] input, int startIdx, int expected) => Assert_NextEvenIdx(input, startIdx, expected);
-
-    // [Theory]
-    // [InlineData(new int[] { 2 } , 1, -1)]
-    // [InlineData(new int[] { 1, 2, 3 } , 2, -1)]
-    // public void NextEvenIdx_StartAfterLastEven_ReturnNegativeOne(int[] input, int startIdx, int expected) => Assert_NextEvenIdx(input, startIdx, expected);
-
-    // [Theory]
-    // [InlineData(new int[] { 1 } , 0, -1)]
-    // [InlineData(new int[] { 1 } , 1, -1)]
-    // public void NextEvenIdx_InputNoEven_ReturnNegativeOne(int[] input, int startIdx, int expected) => Assert_NextEvenIdx(input, startIdx, expected);
-
-    // private void Assert_NextEvenIdx(int[] input, int startIdx, int expected)
-    // {
-    //     var sut = new Q905_SortArrayByParity();
-    //     var actual = sut.NextEvenIdx(input, startIdx);
-    //     Assert.Equal(expected, actual);
-    // }
 }

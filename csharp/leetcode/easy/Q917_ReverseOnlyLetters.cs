1b544836d7269a09fd092ede981e81094c504094
Ferry To <ferrywlto@gmail.com>
2024-04-23 13:25:35 +0800

    test: All test cases of Q917 passed

--- csharp/leetcode/easy/Q917_ReverseOnlyLetters.cs
+++ csharp/leetcode/easy/Q917_ReverseOnlyLetters.cs
@@ -1,15 +1,33 @@
+using System.Text;
 
 class Q917_ReverseOnlyLetters
 {
-    public string ReverseOnlyLetters(string s) 
+    // TC: O(n), n is length of s/2
+    // SC: O(n), n is the length of s used by string builder
+    public string ReverseOnlyLetters(string s)
     {
-        return string.Empty;    
+        var sb = new StringBuilder(s);
+        var startIdx = 0;
+        var endIdx = sb.Length - 1;
+        while (startIdx < endIdx)
+        {
+            if (char.IsLetter(sb[startIdx]) && char.IsLetter(sb[endIdx]))
+            {
+                // swap
+                (sb[endIdx], sb[startIdx]) = (sb[startIdx], sb[endIdx]);
+                startIdx++;
+                endIdx--;
+            }
+            if (!char.IsLetter(sb[startIdx])) startIdx++;
+            if (!char.IsLetter(sb[endIdx])) endIdx--;
+        }
+        return sb.ToString();
     }
 }
 
 class Q917_ReverseOnlyLettersTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["ab-cd", "dc-ba"],
         ["a-bC-dEf-ghIj", "j-Ih-gfE-dCba"],

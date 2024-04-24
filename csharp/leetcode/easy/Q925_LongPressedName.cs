9289f717ea1adfd1206bac1be2dc621e4328511d
Ferry To <ferrywlto@gmail.com>
2024-04-25 00:42:01 +0800

    test: All test cases of Q925 passed

--- csharp/leetcode/easy/Q925_LongPressedName.cs
+++ csharp/leetcode/easy/Q925_LongPressedName.cs
@@ -1,15 +1,29 @@
+using System.Text;
+using System.Text.RegularExpressions;
 
 class Q925_LongPressedName
 {
-    public bool IsLongPressedName(string name, string typed) 
+    // TC: O(n), n is length of name
+    // SC: O(n), n is size of string builder, which is also the length of name 
+    public bool IsLongPressedName(string name, string typed)
     {
-        return false;
+        var regexInput = new StringBuilder("^");
+        for (var i = 0; i < name.Length; i++)
+        {
+            regexInput.Append($"[{name[i]}]+");
+        }
+        regexInput.Append('$');
+
+        var regex = new Regex(regexInput.ToString());
+        var matches = regex.Matches(typed);
+
+        return matches.Count == 1;
     }
 }
 
 class Q925_LongPressedNameTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["alex", "aaleex", true],
         ["saeed", "ssaaedd", false],

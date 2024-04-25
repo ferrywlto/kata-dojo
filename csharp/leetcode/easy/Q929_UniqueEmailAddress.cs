f21fdd488a577fbed28a2a87e4670892c97b8148
Ferry To <ferrywlto@gmail.com>
2024-04-25 12:29:49 +0800

    test: All test cases of Q9929 passed

--- csharp/leetcode/easy/Q929_UniqueEmailAddress.cs
+++ csharp/leetcode/easy/Q929_UniqueEmailAddress.cs
@@ -1,15 +1,36 @@
-
 class Q929_UniqueEmailAddress
 {
+    // TC: O(n), n is number of emails, iterate each element once
+    // SC: O(n), n is number of unique emails
     public int NumUniqueEmails(string[] emails)
     {
-        return 0;
+        var dict = new Dictionary<string, HashSet<string>>();
+        foreach (var email in emails)
+        {
+            var parts = email.Split('@');
+            var localName = parts[0];
+            var domainName = parts[1];
+
+            var firstPlusIdx = localName.IndexOf('+');
+            var cleansed = firstPlusIdx == -1 ? localName : localName[..firstPlusIdx];
+            cleansed = cleansed.Replace(".", string.Empty);
+
+            if (dict.TryGetValue(domainName, out var localNames))
+            {
+                localNames.Add(cleansed);
+            }
+            else
+            {
+                dict.Add(domainName, [cleansed]);
+            }
+        }
+        return dict.Sum(p => p.Value.Count);
     }
 }
 
 class Q929_UniqueEmailAddressTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [
             new string[] {

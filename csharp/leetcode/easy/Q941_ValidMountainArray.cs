1f944dfeb1f8fa89725bcd593cb3876e46fb9ba2
Ferry To <ferrywlto@gmail.com>
2024-04-28 11:59:04 +0800

    test: All test cases of Q941 passed

--- csharp/leetcode/easy/Q941_ValidMountainArray.cs
+++ csharp/leetcode/easy/Q941_ValidMountainArray.cs
@@ -1,20 +1,45 @@
 
 class Q941_ValidMountainArray
 {
-        public bool ValidMountainArray(int[] arr) {
-        return false;   
+    // TC: O(n), n is length of array
+    // SC: O(1), no extra data structure used
+    public bool ValidMountainArray(int[] arr)
+    {
+        if (arr.Length < 3) return false;
+
+        bool wentUp = false;
+        bool wentDown = false;
+        for(var i=1; i<arr.Length; i++)
+        {
+            if (arr[i] > arr[i - 1])
+            {
+                if (!wentUp && !wentDown) wentUp = true;
+                // cannot go up once it goes down
+                else if (wentDown) return false;
+            }
+            else if (arr[i] < arr[i - 1])
+            {
+                if (wentUp && !wentDown) wentDown = true;
+                // cannot go down before go up
+                else if (!wentUp) return false;
+            }
+            else return false;
+        }
+        return wentUp && wentDown;
     }
 }
 
 class Q941_ValidMountainArrayTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[]{2,1}, false],
         [new int[]{3,5,5}, false],
         [new int[]{0,3,2,1}, true],
         [new int[]{0,3,2,1,1}, false],
         [new int[]{0,3,2,1,3}, false],
+        [new int[]{0,0,3,2,1}, false],
+        [new int[]{0,1,2,3,4,5,6,7,8,9}, false],
     ];
 }
 

3694cbaa3d35d2c66f6acb2232bc62875f34a9a1
Ferry To <ferrywlto@gmail.com>
2024-04-29 15:48:22 +0800

    test: All test cases of Q944 passed

--- csharp/leetcode/easy/Q944_DeleteColumnToMakeSorted.cs
+++ csharp/leetcode/easy/Q944_DeleteColumnToMakeSorted.cs
@@ -1,8 +1,27 @@
 class Q944_DeleteColumnToMakeSorted
 {
+    // TC: O(n), n is total characters in strs
+    // SC: O(1), didn't use any data structure
     public int MinDeletionSize(string[] strs)
     {
-        return 0;
+        if (strs.Length <= 1 || strs[0].Length == 0) return 0;
+
+        var cols = strs[0].Length;
+        
+        var result = 0;
+
+        for(var col = 0; col<cols; col++)
+        {
+            for(var row = 1; row < strs.Length; row++)
+            {
+                if(strs[row][col] < strs[row - 1][col]) 
+                {
+                    result++;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
 

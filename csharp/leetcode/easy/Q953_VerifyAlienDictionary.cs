b0ff74fba5c850f18dce54def8016743e4185769
Ferry To <ferrywlto@gmail.com>
2024-05-17 00:42:11 +0100

    test: All test cases of Q953 passed

--- csharp/leetcode/easy/Q953_VerifyAlienDictionary.cs
+++ csharp/leetcode/easy/Q953_VerifyAlienDictionary.cs
@@ -1,8 +1,36 @@
 class Q953_VerifyAlienDictionary
 {
+    Dictionary<char, int> alienDict = [];
     public bool IsAlienSorted(string[] words, string order)
     {
-        return false;
+        alienDict = [];
+        for (var i = 0; i < order.Length; i++)
+        {
+            alienDict.Add(order[i], i + 1);
+        }
+        var maxLength = words.Max(x => x.Length);
+        var vector = new int[words.Length];
+
+        for (var i = 0; i < maxLength; i++)
+        {
+            UpdateColumnVector(words, i, vector);
+            var acsending = true;
+            for (var j = 0; j < vector.Length - 1; j++)
+            {
+                if (vector[j + 1] < vector[j]) return false;
+                acsending = acsending && (vector[j + 1] > vector[j]);
+            }
+            if (acsending) return true;
+        }
+        return true;
+    }
+    public void UpdateColumnVector(string[] words, int idx, int[] vector)
+    {
+        for (var i = 0; i < words.Length; i++)
+        {
+            if (idx >= words[i].Length) vector[i] = 0;
+            else vector[i] = alienDict[words[i][idx]];
+        }
     }
 }
 
@@ -10,6 +38,8 @@ class Q953_VerifyAlienDictionaryTestData : TestData
 {
     protected override List<object[]> Data =>
     [
+        [new string[] {"iekm","tpnhnbe"}, "loxbzapnmstkhijfcuqdewyvrg", false],
+        [new string[] {"hello","hello"}, "abcdefghijklmnopqrstuvwxyz", true],
         [new string[] {"hello","leetcode"}, "hlabcdefgijkmnopqrstuvwxyz", true],
         [new string[] {"word","world","row"}, "worldabcefghijkmnpqstuvxyz", false],
         [new string[] {"apple","app"}, "abcdefghijklmnopqrstuvwxyz", false],

f14d007d871ddc84d0a1e9749ccbf66fa3998577
Ferry To <ferrywlto@gmail.com>
2024-05-17 14:46:46 +0100

    test: All test cases of Q977 passed

--- csharp/leetcode/easy/Q977_SquaresOfSortedArray.cs
+++ csharp/leetcode/easy/Q977_SquaresOfSortedArray.cs
@@ -1,18 +1,68 @@
-
-using Microsoft.VisualBasic;
-
 class Q977_SquaresOfSortedArray
 {
-    public int[] SortedSquares(int[] nums) {
-        return [];    
+    // This is non-trival solution required for O(n) time instead of O(n * n log n) time with direct squaring each element and then sort again
+    // TC: O(2n), n is length of nums
+    // SC: O(n*h), n is length of nums, n tree nodes + h tree height for stack 
+    public int[] SortedSquares(int[] nums)
+    {
+        if (nums.Length == 1) return [nums[0] * nums[0]];
+
+        var root = new TreeNode(nums[0] * nums[0]);
+
+        for(var i=1; i<nums.Length; i++)
+        {
+            var node = root;
+            while(node!=null)
+            {
+                var square = nums[i] * nums[i];
+                if(square >= node.val)
+                {
+                    if (node.right == null)
+                    {
+                        node.right = new TreeNode(square);
+                        break;
+                    }
+                    else node = node.right;
+                }
+                else 
+                {
+                    if (node.left == null)
+                    {
+                        node.left = new TreeNode(square);
+                        break;
+                    }
+                    else node = node.left;
+                }
+            }
+        }
+        
+        // Inorder traversal to get sorted result
+        var stack = new Stack<TreeNode>();
+        TreeNode? current = root;
+        var result = new int[nums.Length];
+        var idx = 0;
+        while (current != null || stack.Count > 0)
+        {
+            // Down to deepest left first
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+            current = stack.Pop();
+            result[idx++] = current.val;
+            current = current.right;
+        }
+
+        return result;
     }
 }
 
 class Q977_SquaresOfSortedArrayTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
-        [new int[]{-4,-1,0,3,10}, new int[] {0,1,9,16,100}],
+        // [new int[]{-4,-1,0,3,10}, new int[] {0,1,9,16,100}],
         [new int[]{-7,-3,2,3,11}, new int[] {4,9,9,49,121}],
     ];
 }

504916f64b459a832bf8ef67baea6dfe303444cf
Ferry To <ferrywlto@gmail.com>
2024-05-17 22:36:38 +0100

    test: Add test cases of Q942 passed

--- csharp/leetcode/easy/Q942_DIStringMatch.cs
+++ csharp/leetcode/easy/Q942_DIStringMatch.cs
@@ -1,9 +1,21 @@
 
 class Q942_DIStringMatch
 {
+    // TC: O(n), n is length of s
+    // SC: O(n), n is result size, if not counting it is O(1) as no interim space used 
     public int[] DiStringMatch(string s)
     {
-        return [];
+        var max = s.Length;
+        var min = 0;
+        var result = new int[max + 1];
+
+        for(var i=0; i<result.Length - 1; i++)
+        {
+            if (s[i] == 'D') result[i] = max--;
+            else result[i] = min++;
+        }
+        result[^1] = min;
+        return result;
     }
 }
 

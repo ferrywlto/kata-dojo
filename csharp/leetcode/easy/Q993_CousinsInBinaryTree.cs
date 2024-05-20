6972fa3b1d1bf8f4e2915cd3d1e0ae83386a986c
Ferry To <ferrywlto@gmail.com>
2024-05-20 10:10:31 +0100

    test: All test cases of Q993 passed

--- csharp/leetcode/easy/Q993_CousinsInBinaryTree.cs
+++ csharp/leetcode/easy/Q993_CousinsInBinaryTree.cs
@@ -1,7 +1,32 @@
 class Q997_CousinsInBinaryTree
 {
+    // TC: O(n), n is nodes in tree
+    // SC: O(n+h), n nodes in tree + h height of stack
     public bool IsCousins(TreeNode root, int x, int y)
     {
+        var dict = new Dictionary<int, (int? parent, int height)>();
+        var stack = new Stack<(TreeNode node, int height)>();
+        stack.Push((root, 0));
+        dict.Add(root.val, (null, 0));
+
+        while(stack.Count > 0) 
+        {
+            var (node, height) = stack.Pop();
+            if(node.right != null)
+            {
+                dict.Add(node.right.val, (node.val, height + 1));
+                stack.Push((node.right, height + 1));
+            }
+            if(node.left != null) 
+            {
+                dict.Add(node.left.val, (node.val, height + 1));
+                stack.Push((node.left, height + 1));
+            }
+            if(dict.TryGetValue(x, out var nodeX) && dict.TryGetValue(y, out var nodeY))
+            {
+                return nodeX.height == nodeY.height && nodeX.parent != nodeY.parent;
+            }
+        }
         return false;
     }
 }
@@ -10,6 +35,9 @@ class Q997_CousinsInBinaryTreeTestData : TestData
 {
     protected override List<object[]> Data =>
     [
+        [new int?[]{1,2}, 1, 2, false],
+        [new int?[]{1,2}, 1, 3, false],
+        [new int?[]{1,null, 2}, 1, 2, false],
         [new int?[]{1,2,3,4}, 4, 3, false],
         [new int?[]{1,2,3,null,4,null,5}, 5, 4, true],
         [new int?[]{1,2,3,null,4}, 2, 3, false],

7de28b88910ec83789d2df55e689eff65b220496
Ferry To <ferrywlto@gmail.com>
2024-05-22 14:58:29 +0100

    test: All test cases of Q999 passed

--- csharp/leetcode/easy/Q999_AvailableCapturesForRook.cs
+++ csharp/leetcode/easy/Q999_AvailableCapturesForRook.cs
@@ -1,15 +1,82 @@
-
 class Q999_AvailableCapturesForRook
 {
-    public int NumRookCaptures(char[][] board) 
+    // TC: O(n), n is the total cells in board
+    // SC: O(1), no extra data structure used
+    public int NumRookCaptures(char[][] board)
     {
-        return 0;   
+        // first of all find the 'R'
+        (var rRow, var rCol) = FindR(board);
+        var capture = 0;
+        if (CanCapturePawnEast(rRow, rCol, board)) capture++;
+        if (CanCapturePawnWest(rRow, rCol, board)) capture++;
+        if (CanCapturePawnNorth(rRow, rCol, board)) capture++;
+        if (CanCapturePawnSouth(rRow, rCol, board)) capture++;
+        return capture;
+    }
+    public bool CanCapturePawnEast(int row, int col, char[][] board)
+    {
+        ++col;
+        while (col < board.Length)
+        {
+            if (board[row][col] == 'p') return true;
+            else if (board[row][col] == 'B') return false;
+            col++;
+        }
+        return false;
+    }
+    public bool CanCapturePawnWest(int row, int col, char[][] board)
+    {
+        --col;
+        while (col >= 0)
+        {
+            if (board[row][col] == 'p') return true;
+            else if (board[row][col] == 'B') return false;
+            col--;
+        }
+        return false;
+    }
+    public bool CanCapturePawnNorth(int row, int col, char[][] board)
+    {
+        ++row;
+        while (row < board.Length)
+        {
+            if (board[row][col] == 'p') return true;
+            else if (board[row][col] == 'B') return false;
+            row++;
+        }
+        return false;
+    }
+    public bool CanCapturePawnSouth(int row, int col, char[][] board)
+    {
+        --row;
+        while (row >= 0)
+        {
+            if (board[row][col] == 'p') return true;
+            else if (board[row][col] == 'B') return false;
+            row--;
+        }
+        return false;
+    }
+    public (int row, int col) FindR(char[][] board)
+    {
+        for (var i = 0; i < board.Length; i++)
+        {
+            // always 8x8
+            for (var j = 0; j < board.Length; j++)
+            {
+                if (board[i][j] == 'R')
+                {
+                    return (i, j);
+                }
+            }
+        }
+        return (-1, -1);
     }
 }
 
 class Q999_AvailableCapturesForRookTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [
             new char[][]

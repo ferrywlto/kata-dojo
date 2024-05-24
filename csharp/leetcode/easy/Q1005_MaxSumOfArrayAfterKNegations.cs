b2565a6a612aeacc80f02f36382dee771c6a41e8
Ferry To <ferrywlto@gmail.com>
2024-05-24 12:33:44 +0100

    test: All test cases of Q1005 passed

--- csharp/leetcode/easy/Q1005_MaxSumOfArrayAfterKNegations.cs
+++ csharp/leetcode/easy/Q1005_MaxSumOfArrayAfterKNegations.cs
@@ -1,9 +1,40 @@
-
 class Q1005_MaxSumOfArrayAfterNegations
 {
+    // TC: O(n+2n log n), n is length of nums
+    // SC: O(1), the operations are done in-place
     public int LargestSumAfterKNegations(int[] nums, int k) 
     {
-        return 0;    
+        Array.Sort(nums);
+        var replaced = 0;
+        var idx = 0;
+        var min = int.MaxValue;
+        while(idx < nums.Length && replaced < k)
+        {
+            if(nums[idx] < 0) 
+            {
+                nums[idx] = -nums[idx];
+                replaced++;
+            }
+            if(nums[idx] < min)
+            {
+                min = nums[idx];
+            }
+            idx++;
+        }
+        // Since the same number can negate multiple times, up to this point all negative numbers become positive or all negate quota used.
+        // If there are still negate quota and it is even, do nothing to maintain the maximium sum.
+        var remaining = k - replaced;
+        if (remaining %2==0) {
+            return nums.Sum(); 
+        }
+
+        // If quota is odd, since it can apply multiple times to a number, 
+        // negate a positive odd times will always negative, thus only to negate the smallest one
+
+        // The remaining numbers are all positive, minus the min neturalize it, 
+        // minus the min again make it equalvalent to make the num negative
+        // that's why we have 2*min here
+        return nums.Sum() - (2*min);
     }
 }
 

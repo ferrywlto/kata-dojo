7ca15920a76e35596d65e2682b3c7bfd23b460f5
Ferry To <ferrywlto@gmail.com>
2024-05-25 20:36:14 +0100

    feat: Add -l switch for lines

--- csharp/challenges/wc-cli/Program.cs
+++ csharp/challenges/wc-cli/Program.cs
@@ -1,15 +1,30 @@
-﻿// ./cli a.txt
-// ./cli a.txt b.txt
-// ./cli -c a.txt
-if(args.Length == 2 && args[0] == "-c")
+﻿// ./cli -c a.txt
+if (args.Length != 2) 
 {
-    var target = args[1];
-    if (!File.Exists(target))
-    {
-        Console.WriteLine("The specificed file does not exist");
-        return;
-    }
+    Console.WriteLine("""
+        Usage: ./wc-cli [switch] [file]
+        List of available switch:
+        -c | list bytes of file
+        -l | list of lines of file
+        """);
+    return;
+}
 
+var target = args[1];
+if (!File.Exists(target))
+{
+    Console.WriteLine("The specificed file does not exist");
+    return;
+}
+
+if(args[0] == "-c")
+{
     var bytes = await File.ReadAllBytesAsync(target);
     Console.WriteLine($"{bytes.Length} {target}");
+    return;
+}
+else if(args[0] == "-l")
+{
+    var lines = await File.ReadAllLinesAsync(target);
+    Console.WriteLine($"{lines.Length} {target}");
 }

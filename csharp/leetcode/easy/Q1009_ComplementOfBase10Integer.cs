3000a9ee0f56aa8aeb034624eefeb8e318b4f49b
Ferry To <ferrywlto@gmail.com>
2024-05-25 18:14:08 +0100

    test: All test cases of Q1009 passed

--- csharp/leetcode/easy/Q1009_ComplementOfBase10Integer.cs
+++ csharp/leetcode/easy/Q1009_ComplementOfBase10Integer.cs
@@ -2,7 +2,11 @@
 class Q1009_ComplementOfBase10Integer
 {
     public int BitwiseComplement(int n) {
-        return 0;        
+        // 0 is special case which has no significant bit
+        if (n == 0) return 1;
+        var bitsOfNum = (int)Math.Log(n, 2) + 1;
+        var bitMask = (int)Math.Pow(2, bitsOfNum) - 1;
+        return n ^ bitMask;     
     }    
 }
 
@@ -10,6 +14,7 @@ class Q1009_ComplementOfBase10IntegerTestData : TestData
 {
     protected override List<object[]> Data =>
     [
+        [0, 1],
         [5, 2],
         [7, 0],
         [10, 5],

db95d430149c88a2fd726942ad487eba97d7f1b0
Ferry To <ferrywlto@gmail.com>
2024-05-25 20:30:20 +0100

    feat: Add -c switch

---
+++ csharp/challenges/wc-cli/Program.cs
@@ -0,0 +1,15 @@
+﻿// ./cli a.txt
+// ./cli a.txt b.txt
+// ./cli -c a.txt
+if(args.Length == 2 && args[0] == "-c")
+{
+    var target = args[1];
+    if (!File.Exists(target))
+    {
+        Console.WriteLine("The specificed file does not exist");
+        return;
+    }
+
+    var bytes = await File.ReadAllBytesAsync(target);
+    Console.WriteLine($"{bytes.Length} {target}");
+}

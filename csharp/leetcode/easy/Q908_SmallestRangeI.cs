2586cd84a127fb8ce113c96e04d5a76e46f5dcc5
Ferry To <ferrywlto@gmail.com>
2024-05-27 15:33:48 +0100

    pref: Add O(n) version for reference

--- csharp/leetcode/easy/Q908_SmallestRangeI.cs
+++ csharp/leetcode/easy/Q908_SmallestRangeI.cs
@@ -1,6 +1,8 @@
 
 class Q908_SmallestRangeI
 {
+    // TC: O(2n) -> O(n), n is length of nums
+    // SC: O(1), no space used
     public int SmallestRangeI(int[] nums, int k)
     {
         if (nums.Length <= 1) return 0;
@@ -22,6 +24,24 @@ class Q908_SmallestRangeI
 
         if (max < min) return 0;
         return max - min;
+    }
+
+    // TC: O(n) version
+    // doesn't need to calculate the average
+    // the smallest range can get by max - min (original range) - 2k (down k from max and up k from min)
+    // thus doesn't need average
+    public int SmallestRangeI_SinglePass(int[] nums, int k)
+    {
+        int min = int.MaxValue;
+        int max = int.MinValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            min = Math.Min(min, nums[i]);
+            max = Math.Max(max, nums[i]);
+        }
+
+        return Math.Max(0, max - min - 2 * k);
     }
 }
 
@@ -46,7 +66,7 @@ public class Q908_SmallestRangeITests
     public void OfficialTestCases(int[] input, int k, int expected)
     {
         var sut = new Q908_SmallestRangeI();
-        var actual = sut.SmallestRangeI(input, k);
+        var actual = sut.SmallestRangeI_SinglePass(input, k);
         Assert.Equal(expected, actual);
     }
 }

2af32980592fd209a8de72afd7172915056a3fd4
Ferry To <ferrywlto@gmail.com>
2024-05-27 15:28:43 +0100

    test: All test cases of Q908 passed

--- csharp/leetcode/easy/Q908_SmallestRangeI.cs
+++ csharp/leetcode/easy/Q908_SmallestRangeI.cs
@@ -3,7 +3,25 @@ class Q908_SmallestRangeI
 {
     public int SmallestRangeI(int[] nums, int k)
     {
-        return 0;
+        if (nums.Length <= 1) return 0;
+        
+        var avg = nums.Average();
+        var max = int.MinValue;
+        var min = int.MaxValue;
+        for(var i=0; i<nums.Length; i++)
+        {
+            if(nums[i] > avg) 
+            {
+                max = Math.Max(max, nums[i] - k);
+            }
+            else if(nums[i] < avg)
+            {
+                min = Math.Min(min, nums[i] + k);
+            }
+        }
+
+        if (max < min) return 0;
+        return max - min;
     }
 }
 
@@ -12,8 +30,12 @@ class Q908_SmallestRangeITestData : TestData
     protected override List<object[]> Data => 
     [
         [new int[]{0}, 0, 0],
+        [new int[]{1,1,1}, 0, 0],
+        [new int[]{1,1,1}, 3, 0],
         [new int[]{0,10}, 2, 6],
         [new int[]{1,3,6}, 3, 0],
+        [new int[]{3,1,10}, 4, 1],
+        [new int[]{9,9,2,8,7}, 4, 0],
     ];
 }
 

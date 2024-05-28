5fb81d3632183540f80a657ca7f212c957576496
Ferry To <ferrywlto@gmail.com>
2024-05-28 12:44:50 +0100

    test: All test cases of Q859 passed

--- csharp/leetcode/easy/Q859_BuddyStrings.cs
+++ csharp/leetcode/easy/Q859_BuddyStrings.cs
@@ -1,18 +1,51 @@
 
 class Q859_BuddyStrings
 {
-    public bool BuddyStrings(string s, string goal) {
+    // TC: O(n), n is length of s
+    // SC: O(n), n is unique characters in s
+    public bool BuddyStrings(string s, string goal)
+    {
+        if (s.Length != goal.Length) return false;
+        if (s.Length < 2) return false;
+        var diffCount = 0;
+        char[,] diff = new char[2, 2];
+        var set = new HashSet<char>();
+        var anyMoreThanOnce = false;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] != goal[i])
+            {
+                if (diffCount >= 2) return false;
+                diff[diffCount, 0] = s[i];
+                diff[diffCount, 1] = goal[i];
+                diffCount++;
+            }
+            if (!set.Add(s[i])) anyMoreThanOnce = true;
+        }
+        if (diffCount == 2)
+        {
+            if (diff[0, 0] == diff[1, 1] && diff[0, 1] == diff[1, 0]) return true;
+        }
+        if (diffCount == 0 && s.Length == 2 && s[0] != s[1]) return false;
+        if (diffCount == 0)
+        {
+            return anyMoreThanOnce;
+        }
         return false;
     }
 }
 
 class Q859_BuddyStringsTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["ab", "ba", true],
         ["ab", "ab", false],
         ["aa", "aa", true],
+        ["abcaa", "abcbb", false],
+        ["abcd", "abcd", false],
+        ["abab", "abab", true],
+        ["aaab", "aaab", true],
     ];
 }
 

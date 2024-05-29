c0e48b68c92762b3991a3d074da5892ad6a6cb4a
Ferry To <ferrywlto@gmail.com>
2024-05-29 22:28:18 +0100

    test: All test cases of Q1018 passed

--- csharp/leetcode/easy/Q1018_BinaryPrefixDivisibleBy5.cs
+++ csharp/leetcode/easy/Q1018_BinaryPrefixDivisibleBy5.cs
@@ -1,8 +1,21 @@
-
 class Q1018_BinaryPrefixDivisibleBy5
 {
-        public IList<bool> PrefixesDivBy5(int[] nums) {
-        return Enumerable.Repeat(false, nums.Length).ToList();
+    // TC: O(n), n is length of nums
+    // SC: O(1), no data structure used
+    public IList<bool> PrefixesDivBy5(int[] nums)
+    {
+        long currentSum = 0;
+        var result = new List<bool>();
+
+        for(var i=0; i<nums.Length; i++)
+        {
+            // distributive property of modular arithmetic
+            // (a + b) % n = ((a % n) + (b % n)) % n
+            // thus can reuse the remainder from the last calculation to prevent integer overflow
+            currentSum = (currentSum * 2 + nums[i]) % 5;
+            result.Add(currentSum == 0);
+        }
+        return result;
     }
 }
 class Q1018_BinaryPrefixDivisibleBy5TestData : TestData
@@ -11,6 +24,14 @@ class Q1018_BinaryPrefixDivisibleBy5TestData : TestData
     [
         [new int[] {0,1,1}, new bool[] {true,false,false}],
         [new int[] {1,1,1}, new bool[] {false,false,false}],
+        [
+            new int[] {1,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,0,0,0,1,1,0,1,0,0,0,1}, 
+            new bool[] {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,true,true,true,true,false}
+        ],
+        [
+            new int[] {1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,1,0,0,1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,1,0},
+            new bool[] {false,false,true,false,false,false,false,false,false,false,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,true,false,false,true,false,false,true,true,true,true,true,true,true,false,false,true,false,false,false,false,true,true}
+        ],
     ];
 }
 public class Q1018_BinaryPrefixDivisibleBy5Tests

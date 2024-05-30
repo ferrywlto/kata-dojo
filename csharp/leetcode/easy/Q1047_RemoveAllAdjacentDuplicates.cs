4d35a00d12ff6eb3361a7919cdbb2d19eff3b1a1
Ferry To <ferrywlto@gmail.com>
2024-05-30 21:45:19 +0100

    test: All test cases of Q1047 passed

--- csharp/leetcode/easy/Q1047_RemoveAllAdjacentDuplicates.cs
+++ csharp/leetcode/easy/Q1047_RemoveAllAdjacentDuplicates.cs
@@ -1,8 +1,25 @@
 class Q1047_RemoveAllAdjacentDuplicates
 {
+    // TC: O(n), n is length of s
+    // SC: O(n), n is length of s in worst cases
     public string RemoveDuplicates(string s)
     {
-        return string.Empty;
+        if (s.Length <= 1) return s;
+        var stack = new Stack<char>();
+        stack.Push(s[0]);
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (stack.Count > 0 && s[i] == stack.Peek())
+            {
+                while (stack.Count > 0 && s[i] == stack.Peek())
+                {
+                    stack.Pop();
+                }
+            }
+            else stack.Push(s[i]);
+        }
+
+        return string.Join(string.Empty,stack.Reverse());
     }
 }
 class Q1047_RemoveAllAdjacentDuplicatesTestData : TestData

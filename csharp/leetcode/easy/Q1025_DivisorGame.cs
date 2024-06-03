055593e80d45bf2660ee84f6a779713b758216f6
Ferry To <ferrywlto@gmail.com>
2024-06-03 15:15:27 +0100

    test: All test cases of Q1025 passed

--- csharp/leetcode/easy/Q1025_DivisorGame.cs
+++ csharp/leetcode/easy/Q1025_DivisorGame.cs
@@ -1,9 +1,15 @@
 
 class Q1025_DivisorGame
 {
+    // TC: O(1)
+    // SC: O(1)
     public bool DivisorGame(int n) 
     {
-        return false;
+        // who start with odd always lose, not matter what.
+        // while whoever start with even always win, 
+        // because at their turn they can simply make the number odd and pass back to opponent. 
+        // Which will eventually force opponent pass 2 back and win
+        return n % 2 == 0;
     }
 }
 class Q1025_DivisorGameTestData : TestData
@@ -12,6 +18,17 @@ class Q1025_DivisorGameTestData : TestData
     [
         [2, true],
         [3, false],
+        [4, true],
+        [5, false],
+        [6, true],
+        [7, false],
+        [8, true],
+        [9, false],
+        [10, true],
+        [11, false],
+        [12, true],
+        [15, false],
+        [int.MaxValue, false],
     ];
 }
 public class Q1025_DivisorGameTests

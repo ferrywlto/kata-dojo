29a5c5d2c3f3e00e2062fdebe3183036e3e66c61
Ferry To <ferrywlto@gmail.com>
2024-06-04 15:20:45 +0100

    test: All test cases of Q1051 passed

--- csharp/leetcode/easy/Q1051_HeightChecker.cs
+++ csharp/leetcode/easy/Q1051_HeightChecker.cs
@@ -1,14 +1,24 @@
-
 class Q1051_HeightCHecker
 {
-    public int HeightChecker(int[] heights) 
+    // TC:O(n log n), n is length of heights, n log n caused by Array.Sort()
+    // SC:O(n), as need ot copy heights
+    public int HeightChecker(int[] heights)
     {
-        return 0;    
-    }    
+        int[] clone = new int[heights.Length];
+        Array.Copy(heights, clone, heights.Length);
+        Array.Sort(clone);
+
+        var count = 0;
+        for (int i = 0; i < heights.Length; i++)
+        {
+            if (heights[i] != clone[i]) count++;
+        }
+        return count;
+    }
 }
 class Q1051_HeightCHeckerTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[]{1,1,4,2,1,3}, 3],
         [new int[]{5,1,2,3,4}, 5],

2a128e4454f58f8e3daf6aa130d1b5cd4468bcf6
Ferry To <ferrywlto@gmail.com>
2024-06-05 22:56:31 +0100

    test: All test cases of Q1078 passed

--- csharp/leetcode/easy/Q1078_OccurrencesAfterBigram.cs
+++ csharp/leetcode/easy/Q1078_OccurrencesAfterBigram.cs
@@ -1,13 +1,25 @@
-
 class Q1078_OccurancesAfterBigram
 {
-        public string[] FindOcurrences(string text, string first, string second) {
-        return [];
+    // TC: O(n), n is words in text
+    // SC: O(n), n is words in text from split 
+    public string[] FindOcurrences(string text, string first, string second)
+    {
+        var words = text.Split(" ");
+        if (words.Length < 3) return [];
+        var result = new List<string>();
+        for(var i=0; i<words.Length-2; i++)
+        {
+            if(words[i] == first && words[i+1] == second)
+            {
+                result.Add(words[i + 2]);
+            }
+        }
+        return result.ToArray();
     }
 }
 class Q1078_OccurancesAfterBigramTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["alice is a good girl she is a good student", "a", "good", new string[] {"girl", "student"}],
         ["we will we will rock you", "we", "will", new string[] {"we", "rock"}],

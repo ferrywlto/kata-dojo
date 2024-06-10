908bf14c7dbae31966058453e7b98f7bd25cff6f
Ferry To <ferrywlto@gmail.com>
2024-06-10 11:49:07 +0100

    test: All test cases of Q1128 passed

--- csharp/leetcode/easy/Q1128_NumberOfEquivalentDominoPairs.cs
+++ csharp/leetcode/easy/Q1128_NumberOfEquivalentDominoPairs.cs
@@ -1,8 +1,22 @@
 class Q1128_NumberOfEquivalentDominoPairs
 {
+    // TC: O(n^2), (n*(n+1))/2 where n is number of dominoes
+    // SC: O(1)
     public int NumEquivDominoPairs(int[][] dominoes)
     {
-        return 0;
+        var pairs = 0;
+        for(var i=0; i<dominoes.Length-1; i++)
+        {
+            var p = dominoes[i];
+            for (var j = i + 1; j < dominoes.Length; j++)
+            {
+                if (
+                    (dominoes[j][0] == p[0] && dominoes[j][1] == p[1]) ||
+                    (dominoes[j][0] == p[1] && dominoes[j][1] == p[0])
+                ) pairs++;
+            }
+        }
+        return pairs;
     }
 }
 class Q1128_NumberOfEquivalentDominoPairsTestData : TestData

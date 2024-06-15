4bc8523427f1cc043d593c5f0caf4193881e006f
Ferry To <ferrywlto@gmail.com>
2024-06-15 14:26:56 +0100

    test: All test cases of Q1189 passed

--- csharp/leetcode/easy/Q1189_MaximumNumberOfBalloons.cs
+++ csharp/leetcode/easy/Q1189_MaximumNumberOfBalloons.cs
@@ -1,17 +1,58 @@
-
 class Q1189_MaximumNUmberOfBalloons
 {
-        public int MaxNumberOfBalloons(string text) {
-        return 0;   
+    private const string balloon = "balloon";
+    // TC: O(n), n is length of text
+    // SC: O(n), n is length of text
+    public int MaxNumberOfBalloons(string text)
+    {
+        var charsCount = text
+            .GroupBy(c => c)
+            .ToDictionary(grp => grp.Key, grp => grp.Count());
+
+        var balloonCount = 0;
+        while(true)
+        {
+            for(var i=0; i<balloon.Length; i++)
+            {
+                if (!charsCount.TryGetValue(balloon[i], out var count) || count == 0) return balloonCount;
+                charsCount[balloon[i]] = --count;
+            }
+            balloonCount++;
+        }
     }
+    // TC: O(n), n is length of text
+    // SC: O(1) the size of dict is fixed
+    public int MaxNumberOfBalloons_Faster(string text)
+    {
+        var dict = new Dictionary<char, int>() {
+            {'b', 0},
+            {'a', 0},
+            {'l', 0},
+            {'o', 0},
+            {'n', 0},
+        };
+        for(var i=0; i<text.Length; i++)
+        {
+            if (dict.ContainsKey(text[i]))
+            {
+                dict[text[i]]++;
+            }
+        }
+        dict['l'] /= 2;
+        dict['o'] /= 2;
+        return dict.Min(x => x.Value);
+    }    
 }
 class Q1189_MaximumNUmberOfBalloonsTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["nlaebolko", 1],
         ["loonbalxballpoon", 2],
         ["leetcode", 0],
+        ["balon", 0],
+        ["balonbalon", 1],
+        ["balloonballoonballon", 2],
     ];
 }
 public class Q1189_MaximumNUmberOfBalloonsTests
@@ -21,7 +62,7 @@ public class Q1189_MaximumNUmberOfBalloonsTests
     public void OfficialTestCases(string input, int expected)
     {
         var sut = new Q1189_MaximumNUmberOfBalloons();
-        var actual = sut.MaxNumberOfBalloons(input);
+        var actual = sut.MaxNumberOfBalloons_Faster(input);
         Assert.Equal(expected, actual);
     }
 }

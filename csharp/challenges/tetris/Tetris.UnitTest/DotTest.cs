f690e5a62b5ae9e521af2717e1ee351900f1326a
Ferry To <ferrywlto@gmail.com>
2024-06-16 23:33:16 +0100

    test: Add Dot tests

--- csharp/challenges/tetris/Tetris.UnitTest/DotTest.cs
+++ csharp/challenges/tetris/Tetris.UnitTest/DotTest.cs
@@ -11,4 +11,26 @@ public class DotTest
         var actual = sut.CanRotateRight(It.IsAny<int[,]>(), It.IsAny<int>(), It.IsAny<int>());
         Assert.False(actual);
     }
+
+    [Fact]
+    public void CanRotateLeft_MustReturn_False()
+    {
+        var sut = new Dot();
+        var actual = sut.CanRotateLeft(It.IsAny<int[,]>(), It.IsAny<int>(), It.IsAny<int>());
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void RotateLeft_MustThrow_NotImplementedException()
+    {
+        var sut = new Dot();
+        Assert.Throws<NotImplementedException>(() => sut.RotateLeft(It.IsAny<int[,]>(), It.IsAny<int>(), It.IsAny<int>()));
+    }
+
+    [Fact]
+    public void RotateRight_MustThrow_NotImplementedException()
+    {
+        var sut = new Dot();
+        Assert.Throws<NotImplementedException>(() => sut.RotateRight(It.IsAny<int[,]>(), It.IsAny<int>(), It.IsAny<int>()));
+    }
 }

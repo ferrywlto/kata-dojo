b3b5785ba183c19080a86f4642885220c3970f1c
Ferry To <ferrywlto@gmail.com>
2024-06-17 21:50:50 +0100

    test: All test cases of Q1207 passed

--- csharp/leetcode/easy/Q1207_UniqueNumberOfOccurances.cs
+++ csharp/leetcode/easy/Q1207_UniqueNumberOfOccurances.cs
@@ -1,8 +1,15 @@
 class Q1207_UniqueNumberOfOccurances
 {
+    // TC: O(n), n is length of arr
+    // SC: O(n), n is distinct element in arr
     public bool UniqueOccurrences(int[] arr)
     {
-        return false;
+        var dict = arr
+            .GroupBy(n => n)
+            .ToDictionary(grp => grp.Key, grp => grp.Count());
+        var occurances = dict.Select(x => x.Value).ToList();
+        return occurances.Count
+            == occurances.ToHashSet().Count;
     }
 }
 class Q1207_UniqueNumberOfOccurancesTestData : TestData
@@ -11,7 +18,7 @@ class Q1207_UniqueNumberOfOccurancesTestData : TestData
     [
         [new int[]{1,2,2,1,1,3}, true],
         [new int[]{1,2}, false],
-        [new int[]{-3,0,1,-3,1,1,1,-3,10,0}, false],
+        [new int[]{-3,0,1,-3,1,1,1,-3,10,0}, true],
     ];
 }
 public class Q1207_UniqueNumberOfOccurancesTests

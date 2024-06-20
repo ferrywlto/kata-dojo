3ce7e6aa14247c19184fca548c96b6b6e2314ad0
Ferry To <ferrywlto@gmail.com>
2024-06-20 12:48:41 +0100

    test: All test cases of Q1260 passed

--- csharp/leetcode/easy/Q1260_Shift2DGrid.cs
+++ csharp/leetcode/easy/Q1260_Shift2DGrid.cs
@@ -1,8 +1,28 @@
 class Q1260_Shift2DGrid
 {
+    // TC: O(n), n is total elements in grid * k times
+    // SC: O(n) if count the conversion needed for the result type, otherwise O(1)
     public IList<IList<int>> ShiftGrid(int[][] grid, int k)
     {
-        return [];
+        int lastRowEnd = int.MinValue;
+        var count = 0;
+        while (count < k)
+        {
+            for (var i = 0; i < grid.Length; i++)
+            {
+                var currentRowLength = grid[i].Length - 1;
+                var currentRowEnd = grid[i][currentRowLength];
+                for (var j = currentRowLength; j > 0; j--)
+                {
+                    grid[i][j] = grid[i][j - 1];
+                }
+                grid[i][0] = lastRowEnd;
+                lastRowEnd = currentRowEnd;
+            }
+            grid[0][0] = lastRowEnd;
+            count++;
+        }
+        return grid.Select(x => (IList<int>)[.. x]).ToList();
     }
 }
 class Q1260_Shift2DGridTestData : TestData
@@ -59,7 +79,7 @@ public class Q1260_Shift2DGridTests
 {
     [Theory]
     [ClassData(typeof(Q1260_Shift2DGridTestData))]
-    public void OfficialTestCases(int[][] input, int times, IList<IList<int>> expected)
+    public void OfficialTestCases(int[][] input, int times, List<List<int>> expected)
     {
         var sut = new Q1260_Shift2DGrid();
         var actual = sut.ShiftGrid(input, times);

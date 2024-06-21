98eeffe58e5df5bd1f69868d9996384fc0dbeaf4
Ferry To <ferrywlto@gmail.com>
2024-06-21 23:37:56 +0100

    test: All test cases of Q1266 passed

--- csharp/leetcode/easy/Q1266_MinimumTimeVisitingAllPoints.cs
+++ csharp/leetcode/easy/Q1266_MinimumTimeVisitingAllPoints.cs
@@ -1,13 +1,26 @@
-
 class Q1266_MinimumTimeVisitingAllPoints
 {
-        public int MinTimeToVisitAllPoints(int[][] points) {
-        return 0;   
+    // TC: O(n), n is size of points
+    // SC: O(1), no space used in operation
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        if (points.Length == 1) return 0;
+        var result = 0;
+        for(var i=0; i<points.Length-1; i++)
+        {
+            var pointA = points[i];
+            var pointB = points[i + 1];
+            var xDiff = Math.Abs(pointA[0] - pointB[0]);
+            var yDiff = Math.Abs(pointA[1] - pointB[1]);
+            var dDiff = Math.Min(xDiff, yDiff);
+            result += dDiff + Math.Abs(xDiff - yDiff);
+        }
+        return result;
     }
 }
 class Q1266_MinimumTimeVisitingAllPointsTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[][] {[1,1],[3,4],[-1,0]}, 7],
         [new int[][] {[3,2],[-2,2]}, 5],

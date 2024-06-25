6e3610b4b1b25dfd7705260e92c25486b1753b4d
Ferry To <ferrywlto@gmail.com>
2024-06-25 13:41:44 +0100

    test: All test cases of Q1295 passed

--- csharp/leetcode/easy/Q1295_FindNumbersWithEvenNumberOfDigits.cs
+++ csharp/leetcode/easy/Q1295_FindNumbersWithEvenNumberOfDigits.cs
@@ -1,14 +1,30 @@
 class Q1295_FindNumbersWithEvenNumberOfDigits
 {
+    // TC: O(n), n is length of nums * how large is each number
+    // SC: O(1), no space used for operations
     public int FindNumbers(int[] nums)
     {
-        return 0;
+        var result = 0;
+        foreach(var n in nums)
+        {
+            if (n == 0) continue;
+            var temp = n;
+            var digits = 0;
+            while(temp > 0)
+            {
+                temp /= 10;
+                digits++;
+            }
+            if (digits % 2 == 0) result++;
+        }
+        return result;
     }
 }
 class Q1295_FindNumbersWithEvenNumberOfDigitsTestData : TestData
 {
     protected override List<object[]> Data =>
     [
+        [new int[] {0,345,2,7896}, 1],
         [new int[] {12,345,2,6,7896}, 2],
         [new int[] {555,901,482,1771}, 1],
     ];

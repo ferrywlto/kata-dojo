d7bc0a0af8a00b8e094677c2cb6cfe1994e15039
Ferry To <ferrywlto@gmail.com>
2024-06-26 11:41:00 +0100

    test: All test cases of Q1299 passed

--- csharp/leetcode/easy/Q1299_ReplaceElementsWithGrestestElementOnRightSide.cs
+++ csharp/leetcode/easy/Q1299_ReplaceElementsWithGrestestElementOnRightSide.cs
@@ -1,8 +1,18 @@
 class Q1299_ReplaceElementsWithGreatestElementOnRightSide
 {
+    // TC: O(n), n is length of arr
+    // SC: O(1), no space used in operations
     public int[] ReplaceElements(int[] arr)
     {
-        return [];
+        var currentMax = arr[^1];
+        for(var i = arr.Length - 2; i>=0; i--)
+        {
+            var temp = arr[i];
+            arr[i] = currentMax;
+            currentMax = Math.Max(temp, currentMax);            
+        }
+        arr[^1] = -1;
+        return arr;
     }
 }
 class Q1299_ReplaceElementsWithGreatestElementOnRightSideTestData : TestData

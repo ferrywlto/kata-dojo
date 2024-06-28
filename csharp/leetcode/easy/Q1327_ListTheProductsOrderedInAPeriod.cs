cd3174c2b944d3cc57a516927c96ce0fcc08ee0b
Ferry To <ferrywlto@gmail.com>
2024-06-28 21:41:13 +0100

    test: All test cases of Q1327 passed

--- csharp/leetcode/easy/Q1327_ListTheProductsOrderedInAPeriod.cs
+++ csharp/leetcode/easy/Q1327_ListTheProductsOrderedInAPeriod.cs
@@ -3,7 +3,12 @@ class Q1327_ListTheProductsOrderedInAPeriod : SqlQuestion
 {
     public override string Query =>
     """
-    select 1;
+    select product_name, sum(unit) as unit from Orders o 
+    left join Products p
+    on o.product_id = p.product_id
+    where order_date between '2020-02-01' and '2020-02-29'
+    group by o.product_id
+    having sum(unit) >= 100;
     """;
 }
 class Q1327_ListTheProductsOrderedInAPeriodTestData : TestData

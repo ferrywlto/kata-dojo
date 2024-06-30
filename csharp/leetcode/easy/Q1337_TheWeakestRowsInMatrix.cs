37074c5382bf8cd74a7a3236c7b8c42693cbb3d5
Ferry To <ferrywlto@gmail.com>
2024-06-30 12:07:01 +0100

    pref: Add O(log n) version using binary search in to improve time for reference

--- csharp/leetcode/easy/Q1337_TheWeakestRowsInMatrix.cs
+++ csharp/leetcode/easy/Q1337_TheWeakestRowsInMatrix.cs
@@ -1,19 +1,26 @@
 class Q1337_TheWeakestRowsInMatrix
 {
     // TC: O(n), scale on number of 1s in matrix + k
-    // SC: O(n), scale on number of distinct soliders, worse case each row have distinct number of soliders thus n entries in dictionary + k space for result holding list  
+    // SC: O(m+k), m is number of row, worse case each row have distinct number of soliders + k space for result holding list
     public int[] KWeakestRows(int[][] mat, int k)
     {
         var dict = new SortedDictionary<int, List<int>>();
         for(var i=0; i<mat.Length; i++)
         {
             var row = mat[i];
-            var soliders = 0;
-            for(var j=0; j<row.Length; j++)
+            // use binary search
+            var start = 0;
+            var end = row.Length - 1;
+            while (start < end)
             {
-                if (row[j] == 0) break;
-                soliders++;
+                var middle = start + (end - start) / 2;
+                if (row[middle] == 1) start = middle + 1;
+                else end = middle;
             }
+            // After the loop, 'start' should point to the first '0' or be equal to row.Length if all are '1's.
+            // To find the number of soldiers (1's), we check if the element at 'start' is a '1' to handle the case where all elements are '1's.
+            int soliders = (start == row.Length || row[start] == 0) ? start : start + 1;
+
             if(dict.TryGetValue(soliders, out var list)) list.Add(i);
             else dict.Add(soliders, [i]);
         }
@@ -55,6 +62,13 @@ class Q1337_TheWeakestRowsInMatrixTestData : TestData
                 [1,0,0,0],
             }, 2,
             new int[] {0,2}
+        ],
+        [
+            new int[][]
+            {
+                [1,0],[0,0],[1,0]
+            }, 2,
+            new int[] {1,0}
         ]
     ];
 }

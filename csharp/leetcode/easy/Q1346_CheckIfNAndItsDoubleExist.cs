9f05b98786081894dbe9e5e170810340e2080e18
Ferry To <ferrywlto@gmail.com>
2024-07-01 12:05:21 +0100

    test: All test cases of Q1346 passed

--- csharp/leetcode/easy/Q1346_CheckIfNAndItsDoubleExist.cs
+++ csharp/leetcode/easy/Q1346_CheckIfNAndItsDoubleExist.cs
@@ -1,7 +1,15 @@
 class Q1346_CheckIfNAndItsDoubleExist
 {
+    // TC: O(n), n scale with length of arr
+    // SC: O(n), n scale with distinct numbers in arr
     public bool CheckIfExist(int[] arr)
     {
+        var hashset = new HashSet<int>();
+        foreach(var num in arr)
+        {
+            if (hashset.Contains(num * 2) || (num % 2 == 0 && hashset.Contains(num / 2))) return true;
+            hashset.Add(num);
+        }
         return false;
     }
 }
@@ -10,7 +18,8 @@ class Q1346_CheckIfNAndItsDoubleExistTestData : TestData
     protected override List<object[]> Data =>
     [
         [new int[] {10,2,5,3}, true],
-        [new int[] {3,1,7,11}, true],
+        [new int[] {3,1,7,11}, false],
+        [new int[] {7,1,14,11}, true],
     ];
 }
 public class Q1346_CheckIfNAndItsDoubleExistTests

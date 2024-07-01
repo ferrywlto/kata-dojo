ecc5f61c8595c2503b482de6c917973b9b96af00
Ferry To <ferrywlto@gmail.com>
2024-07-01 12:28:27 +0100

    test: All test cases of Q1317 passed

--- csharp/leetcode/easy/Q1317_ConvertIntegerToSumOfTwoNonZeroIntegers.cs
+++ csharp/leetcode/easy/Q1317_ConvertIntegerToSumOfTwoNonZeroIntegers.cs
@@ -1,8 +1,30 @@
 class Q1317_ConvertIntegerToSumOfTwoNonZeroIntegers
 {
+    // TC: O(n), it have to iterate at least half the elements in worst case to check if all numbers contains no zero
+    // SC: O(1), no space used for calculation
     public int[] GetNoZeroIntegers(int n)
     {
+        if (n == 2) return [1, 1];
+        var times = n / 2;
+        if (n % 2 != 0) times++;
+        // try brute force method
+        for(var i=1; i<times; i++)
+        {
+            var compement = n - i;
+            if (HasZeroInDigits(compement) || HasZeroInDigits(i)) continue;
+            else return [i, compement];
+        }
+
         return [];
+    }
+    public bool HasZeroInDigits(int input)
+    {
+        while(input > 0)
+        {
+            if (input % 10 == 0) return true;
+            input /= 10;
+        }
+        return false;
     }
 }
 class Q1317_ConvertIntegerToSumOfTwoNonZeroIntegersTestData : TestData
@@ -10,6 +32,7 @@ class Q1317_ConvertIntegerToSumOfTwoNonZeroIntegersTestData : TestData
     protected override List<object[]> Data =>
     [
         [2, new int[2]{1,1}],
+        [3, new int[2]{1,2}],
         [11, new int[2]{2,9}],
     ];
 }

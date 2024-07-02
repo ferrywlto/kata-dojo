c2dbbfe196a6392bde6c9381245782366b94255c
Ferry To <ferrywlto@gmail.com>
2024-07-02 12:21:09 +0100

    chore: Move angular folder under javascript

---
+++ javascript/angular/todo/src/app/todoItem.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, output } from '@angular/core';
+import { TodoState } from './todoList.component';
+
+@Component({
+    selector: 'todo-item',
+    standalone: true,
+    template: `
+    <li class="list-group-item">
+        <input class="form-check-input me-1" type="checkbox" [value]="todoState.done" (change)="updateParent($event)">
+        Item: {{todoState.title}}&nbsp;
+    </li>`,
+    styles: `.todo-item {
+        color: red;
+    }`,
+})
+export class TodoItem {
+    @Input() todoState!: TodoState;
+    onClick = output<TodoState>()
+
+    updateParent(evt: Event) {
+        let state = {...this.todoState}
+        this.onClick.emit(state);
+    }
+}

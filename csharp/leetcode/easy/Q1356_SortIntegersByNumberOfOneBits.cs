3955984da30c8dba07c691d2fdd4365aa80f46e5
Ferry To <ferrywlto@gmail.com>
2024-07-02 12:10:10 +0100

    perf: Add one liner LINQ approach for reference and fix time analysis

--- csharp/leetcode/easy/Q1356_SortIntegersByNumberOfOneBits.cs
+++ csharp/leetcode/easy/Q1356_SortIntegersByNumberOfOneBits.cs
@@ -1,40 +1,41 @@
+using System.Numerics;
+
 class Q1356_SortIntegersByNumberOfOneBits
 {
-    // TC: O(n+m log n), it iterate each element in arr once then sort each bucket
+    // TC: O(n log n), it iterate each element in arr once then sort each bucket
     // SC: O(n+m), n holds all number of bits of each number, another m for buckets  
     public int[] SortByBits(int[] arr)
     {
         var bits = new Dictionary<int, int>();
         var buckets = new SortedDictionary<int, List<int>>();
-        foreach(var num in arr)
+        foreach (var num in arr)
         {
-            if(!bits.TryGetValue(num, out var numOfBits))
+            if (!bits.TryGetValue(num, out var numOfBits))
             {
                 numOfBits = 0;
                 var n = num;
-                while(n > 0)
+                while (n > 0)
                 {
                     if ((n & 1) == 1) numOfBits++;
                     n >>= 1;
                 }
                 bits.Add(num, numOfBits);
             }
-            if(buckets.TryGetValue(numOfBits, out var list))
-            {
-                list.Add(num);
-            }
-            else
-            {
-                buckets.Add(numOfBits, [num]);
-            }
+            if (buckets.TryGetValue(numOfBits, out var list)) list.Add(num);
+            else buckets.Add(numOfBits, [num]);
         }
         var result = new List<int>();
-        foreach(var p in buckets)
+        foreach (var p in buckets)
         {
-            p.Value.Sort();
-            result.AddRange(p.Value);
+            result.AddRange(p.Value.OrderBy(x => x));
         }
         return result.ToArray();
+    }
+    public int[] SortByBits_OneLiner(int[] arr)
+    {
+        return arr.OrderBy(num => BitOperations.PopCount((uint)num))
+            .ThenBy(num => num)
+            .ToArray();
     }
 }
 class Q1356_SortIntegersByNumberOfOneBitsTestData : TestData

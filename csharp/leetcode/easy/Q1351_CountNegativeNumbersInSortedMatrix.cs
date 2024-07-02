6d14a0e7c4e15026c79734b8d97f4f685caef39f
Ferry To <ferrywlto@gmail.com>
2024-07-02 10:37:38 +0100

    test: All test cases of Q1351 passed

--- csharp/leetcode/easy/Q1351_CountNegativeNumbersInSortedMatrix.cs
+++ csharp/leetcode/easy/Q1351_CountNegativeNumbersInSortedMatrix.cs
@@ -1,8 +1,19 @@
 class Q1351_CountNegativeNumbersInSortedMatrix
 {
+    // TC: O(n), n = total elements in grid worst case (all negative)
+    // SC: O(1), no space used for operation
     public int CountNegatives(int[][] grid)
     {
-        return 0;
+        var count = 0;
+        for(var i=grid.Length-1; i>=0; i--)
+        {
+            for(var j=grid[i].Length-1; j>=0; j--)
+            {
+                if (grid[i][j] >= 0) break;
+                count++;
+            }
+        }
+        return count;
     }
 }
 class Q1351_CountNegativeNumbersInSortedMatrixTestData : TestData

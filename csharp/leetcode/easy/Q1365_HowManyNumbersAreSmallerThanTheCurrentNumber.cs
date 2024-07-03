bfbb2b9c41e0fd12a7e9d0659c49adf5ca6a47db
Ferry To <ferrywlto@gmail.com>
2024-07-03 17:50:58 +0100

    test: All test cases of Q1365 passed

--- csharp/leetcode/easy/Q1365_HowManyNumbersAreSmallerThanTheCurrentNumber.cs
+++ csharp/leetcode/easy/Q1365_HowManyNumbersAreSmallerThanTheCurrentNumber.cs
@@ -1,8 +1,26 @@
 class Q1365_HowManyNumbersAreSmallerThanTheCurrentNumber
 {
+    // TC: O(n log n), due to Array.Sort()
+    // SC: O(n+m) for the n unique number in dictionary, and m for the copied array
     public int[] SmallerNumbersThanCurrent(int[] nums)
     {
-        return [];
+        var dict = new Dictionary<int, int>();
+        var copy = new int[nums.Length];
+        Array.Copy(nums, copy, nums.Length);
+        Array.Sort(copy);
+        for(var i=copy.Length-1; i>=1; i--)
+        {
+            if (copy[i] > copy[i - 1]) 
+                dict.TryAdd(copy[i], i);
+        }
+        for(var j=0; j<nums.Length; j++)
+        {
+            if (dict.TryGetValue(nums[j], out var count)) 
+                nums[j] = count;
+            else 
+                nums[j] = 0;
+        }
+        return nums;
     }
 }
 class Q1365_HowManyNumbersAreSmallerThanTheCurrentNumberTestData : TestData

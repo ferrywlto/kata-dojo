b3c9d59f0b6c9ea74809841584e8ea925ef7081b
Ferry To <ferrywlto@gmail.com>
2024-07-04 11:24:41 +0100

    test: All test cases of Q1375 passed

--- csharp/leetcode/easy/Q1375_GenerateStringWithCharactersHaveOddCounts.cs
+++ csharp/leetcode/easy/Q1375_GenerateStringWithCharactersHaveOddCounts.cs
@@ -1,9 +1,33 @@
+using System.Text;
 
 class Q1375_GenerateStringWithCharactersHaveOddCounts
 {
+    // TC: O(n), it have to generate n characters from Enumerable.Repeat()
+    // SC: O(n), it have to hold the result of length n
     public string GenerateTheString(int n)
     {
-        return string.Empty;
+        if (n == 1) return "a";
+        if (n == 2) return "ab";
+        var sb = new StringBuilder();
+        if (n % 2 != 0)
+        {
+            sb.Append(string.Join(string.Empty, Enumerable.Repeat('a', n)));
+        } 
+        else 
+        {
+            var half = n / 2;
+            if (half % 2 != 0)
+            {
+                sb.Append(string.Join(string.Empty, Enumerable.Repeat('a', half)));
+                sb.Append(string.Join(string.Empty, Enumerable.Repeat('b', half)));
+            }
+            else 
+            {
+                sb.Append(string.Join(string.Empty, Enumerable.Repeat('a', half - 1)));
+                sb.Append(string.Join(string.Empty, Enumerable.Repeat('b', half + 1)));                
+            }
+        }
+        return sb.ToString();
     }
 }
 class Q1375_GenerateStringWithCharactersHaveOddCountsTestData : TestData
@@ -12,6 +36,8 @@ class Q1375_GenerateStringWithCharactersHaveOddCountsTestData : TestData
     [
         [4, "pppz"],
         [2, "xy"],
+        [3, "abc"],
+        [6, "aaaaab"],
         [7, "holasss"],
     ];
 }
@@ -23,6 +49,7 @@ public class Q1375_GenerateStringWithCharactersHaveOddCountsTests
     {
         var sut = new Q1375_GenerateStringWithCharactersHaveOddCounts();
         var actual = sut.GenerateTheString(input);
+
         Assert.Equal(actual.Length, input);
         Assert.Equal(IsAllOdd(actual), IsAllOdd(expected));
     }

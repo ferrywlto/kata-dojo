38617285cd147f3559aedf5ab3c12e5936eb4811
Ferry To <ferrywlto@gmail.com>
2024-07-05 16:27:26 +0100

    feat: Add add todo

--- javascript/angular/todo/src/components/todoList/todoList.component.ts
+++ javascript/angular/todo/src/components/todoList/todoList.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from "@angular/forms"
 import { TodoItem } from "./todoItem/todoItem.component";
 import { TodoState } from "./models/todoState";
+import { max } from "rxjs";
 @Component({
     standalone: true,
     selector: 'todo-list',
@@ -10,15 +11,12 @@ import { TodoState } from "./models/todoState";
     styleUrl: './todoList.component.css',
 })
 export class TodoList {
-    currentDate: Date = new Date();
     showDone: boolean = false;
     todos: TodoState[] = [
         { id: 1, title: "Sweeping floor",  done: false, due: new Date(2024,6,15,21,45) },
         { id: 2, title: "Leetcode", done: true, due: new Date(2024,7,1,23,59) },
         { id: 3, title: "Play with cats", done: false, due: new Date(2024,5,23) },
     ]
-    todoTitle: string = "hello";
-
     todoForm = new FormGroup({
         titleControl: new FormControl('', [
             Validators.minLength(4),
@@ -31,6 +29,9 @@ export class TodoList {
     });
     get titleControl() { return this.todoForm.get('titleControl'); }
     get dueControl() { return this.todoForm.get('dueControl'); }
+    get isFormValid() {
+        return this.todoForm.status === "VALID";
+    }
     
     dueDateCannotPast(): ValidatorFn {
         return (control: AbstractControl): ValidationErrors | null => {
@@ -44,6 +45,27 @@ export class TodoList {
         if (targetState) {
             targetState.done = !targetState?.done
         }
+    }
+    addTodo() {
+        let max_id;
+        if (this.todos.length > 0) {
+            max_id = this.todos.map(x => x.id)
+                .reduce((prev, curr) => Math.max(prev, curr)) + 1;
+        }
+        else max_id = 1;
+        console.log(`max_id: ${max_id}`);
+        let date = new Date(this.dueControl?.value ?? '');
+        this.todos.push({
+            id: max_id,
+            done: false,
+            title: this.titleControl?.value ?? '',
+            due: date,
+        })
+        this.clearInput();
+    }
+    clearInput() {
+        this.titleControl?.reset();
+        this.dueControl?.reset();
     }
     removeTodo(id: number) {
         this.todos = this.todos.filter(x => x.id !== id);
@@ -59,9 +81,6 @@ export class TodoList {
     }
     onAddClick() {
         console.log(this.todoForm.status);
+        this.addTodo();
     }
-    isFormValid() {
-        return this.todoForm.status === "VALID";
-    }
-
 }

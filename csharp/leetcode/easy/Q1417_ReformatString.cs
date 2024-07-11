478121fab4b95b1a3d6ee812a140355ebcb8b81c
Ferry To <ferrywlto@gmail.com>
2024-07-11 14:18:26 +0100

    test: Add official test cases of Q1417

--- csharp/leetcode/easy/Q1417_ReformatString.cs
+++ csharp/leetcode/easy/Q1417_ReformatString.cs
@@ -1,9 +1,38 @@
+using System.Text;
 
 class Q1417_ReformatString
 {
+    // TC: O(n), n is the length of s, first n to analyse the frequency, another n for forming the result string
+    // SC: O(n), n is the length of s
     public string Reformat(string s) 
     {
-        return string.Empty;
+        if (s.Length == 1) return s;
+
+        var chars = new Stack<char>();
+        var nums = new Stack<char>();
+        foreach(var c in s)
+        {
+            if (char.IsAsciiDigit(c)) nums.Push(c);
+            else if (char.IsAsciiLetterLower(c)) chars.Push(c);
+        }
+        var lengthDiff = Math.Abs(chars.Count - nums.Count);
+        if (chars.Count == 0 || nums.Count == 0 || lengthDiff > 1) return string.Empty;
+
+        var sb = new StringBuilder();
+        if(chars.Count > nums.Count) 
+        {
+            sb.Append(chars.Pop());
+        }
+        while(chars.Count > 0 && nums.Count > 0)
+        {
+            sb.Append(nums.Pop());
+            sb.Append(chars.Pop());
+        }
+        if (chars.Count < nums.Count)
+        {
+            sb.Append(nums.Pop());
+        }
+        return sb.ToString();
     }
 }
 class Q1417_ReformatStringTestData : TestData
@@ -13,6 +42,8 @@ class Q1417_ReformatStringTestData : TestData
         ["a0b1c2", "0a1b2c"],
         ["leetcode", string.Empty],
         ["1229857369", string.Empty],
+        ["j", "j"],
+        ["1", "1"],
     ];
 }
 public class Q1417_ReformatStringTests

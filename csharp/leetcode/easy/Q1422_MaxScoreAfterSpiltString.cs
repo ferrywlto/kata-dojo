ace875195a7fd1628d0f181b8178b29d799cac3a
Ferry To <ferrywlto@gmail.com>
2024-07-12 23:19:32 +0100

    test: All test cases of Q1422 passed

--- csharp/leetcode/easy/Q1422_MaxScoreAfterSpiltString.cs
+++ csharp/leetcode/easy/Q1422_MaxScoreAfterSpiltString.cs
@@ -1,9 +1,27 @@
-
 class Q1422_MaxScoreAfterSpiltString
-{
+{   
+    // TC: O(n), n for initial left and right score, + n-1 ops for sliding window calculate new scores  
+    // SC: O(1), comparisons doing in-place
     public int MaxScore(string s)
     {
-        return 0;
+        var leftScore = 0; 
+        var rightScore = 0;
+
+        if (s[0] == '0') leftScore++;
+        for(var i=s.Length-1; i>=1; i--)
+        {
+            if (s[i] == '1') rightScore++;
+        }
+        var max = leftScore + rightScore;
+        for(var j=1; j<s.Length-1; j++)
+        {
+            if (s[j] == '0') leftScore++;
+            else rightScore--;
+
+            if (leftScore + rightScore > max) 
+                max = leftScore + rightScore;
+        }
+        return max;
     }
 }
 class Q1422_MaxScoreAfterSpiltStringTestData : TestData
@@ -12,7 +30,12 @@ class Q1422_MaxScoreAfterSpiltStringTestData : TestData
     [
         ["011101", 5],
         ["00111", 5],
+        ["00110", 4],
+        ["00101", 4],
+        ["10101", 3],
+        ["01001", 4],
         ["1111", 3],
+        ["0000", 3],
     ];
 }
 public class Q1422_MaxScoreAfterSpiltStringTests

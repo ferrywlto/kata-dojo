cad17bfc619248192706e65cc70863058659843a
Ferry To <ferrywlto@gmail.com>
2024-07-14 19:22:36 +0100

    test: All test cases of Q1437 passed

--- csharp/leetcode/easy/Q1437_CheckIfAllOnesAtLeastLengthKPlacesAway.cs
+++ csharp/leetcode/easy/Q1437_CheckIfAllOnesAtLeastLengthKPlacesAway.cs
@@ -1,10 +1,23 @@
 
 class Q1437_CheckIfAllOnesAtLeastLengthKPlacesAway
 {
-    public bool KLengthApart(int[] nums, int k) 
+    // TC: O(n+m), it have to iterate all n elements in nums, then iterate m 1's index
+    // SC: O(m), where m is the number of 1's in nums 
+    public bool KLengthApart(int[] nums, int k)
     {
-        return false;    
-    }    
+        var oneIndexes = new List<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1) oneIndexes.Add(i);
+        }
+        if (oneIndexes.Count == 0) return true;
+
+        for (var j = 1; j < oneIndexes.Count; j++)
+        {
+            if (oneIndexes[j] - oneIndexes[j - 1] - 1 < k) return false;
+        }
+        return true;
+    }
 }
 class Q1437_CheckIfAllOnesAtLeastLengthKPlacesAwayTestData : TestData
 {
@@ -14,6 +27,7 @@ class Q1437_CheckIfAllOnesAtLeastLengthKPlacesAwayTestData : TestData
         [new int[] {1,0,0,1,0,1}, 2, false],
         [new int[] {0,0,1,0,1}, 2, false],
         [new int[] {0,0,1,0,0}, 2, true],
+        [new int[] {0,0,0,0,0}, 1, true],
     ];
 }
 public class Q1437_CheckIfAllOnesAtLeastLengthKPlacesAwayTests

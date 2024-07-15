48b08dc2baa610c509abdbe42d875339832aa1b0
Ferry To <ferrywlto@gmail.com>
2024-07-15 12:25:41 +0100

    test: All test cases of Q1466 passed

--- csharp/leetcode/easy/Q1446_ConsecutiveCharacters.cs
+++ csharp/leetcode/easy/Q1446_ConsecutiveCharacters.cs
@@ -1,18 +1,34 @@
-
-using System.Runtime.CompilerServices;
-
 class Q1446_ConsecutiveCharacters
 {
-    public int MaxPower(string s) {
-        return 0;   
-    }    
+    // TC: O(n), it have to iterate to the end to determine the longest
+    // SC: O(1), no space used in operations
+    public int MaxPower(string s)
+    {
+        var maxLength = 1;
+        var currentLength = 1;
+        var lastChar = s[0];
+        for(var i=1; i<s.Length; i++)
+        {
+            if(s[i] == lastChar) currentLength++;
+            else
+            {
+                if (currentLength > maxLength) 
+                    maxLength = currentLength;
+                lastChar = s[i];
+                currentLength = 1;
+            }
+        }
+        if (currentLength > maxLength) 
+            maxLength = currentLength;
+        return maxLength;
+    }
 }
 class Q1446_ConsecutiveCharactersTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["leetcode", 2],
-        ["abbcccddddeeeeedcba", 2],
+        ["abbcccddddeeeeedcba", 5],
     ];
 }
 public class Q1446_ConsecutiveCharactersTests

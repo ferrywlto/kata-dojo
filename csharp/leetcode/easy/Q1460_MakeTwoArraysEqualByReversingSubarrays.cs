a726a651702061897fdb01f139749987459bee6f
Ferry To <ferrywlto@gmail.com>
2024-07-16 11:22:05 +0100

    test: All test cases of Q1460 passed

--- csharp/leetcode/easy/Q1460_MakeTwoArraysEqualByReversingSubarrays.cs
+++ csharp/leetcode/easy/Q1460_MakeTwoArraysEqualByReversingSubarrays.cs
@@ -1,9 +1,14 @@
 
 class Q1460_MakeTwoArraysEqualByReversingSubarrays
 {
+    // TC: O(n log n), where n log n from Array.Sort() dominates n from SequenceEqual()
+    // SC: O(1), no space used in operation 
     public bool CanBeEqual(int[] target, int[] arr) 
     {
-        return false;
+        // if we break down the question to keep reversing 2 elements only, this is literally a sort 
+        Array.Sort(target);
+        Array.Sort(arr);
+        return target.SequenceEqual(arr);
     }     
 }
 class Q1460_MakeTwoArraysEqualByReversingSubarraysTestData : TestData
@@ -13,6 +18,7 @@ class Q1460_MakeTwoArraysEqualByReversingSubarraysTestData : TestData
         [new int[] {1,2,3,4}, new int[] {2,4,1,3}, true],
         [new int[] {7}, new int[] {7}, true],
         [new int[] {3,7,9}, new int[] {3,7,11}, false],
+        [new int[] {392,360}, new int[] {392,360}, true],
     ];
 }
 public class Q1460_MakeTwoArraysEqualByReversingSubarraysTests

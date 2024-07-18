375c773f8012c8e6630db2f06677b7f14d178e26
Ferry To <ferrywlto@gmail.com>
2024-07-18 23:36:20 +0100

    test: All test cases of Q1475 passed

--- csharp/leetcode/easy/Q1475_FinalPricesWithSpecialDiscountInShop.cs
+++ csharp/leetcode/easy/Q1475_FinalPricesWithSpecialDiscountInShop.cs
@@ -1,14 +1,30 @@
-
 class Q1475_FinalPricesWithSpecialDiscountInShop
 {
+    // TC: O(n^2), it cannot be faster due to question constraints
+    // SC: O(1), the space used is fixed, array items are updated in-place and
     public int[] FinalPrices(int[] prices)
     {
-        return [];
+        // worst case it will be O((n-1)+(n-2)+(n-3)...) => O(n^2)/2 => O(n^2) if we use brute force approach
+        // but we also cannot use binary search as the array cannot be sorted
+        for (var i = 0; i < prices.Length - 1; i++)
+        {
+            var lower = FindFirstLower(prices, i + 1, prices[i]);
+            prices[i] = prices[i] - lower;
+        }
+        return prices;
+    }
+    public int FindFirstLower(int[] input, int startIdx, int target)
+    {
+        for (var i = startIdx; i < input.Length; i++)
+        {
+            if (input[i] <= target) return input[i];
+        }
+        return 0;
     }
 }
 class Q1475_FinalPricesWithSpecialDiscountInShopTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[] {8,4,6,2,3}, new int[] {4,2,4,2,3}],
         [new int[] {1,2,3,4,5}, new int[] {1,2,3,4,5}],

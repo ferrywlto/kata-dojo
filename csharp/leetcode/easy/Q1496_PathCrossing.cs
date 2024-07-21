700557c400486fcab0bc46fb11aa1d3035d9b3d7
Ferry To <ferrywlto@gmail.com>
2024-07-21 14:59:33 +0100

    test: All test cases of Q1496 passed

--- csharp/leetcode/easy/Q1496_PathCrossing.cs
+++ csharp/leetcode/easy/Q1496_PathCrossing.cs
@@ -1,16 +1,35 @@
-
 class Q1496_PathCrossing
 {
+    // TC: O(n), where n is the length of path string
+    // SC: O(n), worst case path never cross itselt thus holding all steps
     public bool IsPathCrossing(string path)
     {
-        return false;    
-    }    
+        var set = new HashSet<(int x, int y)>() { (0, 0) };
+        var x = 0;
+        var y = 0;
+
+        foreach (var c in path)
+        {
+            switch (c)
+            {
+                case 'N': y++; break;
+                case 'S': y--; break;
+                case 'E': x++; break;
+                case 'W': x--; break;
+            }
+            if (set.Contains((x, y))) return true;
+            else set.Add((x, y));
+        }
+        return false;
+    }
 }
 class Q1496_PathCrossingTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["NES", false],
+        ["EESWN", true],
+        ["NNSWWEWSSESSWENNW", true],
         ["NESWW", true],
     ];
 }

0a2f75b5a6c5c762b4c622374b2e6cb1b13f5b77
Ferry To <ferrywlto@gmail.com>
2024-07-22 13:33:00 +0100

    test: All test cases of Q1507 passed

--- csharp/leetcode/easy/Q1507_ReformatDate.cs
+++ csharp/leetcode/easy/Q1507_ReformatDate.cs
@@ -1,14 +1,35 @@
-
 class Q1507_ReformatDate
 {
-    public string ReformatDate(string date) 
+    // TC: O(1)
+    // SC: O(1)
+    readonly Dictionary<string, string> Months = new Dictionary<string, string>() {
+        {"Jan", "01" },
+        {"Feb", "02"},
+        {"Mar", "03"},
+        {"Apr", "04"},
+        {"May", "05"},
+        {"Jun", "06"},
+        {"Jul", "07"},
+        {"Aug", "08"},
+        {"Sep", "09"},
+        {"Oct", "10"},
+        {"Nov", "11"},
+        {"Dec", "12"},
+    };
+    public string ReformatDate(string date)
     {
-        return string.Empty;    
-    }    
+        var parts = date.Split(' ');
+        var day = int.Parse(parts[0]
+            .Replace("th", string.Empty)
+            .Replace("st", string.Empty)
+            .Replace("nd", string.Empty)
+            .Replace("rd", string.Empty));
+        return $"{parts[2]}-{Months[parts[1]]}-{day:d2}";
+    }
 }
 class Q1507_ReformatDateTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["20th Oct 2052", "2052-10-20"],
         ["6th Jun 1933", "1933-06-06"],

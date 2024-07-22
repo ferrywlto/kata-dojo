a8ab11702031c08b0b8959aa84d4889a1d974de7
Ferry To <ferrywlto@gmail.com>
2024-07-22 13:07:18 +0100

    test: All test cases of Q1502 passed

--- csharp/leetcode/easy/Q1502_CanMakeArithmeticProgressionFromSequence.cs
+++ csharp/leetcode/easy/Q1502_CanMakeArithmeticProgressionFromSequence.cs
@@ -1,14 +1,21 @@
-
 class Q1502_CanMakeArithmeticProgressionFromSequence
 {
-    public bool CanMakeArithmeticProgression(int[] arr) 
+    // TC: O(n log n), dominated by Array.Sort(), then n-1 to get the diff of each pair, thus O(n log n)
+    // SC: O(1), the space used are fixed.
+    public bool CanMakeArithmeticProgression(int[] arr)
     {
-        return false;    
-    }    
+        Array.Sort(arr);
+        var diff = arr[1] - arr[0];
+        for (var i = 2; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] != diff) return false;
+        }
+        return true;
+    }
 }
 class Q1502_CanMakeArithmeticProgressionFromSequenceTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new int[] {3,5,1}, true],
         [new int[] {1,2,4}, false],

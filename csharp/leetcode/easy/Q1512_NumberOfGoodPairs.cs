919650b9750ce07c6f0c9935c94431106c444afc
Ferry To <ferrywlto@gmail.com>
2024-07-23 18:17:12 +0100

    test: All test cases of Q1512 passed

--- csharp/leetcode/easy/Q1512_NumberOfGoodPairs.cs
+++ csharp/leetcode/easy/Q1512_NumberOfGoodPairs.cs
@@ -1,9 +1,22 @@
 
 class Q1512_NumberOfGoodPairs
 {
+    // TC: O(n), where n is length of nums as all items need to iterate once
+    // TC: O(m), where m is the number of unique numbers in nums
     public int NumIdenticalPairs(int[] nums) 
     {
-        return 0;    
+        var pairs = 0;
+        var dict = new Dictionary<int, int>();
+        foreach(var n in nums)
+        {
+            if (!dict.TryGetValue(n, out var value)) dict.Add(n, 0);
+            else
+            {
+                dict[n] = ++value;
+                pairs += value;
+            }
+        }
+        return pairs;
     }
 }
 class Q1512_NumberOfGoodPairsTestData : TestData
@@ -11,6 +24,7 @@ class Q1512_NumberOfGoodPairsTestData : TestData
     protected override List<object[]> Data => 
     [
         [new int[] {1,2,3,1,1,3}, 4],
+        [new int[] {1,1,1}, 3],
         [new int[] {1,1,1,1}, 6],
         [new int[] {1,2,3}, 0],
     ];

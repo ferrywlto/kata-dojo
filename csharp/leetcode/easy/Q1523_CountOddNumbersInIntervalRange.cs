321235e7d043c5f22e12e7dd2e2c7bcb24586735
Ferry To <ferrywlto@gmail.com>
2024-07-24 19:13:20 +0100

    test: All test cases of Q1523 passed

--- csharp/leetcode/easy/Q1523_CountOddNumbersInIntervalRange.cs
+++ csharp/leetcode/easy/Q1523_CountOddNumbersInIntervalRange.cs
@@ -1,9 +1,12 @@
 
 class Q1523_CountOddNumbersInIntervalRange
 {
+    // TC: O(1)
+    // SC: O(1)
     public int CountOdds(int low, int high)
     {
-        return 0;
+        var temp = (high - low) / 2;
+        return low % 2 == 0 && high % 2 == 0 ? temp : temp + 1;
     }
 }
 class Q1523_CountOddNumbersInIntervalRangeTestData : TestData
@@ -12,6 +15,15 @@ class Q1523_CountOddNumbersInIntervalRangeTestData : TestData
     [
         [3,7,3],
         [8,10,1],
+        [0,10,5],
+        [0,8,4],
+        [2,6,2],
+        [1,11,6],
+        [3,9,4],
+        [5,9,3],
+        [1,10,5],
+        [1,8,4],
+        [2,9,4],
     ];
 }
 public class Q1523_CountOddNumbersInIntervalRangeTests

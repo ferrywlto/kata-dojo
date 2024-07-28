fa8ebed27004ad735d5eb9fb8655e51b4d0e8651
Ferry To <ferrywlto@gmail.com>
2024-07-28 20:12:00 +0100

    test: All test cases of Q1534 passed

--- csharp/leetcode/easy/Q1534_CountGoodTriplets.cs
+++ csharp/leetcode/easy/Q1534_CountGoodTriplets.cs
@@ -1,8 +1,25 @@
 class Q1534_CountGoodTriplets
 {
+    // TC: O(n^3), where n is length of arr -3
+    // SC: O(1), the space used is fixed
     public int CountGoodTriplets(int[] arr, int a, int b, int c)
     {
-        return 0;
+        var result = 0;
+        for(var i=0; i<arr.Length-2; i++)
+        {
+            for(var j=i+1; j<arr.Length-1; j++)
+            {
+                for(var k=j+1; k<arr.Length; k++)
+                {
+                    if (
+                        Math.Abs(arr[j] - arr[i]) <= a &&
+                        Math.Abs(arr[j] - arr[k]) <= b &&
+                        Math.Abs(arr[k] - arr[i]) <= c
+                    ) result++;
+                }
+            }
+        }
+        return result;
     }
 }
 class Q1534_CountGoodTripletsTestData : TestData

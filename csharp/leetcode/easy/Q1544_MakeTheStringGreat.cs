fe202b84ad85203caa71b1e027af9710503bd740
Ferry To <ferrywlto@gmail.com>
2024-07-29 20:41:35 +0100

    perf: Improve time by not repeatly scan and replace

--- csharp/leetcode/easy/Q1544_MakeTheStringGreat.cs
+++ csharp/leetcode/easy/Q1544_MakeTheStringGreat.cs
@@ -2,26 +2,23 @@ using System.Text;
 
 class Q1544_MakeTheStringGreat
 {
+    // TC: O(n), where n is the length of s, using this improved version it only need to iterate in one pass
+    // SC: O(n), where n is the length of s, no matter which version used, the same amount of space are needed for the string builder
     public string MakeGood(string s)
     {
-        var sb = new StringBuilder(s);
-        var matchFound = true;
-        while(matchFound)
-        {
-            matchFound = false;
-            for(var i=0; i<sb.Length-1; i++)
+            var sb = new StringBuilder(s);
+            int i = 0;
+            while (i < sb.Length - 1)
             {
-                if(Math.Abs(sb[i]-sb[i+1]) == 32)
+                // Both version use this to detect upper-lower pair
+                if (Math.Abs(sb[i] - sb[i + 1]) == 32)
                 {
-                    var pattern1 = $"{sb[i]}{sb[i + 1]}";
-                    var pattern2 = $"{sb[i + 1]}{sb[i]}";
-                    sb.Replace(pattern1, string.Empty);
-                    sb.Replace(pattern2, string.Empty);
-                    matchFound = true;
+                    sb.Remove(i, 2); // Remove the matching pair directly.
+                    if (i > 0) i--; // Step back to check for new adjacent matches.
                 }
+                else i++;
             }
-        }
-        return sb.ToString();
+            return sb.ToString();
     }
 }
 class Q1544_MakeTheStringGreatTestData : TestData

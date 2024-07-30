6ead453c5d4b6d785756b0bf4687c0196f7c65f4
Ferry To <ferrywlto@gmail.com>
2024-07-30 20:26:54 +0100

    test: All test cases of Q1556 passed

--- csharp/leetcode/easy/Q1556_ThousandSeparator.cs
+++ csharp/leetcode/easy/Q1556_ThousandSeparator.cs
@@ -1,8 +1,22 @@
+using System.Text;
+
 class Q1556_ThousandSeparator
 {
+    // TC: O(n), where n is number of digits of n
+    // SC: O(n+m), where n is the digits of n and m number of separator added to hold the result
     public string ThousandSeparator(int n)
     {
-        return string.Empty;
+        var sb = new StringBuilder(n.ToString());
+        var count = 0;
+        for(var i=sb.Length-1; i>0; i--)
+        {
+            count++;
+            if(count == 3) {
+                sb.Insert(i, ".");
+                count = 0;
+            }
+        }
+        return sb.ToString();
     }
 }
 class Q1556_ThousandSeparatorTestData : TestData
@@ -11,6 +25,9 @@ class Q1556_ThousandSeparatorTestData : TestData
     [
         [987, "987"],
         [1234, "1.234"],
+        [12345, "12.345"],
+        [123456, "123.456"],
+        [1234567, "1.234.567"],
     ];
 }
 public class Q1556_ThousandSeparatorTests

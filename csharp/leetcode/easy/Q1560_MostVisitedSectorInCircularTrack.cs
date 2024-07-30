ab96cc12c70290bee41ae330d90237d4a544198b
Ferry To <ferrywlto@gmail.com>
2024-07-30 23:23:15 +0100

    test: All test cases of Q1560 passed

--- csharp/leetcode/easy/Q1560_MostVisitedSectorInCircularTrack.cs
+++ csharp/leetcode/easy/Q1560_MostVisitedSectorInCircularTrack.cs
@@ -1,14 +1,49 @@
-
 class Q1560_MostVisitedSectorInCircularTrack
 {
-    public IList<int> MostVisited(int n, int[] rounds) 
+    // TC: O(n*m), where n is rounds - 1 * sectors visited per round
+    // SC: O(n), where n is number of visited sector
+    public IList<int> MostVisited(int n, int[] rounds)
     {
-        return [];        
+        if (rounds.Length == 1) return rounds;
+        var maxCount = 0;
+        var dict = new Dictionary<int, int>();
+
+        for(var i=1; i<rounds.Length; i++)
+        {
+            var current = rounds[i - 1];
+            var next = rounds[i];
+            while (current != next)
+            {
+                if (dict.TryGetValue(current, out var value))
+                    dict[current] = ++value;
+                else
+                    dict[current] = 1;
+
+                if (dict[current] > maxCount) 
+                    maxCount = dict[current];
+
+                if (current == n) current = 1;
+                else current++;
+            }
+        }
+        var last = rounds[^1];
+        if (dict.TryGetValue(last, out var value2))
+            dict[last] = ++value2;
+        else
+            dict[last] = 1;
+
+        if (dict[last] > maxCount) 
+            maxCount = dict[last];
+
+        return dict
+            .Where(x => x.Value == maxCount)
+            .Select(x => x.Key)
+            .ToList();
     }
 }
 class Q1560_MostVisitedSectorInCircularTrackTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [4, new int[] {1,3,1,2}, new List<int> {1,2}],
         [2, new int[] {2,1,2,1,2,1,2,1,2}, new List<int> {2}],

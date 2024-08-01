c07309dd578af72bdc4646f40b1b641092f13eda
Ferry To <ferrywlto@gmail.com>
2024-08-01 20:45:51 +0100

    test: All test cases of Q1576 passed

--- csharp/leetcode/easy/Q1576_ReplaceAllQuestionMarksToAvoidConsecutiveRepeatingCharacters.cs
+++ csharp/leetcode/easy/Q1576_ReplaceAllQuestionMarksToAvoidConsecutiveRepeatingCharacters.cs
@@ -1,13 +1,37 @@
+using System.Text;
+
 class Q1576_ReplaceAllQuestionMarksToAvoidConsecutiveRepeatingCharacters
 {
-    public string ModifyString(string s) 
+    // TC: O(n), where n is the length of s * 24 at most
+    // SC: O(n), where n is the length of s to hold the result
+    public string ModifyString(string s)
     {
-        return string.Empty;    
-    }    
+        if (s.Length == 1) return s[0] == '?' ? "a" : s;
+
+        var sb = new StringBuilder(s);
+        for (var i = 0; i < sb.Length; i++)
+        {
+            if (sb[i] == '?')
+            {
+                char prev = i > 0 ? sb[i - 1] : '\0';
+                char next = i < sb.Length - 1 ? sb[i + 1] : '\0';
+                sb[i] = GetNextAvailable(prev, next);
+            }
+        }
+        return sb.ToString();
+    }
+    public char GetNextAvailable(char a, char b)
+    {
+        for (char i = 'a'; i <= 'z'; i++)
+        {
+            if (i != a && i != b) return i;
+        }
+        return 'a';
+    }
 }
 class Q1576_ReplaceAllQuestionMarksToAvoidConsecutiveRepeatingCharactersTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["?zs", "azs"],
         ["ubv?w", "ubvaw"],

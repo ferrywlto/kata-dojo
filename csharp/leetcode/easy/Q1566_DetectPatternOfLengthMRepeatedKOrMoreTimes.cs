745aafaa4440b96fed2197a00022e9942a5d5fe1
Ferry To <ferrywlto@gmail.com>
2024-08-04 12:30:56 +0100

    test: All test cases of Q1566 passed

--- csharp/leetcode/easy/Q1566_DetectPatternOfLengthMRepeatedKOrMoreTimes.cs
+++ csharp/leetcode/easy/Q1566_DetectPatternOfLengthMRepeatedKOrMoreTimes.cs
@@ -2,6 +2,22 @@ class Q1566_DetectPatternOfLengthMRepeatedKOrMoreTimes
 {
     public bool ContainsPattern(int[] arr, int m, int k)
     {
+        var full = string.Join(',', arr);
+
+        for(var i=0; i<arr.Length-m+1; i++)
+        {
+            var list = new List<int>();
+            for(var j=i; j<i+m; j++) list.Add(arr[j]);
+
+            var repeatedList = new List<int>();
+            var x = Enumerable.Repeat(list, k);
+            foreach(var rl in x)
+                repeatedList.AddRange(rl);
+
+            var patternToFind = string.Join(',', repeatedList);
+
+            if (full.Contains(patternToFind)) return true;
+        }
         return false;
     }
 }
@@ -12,6 +28,11 @@ class Q1566_DetectPatternOfLengthMRepeatedKOrMoreTimesTestData : TestData
         [new int[] {1,2,4,4,4,4}, 1, 3, true],
         [new int[] {1,2,1,2,1,1,1,3}, 2, 2, true],
         [new int[] {1,2,1,2,1,3}, 2, 3, false],
+        [new int[] {1,2,3,1,2}, 2, 2, false],
+        [new int[] {3,2,2,1,2,2,1,1,1,2,3,2,2}, 3, 2, true],
+        [new int[] {2,2}, 1, 2, true],
+        [new int[] {2,1,2,2,2,2,2,2}, 2, 2, true],
+        [new int[] {2,2,2,2}, 2, 3, false],
     ];
 }
 public class Q1566_DetectPatternOfLengthMRepeatedKOrMoreTimesTests

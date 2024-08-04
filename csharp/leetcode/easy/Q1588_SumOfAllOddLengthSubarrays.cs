c6ca2dfafac32828c4451ba5764d7c1d9a58ea40
Ferry To <ferrywlto@gmail.com>
2024-08-04 17:45:50 +0100

    test: All test cases of Q1588 passed

--- csharp/leetcode/easy/Q1588_SumOfAllOddLengthSubarrays.cs
+++ csharp/leetcode/easy/Q1588_SumOfAllOddLengthSubarrays.cs
@@ -1,8 +1,30 @@
 class Q1588_SumOfAllOddLengthSubarrays
 {
+    // TC: O(n*m*k), where n is length of arr n * group size m * number of groups k
+    // SC: O(1), space used is fixed 
     public int SumOddLengthSubarrays(int[] arr)
     {
-        return 0;
+        if (arr.Length == 1) return arr[0];
+
+        var result = arr.Sum();
+        if(arr.Length % 2 == 1) result *= 2;
+
+        for(var groupSize=3; groupSize<arr.Length; groupSize+=2)
+        {
+            var last = 0;
+            for(var j=0; j<groupSize; j++) last += arr[j];
+
+            result += last;
+
+            for(var i=1; i<arr.Length-groupSize+1; i++)
+            {
+                // sliding window, save the repeat add in between, espcially in large group size 
+                var temp = last-arr[i-1]+arr[i+groupSize-1];
+                result += temp;
+                last = temp;
+            }
+        }
+        return result;
     }
 }
 class Q1588_SumOfAllOddLengthSubarraysTestData : TestData

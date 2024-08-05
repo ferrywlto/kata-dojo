aeb1432e8673c4280f831c80b33085dac51af3d0
Ferry To <ferrywlto@gmail.com>
2024-08-05 19:10:54 +0100

    test: All test cases of Q1598 passed

--- csharp/leetcode/easy/Q1598_CrawlerLogFolder.cs
+++ csharp/leetcode/easy/Q1598_CrawlerLogFolder.cs
@@ -1,18 +1,29 @@
-
 class Q1598_CrawlerLogFolder
 {
-    public int MinOperations(string[] logs) 
+    // TC: O(n), where n is the size of logs, it have to iterate once to know the result
+    // SC: O(1), space used is fixed
+    public int MinOperations(string[] logs)
     {
-        return 0;    
+        var level = 0;
+        foreach (var log in logs)
+        {
+            if (log == "../")
+            {
+                if(level > 0) level--;
+            } 
+            else if(log != "./") level++;
+        }
+        return level;
     }
 }
 class Q1598_CrawlerLogFolderTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [new string[] {"d1/","d2/","../","d21/","./"}, 2],
         [new string[] {"d1/","d2/","./","d3/","../","d31/"}, 3],
         [new string[] {"d1/","../","../","../"}, 0],
+        [new string[] {"./","../","./"}, 0],
     ];
 }
 public class Q1598_CrawlerLogFolderTests

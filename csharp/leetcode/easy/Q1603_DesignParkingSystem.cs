d9f562d4f1916b25c6c8516dc8290e8bf626f40a
Ferry To <ferrywlto@gmail.com>
2024-08-06 22:18:31 +0100

    test: All test cases of Q1603 passed

--- csharp/leetcode/easy/Q1603_DesignParkingSystem.cs
+++ csharp/leetcode/easy/Q1603_DesignParkingSystem.cs
@@ -1,13 +1,23 @@
 class Q1603_DesignParkingSystem
 {
+    // TC: O(1), the time for method AddCar does not scale with input data
+    // SC: O(1), space used are fixed to integer array with 3 elements
+    readonly int[] vacancy = new int[3];
     public Q1603_DesignParkingSystem(int big, int medium, int small)
     {
-
+        vacancy[0] = big;
+        vacancy[1] = medium;
+        vacancy[2] = small;
     }
 
     public bool AddCar(int carType)
     {
-        return false;
+        if (vacancy[carType - 1] > 0)
+        {
+            vacancy[carType - 1]--;
+            return true;
+        }
+        else return false;
     }
 }
 class Q1603_DesignParkingSystemTestData : TestData
@@ -24,7 +34,7 @@ public class Q1603_DesignParkingSystemTests
     public void OfficialTestCases((int big, int medium, int small) input, int[] cars, bool[] expected)
     {
         var sut = new Q1603_DesignParkingSystem(input.big, input.medium, input.small);
-        for(var i=0; i<cars.Length; i++)
+        for (var i = 0; i < cars.Length; i++)
         {
             var actual = sut.AddCar(cars[i]);
             Assert.Equal(expected[i], actual);

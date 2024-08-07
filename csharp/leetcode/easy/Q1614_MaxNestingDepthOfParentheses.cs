a4bd16559cc2444165faa5dbdcdd6fda957d41e0
Ferry To <ferrywlto@gmail.com>
2024-08-07 18:32:49 +0100

    test: All test cases of Q1614 passed

--- csharp/leetcode/easy/Q1614_MaxNestingDepthOfParentheses.cs
+++ csharp/leetcode/easy/Q1614_MaxNestingDepthOfParentheses.cs
@@ -1,13 +1,26 @@
 class Q1614_MaxNestingDepthOfParaentheses
 {
-    public int MaxDepth(string s) 
+    // TC: O(n), where n is length of s, it has to run through the whole string to find the max depth
+    // SC: O(1), space used is fixed
+    public int MaxDepth(string s)
     {
-        return 0;    
-    }    
+        var depth = 0;
+        var maxDepth = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(')
+            {
+                depth++;
+                if (depth > maxDepth) maxDepth = depth;
+            }
+            else if (s[i] == ')') depth--;
+        }
+        return maxDepth;
+    }
 }
 class Q1614_MaxNestingDepthOfParaenthesesTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["(1+(2*3)+((8)/4))+1", 3],
         ["(1)+((2))+(((3)))", 3],

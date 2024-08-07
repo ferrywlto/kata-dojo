76c4a6e69d3f1cdb93a082e9d49f2b0c0c6726a8
Ferry To <ferrywlto@gmail.com>
2024-08-07 18:22:10 +0100

    test: Add official test cases of Q1603

--- csharp/leetcode/easy/Q1608_SpecialArrayWithXElementsGreaterThanOrEqualX.cs
+++ csharp/leetcode/easy/Q1608_SpecialArrayWithXElementsGreaterThanOrEqualX.cs
@@ -1,15 +1,33 @@
-
 class Q1608_SpecialArrayWithXElementsGreaterThanOrEqualX
 {
+    // TC: O(n log n), where n is the length of nums and dominated by Array.Sort();
+    // SC: O(1), space used is fixed
     public int SpecialArray(int[] nums)
     {
-        return 0;
+        Array.Sort(nums);
+        // 1. it doesn't need to check x = 0, as 0 elements >= 0 is not possible as the array contains only positive integers 
+        // 2. x must be less than or equal to nums.Length, it cannot have 8 elements larger than or equals to 8 in a 7 elements array 
+        if (nums[0] >= nums.Length) return nums.Length;
+
+        for(var i=0; i<nums.Length-1; i++)
+        {
+            var normalized = i + 1;
+            var larger = nums[^normalized];
+            var smaller = nums[^(normalized+1)];
+            Console.WriteLine($"x: {normalized}, larger: {larger}, smaller: {smaller}");
+            if (normalized > smaller && normalized <= larger) return normalized;
+        }
+        return -1;
     }
 }
 class Q1608_SpecialArrayWithXElementsGreaterThanOrEqualXTestData : TestData
 {
     protected override List<object[]> Data => 
     [
+        [new int[] {2}, 1],
+        [new int[] {2,1}, -1],
+        [new int[] {0,0,0,0,0,9}, 1],
+        [new int[] {7,6,9,8,7,9}, 6],
         [new int[] {3,5}, 2],
         [new int[] {0,0}, -1],
         [new int[] {0,4,3,0,4}, 3],

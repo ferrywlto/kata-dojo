d4b7c1c4730603df1506a1428f7609cd846a9b50
Ferry To <ferrywlto@gmail.com>
2024-08-08 21:24:23 +0100

    test: All test cases of Q1624 passed

--- csharp/leetcode/easy/Q1624_LargestSubstringBetweenTwoEqualCharacters.cs
+++ csharp/leetcode/easy/Q1624_LargestSubstringBetweenTwoEqualCharacters.cs
@@ -1,18 +1,33 @@
-
 class Q1624_LargestSubstringBetweenTwoEqualCharacters
 {
+    // TC: O(n), where n is length of s
+    // SC: O(n), where n is number of unique characters in s
     public int MaxLengthBetweenEqualCharacters(string s)
     {
-        return 0;
+        var maxLength = -1;
+        var dict = new Dictionary<char, int[]>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (dict.TryGetValue(s[i], out var value))
+            {
+                dict[s[i]][1] = i;
+                var diff = dict[s[i]][1] - dict[s[i]][0] - 1;
+                if (diff > maxLength) maxLength = diff;
+            }
+            else dict.Add(s[i], [i, -1]);
+        }
+        return maxLength;
     }
 }
 class Q1624_LargestSubstringBetweenTwoEqualCharactersTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["aa", 0],
         ["abca", 2],
         ["cbzxy", -1],
+        ["mgntdygtxrvxjnwksqhxuxtrv", 18],
     ];
 }
 public class Q1624_LargestSubstringBetweenTwoEqualCharactersTests

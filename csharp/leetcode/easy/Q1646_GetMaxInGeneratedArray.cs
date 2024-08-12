2c4283944a29b45d9fe4ef4897f98ba924b50aa7
Ferry To <ferrywlto@gmail.com>
2024-08-12 19:02:11 +0100

    test: All test cases of Q1646 passed

--- csharp/leetcode/easy/Q1646_GetMaxInGeneratedArray.cs
+++ csharp/leetcode/easy/Q1646_GetMaxInGeneratedArray.cs
@@ -1,14 +1,31 @@
-
 class Q1646_GetMaxInGeneratedArray
 {
+    // TC: O(n), scale with how large n is
+    // SC: O(n), scale with how large n is, as have to cache value for fast calculation
     public int GetMaximumGenerated(int n)
     {
-        return 0;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        var nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        var max = 1;
+        
+        for (var i = 2; i <= n; i++)
+        {
+            var temp = nums[i / 2];
+            if (i % 2 != 0)
+                temp += nums[i / 2 + 1];
+
+            nums[i] = temp;
+            if (temp > max) max = temp;
+        }
+        return max;
     }
 }
 class Q1646_GetMaxInGeneratedArrayTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         [7,3],
         [2,1],

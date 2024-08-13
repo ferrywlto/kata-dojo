4aa2c8c46ae9aa769a280364357c541518e629c3
Ferry To <ferrywlto@gmail.com>
2024-08-13 20:42:38 +0100

    test: All test cases of Q1652 passed

--- csharp/leetcode/easy/Q1652_DefuseTheBomb.cs
+++ csharp/leetcode/easy/Q1652_DefuseTheBomb.cs
@@ -1,8 +1,34 @@
 class Q1652_DefuseTheBomb
 {
+    // TC: O(n), where n is length of code
+    // SC: O(n), where n is length of code to hold the result array
     public int[] Decrypt(int[] code, int k)
     {
-        return [];
+        var result = new int[code.Length];
+        for(var i=0; i<code.Length; i++)
+        {
+            var temp = 0;
+            if(k > 0)
+            {
+                for(var j=0; j<k; j++)
+                {
+                    var idx = (i + 1 + j) % code.Length;
+                    temp += code[idx];
+                }
+            }
+            else if(k < 0)
+            {
+                for(var j=0; j>k; j--)
+                {
+                    var idx = (i - 1 + j) % code.Length;
+                    // the tricky part here is to deal with negative modulus
+                    if (idx < 0) idx += code.Length;
+                    temp += code[idx];
+                }                
+            }
+            result[i] = temp;
+        }
+        return result;
     }
 }
 class Q1652_DefuseTheBombTestData : TestData

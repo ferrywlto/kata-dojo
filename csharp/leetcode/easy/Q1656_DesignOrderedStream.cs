35c8bb0243918770d24cc82d7e2edfbadeadbac7
Ferry To <ferrywlto@gmail.com>
2024-08-13 21:36:55 +0100

    test: All test cases of Q1656 passed

--- csharp/leetcode/easy/Q1656_DesignOrderedStream.cs
+++ csharp/leetcode/easy/Q1656_DesignOrderedStream.cs
@@ -1,9 +1,29 @@
 using Row = (int key, string value);
 class Q1656_DesignOrderedStream
 {
+    public readonly string[] stream;
+    private int pointer = 0;
+    public Q1656_DesignOrderedStream(int n)
+    {
+        stream = new string[n];
+    }
+
+    // TC: O(n), worst case the whole stream have to read through
+    // SC: O(n), worst case the whole stream have to add to result list
     public IList<string> Insert(int idKey, string value)
     {
-        return [];
+        var idx = idKey - 1;
+        stream[idx] = value;
+
+        if (idx != pointer) return [];
+
+        var result = new List<string>();
+        while (pointer < stream.Length && !string.IsNullOrEmpty(stream[pointer]))
+        {
+            result.Add(stream[pointer]);
+            pointer++;
+        }
+        return result;
     }
 }
 
@@ -38,11 +58,12 @@ public class Q1656_DesignOrderedStreamTests
     [ClassData(typeof(Q1656_DesignOrderedStreamTestData))]
     public void OfficialTestCases(Row[] input, string[][] expected)
     {
-        var sut = new Q1656_DesignOrderedStream();
+        var sut = new Q1656_DesignOrderedStream(input.Length);
         for (var i = 0; i < input.Length; i++)
         {
             (var key, var value) = input[i];
             var actual = sut.Insert(key, value);
+
             Assert.Equal(expected[i], actual);
         }
     }

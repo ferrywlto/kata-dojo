b5e997060f3dc3521e11bba48adb8f5ac74fe470
Ferry To <ferrywlto@gmail.com>
2024-08-15 20:44:56 +0100

    test: Add official test cases of Q1667

---
+++ csharp/leetcode/easy/Q1667_FixNamesInTable.cs
@@ -0,0 +1,48 @@
+using Row = (int user_id, string name);
+class Q1667_FixNamesInTable : SqlQuestion
+{
+    public override string Query =>
+    """
+    select 1;
+    """;
+}
+class Q1667_FixNamesInTableTestData : TestData
+{
+    protected override List<object[]> Data =>
+    [
+        [
+            """
+            insert into Users (user_id, name) values ('1', 'aLice');
+            insert into Users (user_id, name) values ('2', 'bOB');
+            """,
+            new Row[]
+            {
+                (1, "Alice"),
+                (2, "Bob"),
+            }
+        ]
+    ];
+}
+public class Q1667_FixNamesInTableTests(ITestOutputHelper output) : SqlTest(output)
+{
+    protected override string TestSchema =>
+    """
+    Create table If Not Exists Users (user_id int, name varchar(40));
+    """;
+
+    [Theory]
+    [ClassData(typeof(Q1667_FixNamesInTableTestData))]
+    public void OfficialTestCases(string testDataSql, Row[] expected)
+    {
+        ArrangeTestData(testDataSql);
+        var sut = new Q1667_FixNamesInTable();
+        var reader = ExecuteQuery(sut.Query, true);
+        AssertResultSchema(reader, ["user_id", "name"]);
+        foreach ((int user_id, string name) in expected)
+        {
+            Assert.True(reader.Read());
+            Assert.Equal(user_id, reader.GetInt32(0));
+            Assert.Equal(name, reader.GetString(1));
+        }
+    }
+}

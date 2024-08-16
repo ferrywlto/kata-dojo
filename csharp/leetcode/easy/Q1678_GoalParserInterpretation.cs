27c60302b2bb08f9b03fdd59ba1285fe5b4b0ddd
Ferry To <ferrywlto@gmail.com>
2024-08-16 23:49:23 +0100

    test: All test cases of Q1678 passed

--- csharp/leetcode/easy/Q1678_GoalParserInterpretation.cs
+++ csharp/leetcode/easy/Q1678_GoalParserInterpretation.cs
@@ -1,9 +1,30 @@
+using System.Text;
 
 class Q1678_GoalParserInterpretation
 {
+    // TC: O(n), where n is length of command
+    // SC: O(n), where n is the number of command in resulting string builder 
     public string Interpret(string command)
     {
-        return string.Empty;
+        var sb = new StringBuilder();
+        for(var i=0; i<command.Length; i++)
+        {
+            if (command[i] == 'G') sb.Append('G');
+            else if(command[i] == '(')
+            {
+                if (command[i + 1] == ')')
+                {
+                    sb.Append('o');
+                    i++;
+                }
+                else 
+                {
+                    sb.Append("al");
+                    i += 3;
+                }
+            }
+        }
+        return sb.ToString();
     }
 }
 class Q1678_GoalParserInterpretationTestData : TestData

8a6f1a56e719ad6112dd67bc6208db16c2e24063
Ferry To <ferrywlto@gmail.com>
2024-08-18 17:01:10 +0100

    test: All test cases of Q1684 passed

--- csharp/leetcode/easy/Q1684_CountTheNumberOfConsistentStrings.cs
+++ csharp/leetcode/easy/Q1684_CountTheNumberOfConsistentStrings.cs
@@ -1,17 +1,34 @@
 class Q1684_CountTheNumberOfConsistentString
 {
-    public int CountConsistentStrings(string allowed, string[] words) 
+    // TC: O(n), it have to walkthough all character in words worst case if all characters are allowed
+    // TC: O(m), m is length of allowed
+    public int CountConsistentStrings(string allowed, string[] words)
     {
-        return 0;    
-    }    
+        var result = 0;
+        var allowSet = allowed.ToHashSet();
+        for (var i = 0; i < words.Length; i++)
+        {
+            var valid = true;
+            for (var j = 0; j < words[i].Length; j++)
+            {
+                if (!allowSet.Contains(words[i][j]))
+                {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) result++;
+        }
+        return result;
+    }
 }
 class Q1684_CountTheNumberOfConsistentStringTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
-        ["ab", new string[] {"ad","bd","aaab","baa","badab"}],
-        ["abc", new string[] {"a","b","c","ab","ac","bc","abc"}],
-        ["cad", new string[] {"cc","acd","b","ba","bac","bad","ac","d"}],
+        ["ab", new string[] {"ad","bd","aaab","baa","badab"}, 2],
+        ["abc", new string[] {"a","b","c","ab","ac","bc","abc"}, 7],
+        ["cad", new string[] {"cc","acd","b","ba","bac","bad","ac","d"}, 4],
     ];
 }
 public class Q1684_CountTheNumberOfConsistentStringTests

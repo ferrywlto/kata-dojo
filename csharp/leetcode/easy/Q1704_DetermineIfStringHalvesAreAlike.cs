d6f151de0405a17f3027af95a41771e0dd19758e
Ferry To <ferrywlto@gmail.com>
2024-08-23 00:26:34 +0100

    test: All test cases of Q1704 passed

--- csharp/leetcode/easy/Q1704_DetermineIfStringHalvesAreAlike.cs
+++ csharp/leetcode/easy/Q1704_DetermineIfStringHalvesAreAlike.cs
@@ -1,8 +1,27 @@
 class Q1704_DetermineIfStringHalvesAreAlike
 {
+    // TC: O(n), where n is length of s, it have to iterate all characters
+    // SC: O(1), space used is fixed
     public bool HalvesAreAlike(string s)
     {
-        return false;
+        var set = new HashSet<char>
+        {
+            {'a'},{'A'},
+            {'e'},{'E'},
+            {'i'},{'I'},
+            {'o'},{'O'},
+            {'u'},{'U'},
+        };
+        var count = 0;
+        for(var i=0; i<s.Length/2; i++)
+        {
+            if(set.Contains(s[i])) count++;
+        }
+        for(var j=s.Length/2; j<s.Length; j++)
+        {
+            if (set.Contains(s[j])) count--;
+        }
+        return count == 0;
     }
 }
 class Q1704_DetermineIfStringHalvesAreAlikeTestData : TestData
@@ -11,6 +30,7 @@ class Q1704_DetermineIfStringHalvesAreAlikeTestData : TestData
     [
         ["book", true],
         ["textbook", false],
+        ["AbCdEfGh", true],
     ];
 }
 public class Q1704_DetermineIfStringHalvesAreAlikeTests

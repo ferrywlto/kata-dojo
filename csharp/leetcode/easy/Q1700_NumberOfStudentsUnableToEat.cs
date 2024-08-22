e000ade68944c525a78f14246593de1b4d7064af
Ferry To <ferrywlto@gmail.com>
2024-08-22 01:22:54 +0100

    test: All test cases of Q1700 passed

--- csharp/leetcode/easy/Q1700_NumberOfStudentsUnableToEat.cs
+++ csharp/leetcode/easy/Q1700_NumberOfStudentsUnableToEat.cs
@@ -1,8 +1,28 @@
 class Q1700_NumberOfStudentsUnableToEat
 {
+    // TC: O(n), where n is number of sandwiches
+    // SC: O(n+m), where 2 queues are needed
     public int CountStudents(int[] students, int[] sandwiches)
     {
-        return 0;
+        var queueStudents = new Queue<int>(students);
+        var queueSandwiches = new Queue<int>(sandwiches);
+        var failedCount = 0;
+
+        while (queueSandwiches.Count > 0 && failedCount < queueStudents.Count)
+        {
+            if (queueStudents.Peek() == queueSandwiches.Peek())
+            {
+                queueStudents.Dequeue();
+                queueSandwiches.Dequeue();
+                failedCount = 0;
+            }
+            else
+            {
+                queueStudents.Enqueue(queueStudents.Dequeue());
+                failedCount++;
+            }
+        }
+        return queueStudents.Count;
     }
 }
 class Q1700_NumberOfStudentsUnableToEatTestData : TestData
@@ -11,6 +31,7 @@ class Q1700_NumberOfStudentsUnableToEatTestData : TestData
     [
         [new int[] {1,1,0,0}, new int[] {0,1,0,1}, 0],
         [new int[] {1,1,1,0,0,1}, new int[] {1,0,0,0,1,1}, 3],
+        [new int[] {0,0,0,1,0,1,1,1,1,0,1}, new int[] {0,0,0,1,0,0,0,0,0,1,0}, 5],
     ];
 }
 public class Q1700_NumberOfStudentsUnableToEatTests

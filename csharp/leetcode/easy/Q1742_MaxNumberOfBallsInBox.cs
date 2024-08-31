ba5c9af86065325945b8613989c85963d4499c32
Ferry To <ferrywlto@gmail.com>
2024-08-31 16:12:48 +0100

    test: All test cases of Q1742 passed

--- csharp/leetcode/easy/Q1742_MaxNumberOfBallsInBox.cs
+++ csharp/leetcode/easy/Q1742_MaxNumberOfBallsInBox.cs
@@ -2,7 +2,31 @@ class Q1742_MaxNumberOfBallsInBox
 {
     public int CountBalls(int lowLimit, int highLimit) 
     {
-        return 0;
+        // TC: O(n log n), where n is high-low-1 and log n from Sum()
+        // SC: O(m), where m is the number of unique sum 
+        var dict = new Dictionary<int, int>();
+        var max = int.MinValue;
+        for(var i=lowLimit; i<= highLimit; i++)
+        {
+            var sum = Sum(i);
+            if(dict.TryGetValue(sum, out var value))
+                dict[sum] = ++value;
+            else 
+                dict.Add(sum, 1);
+
+            if (dict[sum] > max) max = dict[sum];
+        }
+        return max;
+    }
+    public int Sum(int input)
+    {
+        var result = 0;
+        while(input > 0)
+        {
+            result += input % 10;
+            input /= 10;
+        }
+        return result;
     }
 }
 class Q1742_MaxNumberOfBallsInBoxTestData : TestData

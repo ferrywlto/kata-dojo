357e442407299709cce745af4c061713e9180d19
Ferry To <ferrywlto@gmail.com>
2024-09-02 23:04:18 +0100

    test: All test cases of Q1768 passed

--- csharp/leetcode/easy/Q1768_MergeStringsAlternately.cs
+++ csharp/leetcode/easy/Q1768_MergeStringsAlternately.cs
@@ -1,14 +1,25 @@
+using System.Text;
 
 class Q1768_MergeStringsAlternately
 {
-    public string MergeAlternately(string word1, string word2) 
+    // TC: O(n), where n is word1.Length + word2.Length for Append();
+    // SC: O(n), where n is word1.Length + word2.Length for string builder
+    public string MergeAlternately(string word1, string word2)
     {
-        return string.Empty;    
+        var idx = 0;
+        var sb = new StringBuilder(word1.Length + word2.Length);
+        while (idx < word1.Length || idx < word2.Length)
+        {
+            if (idx < word1.Length) sb.Append(word1[idx]);
+            if (idx < word2.Length) sb.Append(word2[idx]);
+            idx++;
+        }
+        return sb.ToString();
     }
 }
 class Q1768_MergeStringsAlternatelyTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["abc", "pqr", "apbqcr"],
         ["ab", "pqrs", "apbqrs"],

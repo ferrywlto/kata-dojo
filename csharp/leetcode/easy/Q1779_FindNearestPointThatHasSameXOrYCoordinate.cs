770d71d66ad9c76b2607802236d0881728448f4c
Ferry To <ferrywlto@gmail.com>
2024-09-03 23:22:29 +0100

    test: All test cases of Q1779 passed

--- csharp/leetcode/easy/Q1779_FindNearestPointThatHasSameXOrYCoordinate.cs
+++ csharp/leetcode/easy/Q1779_FindNearestPointThatHasSameXOrYCoordinate.cs
@@ -1,8 +1,25 @@
 class Q1779_FindNearestPointThatHasSameXOrYCoordinate
 {
+    // TC: O(n), where n is length of points
+    // SC: O(1), space used is fixed
     public int NearestValidPoint(int x, int y, int[][] points)
     {
-        return 0;
+        var min = int.MaxValue;
+        var minIdx = -1;
+        for (var i = 0; i < points.Length; i++)
+        {
+            var p = points[i];
+            if (p[0] == x || p[1] == y)
+            {
+                var temp = Math.Abs(p[0] - x) + Math.Abs(p[1] - y);
+                if (temp < min)
+                {
+                    min = temp;
+                    minIdx = i;
+                }
+            }
+        }
+        return minIdx;
     }
 }
 class Q1779_FindNearestPointThatHasSameXOrYCoordinateTestData : TestData
@@ -10,7 +27,7 @@ class Q1779_FindNearestPointThatHasSameXOrYCoordinateTestData : TestData
     protected override List<object[]> Data =>
     [
         [3,4, new int[][] {[1,2],[3,1],[2,4],[2,3],[4,4]}, 2],
-        [3,4, new int[][] {[3,4]}, 1],
+        [3,4, new int[][] {[3,4]}, 0],
         [3,4, new int[][] {[2,3]}, -1],
     ];
 }

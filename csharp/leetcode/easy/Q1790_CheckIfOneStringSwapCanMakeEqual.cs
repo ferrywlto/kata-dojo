a6b82f06b2c0295890fbc2123645edac53b611c2
Ferry To <ferrywlto@gmail.com>
2024-09-04 21:43:12 +0100

    test: All test cases of Q1790 passed

--- csharp/leetcode/easy/Q1790_CheckIfOneStringSwapCanMakeEqual.cs
+++ csharp/leetcode/easy/Q1790_CheckIfOneStringSwapCanMakeEqual.cs
@@ -1,7 +1,32 @@
 class Q1790_CheckIfOneStringSwapCanMakeEqual
 {
+    // TC: O(n), where n is the length of s1
+    // SC: O(1), space used for c is fixed to 4
     public bool AreAlmostEqual(string s1, string s2)
     {
+        var diff = 0;
+        var c = new char[4];
+        for (var i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+
+                if (diff == 0)
+                {
+                    c[0] = s1[i];
+                    c[1] = s2[i];
+                }
+                else if (diff == 1)
+                {
+                    c[2] = s1[i];
+                    c[3] = s2[i];
+                }
+                else return false;
+                diff++;
+            }
+        }
+        if (diff == 0) return true;
+        else if (diff == 2 && c[0] == c[3] && c[1] == c[2]) return true;
         return false;
     }
 }
@@ -12,6 +37,7 @@ class Q1790_CheckIfOneStringSwapCanMakeEqualTestData : TestData
         ["bank", "kanb", true],
         ["attack", "defend", false],
         ["kelb", "kelb", true],
+        ["caa", "aaz", false],
     ];
 }
 public class Q1790_CheckIfOneStringSwapCanMakeEqualTests

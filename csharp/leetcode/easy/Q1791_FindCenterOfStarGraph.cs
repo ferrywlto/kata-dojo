88a21fe09f1ec8ceafd3c53bbad36f4a476ecb3f
Ferry To <ferrywlto@gmail.com>
2024-09-06 00:15:56 +0100

    refactor: Extract common logic to a local function

--- csharp/leetcode/easy/Q1791_FindCenterOfStarGraph.cs
+++ csharp/leetcode/easy/Q1791_FindCenterOfStarGraph.cs
@@ -7,21 +7,20 @@ class Q1791_FindCenterOfStartGraph
         var dict = new Dictionary<int, int>();
         foreach (var e in edges)
         {
-            if (dict.TryGetValue(e[0], out var first))
-            {
-                dict[e[0]] = ++first;
-                if (dict[e[0]] >= edges.Length - 1) return e[0];
-            }
-            else dict.Add(e[0], 1);
-
-            if (dict.TryGetValue(e[1], out var second))
-            {
-                dict[e[1]] = ++second;
-                if (dict[e[1]] >= edges.Length - 1) return e[1];
-            }
-            else dict.Add(e[1], 1);
+            if (Check(e[0])) return e[0];
+            if (Check(e[1])) return e[1];
         }
         return 0;
+        bool Check(int n)
+        {
+            if (dict.TryGetValue(n, out var second))
+            {
+                dict[n] = ++second;
+                if (dict[n] >= edges.Length - 1) return true;
+            }
+            else dict.Add(n, 1);
+            return false;
+        }
     }
 }
 class Q1791_FindCenterOfStartGraphTestData : TestData

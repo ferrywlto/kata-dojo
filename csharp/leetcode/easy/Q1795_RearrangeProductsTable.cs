bc1bc2cd74cb5c93dc556af16a4daa9414df442a
Ferry To <ferrywlto@gmail.com>
2024-09-06 18:09:08 +0100

    test: All test cases of Q1795 passed

--- csharp/leetcode/easy/Q1795_RearrangeProductsTable.cs
+++ csharp/leetcode/easy/Q1795_RearrangeProductsTable.cs
@@ -3,7 +3,11 @@ class Q1795_RearrangeProductsTable : SqlQuestion
 {
     public override string Query =>
     """
-    select 1;
+    select p1.product_id, 'store1' as 'store', store1 as 'price' from Products p1 where store1 is not null
+    union 
+    select p2.product_id, 'store2' as 'store', store2 as 'price' from Products p2 where store2 is not null
+    union 
+    select p3.product_id, 'store3' as 'store', store3 as 'price' from Products p3 where store3 is not null;
     """;
 }
 class Q1795_RearrangeProductsTableTestData : TestData
@@ -13,7 +17,7 @@ class Q1795_RearrangeProductsTableTestData : TestData
         [
             """
             insert into Products (product_id, store1, store2, store3) values ('0', '95', '100', '105');
-            insert into Products (product_id, store1, store2, store3) values ('1', '70', 'None', '80');
+            insert into Products (product_id, store1, store2, store3) values ('1', '70', null, '80');
             """,
             new Row[]
             {

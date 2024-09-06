e158f45caf13dd0bdf760287cf2052f08031b9fc
Ferry To <ferrywlto@gmail.com>
2024-09-06 18:38:48 +0100

    test: All test cases of Q1796 passed

--- csharp/leetcode/easy/Q1796_SecondLargestDigitInString.cs
+++ csharp/leetcode/easy/Q1796_SecondLargestDigitInString.cs
@@ -1,8 +1,29 @@
 class Q1796_SecondLargestDigitInString
 {
+    // TC: O(n), where n is length of s
+    // SC: O(1), space used is fixed
     public int SecondHighest(string s)
     {
-        return 0;
+        var secondLargest = -1;
+        var largest = -1;
+        foreach (var c in s)
+        {
+            if (c >= '0' && c <= '9')
+            {
+                var digit = c - '0';
+                if (digit > largest)
+                {
+                    secondLargest = largest;
+                    largest = digit;
+                }
+                else if (digit == largest) continue;
+                else if (digit > secondLargest)
+                {
+                    secondLargest = digit;
+                }
+            }
+        }
+        return secondLargest;
     }
 }
 class Q1796_SecondLargestDigitInStringTestData : TestData
@@ -11,6 +32,7 @@ class Q1796_SecondLargestDigitInStringTestData : TestData
     [
         ["dfa12321afd", 2],
         ["abc1111", -1],
+        ["sjhtz8344", 4],
     ];
 }
 public class Q1796_SecondLargestDigitInStringTests

46b72e043a9e1cc8e8870db349d98007cd86a35f
Ferry To <ferrywlto@gmail.com>
2024-09-07 12:08:04 +0100

    test: All test cases of Q1800 passed

--- csharp/leetcode/easy/Q1800_MaxAscendingSubarraySum.cs
+++ csharp/leetcode/easy/Q1800_MaxAscendingSubarraySum.cs
@@ -1,8 +1,24 @@
 class Q1800_MaxAscendingSubarraySum
 {
+    // TC: O(n), where n is the length of nums
+    // SC: O(1), space used is fixed
     public int MaxAscendingSum(int[] nums)
     {
-        return 0;
+        var maxSum = 0;
+        var currentSum = 0;
+        var previousNum = int.MinValue;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > previousNum) currentSum += nums[i];
+            else
+            {
+                if (currentSum > maxSum) maxSum = currentSum;
+
+                currentSum = nums[i];
+            }
+            previousNum = nums[i];
+        }
+        return currentSum > maxSum ? currentSum : maxSum;
     }
 }
 class Q1800_MaxAscendingSubarraySumTestData : TestData
@@ -12,6 +28,7 @@ class Q1800_MaxAscendingSubarraySumTestData : TestData
         [new int[] {10,20,30,5,10,50}, 65],
         [new int[] {10,20,30,40,50}, 150],
         [new int[] {12,17,15,13,10,11,12}, 33],
+        [new int[] {100,17,15,13,10,11,12,13}, 100],
     ];
 }
 public class Q1800_MaxAscendingSubarraySumTests

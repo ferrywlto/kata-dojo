ed28b2d577957008da13ef4ae852eb2d15fecc8c
Ferry To <ferrywlto@gmail.com>
2024-09-07 16:27:09 +0100

    test: All test cases of Q1805 passed

--- csharp/leetcode/easy/Q1805_NumDiffIntegersInString.cs
+++ csharp/leetcode/easy/Q1805_NumDiffIntegersInString.cs
@@ -1,8 +1,38 @@
 class Q1805_NumDiffIntegersInString
 {
+    // TC: O(n), where n is lenght of word
+    // SC: O(m), where m is unique number exists in word
     public int NumDifferentIntegers(string word)
     {
-        return 0;
+        var set = new HashSet<string>();
+        var startIdx = -1;
+        var inSequence = false;
+        for(var i=0; i<word.Length; i++)
+        {
+            if(word[i] <= '9' && !inSequence)
+            {
+                startIdx = i;
+                inSequence = true;
+            }
+            else if(word[i] > '9' && inSequence) 
+            {
+                set.Add(word[startIdx..i].TrimStart('0'));
+                inSequence = false;
+            }
+        }
+        if (inSequence)
+        {
+            set.Add(word[startIdx..].TrimStart('0'));
+        }
+        return set.Count;
+    }
+    public string removeLeadingZeros(string input)
+    {
+        for(var i=0; i<input.Length; i++)
+        {
+            if (input[i] > '0') return input[i..];
+        }
+        return "0";
     }
 }
 class Q1805_NumDiffIntegersInStringTestData : TestData
@@ -13,6 +43,8 @@ class Q1805_NumDiffIntegersInStringTestData : TestData
         ["leet1234code234", 2],
         ["a1b01c001", 1],
         ["abc", 0],
+        ["abc1", 1],
+        ["abc"+int.MaxValue, 1],
     ];
 }
 public class Q1805_NumDiffIntegersInStringTests

f7e8025605c2fe431452d347d89734eeff48b52b
Ferry To <ferrywlto@gmail.com>
2024-09-08 14:09:12 +0100

    test: All test cases of Q1816 passed

--- csharp/leetcode/easy/Q1816_TruncateSentence.cs
+++ csharp/leetcode/easy/Q1816_TruncateSentence.cs
@@ -1,14 +1,31 @@
 class Q1816_TruncateSentence
 {
-    public string TruncateSentence(string s, int k) 
+    // TC: O(n), where n is the length of s
+    // SC: O(1), space used is fixed
+    public string TruncateSentence(string s, int k)
     {
-        return string.Empty;    
-    }    
+        var endIdx = 0;
+        var wordCount = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+            {
+                wordCount++;
+                endIdx = i;
+            }
+            if (wordCount == k)
+            {
+                return s[..endIdx];
+            }
+        }
+        return s;
+    }
 }
 class Q1816_TruncateSentenceTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
+        ["Hello how are you Contestant", 3, "Hello how are"],
         ["Hello how are you Contestant", 4, "Hello how are you"],
         ["What is the solution to this problem", 4, "What is the solution"],
         ["chopper is not a tanuki", 5, "chopper is not a tanuki"],

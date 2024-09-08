cf0ca63f316b095c9f347454e12f8284ed2bfa66
Ferry To <ferrywlto@gmail.com>
2024-09-08 13:07:40 +0100

    test: All test cases of Q1812 passed

--- csharp/leetcode/easy/Q1812_DetermineColorOfChessboardSquare.cs
+++ csharp/leetcode/easy/Q1812_DetermineColorOfChessboardSquare.cs
@@ -1,17 +1,28 @@
 class Q1812_DetermineColorOfChessboardSquare
 {
-    public bool SquareIsWhite(string coordinates) 
+    // 97 - 104
+    // readonly char[] d1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    // 49 - 56
+    // readonly char[] d2 = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+    // TC: O(1), computation is fixed
+    // SC: O(1), space used is fixed
+    public bool SquareIsWhite(string coordinates)
     {
-        return false;    
-    }    
+        return (coordinates[0] % 2 == 1 && coordinates[1] % 2 == 0) || (coordinates[0] % 2 == 0 && coordinates[1] % 2 == 1);
+    }
 }
 class Q1812_DetermineColorOfChessboardSquareTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["a1", false],
         ["h3", true],
         ["c7", false],
+        ["g7", false],
+        ["e6", true],
+        ["h1", true],
+        ["d8", false],
     ];
 }
 public class Q1812_DetermineColorOfChessboardSquareTests

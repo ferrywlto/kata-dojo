f479cf6facc24885370f816b16dc3dde0d6613f8
Ferry To <ferrywlto@gmail.com>
2024-09-10 20:35:56 +0100

    test: All test cases of Q1832 passed

--- csharp/leetcode/easy/Q1832_CheckSentenceIsPangram.cs
+++ csharp/leetcode/easy/Q1832_CheckSentenceIsPangram.cs
@@ -1,13 +1,21 @@
 class Q1832_CheckSentenceIsPangram
 {
+    // TC: O(n), where n is the length of sentence
+    // SC: O(1), it is always cap at 26 characters to store 
     public bool CheckIfPangram(string sentence)
     {
+        var set = new HashSet<char>();
+        foreach (var c in sentence)
+        {
+            set.Add(c);
+            if (set.Count >= 26) return true;
+        }
         return false;
     }
 }
 class Q1832_CheckSentenceIsPangramTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["thequickbrownfoxjumpsoverthelazydog", true],
         ["leetcode", false],

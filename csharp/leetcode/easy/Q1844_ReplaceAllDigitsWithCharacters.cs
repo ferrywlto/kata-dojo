8c54417584e43d9ca6827df9d6697ca546e902f1
Ferry To <ferrywlto@gmail.com>
2024-09-11 20:42:18 +0100

    test: All test cases of Q1844 passed

--- csharp/leetcode/easy/Q1844_ReplaceAllDigitsWithCharacters.cs
+++ csharp/leetcode/easy/Q1844_ReplaceAllDigitsWithCharacters.cs
@@ -1,8 +1,17 @@
+using System.Text;
+
 class Q1844_ReplaceAllDigitsWithCharacters
 {
+    // TC: O(n), where n is length of s/2
+    // SC: O(n), where n is length of s due to string builder to hold the string
     public string ReplaceDigits(string s)
     {
-        return string.Empty;
+        var sb = new StringBuilder(s);
+        for (var i = 1; i < s.Length; i += 2)
+        {
+            sb[i] = (char)(s[i - 1] + (s[i] - 48));
+        }
+        return sb.ToString();
     }
 }
 class Q1844_ReplaceAllDigitsWithCharactersTestData : TestData
@@ -10,6 +19,7 @@ class Q1844_ReplaceAllDigitsWithCharactersTestData : TestData
     protected override List<object[]> Data =>
     [
         ["a1c1e1", "abcdef"],
+        ["a1c1e", "abcde"],
         ["a1b2c3d4e", "abbdcfdhe"],
     ];
 }

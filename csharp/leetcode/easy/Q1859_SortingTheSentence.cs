14ad32aea792616827277b8409ce7cd200d034a6
Ferry To <ferrywlto@gmail.com>
2024-09-12 22:22:32 +0100

    perf: Use array to improve lookup time and space overhead to O(1)

--- csharp/leetcode/easy/Q1859_SortingTheSentence.cs
+++ csharp/leetcode/easy/Q1859_SortingTheSentence.cs
@@ -1,28 +1,28 @@
 class Q1859_SortingTheSentences
 {
     // TC: O(n), where n is length of s
-    // SC: O(m), where m is words in s
+    // SC: O(1), the question constraints there are maximum 9 words
     public string SortSentence(string s)
     {
-        var dict = new SortedDictionary<int, string>();
+        var words = new string[9];
         var startIdx = 0;
-        for(var i=0; i<s.Length; i++)
+        for (var i = 0; i < s.Length; i++)
         {
-            if(s[i]==' ')
+            if (s[i] == ' ')
             {
                 var position = int.Parse(s[i - 1].ToString());
-                var word = s[startIdx..(i-1)];
-                dict.Add(position, word);
-                startIdx = i+1;
+                var word = s[startIdx..(i - 1)];
+                words[position] = word;
+                startIdx = i + 1;
             }
         }
-        dict.Add(int.Parse(s[^1].ToString()), s[startIdx..^1]);
-        return string.Join(' ', dict.Select(p => p.Value));
+        words[int.Parse(s[^1].ToString())] = s[startIdx..^1];
+        return string.Join(' ', words.Where(p => p is not null));
     }
 }
 class Q1859_SortingTheSentencesTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["is2 sentence4 This1 a3", "This is a sentence"],
         ["Myself2 Me1 I4 and3", "Me Myself and I"],

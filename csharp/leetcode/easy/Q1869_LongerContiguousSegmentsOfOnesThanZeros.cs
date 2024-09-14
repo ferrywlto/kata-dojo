c1ba4d769860c340b00b9dedbf439ee1b8a438cf
Ferry To <ferrywlto@gmail.com>
2024-09-14 20:23:48 +0100

    test: All test cases of Q1869 passed

--- csharp/leetcode/easy/Q1869_LongerContiguousSegmentsOfOnesThanZeros.cs
+++ csharp/leetcode/easy/Q1869_LongerContiguousSegmentsOfOnesThanZeros.cs
@@ -1,8 +1,30 @@
 class Q1869_LongerContiguousSegmentsOfOnesThanZeros
 {
+    // TC: O(n), where n is length of s
+    // SC: O(1), space used does not scale with input
     public bool CheckZeroOnes(string s)
     {
-        return false;
+        var lengthZeros = 0;
+        var lengthOnes = 0;
+        var maxLengthZeros = int.MinValue;
+        var maxLengthOnes = int.MinValue;
+        for(var i = 0; i<s.Length; i++)
+        {
+            if(s[i] == '0')
+            {
+                lengthOnes = 0;
+                lengthZeros++;
+                if (lengthZeros > maxLengthZeros) maxLengthZeros = lengthZeros;
+            }
+            else 
+            {
+                lengthZeros = 0;
+                lengthOnes++;
+                if (lengthOnes > maxLengthOnes) maxLengthOnes = lengthOnes;
+
+            }
+        }
+        return maxLengthOnes > maxLengthZeros;
     }
 }
 class Q1869_LongerContiguousSegmentsOfOnesThanZerosTestData : TestData
@@ -12,6 +34,11 @@ class Q1869_LongerContiguousSegmentsOfOnesThanZerosTestData : TestData
         ["1101", true],
         ["111000", false],
         ["110100010", false],
+        ["01111110", true],
+        ["0", false],
+        ["000", false],
+        ["1", true],
+        ["111", true],
     ];
 }
 public class Q1869_LongerContiguousSegmentsOfOnesThanZerosTests

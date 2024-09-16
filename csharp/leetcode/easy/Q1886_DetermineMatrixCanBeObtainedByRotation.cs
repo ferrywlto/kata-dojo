d52ce1a17895981b1abdf32c8fd4d105f2fc5f77
Ferry To <ferrywlto@gmail.com>
2024-09-16 14:44:31 +0100

    test: All test cases of Q1866 passed

--- csharp/leetcode/easy/Q1886_DetermineMatrixCanBeObtainedByRotation.cs
+++ csharp/leetcode/easy/Q1886_DetermineMatrixCanBeObtainedByRotation.cs
@@ -1,8 +1,45 @@
 class Q1886_DetermineMatrixCanBeObtainedByRotation
 {
+    // TC: O(n^2), where n is dimension of mat
+    // SC: O(n^2), where n scale with dimension of mat for Rotate
     public bool FindRotation(int[][] mat, int[][] target)
     {
+        for(var i=0; i<4; i++)
+        {
+            if (!Equals(mat, target))
+            {
+                mat = Rotate(mat);
+            }
+            else return true;
+        }
         return false;
+    }
+    public int[][] Rotate(int[][] source)
+    {
+        var temp = new int[source.Length][];
+        for (var i = 0; i<temp.Length; i++)
+        {
+            temp[i] = new int[source.Length];
+        }
+        for(var i=0; i<source.Length; i++)
+        {
+            for(var j=0; j<source[i].Length; j++)
+            {
+                temp[j][^(i + 1)] = source[i][j]; 
+            }
+        }
+        return temp;
+    }
+    public bool Equals(int[][] source, int[][] target)
+    {
+        for(var i=0; i<source.Length; i++)
+        {
+            for(var j=0; j<source[i].Length; j++)
+            {
+                if(source[i][j] != target[i][j]) return false; 
+            }
+        }
+        return true;
     }
 }
 class Q1886_DetermineMatrixCanBeObtainedByRotationTestData : TestData

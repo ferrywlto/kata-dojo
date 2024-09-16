d7ea8c7ffb01ec4a168b73d64cf98124da7e5180
Ferry To <ferrywlto@gmail.com>
2024-09-16 22:21:59 +0100

    perf: Improve space to O(1) with in-place rotation

--- csharp/leetcode/easy/Q1886_DetermineMatrixCanBeObtainedByRotation.cs
+++ csharp/leetcode/easy/Q1886_DetermineMatrixCanBeObtainedByRotation.cs
@@ -1,42 +1,59 @@
 class Q1886_DetermineMatrixCanBeObtainedByRotation
 {
     // TC: O(n^2), where n is dimension of mat
-    // SC: O(n^2), where n scale with dimension of mat for Rotate
+    // SC: O(1), space used does not scale with dimension of mat
     public bool FindRotation(int[][] mat, int[][] target)
     {
-        for(var i=0; i<4; i++)
+        for (var i = 0; i < 4; i++)
         {
             if (!Equals(mat, target))
             {
-                mat = Rotate(mat);
+                Rotate(mat);
             }
             else return true;
         }
         return false;
     }
-    public int[][] Rotate(int[][] source)
+    public void Rotate(int[][] mat)
     {
-        var temp = new int[source.Length][];
-        for (var i = 0; i<temp.Length; i++)
+        int n = mat.Length; // Get the dimension of the matrix
+
+        // Loop through each layer
+        for (int i = 0; i < n / 2; i++)
         {
-            temp[i] = new int[source.Length];
-        }
-        for(var i=0; i<source.Length; i++)
-        {
-            for(var j=0; j<source[i].Length; j++)
+            Console.WriteLine($"======");
+            // Loop through each element in the current layer
+            for (int j = i; j < n - i - 1; j++)
             {
-                temp[j][^(i + 1)] = source[i][j]; 
+                Console.WriteLine($"------");
+
+                // Save the top element
+                int temp = mat[i][j];
+                Console.WriteLine($"[{i},{j}] <= [{n - j - 1},{i}]");
+                // Move left element to top
+                mat[i][j] = mat[n - j - 1][i];
+
+                Console.WriteLine($"[{n - j - 1},{i}] <= [{n - i - 1},{n - j - 1}]");
+                // Move bottom element to left
+                mat[n - j - 1][i] = mat[n - i - 1][n - j - 1];
+
+                Console.WriteLine($"[{n - i - 1},{n - j - 1}] <= [{j},{n - i - 1}]");
+                // Move right element to bottom
+                mat[n - i - 1][n - j - 1] = mat[j][n - i - 1];
+
+                Console.WriteLine($"[{j},{n - i - 1}] <= [{i},{j}]");
+                // Move top element to right
+                mat[j][n - i - 1] = temp;
             }
         }
-        return temp;
     }
     public bool Equals(int[][] source, int[][] target)
     {
-        for(var i=0; i<source.Length; i++)
+        for (var i = 0; i < source.Length; i++)
         {
-            for(var j=0; j<source[i].Length; j++)
+            for (var j = 0; j < source[i].Length; j++)
             {
-                if(source[i][j] != target[i][j]) return false; 
+                if (source[i][j] != target[i][j]) return false;
             }
         }
         return true;

d975b6f299c394d6e73064998ce93c39fa32b3c6
Ferry To <ferrywlto@gmail.com>
2024-09-18 19:22:06 +0100

    test: All test cases of Q1903 passed

--- csharp/leetcode/easy/Q1903_LargestOddNumberInString.cs
+++ csharp/leetcode/easy/Q1903_LargestOddNumberInString.cs
@@ -1,13 +1,22 @@
 class Q1903_LargestOddNumberInString
 {
+    // TC: O(n), where n is the length of num worst case (no odd number found)
+    // SC: O(1), space used does not scale with num 
     public string LargestOddNumber(string num)
     {
+        for (var i = num.Length - 1; i >= 0; i--)
+        {
+            if (num[i] % 2 == 1)
+            {
+                return num[..(i + 1)];
+            }
+        }
         return string.Empty;
     }
 }
 class Q1903_LargestOddNumberInStringTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["52", "5"],
         ["4206", ""],

69bf88f0f2a7f270b039cb1e4a01a4067dd7a5c5
Ferry To <ferrywlto@gmail.com>
2024-09-19 13:51:02 +0100

    test: Add official test cases of Q1920

---
+++ csharp/leetcode/easy/Q1920_BuildArrayFromPermutation.cs
@@ -0,0 +1,26 @@
+class Q1920_BuildArrayFromPermutation
+{
+    public int[] BuildArray(int[] nums)
+    {
+        return [];
+    }
+}
+class Q1920_BuildArrayFromPermutationTestData : TestData
+{
+    protected override List<object[]> Data =>
+    [
+        [new int[] {0,2,1,5,3,4}, new int[] {0,1,2,4,5,3}],
+        [new int[] {5,0,1,2,3,4}, new int[] {4,5,0,1,2,3}],
+    ];
+}
+public class Q1920_BuildArrayFromPermutationTests
+{
+    [Theory]
+    [ClassData(typeof(Q1920_BuildArrayFromPermutationTestData))]
+    public void OfficialTestCases(int[] input, int[] expected)
+    {
+        var sut = new Q1920_BuildArrayFromPermutation();
+        var actual = sut.BuildArray(input);
+        Assert.Equal(expected, actual);
+    }
+}

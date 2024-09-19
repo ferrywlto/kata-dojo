b08d21196519535a4e3671b75a024edfbb8c93a2
Ferry To <ferrywlto@gmail.com>
2024-09-19 14:25:33 +0100

    test: All test cases of Q1920 passed

--- csharp/leetcode/easy/Q1920_BuildArrayFromPermutation.cs
+++ csharp/leetcode/easy/Q1920_BuildArrayFromPermutation.cs
@@ -1,16 +1,25 @@
 class Q1920_BuildArrayFromPermutation
 {
+    // TC: O(n), where n is length of nums
+    // SC: O(n), where n scale with length of nums
     public int[] BuildArray(int[] nums)
     {
-        return [];
+        var ans = new int[nums.Length];
+        for(var i=0; i<nums.Length; i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
     }
 }
 class Q1920_BuildArrayFromPermutationTestData : TestData
 {
     protected override List<object[]> Data =>
     [
-        [new int[] {0,2,1,5,3,4}, new int[] {0,1,2,4,5,3}],
-        [new int[] {5,0,1,2,3,4}, new int[] {4,5,0,1,2,3}],
+        [new int[] {0,2,1,5,3,4}, 
+         new int[] {0,1,2,4,5,3}],
+        [new int[] {5,0,1,2,3,4}, 
+         new int[] {4,5,0,1,2,3}],
     ];
 }
 public class Q1920_BuildArrayFromPermutationTests

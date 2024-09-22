ce991197165a5594370fd6b900904ac732bea85a
Ferry To <ferrywlto@gmail.com>
2024-09-22 18:56:35 +0100

    test: All test cases of Q1945 passed

--- csharp/leetcode/easy/Q1945_SumOfDigitsOfStringAfterConvert.cs
+++ csharp/leetcode/easy/Q1945_SumOfDigitsOfStringAfterConvert.cs
@@ -1,13 +1,41 @@
+using System.Text;
+
 class Q1945_SumOfDigitsOfStringAfterConvert
 {
+    // TC: O(n), where n scale with length of s times k
+    // SC: O(n), where n scale with length of s
     public int GetLucky(string s, int k)
     {
-        return 0;
+        var sb = new StringBuilder();
+        foreach (var c in s) sb.Append(GetCharValue(c));
+        Console.WriteLine($"input: {sb}");
+        var sum = 0;
+        var runCount = 0;
+        while (runCount < k)
+        {
+            var intStr = sb.ToString();
+
+            sb.Clear();
+            Console.WriteLine($"loop {runCount}: {intStr}");
+            var innerSum = 0;
+            foreach (var c in intStr)
+            {
+                var num = c - '0';
+                Console.WriteLine($"c: {c} v: {num}");
+                innerSum += num;
+            }
+            sum = innerSum;
+            sb.Append(innerSum);
+            runCount++;
+        }
+        Console.WriteLine($"final: {sb}");
+        return sum;
     }
+    public int GetCharValue(char c) => c - 'a' + 1;
 }
 class Q1945_SumOfDigitsOfStringAfterConvertTestData : TestData
 {
-    protected override List<object[]> Data => 
+    protected override List<object[]> Data =>
     [
         ["iiii", 1, 36],
         ["leetcode", 2, 6],

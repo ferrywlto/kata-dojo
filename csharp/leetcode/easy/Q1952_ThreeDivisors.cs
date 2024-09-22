d7362ec0ef22d617952f19624def2b1302257068
Ferry To <ferrywlto@gmail.com>
2024-09-23 00:04:15 +0100

    test: All test cases of Q1952 passed

--- csharp/leetcode/easy/Q1952_ThreeDivisors.cs
+++ csharp/leetcode/easy/Q1952_ThreeDivisors.cs
@@ -1,15 +1,31 @@
-public class Q1952_ThreeDivisors(ITestOutputHelper output)
+public class Q1952_ThreeDivisors
 {
+    // TC: O(n), n scale with size of n
+    // SC: O(1), space used does not sacle with input
     public bool IsThree(int n)
     {
-        output.WriteLine("test");
-        return false;
+        if (n % 2 == 0) return n == 4;
+
+        var divideCount = 0;
+        var limit = (int)Math.Ceiling(Math.Sqrt(n));
+        for (var i = 3; i <= limit; i += 2)
+        {
+            if (n % i == 0)
+            {
+                if (i * i == n) divideCount++;
+                else return false;
+            }
+        }
+        return divideCount == 1;
     }
 
     public static List<object[]> TestData =>
     [
         [2, false],
         [4, true],
+        [13, false],
+        [9, true],
+        [33, false],
     ];
 
     [Theory]

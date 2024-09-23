bf7def277fe3003e5b206ced4bdb55a2d6930d4d
Ferry To <ferrywlto@gmail.com>
2024-09-23 11:14:21 +0100

    test: All test cases of Q1957 passed

--- csharp/leetcode/easy/Q1957_DeleteCharactersToMakeFancyString.cs
+++ csharp/leetcode/easy/Q1957_DeleteCharactersToMakeFancyString.cs
@@ -1,8 +1,44 @@
+using System.Text;
+
 public class Q1957_DeleteCharactersToMakeFancyString
 {
     public string MakeFancyString(string s)
     {
-        return string.Empty;
+        if (s.Length <= 2) return s;
+
+        var lastChar = s[0];
+        var reoccurance = 1;
+        var sb = new StringBuilder();
+        sb.Append(s[0]);
+
+        if (s[1] == lastChar) reoccurance++;
+        else
+        {
+            reoccurance = 1;
+            lastChar = s[1];
+        }
+        sb.Append(s[1]);
+
+        for (var i = 2; i < s.Length; i++)
+        {
+            if (s[i] == lastChar)
+            {
+                if (reoccurance == 2) continue;
+                else
+                {
+                    reoccurance++;
+                    sb.Append(s[i]);
+                }
+            }
+            else
+            {
+                lastChar = s[i];
+                reoccurance = 1;
+                sb.Append(s[i]);
+            }
+        }
+
+        return sb.ToString();
     }
 
     public static IEnumerable<object[]> TestData =>

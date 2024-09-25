1acba67b507f511306172b9d961d30ffb6d52fcd
Ferry To <ferrywlto@gmail.com>
2024-09-25 10:48:16 +0100

    test: All test cases of Q1965 passed

--- csharp/leetcode/easy/Q1965_EmployeesWithMissingInformation.cs
+++ csharp/leetcode/easy/Q1965_EmployeesWithMissingInformation.cs
@@ -1,22 +1,34 @@
 public class Q1965_EmployeesWithMissingInformation(ITestOutputHelper output) : SqlTest(output)
 {
-    public static string Query => "select 1;";
+    public static string Query => 
+    """
+    SELECT employee_id FROM 
+    Employees e 
+    WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+
+    UNION
+
+    SELECT employee_id FROM 
+    Salaries s
+    WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+    
+    ORDER BY employee_id
+    ;
+    """;
     public static List<object[]> TestData =>
-    [
-        [
-            """
-            insert into Employees (employee_id, name) values 
-            ('2', 'Crew'),
-            ('4', 'Haven'),
-            ('5', 'Kristian');
-            insert into Salaries (employee_id, salary) values 
-            ('5', '76071'),
-            ('1', '22517'),
-            ('4', '63539');
-            """,
-            new int[] {1,2}
-        ]
-    ];
+    [[
+        """
+        insert into Employees (employee_id, name) values 
+        ('2', 'Crew'),
+        ('4', 'Haven'),
+        ('5', 'Kristian');
+        insert into Salaries (employee_id, salary) values 
+        ('5', '76071'),
+        ('1', '22517'),
+        ('4', '63539');
+        """,
+        new int[] {1,2}
+    ]];
 
     protected override string TestSchema =>
     """

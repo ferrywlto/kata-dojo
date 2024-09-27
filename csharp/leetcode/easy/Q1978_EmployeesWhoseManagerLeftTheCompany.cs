cdd190247d212afc9f4fef1128d9dc40a035c079
Ferry To <ferrywlto@gmail.com>
2024-09-27 12:05:35 +0100

    test: All test cases of Q1978 passed

--- csharp/leetcode/easy/Q1978_EmployeesWhoseManagerLeftTheCompany.cs
+++ csharp/leetcode/easy/Q1978_EmployeesWhoseManagerLeftTheCompany.cs
@@ -17,9 +17,42 @@ public class Q1978_EmployeesWhoseManagerLeftTheCompany(ITestOutputHelper output)
             ('11', 'Joziah', '6', '28485');
             """,
             new int[] {11},
+        ],
+        [
+            """
+            insert into Employees (employee_id, name, manager_id, salary) values 
+            ('18', 'Drew', NULL, '41568'),
+            ('20', 'Ronan', '3', '65209'),
+            ('10', 'Jaxton', '15', '96667'),
+            ('13', 'Louie', '16', '6801'),
+            ('17', 'Mylah', '20', '26540'),
+            ('21', 'Kenia', '15', '98690'),
+            ('7', 'Hadley', '6', '23590'),
+            ('9', 'Hayden', '4', '90798'),
+            ('2', 'Nixon', NULL, '25560'),
+            ('8', 'Arthur', '11', '67027'),
+            ('11', 'Brycen', NULL, '42570'),
+            ('3', 'Noemi', NULL, '87321'),
+            ('14', 'Hayden', NULL, '4123'),
+            ('19', 'Astrid', '20', '37680');
+            """,
+            new int[] {7,13}
         ]
     ];
-    private string Query => "select 1;";
+    private string Query =>
+    """
+    select e1.employee_id from Employees e1
+    where e1.manager_id is not null
+    left join 
+    (select employee_id, name from Employees) e2
+    on e1.manager_id = e2.employee_id
+    where 
+    e1.manager_id is not null 
+    and e2.employee_id is null
+    and e1.salary < 30000
+    order by e1.employee_id;
+    """;
+
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(string testDataSql, int[] expected)

1e3181f8f372bac425889081a5e4a2d10eb9005e
Ferry To <ferrywlto@gmail.com>
2024-09-27 19:05:28 +0100

    test: All test cases of Q1979 passed

--- csharp/leetcode/easy/Q1979_FindGreatestCommonDivisorOfArray.cs
+++ csharp/leetcode/easy/Q1979_FindGreatestCommonDivisorOfArray.cs
@@ -1,14 +1,33 @@
 public class Q1979_FindGreatestCommonDivisorOfArray
 {
+    // TC: O(nlogn), due to Array.Sort()
+    // SC: O(1), space used does not scale with nums 
     public int FindGCD(int[] nums)
     {
-        return 0;
+        Array.Sort(nums);
+        if (nums[^1] % nums[0] == 0) 
+            return nums[0];
+
+        var largest = 1;
+        for (var i = 2; i * i <= nums[0]; i++)
+        {
+            if (nums[0] % i == 0)
+            {
+                if (nums[^1] % i == 0 && i > largest)
+                    largest = i;
+                var residue = nums[0] / i;
+                if (nums[^1] % residue == 0 && residue > largest)
+                    largest = residue;
+            }
+        }
+        return largest;
     }
     public static List<object[]> TestData =>
     [
         [new int[]{2,5,6,9,10}, 2],
-        [new int[]{7,5,6,8,3}, 3],
+        [new int[]{7,5,6,8,3}, 1],
         [new int[]{3,3}, 3],
+        [new int[]{6,7,9}, 3],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

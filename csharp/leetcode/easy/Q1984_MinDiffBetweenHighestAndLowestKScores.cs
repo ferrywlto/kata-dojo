988c12fec7150d98128ed045b22586a8c4b71581
Ferry To <ferrywlto@gmail.com>
2024-09-28 12:07:14 +0100

    test: All test cases of Q1984 passed

--- csharp/leetcode/easy/Q1984_MinDiffBetweenHighestAndLowestKScores.cs
+++ csharp/leetcode/easy/Q1984_MinDiffBetweenHighestAndLowestKScores.cs
@@ -1,13 +1,26 @@
 public class Q1984_MinDiffBetweenHighestAndLowestKScores
 {
+
+    // TC: O(n log n), due to Array.Sort() + (n - k) 
+    // SC: O(1), space used does not scale with nums or k
     public int MinimumDifference(int[] nums, int k)
     {
-        return 0;
+        if (k == 1) return 0;
+
+        Array.Sort(nums);
+        var min = int.MaxValue;
+        for (var i = 0; i < nums.Length - k + 1; i++)
+        {
+            var diff = Math.Abs(nums[i] - nums[i + k - 1]);
+            if (diff < min) min = diff;
+        }
+        return min;
     }
     public static List<object[]> TestData =>
     [
         [new int[] {90}, 1, 0],
         [new int[] {9,4,1,7}, 2, 2],
+        [new int[] {87063,61094,44530,21297,95857,93551,9918}, 6, 74560],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

f80b181062ac46a28c8b2c33e84049d7523e10c6
Ferry To <ferrywlto@gmail.com>
2024-09-29 12:38:52 +0100

    test: All test cases of Q1991 passed

--- csharp/leetcode/easy/Q1991_FindMiddleIndexInArray.cs
+++ csharp/leetcode/easy/Q1991_FindMiddleIndexInArray.cs
@@ -1,14 +1,26 @@
 public class Q1991_FindMiddleIndexInArray
 {
+    // TC: O(n), n scale with length of nums, n for first sum, another n to find the middle idx
+    // SC: O(1), space used does not sacle with nums
     public int FindMiddleIndex(int[] nums)
     {
-        return 0;
+        if (nums.Length == 1) return 0;
+        var total = nums.Sum();
+        var currentSum = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            total -= nums[i];
+            if (currentSum == total) return i;
+            currentSum += nums[i];
+        }
+        return -1;
     }
     public static List<object[]> TestData =>
     [
         [new int[] {2,3,-1,8,4}, 3],
         [new int[] {1,-1,4}, 2],
         [new int[] {2,5}, -1],
+        [new int[] {9}, 0],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

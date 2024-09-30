1f669faf0c5ba9dfdf1f9dc566cdf5992148d304
Ferry To <ferrywlto@gmail.com>
2024-09-30 18:15:55 +0100

    test: All test cases of Q2000 passed

--- csharp/leetcode/easy/Q2000_ReversePrefixOfWord.cs
+++ csharp/leetcode/easy/Q2000_ReversePrefixOfWord.cs
@@ -1,8 +1,28 @@
+using System.Text;
+
 public class Q2000_PreversePrefixOfWord
 {
+    // TC: O(n), where n sacle with length of word.
+    // SC: O(n), where n scale with length of word for StringBuilder to hold the result.
     public string ReversePrefix(string word, char ch)
     {
-        return string.Empty;
+        for (var i = 0; i < word.Length; i++)
+        {
+            if (word[i] == ch)
+            {
+                var sb = new StringBuilder(word.Length);
+                for (var j = i; j >= 0; j--)
+                {
+                    sb.Append(word[j]);
+                }
+                for (var k = i + 1; k < word.Length; k++)
+                {
+                    sb.Append(word[k]);
+                }
+                return sb.ToString();
+            }
+        }
+        return word;
     }
     public static List<object[]> TestData =>
     [

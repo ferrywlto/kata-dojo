49097d174f06311770db7c8dc0424c6c19241c27
Ferry To <ferrywlto@gmail.com>
2024-09-30 18:36:22 +0100

    test: All test cases of Q2006 passed

--- csharp/leetcode/easy/Q2006_CountNumberOfPairsWithAbsoluteDifference.cs
+++ csharp/leetcode/easy/Q2006_CountNumberOfPairsWithAbsoluteDifference.cs
@@ -1,14 +1,26 @@
 public class Q2006_CountNumberOfPairsWithAbsoluteDifference
 {
+    // TC: O(n), where n sacle with length of nums
+    // SC: O(1), space used does not scale with input
     public int CountKDifference(int[] nums, int k)
     {
-        return 0;
+        if (nums.Length == 1) return 0;
+        var result = 0;
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (Math.Abs(nums[i] - nums[j]) == k) result++;
+            }
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         [new int[] {1,2,2,1}, 1, 4],
         [new int[] {1,3}, 3, 0],
         [new int[] {3,2,1,5,4}, 2, 3],
+        [new int[] {1}, 1, 0],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

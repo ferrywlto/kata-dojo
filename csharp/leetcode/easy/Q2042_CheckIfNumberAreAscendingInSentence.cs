6bd1e7036f7db354df33179116829a4b42be100e
Ferry To <ferrywlto@gmail.com>
2024-10-03 19:03:39 +0100

    test: All test cases of Q2042 passed

--- csharp/leetcode/easy/Q2042_CheckIfNumberAreAscendingInSentence.cs
+++ csharp/leetcode/easy/Q2042_CheckIfNumberAreAscendingInSentence.cs
@@ -1,8 +1,33 @@
 public class Q2042_CheckIfNumberAreAscendingInSentence
 {
-    public bool AreNumbersAscending(string s) 
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used does not scale with input
+    public bool AreNumbersAscending(string s)
     {
-        return false;    
+        var currNum = -1;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            if (c >= '0' && c <= '9')
+            {
+                if (i < s.Length - 1 && s[i + 1] >= '0' && s[i + 1] <= '9')
+                {
+                    var num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+                    i++;
+                    if (num > currNum) currNum = num;
+                    else return false;
+                }
+                else
+                {
+                    var num = s[i] - '0';
+                    if (num > currNum) currNum = num;
+                    else return false;
+                }
+            }
+        }
+        if (currNum != -1) return true;
+        return false;
     }
     public static List<object[]> TestData =>
     [

ced6e9de42b6041d099945b46fef8c0eb3266d4a
Ferry To <ferrywlto@gmail.com>
2024-10-06 16:59:50 +0100

    test: All test cases of Q2062 passed

--- csharp/leetcode/easy/Q2062_CountVowelSubstringsOfString.cs
+++ csharp/leetcode/easy/Q2062_CountVowelSubstringsOfString.cs
@@ -1,14 +1,42 @@
+using System.Text;
+
 public class Q2062_CountVowelSubstringsOfString
 {
+    readonly HashSet<char> vowels = ['a', 'e', 'i', 'o', 'u'];
+
     public int CountVowelSubstrings(string word)
     {
-        return 0;
+        if (word.Length < 5) return 0;
+
+        var result = 0;
+        for(var len=word.Length; len>=5; len--)
+        {
+            for(var i=0; i<word.Length - len + 1; i++)
+            {
+                var hashSet = new HashSet<char>();
+                var valid = true;
+                for (var j = i; j < i + len; j++)
+                {
+                    var c = word[j];
+                    if (!vowels.Contains(c))
+                    {
+                        valid = false;
+                        break;
+                    }
+                    hashSet.Add(c);
+                }
+                if (valid && hashSet.Count == 5) result++;
+            }
+
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         ["aeiouu", 2],
         ["unicornarihan", 0],
         ["cuaieuouac", 7],
+        ["cuaieuouacaeiou", 8],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

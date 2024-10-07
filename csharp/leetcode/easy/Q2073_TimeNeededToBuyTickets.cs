4a1a869a50be3702a48c49bc0bb0fc3267bb5576
Ferry To <ferrywlto@gmail.com>
2024-10-07 15:55:41 +0100

    test: All test cases of Q2073 passed

--- csharp/leetcode/easy/Q2073_TimeNeededToBuyTickets.cs
+++ csharp/leetcode/easy/Q2073_TimeNeededToBuyTickets.cs
@@ -1,8 +1,23 @@
 public class Q2073_TimeNeededToBuyTickets
 {
+    // TC: O(n), n scale with length of tickets
+    // SC: O(1), space used does not scale with input 
     public int TimeRequiredToBuy(int[] tickets, int k)
     {
-        return 0;
+        var pointerIdx = k;
+        var result = k;
+        for (var i = 0; i < k; i++) tickets[i]--;
+
+        while (tickets[k] > 0)
+        {
+            if (tickets[pointerIdx] > 0)
+            {
+                tickets[pointerIdx]--;
+                result++;
+            }
+            pointerIdx = (pointerIdx + 1) % tickets.Length;
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [

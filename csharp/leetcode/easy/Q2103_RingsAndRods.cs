58b2bc4a06ac0d3239474a0dcf6b9cebe615be12
Ferry To <ferrywlto@gmail.com>
2024-10-09 23:28:55 +0100

    test: All test cases of Q2103 passed

--- csharp/leetcode/easy/Q2103_RingsAndRods.cs
+++ csharp/leetcode/easy/Q2103_RingsAndRods.cs
@@ -1,8 +1,24 @@
 public class Q2103_RingsAndRods
 {
-    public int CountPoints(string rings) 
+    // TC: O(n), n scale with length of rings
+    // SC: O(1), as space used in dictionary will never exceed 10 items, worst case always 10 entries with a 3 entries hashset 
+    public int CountPoints(string rings)
     {
-        return 0;    
+        var rods = new Dictionary<int, HashSet<char>>();
+        var result = 0;
+        for (var i = 1; i < rings.Length; i += 2)
+        {
+            var rodId = rings[i];
+            var color = rings[i - 1];
+            if (rods.TryGetValue(rodId, out var colors))
+            {
+                if (colors.Count == 3) continue;
+                colors.Add(color);
+                if (colors.Count == 3) result++;
+            }
+            else rods.Add(rodId, [color]);
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [

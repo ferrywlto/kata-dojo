c20a5ebe1f73aba78c6c0cd9a02b4c6e597f2a0e
Ferry To <ferrywlto@gmail.com>
2024-10-10 12:15:57 +0100

    test: All test cases of Q2114 passed

--- csharp/leetcode/easy/Q2114_MaxNumWordsFoundInSentences.cs
+++ csharp/leetcode/easy/Q2114_MaxNumWordsFoundInSentences.cs
@@ -1,22 +1,34 @@
 public class Q2114_MaxNumWordsFOundInSentences
 {
+    // TC: O(n), n scale with length of sentences
+    // SC: O(n), as each sentence need to split into array to count words
     public int MostWordsFound(string[] sentences)
     {
-        return 0;
+        var maxWords = 0;
+        foreach(var s in sentences)
+        {
+            var words = s.Split(' ').Length;
+            if (words> maxWords) maxWords = words;
+        }
+        return maxWords;
     }
     public static List<object[]> TestData =>
     [
-        [new string[] {
-            "alice and bob love leetcode",
-            "i think so too",
-            "this is great thanks very much",
-        }],
         [
-            new string[] {
+            new string[] 
+            {
+                "alice and bob love leetcode",
+                "i think so too",
+                "this is great thanks very much",
+            }, 6
+        ],
+        [
+            new string[] 
+            {
                 "please wait",
                 "continue to fight",
                 "continue to win"
-            }
+            }, 3
         ],
     ];
     [Theory]

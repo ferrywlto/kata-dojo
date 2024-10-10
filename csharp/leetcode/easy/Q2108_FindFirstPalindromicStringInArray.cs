8366f3a9c126e68ba8e7f15a7533e9c54064f8d3
Ferry To <ferrywlto@gmail.com>
2024-10-10 12:06:32 +0100

    test: All test cases of Q2108 passed

--- csharp/leetcode/easy/Q2108_FindFirstPalindromicStringInArray.cs
+++ csharp/leetcode/easy/Q2108_FindFirstPalindromicStringInArray.cs
@@ -1,7 +1,22 @@
 public class Q2108_FindFirstPalindromicStringInArray
 {
+    // TC: O(n), n scales with length of words
+    // SC: O(1), space used does not scale with input
     public string FirstPalindrome(string[] words)
     {
+        foreach (var w in words)
+        {
+            var isPalindromic = true;
+            for (var i = 0; i < w.Length / 2; i++)
+            {
+                if (w[i] != w[^(i + 1)])
+                {
+                    isPalindromic = false;
+                    break;
+                }
+            }
+            if (isPalindromic) return w;
+        }
         return string.Empty;
     }
     public static List<object[]> TestData =>

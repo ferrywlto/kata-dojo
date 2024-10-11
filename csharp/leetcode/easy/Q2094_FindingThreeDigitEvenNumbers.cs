7a42ba8c7517e7784bd12d8cefea26d64e028366
Ferry To <ferrywlto@gmail.com>
2024-10-12 00:34:34 +0100

    perf: Further slightly improve space and time by using list and backtracking technique instead of create and copy new array

--- csharp/leetcode/easy/Q2094_FindingThreeDigitEvenNumbers.cs
+++ csharp/leetcode/easy/Q2094_FindingThreeDigitEvenNumbers.cs
@@ -1,25 +1,31 @@
 public class Q2094_FindingThreeDigitEvenNumbers(ITestOutputHelper output)
 {
-    int[]? _sharedDigits = null;
+    int[]? _d = null;
     readonly HashSet<int> result = [];
     public int[] FindEvenNumbers(int[] digits)
     {
-        _sharedDigits = digits;
+        _d = digits;
         RecursiveInnerProduct(digits, 0, 3, []);
-        // var l = result.Select(x => string.Join(',', x));
-        // output.WriteLine(string.Join(Environment.NewLine, l));
+        var l = result.Select(x => string.Join(',', x));
+        output.WriteLine(string.Join(Environment.NewLine, l));
 
         var sorted = result
             .Order()
             .ToArray();
         return sorted;
     }
+    private int ConstructThreeDigitNumberFromDigits(List<int> input)
+    {
+        return _d![input[0]] * 100 +
+        _d[input[1]] * 10 +
+        _d[input[2]];
+    }
 
-    private void RecursiveInnerProduct(int[] input, int depth, int depthMax, int[] list)
+    private void RecursiveInnerProduct(int[] input, int depth, int depthMax, List<int> list)
     {
         if (depth == depthMax)
         {
-            result.Add(_sharedDigits![list[0]]*100 + _sharedDigits[list[1]]* 10 + _sharedDigits[list[2]]);
+            result.Add(ConstructThreeDigitNumberFromDigits(list));
             return;
         }
 
@@ -30,10 +36,9 @@ public class Q2094_FindingThreeDigitEvenNumbers(ITestOutputHelper output)
             else if (depth == depthMax - 1 && n % 2 != 0) continue;
             else if (list.Contains(i)) continue;
 
-            var newList = new int[list.Length + 1];
-            Array.Copy(list, newList, list.Length);
-            newList[^1] = i;
-            RecursiveInnerProduct(input, depth + 1, depthMax, newList);
+            list.Add(i);
+            RecursiveInnerProduct(input, depth + 1, depthMax, list);
+            list.RemoveAt(list.Count - 1);
         }
     }
     public static List<object[]> TestData =>

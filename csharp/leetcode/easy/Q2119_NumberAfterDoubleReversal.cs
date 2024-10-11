8e326ccd36b662b6d6a526050f85cf8de716a500
Ferry To <ferrywlto@gmail.com>
2024-10-11 22:48:27 +0100

    test: All test cases of Q2119 passed

--- csharp/leetcode/easy/Q2119_NumberAfterDoubleReversal.cs
+++ csharp/leetcode/easy/Q2119_NumberAfterDoubleReversal.cs
@@ -1,14 +1,23 @@
 public class Q2119_NumberAfterDoubleReversal
 {
+    // TC: O(1) obviously
+    // SC: O(1) obviously
     public bool IsSameAfterReversals(int num)
     {
-        return false;
+        if (num < 10) return true;
+
+        return num % 10 != 0;
     }
     public static List<object[]> TestData =>
     [
         [526, true],
         [1800, false],
         [0, true],
+        [10, false],
+        [12, true],
+        [110, false],
+        [111, true],
+        [101, true],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

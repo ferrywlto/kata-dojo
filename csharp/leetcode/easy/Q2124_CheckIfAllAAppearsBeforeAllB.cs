9f03e1fe45dd3df3bdbe8a2bb58f373c33c15972
Ferry To <ferrywlto@gmail.com>
2024-10-11 23:00:00 +0100

    test: All test cases of Q2124 passed

--- csharp/leetcode/easy/Q2124_CheckIfAllAAppearsBeforeAllB.cs
+++ csharp/leetcode/easy/Q2124_CheckIfAllAAppearsBeforeAllB.cs
@@ -1,14 +1,28 @@
 public class Q2124_CheckIfAllAAppearsBeforeAllB
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used does not scale with input 
     public bool CheckString(string s)
     {
-        return false;
+        var lastAIdx = -1;
+        var firstBIdx = int.MaxValue;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'a')
+            {
+                if (firstBIdx == int.MaxValue) lastAIdx = i;
+                else return false;
+            }
+            else if (firstBIdx == int.MaxValue) firstBIdx = i;
+        }
+        return lastAIdx < firstBIdx;
     }
     public static List<object[]> TestData =>
     [
         ["aaabbb", true],
         ["abab", false],
         ["bbb", true],
+        ["aaa", true],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

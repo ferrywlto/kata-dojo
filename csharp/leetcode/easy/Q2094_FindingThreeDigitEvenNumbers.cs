92d62b9a3ae41e80760492bfe6e7aaaa58ab4f2f
Ferry To <ferrywlto@gmail.com>
2024-10-12 00:15:23 +0100

    test: All test cases of Q2094 passed

--- csharp/leetcode/easy/Q2094_FindingThreeDigitEvenNumbers.cs
+++ csharp/leetcode/easy/Q2094_FindingThreeDigitEvenNumbers.cs
@@ -1,8 +1,41 @@
-public class Q2094_FindingThreeDigitEvenNumbers
+public class Q2094_FindingThreeDigitEvenNumbers(ITestOutputHelper output)
 {
-    public int[] FindEvenNumbers(int[] digits) 
+    public int[] FindEvenNumbers(int[] digits)
     {
-        return [];    
+        RecursiveInnerProduct(digits, 0, 3, []);
+        var l = result.Select(x => string.Join(',', x.Select(x => digits[x]).ToArray()));
+        output.WriteLine(string.Join(Environment.NewLine, l));
+
+        var sorted = result
+            .Select(x => digits[x[0]] * 100 + digits[x[1]] * 10 + digits[x[2]])
+            .Order()
+            .Distinct()
+            .ToArray();
+        return sorted;
+    }
+
+    // This is for reference, will make it to use generic 
+    List<int[]> result = [];
+    private void RecursiveInnerProduct(int[] input, int depth, int depthMax, int[] list)
+    {
+        if (depth == depthMax)
+        {
+            result.Add(list);
+            return;
+        }
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var n = input[i];
+            if (depth == 0 && n == 0) continue;
+            else if (depth == depthMax - 1 && n % 2 != 0) continue;
+            else if (list.Contains(i)) continue;
+
+            var newList = new int[list.Length + 1];
+            Array.Copy(list, newList, list.Length);
+            newList[^1] = i;
+            RecursiveInnerProduct(input, depth + 1, depthMax, newList);
+        }
     }
     public static List<object[]> TestData =>
     [

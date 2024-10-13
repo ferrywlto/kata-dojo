c3c422c9e4eacc65806a6c937e3222fb6589fb08
Ferry To <ferrywlto@gmail.com>
2024-10-13 18:56:26 +0100

    perf: Use priority queue correctly

--- csharp/leetcode/easy/Q2099_FindSubsequenceOfLengthKWithLargestSum.cs
+++ csharp/leetcode/easy/Q2099_FindSubsequenceOfLengthKWithLargestSum.cs
@@ -1,13 +1,59 @@
 public class Q2099_FindSubsequenceOfLengthKWithLargestSum
 {
+    // TC: O(nlogn), n scale with length of nums, adding all to PriorityQueue is n log n.
+    // SC: O(k), k scale with k
+
+    // Correct implementation using PriorityQueue
     public int[] MaxSubsequence(int[] nums, int k)
     {
+        var pq = new PriorityQueue<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            pq.Enqueue(i, -nums[i]);
+        }
+        var list = new List<int>();
+        while (pq.Count > 0 && list.Count < k)
+        {
+            list.Add(pq.Dequeue());
+        }
+        return list
+            .OrderBy(x => x)
+            .Select(x => nums[x])
+            .ToArray();
+    }
+    // Slower because of array transforming and then sort 
+    public int[] MaxSubsequence_Simpler(int[] nums, int k)
+    {
+        var temp = nums
+            .Select((x, idx) => (x, idx))
+            .ToArray();
+
         var comparer = Comparer<(int val, int idx)>
-        .Create((a, b) => {
+        .Create((a, b) =>
+        {
             var result = a.val.CompareTo(b.val);
             // if a == b, return index comparison
-            return result == 0 
-                ? a.idx.CompareTo(b.idx) 
+            return result == 0
+                ? a.idx.CompareTo(b.idx)
+                : result;
+        });
+        Array.Sort(temp, comparer);
+        var result = temp[^k..];
+        return result
+            .OrderBy(x => x.idx)
+            .Select(x => x.x)
+            .ToArray();
+    }
+    // example for using SortedSet 
+    public int[] MaxSubsequence_SortedSet(int[] nums, int k)
+    {
+        var comparer = Comparer<(int val, int idx)>
+        .Create((a, b) =>
+        {
+            var result = a.val.CompareTo(b.val);
+            // if a == b, return index comparison
+            return result == 0
+                ? a.idx.CompareTo(b.idx)
                 : result;
         });
         var priorityQueue = new SortedSet<(int val, int idx)>(comparer);

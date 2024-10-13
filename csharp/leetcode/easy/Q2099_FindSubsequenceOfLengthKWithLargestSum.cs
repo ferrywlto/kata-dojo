cfd2fd1fb04c5206ea880e09689514dd638b7eed
Ferry To <ferrywlto@gmail.com>
2024-10-13 17:49:43 +0100

    test: All test cases of Q2099 passed

--- csharp/leetcode/easy/Q2099_FindSubsequenceOfLengthKWithLargestSum.cs
+++ csharp/leetcode/easy/Q2099_FindSubsequenceOfLengthKWithLargestSum.cs
@@ -1,14 +1,44 @@
 public class Q2099_FindSubsequenceOfLengthKWithLargestSum
 {
+    private void SortStack(Stack<(int idx, int val)> stack, int i, int n, int maxDpeth)
+    {
+        var temp = new Stack<(int idx, int val)>();
+        while (stack.Count != 0)
+        {
+            var peek = stack.Peek();
+            if (peek.val < n)
+            {
+                peek = stack.Pop();
+                temp.Push(peek);
+            }
+            else break;
+        }
+        if (stack.Count < maxDpeth) stack.Push((i, n));
+
+        var spaceLeft = maxDpeth - stack.Count;
+        for (var j = 0; j < spaceLeft; j++)
+        {
+            if (temp.Count > 0) stack.Push(temp.Pop());
+        }
+    }
     public int[] MaxSubsequence(int[] nums, int k)
     {
-        return [];
+        var s = new Stack<(int idx, int val)>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            SortStack(s, i, nums[i], k);
+        }
+        return s
+            .OrderBy(x => x.idx)
+            .Select(x => x.val)
+            .ToArray();
     }
     public static List<object[]> TestData =>
     [
         [new int[]{2,1,3,3}, 2, new int[]{3,3}],
         [new int[]{-1,-2,3,4}, 3, new int[]{-1,3,4}],
         [new int[]{3,4,3,3}, 2, new int[]{3,4}],
+        [new int[]{50,-75}, 2, new int[]{50,-75}],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

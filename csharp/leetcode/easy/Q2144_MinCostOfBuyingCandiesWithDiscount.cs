2f7f52fcd72061a72da59d094db93eeaca745293
Ferry To <ferrywlto@gmail.com>
2024-10-15 14:13:31 +0100

    test: All test cases of Q2144 passed

--- csharp/leetcode/easy/Q2144_MinCostOfBuyingCandiesWithDiscount.cs
+++ csharp/leetcode/easy/Q2144_MinCostOfBuyingCandiesWithDiscount.cs
@@ -1,14 +1,27 @@
 public class Q2144_MinCostOfBuyingCandiesWithDiscount
 {
-    public int MinimumCost(int[] cost) 
+    // TC: O(nlogn), n scale with length of cost, dominant by Array.Sort();
+    // SC: O(1), space used does not scale with input
+    public int MinimumCost(int[] cost)
     {
-        
-        return 0;    
+        if (cost.Length == 1) return cost[0];
+        else if (cost.Length == 2) return cost[0] + cost[1];
+
+        Array.Sort(cost);
+        var result = 0;
+        for (var i = cost.Length - 1; i >= 0; i -= 3)
+        {
+            if (i == 0) result += cost[0];
+            else result += cost[i] + cost[i - 1];
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         [new int[] {1,2,3}, 5],
         [new int[] {6,5,7,9,2,2}, 23],
+        [new int[] {6,5,7,9,2,2,2}, 25],
+        [new int[] {6,5,7,9,2,2,2,2}, 27],
         [new int[] {5,5}, 10],
     ];
     [Theory]

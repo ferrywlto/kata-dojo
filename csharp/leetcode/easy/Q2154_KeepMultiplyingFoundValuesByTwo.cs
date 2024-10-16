c6885e296f74b1673c9696c4d0153ae6aa182903
Ferry To <ferrywlto@gmail.com>
2024-10-16 18:27:36 +0100

    test: All test cases of Q2154 passed

--- csharp/leetcode/easy/Q2154_KeepMultiplyingFoundValuesByTwo.cs
+++ csharp/leetcode/easy/Q2154_KeepMultiplyingFoundValuesByTwo.cs
@@ -1,13 +1,26 @@
 public class Q2154_KeepMultiplyingFoundValuesByTwo
 {
+    // TC: O(n log n), n scale with length of nums, dominant by Array.Sort() thus n log n times log n for binary search, 
+    // SC: O(1), space used does not scale with input
     public int FindFinalValue(int[] nums, int original)
     {
-        return 0;
+        Array.Sort(nums);
+        var result = original;
+        var searchResult = Array.BinarySearch(nums, result);
+
+        while (searchResult >= 0)
+        {
+            result = nums[searchResult] * 2;
+            searchResult = Array.BinarySearch(nums, 
+                searchResult, nums.Length - searchResult, result);
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         [new int[] {5,3,6,1,12}, 3, 24],
         [new int[] {2,7,9}, 4, 4],
+        [new int[] {2}, 2, 4],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

9df52322686609b4960696334e7fd9f4cb142f9d
Ferry To <ferrywlto@gmail.com>
2024-10-16 18:02:19 +0100

    test: All test cases of Q2068 passed

--- csharp/leetcode/easy/Q2068_CheckWhetherTwoStringsAreAlmostEquivalent.cs
+++ csharp/leetcode/easy/Q2068_CheckWhetherTwoStringsAreAlmostEquivalent.cs
@@ -1,8 +1,21 @@
 public class Q2068_CheckWhetherTwoStringsAreAlmostEquivalent
 {
+    // TC: O(n), n scale with length of word1
+    // SC: O(1), space used does not scale with input
     public bool CheckAlmostEquivalent(string word1, string word2)
     {
-        return false;
+        var freq1 = new int[26];
+        foreach(var c in word1) freq1[c - 'a']++;
+
+        var freq2 = new int[26];
+        foreach(var c in word2) freq2[c - 'a']++;
+
+        for(var i=0; i<freq1.Length; i++)
+        {
+            var diff = Math.Abs(freq1[i] - freq2[i]);
+            if (diff > 3) return false;
+        }
+        return true;
     }
     public static List<object[]> TestData =>
     [

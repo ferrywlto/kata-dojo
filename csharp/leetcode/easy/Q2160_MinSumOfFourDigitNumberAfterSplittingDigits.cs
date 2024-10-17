283f380184cf107d3064cd44bf7ac00cafdc8fcf
Ferry To <ferrywlto@gmail.com>
2024-10-17 22:16:31 +0100

    test: All test cases of Q2160 passed

--- csharp/leetcode/easy/Q2160_MinSumOfFourDigitNumberAfterSplittingDigits.cs
+++ csharp/leetcode/easy/Q2160_MinSumOfFourDigitNumberAfterSplittingDigits.cs
@@ -1,8 +1,17 @@
 public class Q2160_MinSumOfFourDigitNumberAfterSplittingDigits
 {
+    // TC: O(1), The question constraint implies that the input is always 4 digits, thus the time used in Array.Sort() is constant.
+    // SC: O(1), The space used is constant, limit to 4 digits due to question constraints.
     public int MinimumSum(int num)
     {
-        return 0;
+        var arr = new int[4];
+        for (var i = 0; i < arr.Length; i++)
+        {
+            arr[i] = num % 10;
+            num /= 10;
+        }
+        Array.Sort(arr);
+        return arr[0] * 10 + arr[2] + arr[1] * 10 + arr[3];
     }
     public static List<object[]> TestData =>
     [

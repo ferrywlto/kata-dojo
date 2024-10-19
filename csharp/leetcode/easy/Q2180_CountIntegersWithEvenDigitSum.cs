ea7bc7cb0ba5f9cc560fe84af895de9268889ee4
Ferry To <ferrywlto@gmail.com>
2024-10-19 20:02:43 +0100

    test: All test cases of Q2180 passed

--- csharp/leetcode/easy/Q2180_CountIntegersWithEvenDigitSum.cs
+++ csharp/leetcode/easy/Q2180_CountIntegersWithEvenDigitSum.cs
@@ -1,8 +1,24 @@
 public class Q2180_CountIntegersWithEvenDigitSum
 {
+    // TC: O(n log 10), n scale with num, then log 10 for each number
+    // SC: O(1), space used does not scale with input
     public int CountEven(int num)
     {
-        return 0;
+        var result = 0;
+        while (num > 0)
+        {
+            var sum = 0;
+            var temp = num;
+            while (temp > 0)
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if (sum % 2 == 0) result++;
+
+            num--;
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [

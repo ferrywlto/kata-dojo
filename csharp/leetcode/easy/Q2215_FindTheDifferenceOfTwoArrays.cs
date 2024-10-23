0cbcf54aef38859a0cf30fbe2cb96c6a55df4f13
Ferry To <ferrywlto@gmail.com>
2024-10-23 17:48:27 +0100

    test: All test cases of Q2210 passed

--- csharp/leetcode/easy/Q2215_FindTheDifferenceOfTwoArrays.cs
+++ csharp/leetcode/easy/Q2215_FindTheDifferenceOfTwoArrays.cs
@@ -1,29 +1,51 @@
 public class Q2215_FindTheDifferenceOfTwoArrays
 {
+    // TC: O(n + m), n scale with length of nums1, m scale with length of nums2
+    // SC: O(j + k), j scale with unique numbers of nums1 and k scale with unique numbers of nums2 for the hash sets
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
     {
-        return [];
+        var result = new List<IList<int>>
+        {
+            ([]),
+            ([])
+        };
+
+        var set1 = nums1.ToHashSet();
+        var set2 = nums2.ToHashSet();
+        foreach (var n in set1)
+        {
+            if (!set2.Contains(n)) result[0].Add(n);
+        }
+        foreach (var n in set2)
+        {
+            if (!set1.Contains(n)) result[1].Add(n);
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         [
             new int[] {1,2,3},
             new int[] {2,4,6},
-            new List<List<int>>
+            new List<IList<int>>
             {
-                new() {1,3},
-                new() {4,6},
-            }
+                new List<int> {1,3},
+                new List<int> {4,6},
+            },
         ],
         [
             new int[] {1,2,3,3},
             new int[] {1,1,2,2},
-            new List<List<int>> { new() { 3 },new() {}}
+            new List<IList<int>>
+            {
+                new List<int> { 3 },
+                new List<int>()
+            },
         ],
     ];
     [Theory]
     [MemberData(nameof(TestData))]
-    public void Test(int[] input1, int[] input2, List<List<int>> expected)
+    public void Test(int[] input1, int[] input2, IList<IList<int>> expected)
     {
         var actual = FindDifference(input1, input2);
         Assert.Equal(expected, actual);

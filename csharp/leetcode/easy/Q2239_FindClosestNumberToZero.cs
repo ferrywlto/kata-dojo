432c2d34799010903382aa26d726e9f5cdd05b5a
Ferry To <ferrywlto@gmail.com>
2024-10-25 16:39:20 +0100

    test: All test cases of Q2239 passed'

--- csharp/leetcode/easy/Q2239_FindClosestNumberToZero.cs
+++ csharp/leetcode/easy/Q2239_FindClosestNumberToZero.cs
@@ -1,13 +1,27 @@
 public class Q2239_FindClosestNumberToZero
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int FindClosestNumber(int[] nums)
     {
-        return 0;
+        var minDiff = int.MaxValue;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var diff = Math.Abs(nums[i]);
+            if (diff < minDiff) minDiff = diff;
+        }
+        var largest = int.MinValue;
+        foreach (var n in nums)
+        {
+            if (Math.Abs(n) == minDiff && n > largest) largest = n;
+        }
+        return largest;
     }
     public static List<object[]> TestData =>
     [
         [new int[]{-4,-2,1,4,8}, 1],
         [new int[]{2,-1,1}, 1],
+        [new int[]{-100000,-100000}, -100000],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

ecfb0e8273e96c1d612beba4ebcd75aa380fe4eb
Ferry To <ferrywlto@gmail.com>
2024-10-27 15:38:40 +0000

    test: All test cases of Q2231 passed

--- csharp/leetcode/easy/Q2231_LargestNumberAfterDigitSwapsByParity.cs
+++ csharp/leetcode/easy/Q2231_LargestNumberAfterDigitSwapsByParity.cs
@@ -1,13 +1,64 @@
-public class Q2231_LargestNumberAfterDigitSwapsByParity
+using System.Text;
+
+public class Q2231_LargestNumberAfterDigitSwapsByParity(ITestOutputHelper output)
 {
+    // TC: O(d log d), d scale with digits in num
+    // SC: O(d), d scale with digits in num
     public int LargestInteger(int num)
     {
-        return 0;
+        var evenNums = new List<int>();
+        var oddNums = new List<int>();
+        var numStr = num.ToString();
+
+        var parity = new int[numStr.Length];
+        for (var i = 0; i < numStr.Length; i++)
+        {
+            var val = numStr[i] - '0';
+            if (val % 2 == 0)
+            {
+                evenNums.Add(val);
+                parity[i] = 0;
+            }
+            else
+            {
+                oddNums.Add(val);
+                parity[i] = 1;
+            }
+
+        }
+        output.WriteLine(string.Join(',', parity));
+        evenNums.Sort();
+        evenNums.Reverse();
+        oddNums.Sort();
+        oddNums.Reverse();
+
+        var sb = new StringBuilder();
+        var evenIdx = 0;
+        var oddIdx = 0;
+        for (var j = 0; j < numStr.Length; j++)
+        {
+            if (parity[j] == 0)
+            {
+                sb.Append(evenNums[evenIdx]);
+                evenIdx++;
+            }
+            else
+            {
+                sb.Append(oddNums[oddIdx]);
+                oddIdx++;
+            }
+        }
+
+        return int.Parse(sb.ToString());
     }
     public static List<object[]> TestData =>
     [
         [1234, 3412],
+        [4120, 4120],
         [65875, 87655],
+        [80546, 86540],
+        [60, 60],
+        [247, 427],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

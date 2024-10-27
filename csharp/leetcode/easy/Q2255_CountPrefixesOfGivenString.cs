fa07155a7a860782ce91779a1ebd49c33b875faa
Ferry To <ferrywlto@gmail.com>
2024-10-27 15:58:55 +0000

    test: All official test cases of Q2255 passed

--- csharp/leetcode/easy/Q2255_CountPrefixesOfGivenString.cs
+++ csharp/leetcode/easy/Q2255_CountPrefixesOfGivenString.cs
@@ -1,13 +1,26 @@
 public class Q2255_CountPrefixesOfGivenString
 {
+    // TC: O(n), where n sacle with number of total characters in words
+    // SC: O(1), space used does not sacle with input
     public int CountPrefixes(string[] words, string s)
     {
-        return 0;
+        var result = 0;
+        foreach (var word in words)
+        {
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (i >= s.Length) break;
+                if (word[i] != s[i]) break;
+                else if (i == word.Length - 1) result++;
+            }
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         [new string[] {"a","b","c","ab","bc","abc"}, "abc", 3],
-        [new string[] {"a","a"}, "a", 3],
+        [new string[] {"a","a"}, "aa", 2],
+        [new string[] {"ab","abc"}, "a", 0],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

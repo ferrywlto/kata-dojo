7b85659ff12f9daa23d62b5851045d98297021e5
Ferry To <ferrywlto@gmail.com>
2024-10-28 16:42:01 +0000

    test: All test cases of Q2264 passed

--- csharp/leetcode/easy/Q2264_LargestThreeSameDigitNumberInString.cs
+++ csharp/leetcode/easy/Q2264_LargestThreeSameDigitNumberInString.cs
@@ -1,12 +1,28 @@
 public class Q2264_LargestThreeSameDigitNumberInString
 {
+    // TC: O(n), n scale with length of num
+    // SC: O(1), space used does not scale with input
     public string LargestGoodInteger(string num)
     {
-        return string.Empty;
+        var maxDigit = '/';
+        var result = string.Empty;
+        for (var i = 0; i < num.Length - 2; i++)
+        {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2])
+            {
+                if (num[i] > maxDigit)
+                {
+                    maxDigit = num[i];
+                    result = num[i..(i + 3)];
+                }
+            }
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         ["6777133339", "777"],
+        ["67771333999", "999"],
         ["2300019", "000"],
         ["42352338", ""],
     ];

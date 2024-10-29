dd53bd60f6bddb76a851d3537211a51d0e401cf4
Ferry To <ferrywlto@gmail.com>
2024-10-29 17:24:15 +0000

    perf: Improve time to O(n) with dictionary instead of string encoding

--- csharp/leetcode/easy/Q2273_FindResultantArrayAfterRemovingAnagrams.cs
+++ csharp/leetcode/easy/Q2273_FindResultantArrayAfterRemovingAnagrams.cs
@@ -1,31 +1,40 @@
 public class Q2273_FindResultantArrayAfterRemovingAnagrams//(ITestOutputHelper output)
 {
+    // TC: O(n * m), n scale with length of words times m unique characters in each word
+    // SC: O(k + m), k scale with number of anagram in words plus m unique characters in each word 
     public IList<string> RemoveAnagrams(string[] words)
     {
         if (words.Length <= 1) return words;
 
         var list = new List<string>();
-        var last = string.Empty;
+        var last = new Dictionary<char, int>();
         for (var i = 0; i < words.Length; i++)
         {
             var str = Encode(words[i]);
-            if (str != last) list.Add(words[i]);
+            foreach (var k in str.Keys)
+            {
+                if (str.Count != last.Count
+                || !last.TryGetValue(k, out var value)
+                || value != str[k])
+                {
+                    list.Add(words[i]);
+                    break;
+                }
+            }
             last = str;
         }
         return list;
     }
-    public string Encode(string input)
+    public Dictionary<char, int> Encode(string input)
     {
-        var x = input
+        return input
             .GroupBy(c => c)
-            .OrderBy(g => g.Key)
-            .Select(g => $"{g.Key}{g.Count()}");
-        return string.Join(',', x);
+            .ToDictionary(g => g.Key, g => g.Count());
     }
-
 
     public static List<object[]> TestData =>
     [
+        [new string [] {"nelduncd","dcnndeul","uendlcnd","nluncedd","fozlsvr","osfvrlz","vozsrfl","dm","md","md","dm","md","dm","md","md","dm","dm","dm","dm","md","eatzkewuyx","a","wulzacir"}, new List<string> {"nelduncd","fozlsvr","dm","eatzkewuyx","a","wulzacir"}],
         [new string [] {"abba","baba","bbaa","cd","cd"}, new List<string> {"abba","cd"}],
         [new string [] {"a","b","c","d","e"}, new List<string> {"a","b","c","d","e"}],
         [new string [] {"a","b","a"}, new List<string> {"a","b","a"}],

20bae3687ed526a1882a61c7cdfd6651a2b7b778
Ferry To <ferrywlto@gmail.com>
2024-10-29 16:08:51 +0000

    test: All test cases of Q2269 passed

--- csharp/leetcode/easy/Q2269_FindTheKBeautyOfNumber.cs
+++ csharp/leetcode/easy/Q2269_FindTheKBeautyOfNumber.cs
@@ -1,8 +1,18 @@
 public class Q2269_FindTheKBeautyOfNumber
 {
+    // TC: O(d), d scale with digits of num
+    // SC: O(k), k scale with size of k to hold k size substring to convert into number
     public int DivisorSubstrings(int num, int k)
     {
-        return 0;
+        var result = 0;
+        var numStr = num.ToString();
+        for (var i = 0; i < numStr.Length - k + 1; i++)
+        {
+            var extract = numStr[i..(i + k)];
+            var divisor = int.Parse(extract);
+            if (divisor != 0 && num % divisor == 0) result++;
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [

f217442f8b18b5deb46a296da78ae828fb70800b
Ferry To <ferrywlto@gmail.com>
2024-10-29 17:12:29 +0000

    test: All test cases of Q2273 passed

--- csharp/leetcode/easy/Q2273_FindResultantArrayAfterRemovingAnagrams.cs
+++ csharp/leetcode/easy/Q2273_FindResultantArrayAfterRemovingAnagrams.cs
@@ -1,17 +1,38 @@
-public class Q2273_FindResultantArrayAfterRemovingAnagrams
+public class Q2273_FindResultantArrayAfterRemovingAnagrams//(ITestOutputHelper output)
 {
     public IList<string> RemoveAnagrams(string[] words)
     {
-        return [];
+        if (words.Length <= 1) return words;
+
+        var list = new List<string>();
+        var last = string.Empty;
+        for (var i = 0; i < words.Length; i++)
+        {
+            var str = Encode(words[i]);
+            if (str != last) list.Add(words[i]);
+            last = str;
+        }
+        return list;
     }
+    public string Encode(string input)
+    {
+        var x = input
+            .GroupBy(c => c)
+            .OrderBy(g => g.Key)
+            .Select(g => $"{g.Key}{g.Count()}");
+        return string.Join(',', x);
+    }
+
+
     public static List<object[]> TestData =>
     [
-        [new string [] {"abba","baba","bbaa","cd","cd"}, new string[] {"abba","cd"}],
-        [new string [] {"a","b","c","d","e"}, new string[] {"a","b","c","d","e"}],
+        [new string [] {"abba","baba","bbaa","cd","cd"}, new List<string> {"abba","cd"}],
+        [new string [] {"a","b","c","d","e"}, new List<string> {"a","b","c","d","e"}],
+        [new string [] {"a","b","a"}, new List<string> {"a","b","a"}],
     ];
     [Theory]
     [MemberData(nameof(TestData))]
-    public void Test(string[] input, string[] expected)
+    public void Test(string[] input, List<string> expected)
     {
         var actual = RemoveAnagrams(input);
         Assert.Equal(expected, actual);

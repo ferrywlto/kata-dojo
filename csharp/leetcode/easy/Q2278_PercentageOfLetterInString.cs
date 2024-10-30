8a6318748f502a1dee331cb424016a05f2616a49
Ferry To <ferrywlto@gmail.com>
2024-10-30 17:03:10 +0000

    test: Add official test cases of Q2278

--- csharp/leetcode/easy/Q2278_PercentageOfLetterInString.cs
+++ csharp/leetcode/easy/Q2278_PercentageOfLetterInString.cs
@@ -1,12 +1,22 @@
 public class Q2278_PercentageOfLetterInString
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used does not scale with input
     public int PercentageLetter(string s, char letter)
     {
-        return 0;
+        var count = 0;
+        foreach (var c in s)
+        {
+            if (c == letter) count++;
+        }
+        var percent = count / (double)s.Length;
+
+        return (int)Math.Floor(percent * 100);
     }
     public static List<object[]> TestData =>
     [
         ["foobar", 'o', 33],
+        ["sgawtb", 's', 16],
         ["jjjj", 'k', 0],
     ];
     [Theory]

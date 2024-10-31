61410c9cfc7ad0e0d8fe448b5ef7b5fdda2b7881
Ferry To <ferrywlto@gmail.com>
2024-10-31 16:07:56 +0000

    test: All test cases of Q2293 passed

--- csharp/leetcode/easy/Q2293_MinMaxGame.cs
+++ csharp/leetcode/easy/Q2293_MinMaxGame.cs
@@ -1,12 +1,28 @@
 public class Q2293_MinMaxGame
 {
+    // TC: O(n log 2), n scale with length of nums, shrink by factor of 2 each iteration
+    // SC: O(n), n scale with length of nums initally, shrink by factor of 2 each time.  
     public int MinMaxGame(int[] nums)
     {
-        return 0;
+        var list = new List<int>(nums);
+        while (list.Count > 1)
+        {
+            var temp = new List<int>();
+            for (var i = 0; i < list.Count / 2; i++)
+            {
+                var val = i % 2 == 0
+                    ? Math.Min(list[2 * i], list[2 * i + 1])
+                    : Math.Max(list[2 * i], list[2 * i + 1]);
+                temp.Add(val);
+            }
+            list = temp;
+        }
+        return list[0];
     }
     public static List<object[]> TestData =>
     [
         [new int[] {1,3,5,2,4,8,2,2}, 1],
+        [new int[] {70,38,21,22}, 22],
         [new int[] {3}, 3],
     ];
     [Theory]

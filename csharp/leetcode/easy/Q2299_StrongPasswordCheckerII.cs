36e4a42a53259ef3f5f3c2988aabc8034d5eda4c
Ferry To <ferrywlto@gmail.com>
2024-11-01 16:51:00 +0000

    test: All test cases of Q2299 passed

--- csharp/leetcode/easy/Q2299_StrongPasswordCheckerII.cs
+++ csharp/leetcode/easy/Q2299_StrongPasswordCheckerII.cs
@@ -1,8 +1,28 @@
 public class Q2299_StrongPasswordCheckerII
 {
+    // TC: O(n), n scales with length of password
+    // SC: O(1), space used does not scale with input
     public bool StrongPasswordCheckerII(string password)
     {
-        return false;
+        if (password.Length < 8) return false;
+
+        var uppercaseCount = 0;
+        var lowercaseCount = 0;
+        var digitCount = 0;
+        var symbolCount = 0;
+        var symbols = "!@#$%^&*()-+".ToHashSet();
+        for (var i = 0; i < password.Length; i++)
+        {
+            if (i > 0 && password[i] == password[i - 1]) return false;
+            else if (char.IsAsciiLetterUpper(password[i])) uppercaseCount++;
+            else if (char.IsAsciiLetterLower(password[i])) lowercaseCount++;
+            else if (char.IsAsciiDigit(password[i])) digitCount++;
+            else if (symbols.Contains(password[i])) symbolCount++;
+        }
+        return uppercaseCount > 0
+            && lowercaseCount > 0
+            && digitCount > 0
+            && symbolCount > 0;
     }
     public static List<object[]> TestData =>
     [

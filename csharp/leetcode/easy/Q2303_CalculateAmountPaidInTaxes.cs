99999cd58aa5cd88e492fda4ef7ff9d625148292
Ferry To <ferrywlto@gmail.com>
2024-11-01 17:11:16 +0000

    test: All test cases of Q2303 passed

--- csharp/leetcode/easy/Q2303_CalculateAmountPaidInTaxes.cs
+++ csharp/leetcode/easy/Q2303_CalculateAmountPaidInTaxes.cs
@@ -1,8 +1,25 @@
 public class Q2303_CalculateAmountPaidInTaxes
 {
+    // TC: O(n), n scale with number of brackets
+    // SC: O(1), space used does not scale with input
     public double CalculateTax(int[][] brackets, int income)
     {
-        return 0;
+        double result = 0;
+        for (var i = 0; i < brackets.Length; i++)
+        {
+            var upper = i == 0
+                ? brackets[i][0]
+                : brackets[i][0] - brackets[i - 1][0];
+
+            double percentage = brackets[i][1];
+            var payable = Math.Min(income, upper);
+            var tax = payable * percentage / 100;
+            result += tax;
+            income -= payable;
+
+            if (income == 0) break;
+        }
+        return Math.Round(result, 5);
     }
     public static List<object[]> TestData =>
     [

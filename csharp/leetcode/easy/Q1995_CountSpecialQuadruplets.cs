57d1fbe30cc9722f3a99c5b82fcc3fbc1def3748
Ferry To <ferrywlto@gmail.com>
2024-11-01 17:41:20 +0000

    test: All test cases of Q1995 passed

--- csharp/leetcode/easy/Q1995_CountSpecialQuadruplets.cs
+++ csharp/leetcode/easy/Q1995_CountSpecialQuadruplets.cs
@@ -1,8 +1,28 @@
 public class Q1995_CountSpecialQuadruplets
 {
+    // TC: O(n^4), n scale with length of nums, n(n-1)(n-2)(n-3) thus n^4
+    // SC: O(m), m scale with unique index combinations in nums 
     public int CountQuadruplets(int[] nums)
     {
-        return 0;
+        var existed = new HashSet<string>();
+        for (var i = nums.Length - 1; i >= 3; i--)
+        {
+            var d = nums[i];
+            for (var j = i - 1; j >= 2; j--)
+            {
+                var c = nums[j];
+                for (var k = j - 1; k >= 1; k--)
+                {
+                    var b = nums[k];
+                    for (var m = k - 1; m >= 0; m--)
+                    {
+                        var a = nums[m];
+                        if (a + b + c == d) existed.Add(string.Join("", new int[] { m, k, j, i }));
+                    }
+                }
+            }
+        }
+        return existed.Count;
     }
     public static List<object[]> TestData =>
     [

fc6f5947d481cbe0271de60e251be49db8f70543
Ferry To <ferrywlto@gmail.com>
2024-11-03 13:12:51 +0000

    test: All test cases of Q1925 passed

--- csharp/leetcode/easy/Q1925_CountSquareSumTriple.cs
+++ csharp/leetcode/easy/Q1925_CountSquareSumTriple.cs
@@ -1,13 +1,27 @@
 public class Q1925_CountSquareSumTriple
 {
+    // TC: O(n^3), n scale with size of n
+    // SC: O(1), space used does not sacle with input 
     public int CountTriples(int n)
     {
-        return 0;
+        var result = 0;
+        for (var i = n; i > 0; i--)
+        {
+            for (var j = 1; j < i; j++)
+            {
+                for (var k = 1; k < i; k++)
+                {
+                    if (k * k + j * j == i * i) result++;
+                }
+            }
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [
         [5,2],
         [10,4],
+        [250,330],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

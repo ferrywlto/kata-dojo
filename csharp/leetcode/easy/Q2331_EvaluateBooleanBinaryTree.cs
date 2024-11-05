155e8c365908f0689b82889c85f5e13d00b42da2
Ferry To <ferrywlto@gmail.com>
2024-11-05 15:53:04 +0000

    test: All test cases of Q2331 passed

--- csharp/leetcode/easy/Q2331_EvaluateBooleanBinaryTree.cs
+++ csharp/leetcode/easy/Q2331_EvaluateBooleanBinaryTree.cs
@@ -1,9 +1,23 @@
 public class Q2331_EvaluateBooleanBinaryTree
 {
+    // TC: O(n), n scale with number of nodes in root.
+    // SC: O(d), d scale with depth of tree
     public bool EvaluateTree(TreeNode root)
     {
-        return false;
+        return EvaluateNode(root);
     }
+    public bool EvaluateNode(TreeNode node)
+    {
+        return node.val switch
+        {
+            0 => false,
+            1 => true,
+            2 => EvaluateNode(node.left!) || EvaluateNode(node.right!),
+            3 => EvaluateNode(node.left!) && EvaluateNode(node.right!),
+            _ => throw new NotImplementedException(),
+        };
+    }
+
     public static List<object[]> TestData =>
     [
         [new int?[] {2,1,3,null,null,0,1}, true],

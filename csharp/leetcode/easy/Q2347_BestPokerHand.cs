e35db0da0315c65601bb43e3d43a9f75138d392f
Ferry To <ferrywlto@gmail.com>
2024-11-06 12:03:09 +0000

    test: All test cases of Q2347 passed

--- csharp/leetcode/easy/Q2347_BestPokerHand.cs
+++ csharp/leetcode/easy/Q2347_BestPokerHand.cs
@@ -1,20 +1,26 @@
 public class Q2347_BestPokerHand
 {
+    // TC: O(n), n scale with length of ranks and length of suits, in only one pass
+    // SC: O(m), m scale with unique ranks plus unique suits
     public string BestHand(int[] ranks, char[] suits)
     {
-        var sSet = suits.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
-        if (sSet.Count == 1) return "Flush";
+        var set = new HashSet<char>(suits);
+        if (set.Count == 1) return "Flush";
 
         var maxCount = 0;
-        var rSet = ranks.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
-        foreach(var p in rSet)
+        var dict = new Dictionary<int, int>();
+        foreach (var r in ranks)
         {
-            if (p.Value > maxCount) maxCount = p.Value;
+            if (dict.TryGetValue(r, out var val)) dict[r] = ++val;
+            else dict.Add(r, 1);
+
+            if (dict[r] > maxCount) maxCount = dict[r];
         }
-        return maxCount switch {
+        return maxCount switch
+        {
             >= 3 => "Three of a Kind",
             2 => "Pair",
-            1 => "High Card", 
+            1 => "High Card",
             _ => "Error"
         };
     }
@@ -23,6 +29,7 @@ public class Q2347_BestPokerHand
         [new int[] {13,2,3,1,9}, new char[] {'a','a','a','a','a'}, "Flush"],
         [new int[] {4,4,2,4,4}, new char[] {'d','a','a','b','c'}, "Three of a Kind"],
         [new int[] {10,10,2,12,9}, new char[] {'a','b','c','a','d'}, "Pair"],
+        [new int[] {13,12,3,4,7}, new char[] {'a','d','c','b','c'}, "High Card"],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

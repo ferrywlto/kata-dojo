8cabb3c91c359fcf4f84ab17d3c7edbd8407a51c
Ferry To <ferrywlto@gmail.com>
2024-11-07 12:49:32 +0000

    test: All test cases of Q2351 passed

--- csharp/leetcode/easy/Q2351_FirstLetterToAppearTwice.cs
+++ csharp/leetcode/easy/Q2351_FirstLetterToAppearTwice.cs
@@ -1,7 +1,16 @@
 public class Q2351_FirstLetterToAppearTwice
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1), as input contains only lowercase characters
     public char RepeatedCharacter(string s)
     {
+        var chars = new int[26];
+        foreach (var c in s)
+        {
+            var idx = c - 'a';
+            if (chars[idx] == 1) return c;
+            chars[idx]++;
+        }
         return ' ';
     }
     public static List<object[]> TestData =>

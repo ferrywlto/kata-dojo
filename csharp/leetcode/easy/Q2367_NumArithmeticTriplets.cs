c2519fc940586050d6e114e33601a6cf6c85f268
Ferry To <ferrywlto@gmail.com>
2024-11-09 14:10:42 +0000

    test: All test cases of Q2367 passed

--- csharp/leetcode/easy/Q2367_NumArithmeticTriplets.cs
+++ csharp/leetcode/easy/Q2367_NumArithmeticTriplets.cs
@@ -1,8 +1,24 @@
 public class Q2367_NumArithmeticTriplets
 {
+    // TC: O(n^3), n scale with length of nums
+    // SC: O(1), space used does not scale with input 
     public int ArithmeticTriplets(int[] nums, int diff)
     {
-        return 0;
+        var result = 0;
+        for (var k = nums.Length - 1; k >= 2; k--)
+        {
+            for (var j = k - 1; j >= 1; j--)
+            {
+                for (var i = j - 1; i >= 0; i--)
+                {
+                    if (nums[k] - nums[j] == diff && nums[j] - nums[i] == diff)
+                    {
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [

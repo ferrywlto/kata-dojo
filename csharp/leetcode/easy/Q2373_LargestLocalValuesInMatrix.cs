e4e0af762eabe75530dae7ad61818c64f5ab575e
Ferry To <ferrywlto@gmail.com>
2024-11-09 15:12:05 +0000

    test: All test cases of Q2373 passed

--- csharp/leetcode/easy/Q2373_LargestLocalValuesInMatrix.cs
+++ csharp/leetcode/easy/Q2373_LargestLocalValuesInMatrix.cs
@@ -1,9 +1,37 @@
+using System.Text;
+
 public class Q2373_LargestLocalValuesInMatrix
 {
+    // TC: O(n^2), n scale with length of grid, for each element it run FindLocalMax once which is O(1)
+    // SC: O(1), space used does not scale with input if not count the result array, O(n^2) otherwise 
     public int[][] LargestLocal(int[][] grid)
     {
-        return [];
+        var n = grid.Length;
+        var result = new int[n - 2][];
+        for (var k = 0; k < result.Length; k++) result[k] = new int[n - 2];
+
+        for (var i = 0; i < n - 2; i++)
+        {
+            for (var j = 0; j < n - 2; j++)
+            {
+                result[i][j] = FindLocalMax(grid, i, j);
+            }
+        }
+        return result;
     }
+    public int FindLocalMax(int[][] grid, int row, int col)
+    {
+        var max = 0;
+        for (var i = row; i < row + 3; i++)
+        {
+            for (var j = col; j < col + 3; j++)
+            {
+                if (grid[i][j] > max) max = grid[i][j];
+            }
+        }
+        return max;
+    }
+
     public static List<object[]> TestData =>
     [
         [
@@ -34,6 +62,18 @@ public class Q2373_LargestLocalValuesInMatrix
                 [2,2,2],
                 [2,2,2],
                 [2,2,2],
+            },
+        ],
+        [
+            new int[][]
+            {
+                [1,1,1],
+                [1,1,1],
+                [1,1,2],
+            },
+            new int[][]
+            {
+                [2],
             },
         ],
     ];

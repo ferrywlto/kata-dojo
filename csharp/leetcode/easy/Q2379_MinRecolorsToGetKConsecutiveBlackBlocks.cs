8e2afd70212fb205aa68af766a776c936decf914
Ferry To <ferrywlto@gmail.com>
2024-11-10 13:56:56 +0000

    test: All test cases of Q2379 passed

--- csharp/leetcode/easy/Q2379_MinRecolorsToGetKConsecutiveBlackBlocks.cs
+++ csharp/leetcode/easy/Q2379_MinRecolorsToGetKConsecutiveBlackBlocks.cs
@@ -1,15 +1,32 @@
 public class Q2379_MinRecolorsToGetKConsecutiveBlackBlocks
 {
+    // TC: O(n), n scale with length of blocks
+    // SC: O(1), space used does not scale with input
     public int MinimumRecolors(string blocks, int k)
     {
-        return 0;
+        var white = 0;
+        for (var i = 0; i < k; i++)
+        {
+            if (blocks[i] == 'W') white++;
+        }
+
+        var minWhite = white;
+        for (var i = k; i < blocks.Length; i++)
+        {
+            var headIdx = i - k;
+
+            if (blocks[headIdx] == 'W') white--;
+            if (blocks[i] == 'W') white++;
+            if (white < minWhite) minWhite = white;
+        }
+        return minWhite;
     }
     public static List<object[]> TestData =>
     [
         ["WBBWWBBWBW", 7, 3],
         ["WBWBBBW", 2, 0],
     ];
-    
+
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(string input, int k, int expected)

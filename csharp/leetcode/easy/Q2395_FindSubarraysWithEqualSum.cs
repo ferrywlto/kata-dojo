e1ea18c485397162f5c0e687b2652b09dc24a0a3
Ferry To <ferrywlto@gmail.com>
2024-11-11 21:19:34 +0000

    test: All test cases of Q2395 passed

--- csharp/leetcode/easy/Q2395_FindSubarraysWithEqualSum.cs
+++ csharp/leetcode/easy/Q2395_FindSubarraysWithEqualSum.cs
@@ -1,7 +1,20 @@
 public class Q2395_FindSubarraysWithEqualSum
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(m), m scale with unique sum from nums
     public bool FindSubarrays(int[] nums)
     {
+        var sums = new HashSet<int>();
+        var sumCount = 0;
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            var sum = nums[i] + nums[i + 1];
+            if (!sums.Add(sum))
+            {
+                sumCount++;
+                if (sumCount == 1) return true;
+            }
+        }
         return false;
     }
     public static List<object[]> TestData =>

46164bc7d3a15b56d669d0593651514a8f5ef722
Ferry To <ferrywlto@gmail.com>
2024-11-12 17:20:01 +0000

    test: All test cases of Q2404 passed

--- csharp/leetcode/easy/Q2404_MostFrequentEvenElement.cs
+++ csharp/leetcode/easy/Q2404_MostFrequentEvenElement.cs
@@ -1,26 +1,27 @@
 public class Q2404_MostFrequentEvenElement
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(m), m scale with unique even numbers in nums
     public int MostFrequentEven(int[] nums)
     {
         var evens = new Dictionary<int, int>();
         var smallest = int.MaxValue;
         var highestFrequency = int.MinValue;
-        foreach(var n in nums)
+        foreach (var n in nums)
         {
-            if(n % 2 == 0)
-            {
-                if (evens.TryGetValue(n, out var val)) evens[n] = ++val;
-                else evens.Add(n, 1);
+            if (n % 2 != 0) continue;
 
-                if (evens[n] > highestFrequency)
-                {
-                    highestFrequency = evens[n];
-                    smallest = n;
-                }
-                else if(evens[n] == highestFrequency && n < smallest) 
-                {
-                    smallest = n;
-                }
+            if (evens.TryGetValue(n, out var val)) evens[n] = ++val;
+            else evens.Add(n, 1);
+
+            if (evens[n] > highestFrequency)
+            {
+                highestFrequency = evens[n];
+                smallest = n;
+            }
+            else if (evens[n] == highestFrequency && n < smallest)
+            {
+                smallest = n;
             }
         }
         return smallest != int.MaxValue ? smallest : -1;
@@ -30,6 +31,7 @@ public class Q2404_MostFrequentEvenElement
         [new int[] {0,1,2,2,4,4,1}, 2],
         [new int[] {4,4,4,9,2,4}, 4],
         [new int[] {29,47,21,41,13,37,25,7}, -1],
+        [new int[] {10000}, 10000],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

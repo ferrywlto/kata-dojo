b7df07e48852afe46b5c2f353e5cf32a430f3628
Ferry To <ferrywlto@gmail.com>
2024-11-12 22:27:39 +0000

    feat: Setup react router

--- javascript/react/router-tutorial/src/main.jsx
+++ javascript/react/router-tutorial/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root'
 import './index.css'
 import App from './App.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+// For React pre v18
+// import ReactDOM from 'react-dom';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

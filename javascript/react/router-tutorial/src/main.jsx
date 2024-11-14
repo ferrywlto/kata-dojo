9ffc630e1eff452c1aa51b3b785c7b8b8b40bc66
Ferry To <ferrywlto@gmail.com>
2024-11-14 21:55:29 +0000

    feat: Pathless route to wrap error message

--- javascript/react/router-tutorial/src/main.jsx
+++ javascript/react/router-tutorial/src/main.jsx
@@ -27,25 +27,30 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      {index: true, element: <Index />},
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
+        errorElement: <ErrorPage />,
+        children: [
+          {index: true, element: <Index />},
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <dir>Unexpected</dir>
+          },
+        ],
       },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <dir>Unexpected</dir>
-      }
-    ]
+    ],
   },
 ]);
 

943f70cd31ca7605c49c770be47e517578e3269a
Ferry To <ferrywlto@gmail.com>
2024-11-16 16:27:41 +0000

    test: All test cases of Q2451 passed

--- csharp/leetcode/easy/Q2451_OddStringDifference.cs
+++ csharp/leetcode/easy/Q2451_OddStringDifference.cs
@@ -1,12 +1,35 @@
 public class Q2451_OddStringDifference
 {
+    // TC: O(n*m), n scale with length of words and m scale with characters in each word
+    // SC: O(n*m), same as time.
     public string OddString(string[] words)
     {
+        var set = new Dictionary<string, List<string>>();
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            var word = words[i];
+            var diffArr = new int[word.Length - 1];
+            for (var j = 1; j < word.Length; j++)
+            {
+                diffArr[j - 1] = word[j] - word[j - 1];
+            }
+            var key = string.Join(',', diffArr);
+
+            if (set.TryGetValue(key, out var value)) value.Add(word);
+            else set.Add(key, [word]);
+        }
+
+        foreach (var p in set)
+        {
+            if (p.Value.Count == 1) return p.Value[0];
+        }
         return string.Empty;
     }
     public static List<object[]> TestData =>
     [
         [new string[] {"adc","wzy","abc"}, "abc"],
+        [new string[] {"ddd","poo","baa","onn"}, "ddd"],
         [new string[] {"aaa","bob","ccc","ddd"}, "bob"],
     ];
     [Theory]

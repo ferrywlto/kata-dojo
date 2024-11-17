9ef2be1f3aa0405aff81ada663e2875318b5b452
Ferry To <ferrywlto@gmail.com>
2024-11-18 03:05:55 +0800

    test: All test cases of Q2455 passed

--- csharp/leetcode/easy/Q2455_AvgValueOfEvenNumbersThatAreDivisibleByThree.cs
+++ csharp/leetcode/easy/Q2455_AvgValueOfEvenNumbersThatAreDivisibleByThree.cs
@@ -1,13 +1,26 @@
 public class Q2455_AvgValueOfEvenNumbersThatAreDivisibleByThree
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int AverageValue(int[] nums)
     {
-        return 0;
+        var sum = 0;
+        var count = 0;
+        foreach (var n in nums)
+        {
+            if (n % 6 == 0)
+            {
+                sum += n;
+                count++;
+            }
+        }
+        if (count == 0) return 0;
+        return (int)Math.Floor((double)sum / count);
     }
     public static List<object[]> TestData =>
     [
         [new int[] {1,3,6,10,12,15}, 9],
-        [new int[] {1,2,4,7,10}, 9],
+        [new int[] {1,2,4,7,10}, 0],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

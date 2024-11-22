fc2250bc84bad0d49468143fa68d89d606be9a7e
Ferry To <ferrywlto@gmail.com>
2024-11-23 02:02:08 +0800

    test: All test cases of Q2485 passed

--- csharp/leetcode/easy/Q2485_FindPivotInteger.cs
+++ csharp/leetcode/easy/Q2485_FindPivotInteger.cs
@@ -1,8 +1,19 @@
 public class Q2485_FindPivotInteger
 {
+    // TC: O(n), n scale with size of n
+    // SC: O(1), space used does not scale with input
     public int PivotInteger(int n)
     {
-        return 0;
+        var sum = (1 + n) * n / 2;
+        var lhs = 0;
+        var rhs = sum;
+        for (var i = 1; i <= n; i++)
+        {
+            lhs += i;
+            rhs = sum - lhs + i;
+            if (lhs == rhs) return i;
+        }
+        return -1;
     }
     public static List<object[]> TestData =>
     [

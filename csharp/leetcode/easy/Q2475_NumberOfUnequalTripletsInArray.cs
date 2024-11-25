318bdb80b9cf86fedf0bb981c17a4825930685a1
Ferry To <ferrywlto@gmail.com>
2024-11-25 22:32:28 +0000

    test: All test cases of Q2475 passed

--- csharp/leetcode/easy/Q2475_NumberOfUnequalTripletsInArray.cs
+++ csharp/leetcode/easy/Q2475_NumberOfUnequalTripletsInArray.cs
@@ -1,8 +1,23 @@
 public class Q2475_NumberOfUnequalTripletsInArray
 {
+    // TC: O(n^3), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int UnequalTriplets(int[] nums)
     {
-        return 0;
+        var count = 0;
+        for (var i = 0; i < nums.Length - 2; i++)
+        {
+            for (var j = i + 1; j < nums.Length - 1; j++)
+            {
+                for (var k = j + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] != nums[j]
+                    && nums[j] != nums[k]
+                    && nums[k] != nums[i]) count++;
+                }
+            }
+        }
+        return count;
     }
     public static List<object[]> TestData =>
     [

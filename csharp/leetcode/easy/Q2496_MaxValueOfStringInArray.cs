7b13f08a7e110efcc0e77846d75f8ec07a596aab
Ferry To <ferrywlto@gmail.com>
2024-11-25 22:23:04 +0000

    test: All test cases of Q2469 passed

--- csharp/leetcode/easy/Q2496_MaxValueOfStringInArray.cs
+++ csharp/leetcode/easy/Q2496_MaxValueOfStringInArray.cs
@@ -1,8 +1,22 @@
 public class Q2496_MaxValueOfStringInArray
 {
+    // TC: O(n), n scale with total characters in strs
+    // SC: O(1), space used does not scale with input
     public int MaximumValue(string[] strs)
     {
-        return 0;
+        var max = int.MinValue;
+        foreach (var s in strs)
+        {
+            if (int.TryParse(s, out var val))
+            {
+                if (val > max) max = val;
+            }
+            else
+            {
+                if (s.Length > max) max = s.Length;
+            }
+        }
+        return max;
     }
     public static List<object[]> TestData =>
     [

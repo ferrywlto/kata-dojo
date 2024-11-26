f9a5d8684a62b7812320fab43738a2733b2c9c34
Ferry To <ferrywlto@gmail.com>
2024-11-26 23:13:23 +0000

    test: All test cases of Q2506 passed

--- csharp/leetcode/easy/Q2506_CountPairsOfSimilarStrings.cs
+++ csharp/leetcode/easy/Q2506_CountPairsOfSimilarStrings.cs
@@ -1,8 +1,23 @@
 public class Q2506_CountPairsOfSimilarStrings
 {
+    // TC: O(n^2)
+    // SC: O(n * m), n scale with length of words and m scale with unique characters in each word
     public int SimilarPairs(string[] words)
     {
-        return 0;
+        var result = 0;
+        for (var i = 0; i < words.Length - 1; i++)
+        {
+            var iSet = new HashSet<char>(words[i]);
+            for (var j = i + 1; j < words.Length; j++)
+            {
+                var jSet = new HashSet<char>(words[j]);
+                if (iSet.OrderBy(c => c).SequenceEqual(jSet.OrderBy(c => c)))
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
     public static List<object[]> TestData =>
     [

a7c3081b5c7e4f93c6d2aef9790d1bc394a37b15
Ferry To <ferrywlto@gmail.com>
2024-11-27 21:58:05 +0000

    perf: Improve time slightly for Q2506

--- csharp/leetcode/easy/Q2506_CountPairsOfSimilarStrings.cs
+++ csharp/leetcode/easy/Q2506_CountPairsOfSimilarStrings.cs
@@ -5,13 +5,18 @@ public class Q2506_CountPairsOfSimilarStrings
     public int SimilarPairs(string[] words)
     {
         var result = 0;
+
+        var chars = new HashSet<char>[words.Length];
+        for(var i=0; i<words.Length; i++)
+        {
+            chars[i] = [.. words[i].ToArray().OrderBy(c => c)];
+        }
+
         for (var i = 0; i < words.Length - 1; i++)
         {
-            var iSet = new HashSet<char>(words[i]);
             for (var j = i + 1; j < words.Length; j++)
             {
-                var jSet = new HashSet<char>(words[j]);
-                if (iSet.OrderBy(c => c).SequenceEqual(jSet.OrderBy(c => c)))
+                if (chars[i].SequenceEqual(chars[j]))
                 {
                     result++;
                 }

1596c3e4977f3d547c5c35525ac26d403d583d67
Ferry To <ferrywlto@gmail.com>
2024-11-28 18:10:26 +0000

    test: All test cases of Q2446 passed

--- csharp/leetcode/easy/Q2446_DetermineIfTwoEventsHaveConflict.cs
+++ csharp/leetcode/easy/Q2446_DetermineIfTwoEventsHaveConflict.cs
@@ -1,8 +1,25 @@
 public class Q2446_DetermineIfTwoEventsHaveConflict
 {
+    // TC: O(1), time used in operation is does not scale with input
+    // SC: O(1), same as time
     public bool HaveConflict(string[] event1, string[] event2)
     {
-        return false;
+        var t1_start = GetTimeInMinutes(event1[0]);
+        var t1_end = GetTimeInMinutes(event1[1]);
+
+        var t2_start = GetTimeInMinutes(event2[0]);
+        var t2_end = GetTimeInMinutes(event2[1]);
+
+        var laterStart = Math.Max(t1_start, t2_start);
+        var soonerEnd = Math.Min(t1_end, t2_end);
+
+        return soonerEnd >= laterStart;
+    }
+    private int GetTimeInMinutes(string input)
+    {
+        var h1_start = int.Parse(input[..2]);
+        var m1_start = int.Parse(input[3..]);
+        return h1_start * 60 + m1_start;
     }
     public static List<object[]> TestData =>
     [

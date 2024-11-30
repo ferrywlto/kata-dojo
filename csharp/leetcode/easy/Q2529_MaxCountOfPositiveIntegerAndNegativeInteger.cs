3abcc5e48f6cebeece7e0c8f5e7417c3ca51c743
Ferry To <ferrywlto@gmail.com>
2024-11-30 22:31:16 +0000

    test: All test cases of Q2529 passed

--- csharp/leetcode/easy/Q2529_MaxCountOfPositiveIntegerAndNegativeInteger.cs
+++ csharp/leetcode/easy/Q2529_MaxCountOfPositiveIntegerAndNegativeInteger.cs
@@ -1,14 +1,23 @@
 public class Q2529_MaxCountOfPositiveIntegerAndNegativeInteger
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int MaximumCount(int[] nums)
     {
-        return 0;
+        var posCount = 0;
+        var negCount = 0;
+        foreach (var n in nums)
+        {
+            if (n < 0) negCount++;
+            else if (n > 0) posCount++;
+        }
+        return Math.Max(posCount, negCount);
     }
     public static List<object[]> TestData =>
     [
         [new int[] {-2,-1,-1,1,2,3}, 3],
-        [new int[] {-2,-1,-1,1,2,3}, 3],
-        [new int[] {-2,-1,-1,1,2,3}, 4],
+        [new int[] {-3,-2,-1,0,0,1,2}, 3],
+        [new int[] {5,20,66,1314}, 4],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

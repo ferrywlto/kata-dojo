43ee8b6684dab586b67373927e90b13253399b35
Ferry To <ferrywlto@gmail.com>
2024-12-01 18:55:22 +0000

    perf: Improve time to O(log n) for Q2529

--- csharp/leetcode/easy/Q2529_MaxCountOfPositiveIntegerAndNegativeInteger.cs
+++ csharp/leetcode/easy/Q2529_MaxCountOfPositiveIntegerAndNegativeInteger.cs
@@ -13,17 +13,63 @@ public class Q2529_MaxCountOfPositiveIntegerAndNegativeInteger
         }
         return Math.Max(posCount, negCount);
     }
+    // TC: O(log n)
+    // SC: O(1)
+    public int MaximumCount_BinarySearch(int[] nums)
+    {
+        var posCount = 0;
+        var negCount = 0;
+        var startIdx = 0;
+        var endIdx = nums.Length - 1;
+
+        // Log n find how many negative
+        while (endIdx - startIdx > 1)
+        {
+            var middleIdx = (startIdx + endIdx) / 2;
+            if (nums[middleIdx] < 0)
+            {
+                negCount = middleIdx + 1;
+                startIdx = middleIdx;
+            }
+            else
+            {
+                endIdx = middleIdx;
+            }
+        }
+        if (nums[endIdx] < 0) negCount++;
+
+        startIdx = 0;
+        endIdx = nums.Length - 1;
+        while (endIdx - startIdx > 1)
+        {
+            var middleIdx = (startIdx + endIdx) / 2;
+            if (nums[middleIdx] > 0)
+            {
+                posCount = nums.Length - middleIdx;
+                endIdx = middleIdx;
+            }
+            else
+            {
+                startIdx = middleIdx;
+            }
+        }
+        if (nums[startIdx] > 0) posCount++;
+
+        return Math.Max(posCount, negCount);
+    }
     public static List<object[]> TestData =>
     [
         [new int[] {-2,-1,-1,1,2,3}, 3],
         [new int[] {-3,-2,-1,0,0,1,2}, 3],
         [new int[] {5,20,66,1314}, 4],
+        [new int[] {-1314, -66, -20, -5}, 4],
+        [new int[] {-1314, -66, -20, -5, 1}, 4],
     ];
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(int[] input, int expected)
     {
-        var actual = MaximumCount(input);
+        var actual = MaximumCount_BinarySearch(input);
         Assert.Equal(expected, actual);
     }
 }

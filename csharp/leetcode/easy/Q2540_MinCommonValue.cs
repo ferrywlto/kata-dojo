8c0e040019bf07c0c098f1d4ee1ee165f88a3905
Ferry To <ferrywlto@gmail.com>
2024-12-02 17:20:40 +0000

    test: All test cases of Q2540 passed

--- csharp/leetcode/easy/Q2540_MinCommonValue.cs
+++ csharp/leetcode/easy/Q2540_MinCommonValue.cs
@@ -1,13 +1,27 @@
 public class Q2540_MinCommonValue
 {
+    // TC: O(n), n scale with total elements in both nums1 and nums2
+    // SC: O(1), space used does not scale with input
     public int GetCommon(int[] nums1, int[] nums2)
     {
-        return 0;
+        var set = new HashSet<int>(nums1);
+        var minCommon = int.MaxValue;
+        foreach (var n in nums2)
+        {
+            if (set.Contains(n))
+            {
+                if (n < minCommon)
+                {
+                    minCommon = n;
+                }
+            }
+        }
+        return minCommon == int.MaxValue ? -1 : minCommon;
     }
     public static List<object[]> TestData =>
     [
-        [new int[] {1,2,3}, new int[] {2,4}],
-        [new int[] {1,2,3,6}, new int[] {2,3,4,5}],
+        [new int[] {1,2,3}, new int[] {2,4}, 2],
+        [new int[] {1,2,3,6}, new int[] {2,3,4,5}, 2],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

1d99800bc4f03dd7fa8a3104f576747d48d5a6a2
Ferry To <ferrywlto@gmail.com>
2024-12-03 21:09:04 +0000

    test: All test cases of Q2553 passed

--- csharp/leetcode/easy/Q2553_SeparateDigitsInArray.cs
+++ csharp/leetcode/easy/Q2553_SeparateDigitsInArray.cs
@@ -1,8 +1,21 @@
 public class Q2553_SeparateDigitsInArray
 {
+    // TC: O(n), n sacle with length of nums
+    // SC: O(m), m scale with total digits among all numbers in nums
     public int[] SeparateDigits(int[] nums)
     {
-        return [];
+        var stack = new Stack<int>();
+        for (var i = nums.Length - 1; i >= 0; i--)
+        {
+            while (nums[i] > 0)
+            {
+                stack.Push(nums[i] % 10);
+                nums[i] /= 10;
+            }
+        }
+        var list = new List<int>();
+        while (stack.Count > 0) list.Add(stack.Pop());
+        return [.. list];
     }
     public static List<object[]> TestData =>
     [

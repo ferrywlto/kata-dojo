1f2ec68af1975786320d5211ea198d43ba3b7ccd
Ferry To <ferrywlto@gmail.com>
2024-12-04 22:10:46 +0000

    test: All test cases of Q2558 passed

--- csharp/leetcode/easy/Q2558_TakeGiftsFromRichestPile.cs
+++ csharp/leetcode/easy/Q2558_TakeGiftsFromRichestPile.cs
@@ -1,8 +1,17 @@
 public class Q2558_TakeGiftsFromRichestPile
 {
+    // TC: O(n log n), n scale with length of gifts, n log n from Array.Sort() * k times, plus n at the end
+    // SC: O(1), space used does not scale with input
     public long PickGifts(int[] gifts, int k)
     {
-        return 0;
+        for (var i = 0; i < k; i++)
+        {
+            Array.Sort(gifts);
+            gifts[^1] = (int)Math.Sqrt(gifts[^1]);
+        }
+        var total = 0L;
+        foreach (var n in gifts) total += n;
+        return total;
     }
     public static List<object[]> TestData =>
     [

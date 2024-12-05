5a5f11d220d270984e41e2ec46d078ae2dd55e35
Ferry To <ferrywlto@gmail.com>
2024-12-05 22:07:46 +0000

    test: All test cases of Q2566 passed

--- csharp/leetcode/easy/Q2566_MaxDifferenceByRemappingDigit.cs
+++ csharp/leetcode/easy/Q2566_MaxDifferenceByRemappingDigit.cs
@@ -1,11 +1,36 @@
 public class Q2566_MaxDifferenceByRemappingDigit
 {
+    // TC: O(d), d scale with digits of num
+    // SC: O(d), same as time for storing the num in string
     public int MinMaxDifference(int num)
     {
-        return 0;
+        var maxStr = num.ToString();
+        var max = num;
+        for (var i = 0; i < maxStr.Length; i++)
+        {
+            var temp = maxStr[i];
+            if (temp != '9')
+            {
+                max = int.Parse(maxStr.Replace(temp, '9'));
+                break;
+            }
+        }
+        var minStr = num.ToString();
+        var min = num;
+        for (var j = 0; j < minStr.Length; j++)
+        {
+            var temp = minStr[j];
+            if (temp != '0')
+            {
+                min = int.Parse(minStr.Replace(temp, '0'));
+                break;
+            }
+        }
+        return max - min;
     }
     public static List<object[]> TestData =>
     [
+        [99999, 99999],
         [11891, 99009],
         [90, 99],
     ];

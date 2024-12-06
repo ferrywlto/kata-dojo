74e37e3f6d44d2b204c9ca16323a51f80cc3b528
Ferry To <ferrywlto@gmail.com>
2024-12-06 23:50:08 +0000

    test: All test cases of Q2578 passed

--- csharp/leetcode/easy/Q2578_SplitWithMinSum.cs
+++ csharp/leetcode/easy/Q2578_SplitWithMinSum.cs
@@ -1,13 +1,45 @@
+using System.Text;
+
 public class Q2578_SplitWithMinSum
 {
+    // TC: O(d), d scale with digits of num, input at most 10 digits
+    // SC: O(d), same as time, length of sb1 and sb2 at most equals to d, dict is also d
     public int SplitNum(int num)
     {
-        return 0;
+        var dict = new Dictionary<int, int>();
+
+        while (num > 0)
+        {
+            var digit = num % 10;
+            if (dict.TryGetValue(digit, out var val)) dict[digit] = ++val;
+            else dict.Add(digit, 1);
+            num /= 10;
+        }
+
+        var sb1 = new StringBuilder();
+        var sb2 = new StringBuilder();
+        var count = 0;
+        for (var i = 0; i < 10; i++)
+        {
+            if (!dict.ContainsKey(i)) continue;
+
+            var freq = dict[i];
+
+            while (freq > 0)
+            {
+                if (count % 2 == 0) sb1.Append(i);
+                else sb2.Append(i);
+                count++;
+                freq--;
+            }
+        }
+        return int.Parse(sb1.ToString()) + int.Parse(sb2.ToString());
     }
     public static List<object[]> TestData =>
     [
         [4325, 59],
         [687, 75],
+        [11, 2],
     ];
     [Theory]
     [MemberData(nameof(TestData))]

1a8bf22bb3c2868bec2bcaad81551876d20be6e4
Ferry To <ferrywlto@gmail.com>
2024-12-09 21:27:09 +0000

    test: All test cases of Q2605 passed

--- csharp/leetcode/easy/Q2605_FormSmallestNumberFromTwoDigitArrays.cs
+++ csharp/leetcode/easy/Q2605_FormSmallestNumberFromTwoDigitArrays.cs
@@ -1,8 +1,32 @@
 public class Q2605_FormSmallestNumberFromTwoDigitArrays
 {
+    // TC: O(n), n scale with length of nums1 plus length of nums2
+    // SC: O(1), space used does not scale with input
     public int MinNumber(int[] nums1, int[] nums2)
     {
-        return 0;
+        var min1 = int.MaxValue;
+        var min2 = int.MaxValue;
+
+        var count = new int[10];
+        for (var i = 0; i < nums1.Length; i++)
+        {
+            if (nums1[i] < min1) min1 = nums1[i];
+            count[nums1[i]]++;
+        }
+        for (var j = 0; j < nums2.Length; j++)
+        {
+            if (nums2[j] < min2) min2 = nums2[j];
+            count[nums2[j]]++;
+        }
+        for (var k = 0; k < count.Length; k++)
+        {
+            if (count[k] > 1) return k;
+        }
+
+        var larger = Math.Max(min1, min2);
+        var smaller = Math.Min(min1, min2);
+
+        return smaller * 10 + larger;
     }
     public static List<object[]> TestData =>
     [

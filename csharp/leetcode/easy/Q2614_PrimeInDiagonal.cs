4d21dd89e61886ba57a121e80202f7025fa9e230
Ferry To <ferrywlto@gmail.com>
2024-12-10 15:48:12 +0000

    test: All test cases of Q2614 passed

--- csharp/leetcode/easy/Q2614_PrimeInDiagonal.cs
+++ csharp/leetcode/easy/Q2614_PrimeInDiagonal.cs
@@ -1,8 +1,46 @@
 public class Q2614_PrimeInDiagonal
 {
+    // TC: O(n), n scale with elements in diagonal
+    // SC: O(m), m scale with unique numbers in diagonal for cache
     public int DiagonalPrime(int[][] nums)
     {
-        return 0;
+        var n = nums.Length;
+        var largest = 0;
+        for (var i = 0; i < n; i++)
+        {
+            var item = nums[i][i];
+            if (IsPrime(item))
+            {
+                if (item > largest)
+                    largest = item;
+            }
+            var lastIdx = n - i - 1;
+            item = nums[i][lastIdx];
+            if (IsPrime(item))
+            {
+                if (item > largest)
+                    largest = item;
+            }
+        }
+        return largest;
+    }
+    Dictionary<int, bool> PrimeTable = [];
+    public bool IsPrime(int input)
+    {
+        if (PrimeTable.TryGetValue(input, out var val)) return val;
+        if (input <= 1) return false;
+        if (input == 2) return true;
+        if (input % 2 == 0) return false;
+        for (var i = 3; i * i <= input; i += 2)
+        {
+            if (input % i == 0)
+            {
+                PrimeTable.Add(input, false);
+                return false;
+            }
+        }
+        PrimeTable.Add(input, true);
+        return true;
     }
     public static TheoryData<int[][], int> TestData => new()
     {
@@ -21,6 +59,15 @@ public class Q2614_PrimeInDiagonal
                 [5,17,7],
                 [9,11,10],
             }, 17
+        },
+        {
+            new int[][]
+            {
+                [788,645,309,559],
+                [624,160,435,724],
+                [770,483,95,695],
+                [510,779,984,238],
+            }, 0
         },
     };
     [Theory]

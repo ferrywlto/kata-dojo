00530737be73e6665603ba77190342ba5aec3ee9
Ferry To <ferrywlto@gmail.com>
2024-12-14 17:33:39 +0000

    test: All test cases of Q2648 passed

--- javascript/leetcode/easy/Q2648_GenerateFibonacciSqeuence.test.js
+++ javascript/leetcode/easy/Q2648_GenerateFibonacciSqeuence.test.js
@@ -1,16 +1,24 @@
-var fibGenerator = function*() {
-    
+var fibGenerator = function* () {
+  yield 0;
+  yield 1;
+  let seq = [0, 1];
+  while (true) {
+    let sum = seq[0] + seq[1];
+    seq.shift();
+    seq.push(sum);
+    yield sum;
+  }
 };
 
-describe('Q2648 Generate Fibonacci Sequence', () => {
-    test.each([
-        [5, [0,1,1,2,3]],
-        [0, []],
-    ])('call count: %i, sequence: %s', (n, expected) => {
-        const gen = fibGenerator();
-        for (var i = 0; i < n; i++)
-        {
-            expect(gen.next().value).toBe(expected[i]);
-        }
-    })
+describe("Q2648 Generate Fibonacci Sequence", () => {
+  test.each([
+    [5, [0, 1, 1, 2, 3]],
+    [0, []],
+  ])("call count: %i, sequence: %s", (n, expected) => {
+    const gen = fibGenerator();
+    for (var i = 0; i < n; i++) {
+      let val = gen.next().value;
+      expect(val).toBe(expected[i]);
+    }
+  });
 });

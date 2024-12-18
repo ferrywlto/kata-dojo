cd9dc59b87ba8057daa0c604a11f064162ecdc1a
Ferry To <ferrywlto@gmail.com>
2024-12-18 19:34:57 +0000

    test: All test cases of Q2670 passed

--- csharp/leetcode/easy/Q2670_FindDistinctDifferenceArray.cs
+++ csharp/leetcode/easy/Q2670_FindDistinctDifferenceArray.cs
@@ -1,8 +1,28 @@
 public class Q2670_FindDistinctDifferenceArray
 {
+    // TC: O(n), n scale with length of nums, 2n in total
+    // SC: O(n + m), n to hold the result plus m where m scale with unique numbers in nums
     public int[] DistinctDifferenceArray(int[] nums)
     {
-        return [];
+        var suffix = new Dictionary<int, int>();
+        var prefix = new HashSet<int>();
+        var result = new int[nums.Length];
+
+        foreach (var n in nums)
+        {
+            if (suffix.TryGetValue(n, out var val)) suffix[n] = ++val;
+            else suffix.Add(n, 1);
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            prefix.Add(nums[i]);
+            suffix[nums[i]]--;
+            if (suffix[nums[i]] == 0) suffix.Remove(nums[i]);
+            result[i] = prefix.Count - suffix.Count;
+        }
+
+        return result;
     }
     public static TheoryData<int[], int[]> TestData => new()
     {

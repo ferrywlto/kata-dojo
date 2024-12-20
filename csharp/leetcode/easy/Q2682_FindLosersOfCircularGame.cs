8169ef1658d75f99e54abf61dd93c92b9b9a6f79
Ferry To <ferrywlto@gmail.com>
2024-12-20 11:32:02 +0000

    test: All test cases of Q2682 passed

--- csharp/leetcode/easy/Q2682_FindLosersOfCircularGame.cs
+++ csharp/leetcode/easy/Q2682_FindLosersOfCircularGame.cs
@@ -1,13 +1,30 @@
 public class Q2682_FindLosersOfCircularGame
 {
+    //TC: O(n), n scale with size of n, worst case all 1 to n are traversed, plus n to select those not traversed into result
+    //SC: O(n), worst case n entries are added
     public int[] CircularGameLosers(int n, int k)
     {
-        return [];
+        var set = new HashSet<int> { 1 };
+        var round = 1;
+        var current = 0;
+        while (true)
+        {
+            current = (current + (round * k)) % n;
+            if (set.Add(current + 1)) round++;
+            else break;
+        }
+        var result = new List<int>();
+        for (var i = 1; i <= n; i++)
+        {
+            if (!set.Contains(i)) result.Add(i);
+        }
+        return result.ToArray();
     }
     public static TheoryData<int, int, int[]> TestData => new()
     {
         {5, 2, [4, 5]},
         {4, 4, [2, 3, 4]},
+        {2, 1, []},
     };
     [Theory]
     [MemberData(nameof(TestData))]

353a0119a5ddde65c2fc462b942d74efec223764
Ferry To <ferrywlto@gmail.com>
2024-12-21 13:41:36 +0000

    test: All test cases of Q2696 passed

--- csharp/leetcode/easy/Q2696_MinStringLengthAfterRemovingSubstrings.cs
+++ csharp/leetcode/easy/Q2696_MinStringLengthAfterRemovingSubstrings.cs
@@ -1,13 +1,35 @@
 public class Q2696_MinStringLengthAfterRemovingSubstrings
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), space used equals to length of s in worst case
     public int MinLength(string s)
     {
-        return 0;
+        var stack = new Stack<char>();
+        var removed = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if
+            (
+                (stack.Count > 0) &&
+                (
+                    (s[i] == 'B' && stack.Peek() == 'A') ||
+                    (s[i] == 'D' && stack.Peek() == 'C')
+                )
+            )
+            {
+                stack.Pop();
+                removed += 2;
+            }
+            else stack.Push(s[i]);
+        }
+
+        return s.Length - removed;
     }
     public static TheoryData<string, int> TestData => new()
     {
         {"ABFCACDB", 2},
         {"ACBBD", 5},
+        {"D", 1},
     };
     [Theory]
     [MemberData(nameof(TestData))]

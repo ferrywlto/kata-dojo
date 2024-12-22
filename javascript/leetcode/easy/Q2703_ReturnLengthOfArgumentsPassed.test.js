15b668adc264004327a2433873117c87ee5971ec
Ferry To <ferrywlto@gmail.com>
2024-12-22 18:01:04 +0000

    test: All test cases of Q2703 passed

--- javascript/leetcode/easy/Q2703_ReturnLengthOfArgumentsPassed.test.js
+++ javascript/leetcode/easy/Q2703_ReturnLengthOfArgumentsPassed.test.js
@@ -1,12 +1,12 @@
-var argumentsLength = function(...args) {
-    
+var argumentsLength = function (...args) {
+  return args.length;
 };
 
-describe('Q2703 Return Length of Arguments Passed', () => {
-    test.each([
-        [[5],1],
-        [[{}, null, "3"], 3],
-    ])('input: %s, expected: %i', (input, expected) => {
-        expect(argumentsLength(input)).toBe(expected);
-    });
+describe("Q2703 Return Length of Arguments Passed", () => {
+  test.each([
+    [[5], 1],
+    [[{}, null, "3"], 3],
+  ])("input: %s, expected: %i", (input, expected) => {
+    expect(argumentsLength(...input)).toBe(expected);
+  });
 });

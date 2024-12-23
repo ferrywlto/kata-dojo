baf06babd3bc76916a1203627001aaa7ca263fd5
Ferry To <ferrywlto@gmail.com>
2024-12-23 22:13:05 +0000

    test: All test cases of Q2710 passed

--- csharp/leetcode/easy/Q2710_RemoveTrailingZerosFromString.cs
+++ csharp/leetcode/easy/Q2710_RemoveTrailingZerosFromString.cs
@@ -1,8 +1,19 @@
 public class Q2710_RemoveTrailingZerosFromString
 {
+    // TC: O(n), n scale with length of num
+    // SC: O(1) if not count the result string, O(n) otherwise 
     public string RemoveTrailingZeros(string num)
     {
-        return string.Empty;
+        var lastIdx = -1;
+        for (var i = num.Length - 1; i >= 0; i--)
+        {
+            if (num[i] != '0')
+            {
+                lastIdx = i + 1;
+                break;
+            }
+        }
+        return new string(num.ToCharArray(), 0, lastIdx);
     }
     public static TheoryData<string, string> TestData => new()
     {

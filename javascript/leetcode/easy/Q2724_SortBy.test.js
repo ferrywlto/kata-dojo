c4c32a9dc50423b712fafcad28784c6a5cf346d0
Ferry To <ferrywlto@gmail.com>
2024-12-26 12:14:47 +0000

    test: All test cases of Q2724 passed

--- javascript/leetcode/easy/Q2724_SortBy.test.js
+++ javascript/leetcode/easy/Q2724_SortBy.test.js
@@ -1,14 +1,39 @@
-var sortBy = function(arr, fn) {
-    
+var sortBy = function (arr, fn) {
+  let result = arr.sort((a, b) => fn(a) - fn(b));
+  return result;
 };
 
-describe('Q2724 Sort By', () => {
-    test('', () => {
-        const arr = [5, 4, 1, 2, 3];
-        let fn = (x) => x;
-        const expected = [1, 2, 3, 4, 5];
+describe("Q2724 Sort By", () => {
+  test("", () => {
+    const arr = [5, 4, 1, 2, 3];
+    let fn = (x) => x;
+    const expected = [1, 2, 3, 4, 5];
 
-        let actual = sortBy(arr, fn);
-        expect(actual).toBe(expected);
-    });
+    let actual = sortBy(arr, fn);
+    expect(actual).toStrictEqual(expected);
+  });
+  test("", () => {
+    const arr = [{ x: 1 }, { x: 0 }, { x: -1 }];
+    let fn = (d) => d.x;
+    const expected = [{ x: -1 }, { x: 0 }, { x: 1 }];
+
+    let actual = sortBy(arr, fn);
+    expect(actual).toStrictEqual(expected);
+  });
+  test("", () => {
+    const arr = [
+      [3, 4],
+      [5, 2],
+      [10, 1],
+    ];
+    let fn = (x) => x[1];
+    const expected = [
+      [10, 1],
+      [5, 2],
+      [3, 4],
+    ];
+
+    let actual = sortBy(arr, fn);
+    expect(actual).toStrictEqual(expected);
+  });
 });

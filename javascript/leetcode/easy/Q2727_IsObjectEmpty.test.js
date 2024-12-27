48f6b207d516d1096e4d1afa9b6bd53fc65b43e5
Ferry To <ferrywlto@gmail.com>
2024-12-27 17:26:05 +0000

    test: All test cases of Q2727 passed

--- javascript/leetcode/easy/Q2727_IsObjectEmpty.test.js
+++ javascript/leetcode/easy/Q2727_IsObjectEmpty.test.js
@@ -1,12 +1,20 @@
-let isEmpty = function (obj) {};
+let isEmpty = function (obj) {
+  if (obj instanceof Object) {
+    return Object.keys(obj).length === 0;
+  } else if (obj instanceof Array) {
+    return obj.length === 0;
+  }
+  return false;
+};
 
 describe("Q2727 Is Object Empty", () => {
   test.each([
     [{ x: 5, y: 42 }, false],
     [{}, true],
     [[null, false, 0], false],
-  ])("", (obj, expected) => {
+  ])("input: %s, expected: %b", (obj, expected) => {
     let actual = isEmpty(obj);
+    console.log(`${obj}, ${actual}, ${expected}`);
     expect(actual).toStrictEqual(expected);
   });
 });

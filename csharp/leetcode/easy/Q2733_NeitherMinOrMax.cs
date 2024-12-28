916e5265d8e377b513ed43f76069f60e9fcbb2bc
Ferry To <ferrywlto@gmail.com>
2024-12-28 15:02:02 +0000

    perf: Improve speed of Q2733 by only sorting the first 3 elements

--- csharp/leetcode/easy/Q2733_NeitherMinOrMax.cs
+++ csharp/leetcode/easy/Q2733_NeitherMinOrMax.cs
@@ -1,11 +1,11 @@
-public class Q2733_NeitherMinOrMax
+public class Q2733_NeitherMinOrMax//(ITestOutputHelper output)
 {
     // TC: O(n log n), due to Array.Sort()
     // SC: O(1), space used does not scale with input
     public int FindNonMinOrMax(int[] nums)
     {
         if (nums.Length <= 2) return -1;
-        Array.Sort(nums);
+        Array.Sort(nums, 0, 3);
         return nums[1];
     }
     public static TheoryData<int[], int> TestData => new()
@@ -13,6 +13,7 @@ public class Q2733_NeitherMinOrMax
         {[3,2,1,4], 2},
         {[1,2], -1},
         {[2,1,3], 2},
+        {[3,30,24], 24},
     };
     [Theory]
     [MemberData(nameof(TestData))]

45e1b002154b9af82a89bb53fc0c5f79add13b58
Ferry To <ferrywlto@gmail.com>
2024-12-28 14:23:39 +0000

    test: All test cases of Q2729 passed

--- csharp/leetcode/easy/Q2729_CheckIfNumberFascinating.cs
+++ csharp/leetcode/easy/Q2729_CheckIfNumberFascinating.cs
@@ -1,8 +1,25 @@
 public class Q2729_CheckIfNumberFascinating
 {
+    // TC: O(n), n scale with number of digits in n
+    // SC: O(1), space used does not scale with input
     public bool IsFascinating(int n)
     {
-        return false;
+        var count = new int[10];
+        var n2 = n * 2;
+        var n3 = n * 3;
+        var numStr = $"{n}{n2}{n3}";
+        var sum = 0;
+        for (var i = 0; i < numStr.Length; i++)
+        {
+            var digit = numStr[i] - '0';
+            if (digit == 0 || count[digit] > 0) return false;
+            else
+            {
+                count[digit]++;
+                sum++;
+            }
+        }
+        return sum == 9;
     }
     public static TheoryData<int, bool> TestData => new()
     {
@@ -15,6 +32,5 @@ public class Q2729_CheckIfNumberFascinating
     {
         var actual = IsFascinating(input);
         Assert.Equal(expected, actual);
-
     }
 }

4b083a687d55313cbc95b2da3c5fc095077a8025
Ferry To <ferrywlto@gmail.com>
2024-12-29 14:59:01 +0000

    test: All test cases of Q2739 passed

--- csharp/leetcode/easy/Q2739_TotalDistanceTraveled.cs
+++ csharp/leetcode/easy/Q2739_TotalDistanceTraveled.cs
@@ -1,13 +1,28 @@
 public class Q2739_TotalDistanceTraveled
 {
+    // TC: O(n), n scale with size of mainTank
+    // SC: O(1), space used does not scale with input
     public int DistanceTraveled(int mainTank, int additionalTank)
     {
-        return 0;
+        var fuelUsed = 0;
+        while (mainTank >= 5)
+        {
+            mainTank -= 5;
+            fuelUsed += 5;
+            if (additionalTank > 0)
+            {
+                additionalTank--;
+                mainTank++;
+            }
+        }
+        fuelUsed += mainTank;
+        return fuelUsed * 10;
     }
     public static TheoryData<int, int, int> TestData => new()
     {
         {5, 10, 60},
         {1, 2, 10},
+        {9, 1, 100},
     };
     [Theory]
     [MemberData(nameof(TestData))]

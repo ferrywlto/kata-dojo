422087a9f506ef85767ecf8244bd4162d7ce20b2
Ferry To <ferrywlto@gmail.com>
2025-01-01 16:41:14 +0000

    test: All test cases of Q2784 passed

--- csharp/leetcode/easy/Q2784_CheckIfArrayIsGood.cs
+++ csharp/leetcode/easy/Q2784_CheckIfArrayIsGood.cs
@@ -1,8 +1,37 @@
 public class Q2784_CheckIfArrayIsGood
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(m), m scale with unique numbers in nums
     public bool IsGood(int[] nums)
     {
-        return false;
+        var freq = new Dictionary<int, int>();
+        var max = int.MinValue;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var current = nums[i];
+            if (current > max)
+            {
+                max = current;
+            }
+
+            if (freq.TryGetValue(current, out var val))
+            {
+                freq[current] = ++val;
+            }
+            else
+            {
+                freq.Add(current, 1);
+            }
+        }
+        if (freq[max] != 2) return false;
+        for (var i = 1; i < max; i++)
+        {
+            if (!freq.TryGetValue(i, out var val) || val != 1)
+            {
+                return false;
+            }
+        }
+        return true;
     }
     public static TheoryData<int[], bool> TestData => new()
     {
@@ -10,6 +39,7 @@ public class Q2784_CheckIfArrayIsGood
         {[1, 3, 3, 2], true},
         {[1, 1], true},
         {[3, 4, 4, 1, 2, 1], false},
+        {[9, 9], false},
     };
     [Theory]
     [MemberData(nameof(TestData))]

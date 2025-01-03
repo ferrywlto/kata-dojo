b036b5db09ed31012b2ef767a3c62c162c90cc95
Ferry To <ferrywlto@gmail.com>
2025-01-03 17:12:01 +0000

    test: All test cases of Q2810 passed

--- csharp/leetcode/easy/Q2810_FaultyKeyboard.cs
+++ csharp/leetcode/easy/Q2810_FaultyKeyboard.cs
@@ -1,8 +1,28 @@
+using System.Text;
+
 public class Q2810_FaultyKeyboard
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), same as time
     public string FinalString(string s)
     {
-        return string.Empty;
+        var sb = new StringBuilder();
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] != 'i')
+            {
+                sb.Append(s[i]);
+            }
+            else
+            {
+                for (var j = 0; j < sb.Length / 2; j++)
+                {
+                    (sb[^(j + 1)], sb[j]) = (sb[j], sb[^(j + 1)]);
+                }
+            }
+        }
+
+        return sb.ToString();
     }
     public static TheoryData<string, string> TestData => new()
     {

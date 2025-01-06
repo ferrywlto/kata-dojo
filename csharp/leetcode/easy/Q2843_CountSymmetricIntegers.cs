7354d6dd5d9098083e3f8862618c3769025edc39
Ferry To <ferrywlto@gmail.com>
2025-01-06 22:12:49 +0000

    test: All test cases of Q2843 passed

--- csharp/leetcode/easy/Q2843_CountSymmetricIntegers.cs
+++ csharp/leetcode/easy/Q2843_CountSymmetricIntegers.cs
@@ -1,8 +1,30 @@
 public class Q2843_CountSymmetricIntegers
 {
+    // TC: O(n), n scale with difference between low and high
+    // SC: O(1), space used does not scale with input
     public int CountSymmetricIntegers(int low, int high)
     {
-        return 0;
+        var result = 0;
+        for (var i = low; i <= high; i++)
+        {
+            if (i >= 10 && i <= 99 && i % 10 != 0)
+            {
+                var lhs = i / 10;
+                var rhs = i % 10;
+                if (lhs == rhs) result++;
+            }
+            else if (i >= 1000 && i <= 9999 && i % 100 != 0)
+            {
+                var lhs = i / 100;
+                var rhs = i % 100;
+
+                var lhsSum = (lhs / 10) + (lhs % 10);
+                var rhsSum = (rhs / 10) + (rhs % 10);
+
+                if (lhsSum == rhsSum) result++;
+            }
+        }
+        return result;
     }
     public static TheoryData<int, int, int> TestData => new()
     {

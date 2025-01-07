97ebe18e3d20f80007422c8c59e6bdc49b368d26
Ferry To <ferrywlto@gmail.com>
2025-01-07 18:03:31 +0000

    test: All test cases of Q2848 passed

--- csharp/leetcode/easy/Q2848_PointsThatIntersectWithCars.cs
+++ csharp/leetcode/easy/Q2848_PointsThatIntersectWithCars.cs
@@ -1,8 +1,21 @@
 public class Q2848_PointsThatIntersectWithCars
 {
+    // TC: O(n), n scale with total sum of range in nums
+    // SC: O(m), m scale with unique number in all range 
     public int NumberOfPoints(IList<IList<int>> nums)
     {
-        return 0;
+        var set = new HashSet<int>();
+        foreach (var pair in nums)
+        {
+            var start = pair[0];
+            var end = pair[1];
+            for (var i = start; i <= end; i++)
+            {
+                set.Add(i);
+            }
+        }
+
+        return set.Count;
     }
     public static TheoryData<IList<IList<int>>, int> TestData => new()
     {

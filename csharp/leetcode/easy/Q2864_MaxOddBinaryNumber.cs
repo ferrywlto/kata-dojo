3d17940d2cc0886b6415cc30e910839b189f48b1
Ferry To <ferrywlto@gmail.com>
2025-01-08 18:02:49 +0000

    test: All test cases of Q2864 passed

--- csharp/leetcode/easy/Q2864_MaxOddBinaryNumber.cs
+++ csharp/leetcode/easy/Q2864_MaxOddBinaryNumber.cs
@@ -1,8 +1,28 @@
+using System.Text;
+
 public class Q2864_MaxOddBinaryNumber
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), same as time to hold the result
     public string MaximumOddBinaryNumber(string s)
     {
-        return string.Empty;
+        var oneCount = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '1') oneCount++;
+        }
+        var sb = new StringBuilder();
+        for (var j = 0; j < oneCount - 1; j++)
+        {
+            sb.Append('1');
+        }
+        for (var k = 0; k < s.Length - oneCount; k++)
+        {
+            sb.Append('0');
+        }
+        sb.Append('1');
+
+        return sb.ToString();
     }
     public static TheoryData<string, string> TestData => new()
     {

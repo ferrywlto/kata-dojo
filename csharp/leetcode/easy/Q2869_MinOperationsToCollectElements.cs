6d8587364967ffa6a452d44aa40488b4414fa6e7
Ferry To <ferrywlto@gmail.com>
2025-01-09 18:09:12 +0000

    test: All test cases of Q2869 passed

--- csharp/leetcode/easy/Q2869_MinOperationsToCollectElements.cs
+++ csharp/leetcode/easy/Q2869_MinOperationsToCollectElements.cs
@@ -1,8 +1,24 @@
 public class Q2869_MinOperationsToCollectElements
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(m), m scale with unique numbers in nums
     public int MinOperations(IList<int> nums, int k)
     {
-        return 0;
+        var sum = (1 + k) * k / 2;
+        var set = new HashSet<int>();
+        for (var i = 0; i < nums.Count; i++)
+        {
+            var last = nums[^(i + 1)];
+            if (last <= k && set.Add(last))
+            {
+                sum -= last;
+                if (sum == 0)
+                {
+                    return i + 1;
+                }
+            }
+        }
+        return -1;
     }
     public static TheoryData<int[], int, int> TestData => new()
     {

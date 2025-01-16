722161a6cc340fedbe2df589f09c3dcce99ffb69
Ferry To <ferrywlto@gmail.com>
2025-01-16 18:01:35 +0000

    test: All test cases of Q2894 passed

--- csharp/leetcode/easy/Q2894_DivisibleAndNonDivisibleSumsDifference.cs
+++ csharp/leetcode/easy/Q2894_DivisibleAndNonDivisibleSumsDifference.cs
@@ -1,8 +1,17 @@
 public class Q2894_DivisibleAndNonDivisibleSumsDifference
 {
+    // TC: O(n), n scale with size of n
+    // SC: O(1), space used does not scale with input
     public int DifferenceOfSums(int n, int m)
     {
-        return 0;
+        var num1 = 0;
+        var num2 = 0;
+        for(var i=1; i<=n; i++)
+        {
+            if(i%m == 0) num2 += i;
+            else num1 += i;
+        }
+        return num1 - num2;
     }
     public static TheoryData<int, int, int> TestData => new()
     {

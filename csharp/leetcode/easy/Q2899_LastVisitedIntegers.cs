74a02b4df7802999dc18175cb92620d145a7db29
Ferry To <ferrywlto@gmail.com>
2025-01-16 18:25:35 +0000

    test: All test cases of Q2899 passed

--- csharp/leetcode/easy/Q2899_LastVisitedIntegers.cs
+++ csharp/leetcode/easy/Q2899_LastVisitedIntegers.cs
@@ -1,8 +1,28 @@
 public class Q2899_LastVisitedIntegers
 {
-    public IList<int> LastVisitedIntegers(int[] nums) {
-     return [];   
-    }    
+    // TC: O(n), n scale with length of nums
+    // SC: O(n+m), n scale with number of positive integers in nums, m scale with length of result
+    public IList<int> LastVisitedIntegers(int[] nums)
+    {
+        var ans = new List<int>();
+        var seen = new List<int>();
+        var neg = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                neg = 0;
+                seen.Add(nums[i]);
+            }
+            else
+            {
+                neg++;
+                if (neg > seen.Count) ans.Add(-1);
+                else ans.Add(seen[^neg]);
+            }
+        }
+        return ans;
+    }
     public static TheoryData<int[], List<int>> TestData => new()
     {
         {[1,2,-1,-1,-1], [2,1,-1]},

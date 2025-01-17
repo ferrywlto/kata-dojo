ecc379d0b7ecdf47598c1cb988bf49e51dca48c0
Ferry To <ferrywlto@gmail.com>
2025-01-17 21:30:52 +0000

    test: All test cases of Q2903 passed

--- csharp/leetcode/easy/Q2903_FindIndicesWithIndexAndValueDifferenceI.cs
+++ csharp/leetcode/easy/Q2903_FindIndicesWithIndexAndValueDifferenceI.cs
@@ -1,8 +1,20 @@
 public class Q2903_FindIndicesWithIndexAndValueDifferenceI
 {
+    // TC: O(n^2), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
     {
-        return [];
+        for (var i = 0; i < nums.Length - indexDifference; i++)
+        {
+            for (var j = i + indexDifference; j < nums.Length; j++)
+            {
+                if (Math.Abs(nums[i] - nums[j]) >= valueDifference)
+                {
+                    return [i, j];
+                }
+            }
+        }
+        return [-1, -1];
     }
     public static TheoryData<int[], int, int, int[]> TestData => new()
     {

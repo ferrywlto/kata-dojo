f065c533c37a11cbd60426eaec16c2090053d9ef
Ferry To <ferrywlto@gmail.com>
2025-01-17 18:33:39 +0000

    test: All test cases of Q2900 passed

--- csharp/leetcode/easy/Q2900_LongestUnequalAdjacentGroupsSubsequenceI.cs
+++ csharp/leetcode/easy/Q2900_LongestUnequalAdjacentGroupsSubsequenceI.cs
@@ -1,8 +1,31 @@
-public class Q2900_LongestUnequalAdjacentGroupsSubsequenceI
+public class Q2900_LongestUnequalAdjacentGroupsSubsequenceI(ITestOutputHelper output)
 {
     public IList<string> GetLongestSubsequence(string[] words, int[] groups)
     {
-        return [];
+        var longestLen = -1;
+        List<int> longestIdxList = new();
+        for (var i=0; i<groups.Length; i++)
+        {
+            var currentList = new List<int> {i};
+            var last = groups[i];
+            for(var j=i+1; j<groups.Length; j++)
+            {
+                if(groups[j] != last) {
+                    currentList.Add(j);
+                    last = groups[j];
+                }
+            }
+            output.WriteLine("list: [{0}], count: {1}", string.Join(',', currentList), currentList.Count);
+            if (currentList.Count > longestLen)
+            {
+                longestLen = currentList.Count;
+                longestIdxList = currentList;
+            }
+            if (groups.Length < longestLen) {
+                break;
+            }
+        }
+        return [.. longestIdxList.Select(x => words[x])];
     }
     public static TheoryData<string[], int[], List<string>> TestData => new()
     {
@@ -11,7 +34,7 @@ public class Q2900_LongestUnequalAdjacentGroupsSubsequenceI
     };
     [Theory]
     [MemberData(nameof(TestData))]
-    public void Test(string[] input, int[] groups, string[] expected)
+    public void Test(string[] input, int[] groups, List<string> expected)
     {
         var actual = GetLongestSubsequence(input, groups);
         Assert.Equal(expected, actual);

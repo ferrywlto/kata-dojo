2c241ede220a6de5d51c6a8f2bc69fbe51eba93b
Ferry To <ferrywlto@gmail.com>
2025-01-18 21:35:26 +0000

    test: All test cases of Q2913 passed

--- csharp/leetcode/easy/Q2913_SubarraysDistinctElementSumOfSquaresI.cs
+++ csharp/leetcode/easy/Q2913_SubarraysDistinctElementSumOfSquaresI.cs
@@ -1,8 +1,23 @@
 public class Q2913_SubarraysDistinctElementSumOfSquaresI
 {
+    // TC: O(n^3), n scale with length of nums
+    // SC: O(m), m scale with unique numbers in nums, worst case nums
     public int SumCounts(IList<int> nums)
     {
-        return 0;
+        var sum = 0;
+        for (var size = 2; size <= nums.Count; size++)
+        {
+            for (var i = 0; i < nums.Count - size + 1; i++)
+            {
+                var set = new HashSet<int>();
+                for (var j = i; j < i + size; j++)
+                {
+                    set.Add(nums[j]);
+                }
+                sum += set.Count * set.Count;
+            }
+        }
+        return sum + nums.Count;
     }
     public static TheoryData<int[], int> TestData => new()
     {

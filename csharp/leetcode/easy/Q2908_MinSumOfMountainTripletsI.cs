6db38204611681916710132125ec9a65d1902381
Ferry To <ferrywlto@gmail.com>
2025-01-18 19:57:43 +0000

    test: All test cases of Q2908 passed

--- csharp/leetcode/easy/Q2908_MinSumOfMountainTripletsI.cs
+++ csharp/leetcode/easy/Q2908_MinSumOfMountainTripletsI.cs
@@ -1,14 +1,35 @@
 public class Q2908_MinSumOfMountainTripletsI
 {
+    // TC: O(n^3), n scale with length of nums because of triplets
+    // SC: O(1), space used does not sacle with input
     public int MinimumSum(int[] nums)
     {
-        return 0;
+        var min = int.MaxValue;
+        for (var i = 0; i < nums.Length - 2; i++)
+        {
+            for (var j = i + 1; j < nums.Length - 1; j++)
+            {
+                for (var k = j + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] < nums[j] && nums[k] < nums[j])
+                    {
+                        var sum = nums[i] + nums[j] + nums[k];
+                        if (sum < min)
+                        {
+                            min = sum;
+                        }
+                    }
+                }
+            }
+        }
+        if (min == int.MaxValue) return -1;
+        return min;
     }
     public static TheoryData<int[], int> TestData => new()
     {
         {[8,6,1,5,3], 9},
         {[5,4,8,7,10,2], 13},
-        {[5,4,8,7,10,2], 13},
+        {[6,5,4,3,4,5], -1},
     };
     [Theory]
     [MemberData(nameof(TestData))]

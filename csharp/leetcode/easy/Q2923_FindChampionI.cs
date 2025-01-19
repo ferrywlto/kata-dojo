bd89e38592be8067fa145ed61017a93a14ef026e
Ferry To <ferrywlto@gmail.com>
2025-01-19 15:57:32 +0000

    test: All test cases of Q2923 passed

--- csharp/leetcode/easy/Q2923_FindChampionI.cs
+++ csharp/leetcode/easy/Q2923_FindChampionI.cs
@@ -1,8 +1,25 @@
 public class Q2923_FindChampionI
 {
+    // TC: O(n), n scale with total number of elements in grid
+    // SC: O(1), space used does not scale with input
     public int FindChampion(int[][] grid)
     {
-        return 0;
+        var largest = 0;
+        var largestIdx = 0;
+        for (var i = 0; i < grid.Length; i++)
+        {
+            var count = 0;
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 1) count++;
+            }
+            if (count > largest)
+            {
+                largest = count;
+                largestIdx = i;
+            }
+        }
+        return largestIdx;
     }
     public static TheoryData<int[][], int> TestData => new()
     {

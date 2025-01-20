fe89aec0b7d08ffd5061fc7e840007c5a77f9b30
Ferry To <ferrywlto@gmail.com>
2025-01-20 19:26:33 +0000

    test: All test cases of Q2832 passed

--- csharp/leetcode/easy/Q2932_MaxStrongPairXorI.cs
+++ csharp/leetcode/easy/Q2932_MaxStrongPairXorI.cs
@@ -1,9 +1,27 @@
 public class Q2932_MaxStrongPairXorI
 {
+    // Even though the question state that the same number can pick twice to
+    // form a strong pair, x ^ x is always 0, so we can ignore same index
+    // TC: O(n^2), n scale with length of nums due to trying all pairs
+    // SC: O(1), space used does not scale with input
     public int MaximumStrongPairXor(int[] nums)
     {
-        return 0;
+        var largestXor = 0;
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j] && IsStrongPair(nums[i], nums[j]))
+                {
+                    var xor = nums[i] ^ nums[j];
+                    if (xor > largestXor) largestXor = xor;
+                }
+            }
+        }
+        return largestXor;
     }
+    private bool IsStrongPair(int x, int y) => Math.Abs(x - y) <= Math.Min(x, y);
+
     public static TheoryData<int[], int> TestData => new()
     {
         {[1,2,3,4,5], 7},

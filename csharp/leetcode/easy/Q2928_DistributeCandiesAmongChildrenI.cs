36bdbb1c13fcd7825ffa7936653f0dfb2545355f
Ferry To <ferrywlto@gmail.com>
2025-01-20 19:09:16 +0000

    test: All test cases of Q2928 passed

--- csharp/leetcode/easy/Q2928_DistributeCandiesAmongChildrenI.cs
+++ csharp/leetcode/easy/Q2928_DistributeCandiesAmongChildrenI.cs
@@ -1,8 +1,19 @@
 public class Q2928_DistributeCandiesAmongChildrenI
 {
+    // TC: O(n^2), due to triplet calculation
+    // SC: O(1), space used does not scale with input
     public int DistributeCandies(int n, int limit)
     {
-        return 0;
+        var result = 0;
+        for (var i = 0; i <= limit; i++)
+        {
+            for (var j = 0; j <= limit; j++)
+            {
+                var possibleK = n - i - j;
+                if(i + j <= n && possibleK <= limit) result++;    
+            }
+        }
+        return result;
     }
     public static TheoryData<int, int, int> TestData => new()
     {

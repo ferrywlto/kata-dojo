dd09e0e0e27b17fcc2e23f445ce821203f05a79a
Ferry To <ferrywlto@gmail.com>
2025-01-21 18:41:24 +0000

    test: All test cases of Q2937 passed

--- csharp/leetcode/easy/Q2937_MakeThreeStringsEqual.cs
+++ csharp/leetcode/easy/Q2937_MakeThreeStringsEqual.cs
@@ -1,13 +1,33 @@
 public class Q2937_MakeThreeStringsEqual
 {
+    // TC: O(n), n scale with min length in s1,s2,s3
+    // SC: O(1), space used does not scale with input
     public int FindMinimumOperations(string s1, string s2, string s3)
     {
-        return 0;
+        var minLen = Math.Min(s1.Length, s2.Length);
+        minLen = Math.Min(minLen, s3.Length);
+
+        var longestPrefixIdx = -1;
+        for (var i = 0; i < minLen; i++)
+        {
+            if (s1[i] != s2[i] || s1[i] != s3[i]) break;
+
+            longestPrefixIdx = i;
+        }
+
+        if (longestPrefixIdx == -1) return -1;
+
+        var idxLen = longestPrefixIdx + 1;
+        var result = s1.Length - idxLen;
+        result += s2.Length - idxLen;
+        result += s3.Length - idxLen;
+        return result;
     }
     public static TheoryData<string, string, string, int> TestData => new()
     {
         {"abc", "abb", "ab", 2},
         {"dac", "bac", "cac", -1},
+        {"a", "a", "a", 0},
     };
     [Theory]
     [MemberData(nameof(TestData))]

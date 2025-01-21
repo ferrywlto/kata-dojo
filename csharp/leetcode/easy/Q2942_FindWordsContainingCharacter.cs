1d4a1504ea9ac1968e5b95b1634bc8a0361c48c8
Ferry To <ferrywlto@gmail.com>
2025-01-21 18:51:15 +0000

    test: All test cases of Q2942 passed

--- csharp/leetcode/easy/Q2942_FindWordsContainingCharacter.cs
+++ csharp/leetcode/easy/Q2942_FindWordsContainingCharacter.cs
@@ -1,8 +1,16 @@
 public class Q2942_FindWordsContainingCharacter
 {
+    // TC: O(n), n scale with length of words, it iterates total characters of times in all words
+    // SC: O(m), m scale with number of words containing x to hold the result
     public IList<int> FindWordsContaining(string[] words, char x)
     {
-        return [];
+        var result = new List<int>();
+        for (var i = 0; i < words.Length; i++)
+        {
+            var word = words[i];
+            if (word.Contains(x)) result.Add(i);
+        }
+        return result;
     }
     public static TheoryData<string[], char, int[]> TestData => new()
     {

7c47b41eeea9c80c8bd2776a2b69d714b2daf632
Ferry To <ferrywlto@gmail.com>
2025-01-22 22:48:30 +0000

    test: All test cases of Q2951 passed

--- csharp/leetcode/easy/Q2951_FindThePeaks.cs
+++ csharp/leetcode/easy/Q2951_FindThePeaks.cs
@@ -1,8 +1,18 @@
 public class Q2951_FindThePeaks
 {
+    // TC: O(n), n scale with length of mountain
+    // SC: O(n), to hold the result, if not counting it then O(1)
     public IList<int> FindPeaks(int[] mountain)
     {
-        return [];
+        var result = new List<int>();
+        for (var i = 1; i < mountain.Length - 1; i++)
+        {
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1])
+            {
+                result.Add(i);
+            }
+        }
+        return result;
     }
     public static TheoryData<int[], List<int>> TestData => new()
     {

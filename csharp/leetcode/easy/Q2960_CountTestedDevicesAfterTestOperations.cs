46b8b5d5e544f4352c2307547124b3da5b3af6a9
Ferry To <ferrywlto@gmail.com>
2025-01-23 18:30:15 +0000

    test: All test cases of Q2960 passed

--- csharp/leetcode/easy/Q2960_CountTestedDevicesAfterTestOperations.cs
+++ csharp/leetcode/easy/Q2960_CountTestedDevicesAfterTestOperations.cs
@@ -1,8 +1,20 @@
 public class Q2960_CountTestedDevicesAfterTestOperations
 {
+    // TC: O(n), n scale with length of batteryPercentages
+    // SC: O(1), space used does not scale with input
     public int CountTestedDevices(int[] batteryPercentages)
     {
-        return 0;
+        var tested = 0;
+        for (var i = 0; i < batteryPercentages.Length; i++)
+        {
+            var device = batteryPercentages[i];
+            if (device > 0)
+            {
+                device -= tested;
+                if (device > 0) tested++;
+            }
+        }
+        return tested;
     }
     public static TheoryData<int[], int> TestData => new()
     {

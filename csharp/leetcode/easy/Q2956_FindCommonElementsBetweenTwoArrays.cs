3c3a30c6cb5a3d310dbf1071a26426f387c3477f
Ferry To <ferrywlto@gmail.com>
2025-01-23 18:18:28 +0000

    test: All test cases of Q2956 passed

--- csharp/leetcode/easy/Q2956_FindCommonElementsBetweenTwoArrays.cs
+++ csharp/leetcode/easy/Q2956_FindCommonElementsBetweenTwoArrays.cs
@@ -1,8 +1,22 @@
 public class Q2956_FindCommonElementsBetweenTwoArrays
 {
+    // TC: O(n), n scale with length of nums1 plus length of nums2
+    // SC: O(m), m scale with unique numbers in nums1 and nums2
     public int[] FindIntersectionValues(int[] nums1, int[] nums2)
     {
-        return [];
+        var set1 = new HashSet<int>(nums1);
+        var set2 = new HashSet<int>(nums2);
+        var ans1 = 0;
+        var ans2 = 0;
+        foreach (var n in nums1)
+        {
+            if (set2.Contains(n)) ans1++;
+        }
+        foreach (var n in nums2)
+        {
+            if (set1.Contains(n)) ans2++;
+        }
+        return [ans1, ans2];
     }
     public static TheoryData<int[], int[], int[]> TestData => new()
     {
@@ -17,5 +31,4 @@ public class Q2956_FindCommonElementsBetweenTwoArrays
         var actual = FindIntersectionValues(input1, input2);
         Assert.Equal(expected, actual);
     }
-
 }

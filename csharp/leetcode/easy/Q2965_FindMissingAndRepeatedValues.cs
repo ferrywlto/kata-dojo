2ccf1ff3c93ec70ba95a89a372556b097071888b
Ferry To <ferrywlto@gmail.com>
2025-01-24 23:52:10 +0000

    test: All test cases of Q2965 passed

--- csharp/leetcode/easy/Q2965_FindMissingAndRepeatedValues.cs
+++ csharp/leetcode/easy/Q2965_FindMissingAndRepeatedValues.cs
@@ -1,8 +1,24 @@
 public class Q2965_FindMissingAndRepeatedValues
 {
+    // TC: O(n), n scale with total numbers in grid
+    // SC: O(n), same as time as all numbers are unique
     public int[] FindMissingAndRepeatedValues(int[][] grid)
     {
-        return [];
+        var len = grid.Length;
+        var sumExpected = (1 + (len * len)) * len * len / 2;
+        var sumActual = 0;
+        var duplicate = 0;
+        var set = new HashSet<int>();
+        foreach (var row in grid)
+        {
+            foreach (var n in row)
+            {
+                if (!set.Add(n)) duplicate = n;
+                else sumActual += n;
+            }
+        }
+
+        return [duplicate, sumExpected - sumActual];
     }
     public static TheoryData<int[][], int[]> TestData => new()
     {

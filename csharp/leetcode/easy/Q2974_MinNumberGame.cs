c3c540b679059342d106fa7c8d83bdde3887170c
Ferry To <ferrywlto@gmail.com>
2025-01-25 00:02:00 +0000

    test: All test cases of Q2974 passed

--- csharp/leetcode/easy/Q2974_MinNumberGame.cs
+++ csharp/leetcode/easy/Q2974_MinNumberGame.cs
@@ -1,8 +1,17 @@
 public class Q2974_MinNumberGame
 {
+    // TC: O(n log n), n scale with length of nums due to Array.Sort()
+    // SC: O(n), n scale with length of nums to hold the result
     public int[] NumberGame(int[] nums)
     {
-        return [];
+        Array.Sort(nums);
+        var result = new int[nums.Length];
+        for (var i = 0; i < nums.Length; i += 2)
+        {
+            result[i] = nums[i + 1];
+            result[i + 1] = nums[i];
+        }
+        return result;
     }
     public static TheoryData<int[], int[]> TestData => new()
     {

fd474209408e42129f789316e9350e5a4b1d5200
Ferry To <ferrywlto@gmail.com>
2025-01-25 21:13:37 +0000

    test: All test cases of Q2980 passed

--- csharp/leetcode/easy/Q2980_CheckIfBitwiseOrHasTrailingZeros.cs
+++ csharp/leetcode/easy/Q2980_CheckIfBitwiseOrHasTrailingZeros.cs
@@ -1,14 +1,25 @@
 public class Q2980_CheckIfBitwiseOrHasTrailingZeros
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public bool HasTrailingZeros(int[] nums)
     {
+        var count = 0;
+        foreach (var n in nums)
+        {
+            if (n % 2 == 0)
+            {
+                count++;
+                if (count == 2) return true;
+            }
+        }
         return false;
     }
     public static TheoryData<int[], bool> TestData => new()
     {
         {[1,2,3,4,5], true},
         {[2,4,8,16], true},
-        {[1,3,5,7,9], true},
+        {[1,3,5,7,9], false},
     };
     [Theory]
     [MemberData(nameof(TestData))]

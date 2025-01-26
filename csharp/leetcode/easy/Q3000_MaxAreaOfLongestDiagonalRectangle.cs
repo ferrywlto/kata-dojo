40744f92e0ee423b5eef6ee40ab6e29b022d6471
Ferry To <ferrywlto@gmail.com>
2025-01-26 11:08:44 +0000

    test: All test cases of Q3000 passed

--- csharp/leetcode/easy/Q3000_MaxAreaOfLongestDiagonalRectangle.cs
+++ csharp/leetcode/easy/Q3000_MaxAreaOfLongestDiagonalRectangle.cs
@@ -1,13 +1,39 @@
 public class Q3000_MaxAreaOfLongestDiagonalRectangle
 {
-    public int AreaOfMaxDiagonal(int[][] dimensions) 
+    // TC: O(n), n scale with length of dimensions.  
+    // SC: O(1), space used does not scale with input
+    public int AreaOfMaxDiagonal(int[][] dimensions)
     {
-        return 0;   
-    }    
-    public static TheoryData<int[][], int> TestData => new ()
+        double longestDiag = -1;
+        int largestArea = -1;
+        for (var i = 0; i < dimensions.Length; i++)
+        {
+            var d = dimensions[i];
+            double diag = Math.Sqrt(d[0] * d[0] + d[1] * d[1]);
+
+            if (diag > longestDiag)
+            {
+                largestArea = d[0] * d[1];
+                longestDiag = diag;
+            }
+            else if (diag == longestDiag)
+            {
+                longestDiag = diag;
+
+                var area = d[0] * d[1];
+                if (area > largestArea)
+                {
+                    largestArea = area;
+                }
+            }
+        }
+        return largestArea;
+    }
+    public static TheoryData<int[][], int> TestData => new()
     {
         {[[9,3],[8,6]], 48},
         {[[3,4],[4,3]], 12},
+        {[[6,5],[8,6],[2,10],[8,1],[9,2],[3,5],[3,5]], 20},
     };
     [Theory]
     [MemberData(nameof(TestData))]

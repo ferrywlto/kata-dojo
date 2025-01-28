6866e2f8b996021471ee63c046d5d336693b620a
Ferry To <ferrywlto@gmail.com>
2025-01-28 22:29:18 +0000

    test: All test cases of Q3024 passed

--- csharp/leetcode/easy/Q3024_TypeOfTriangle.cs
+++ csharp/leetcode/easy/Q3024_TypeOfTriangle.cs
@@ -1,9 +1,29 @@
 public class Q3024_TypeOfTriangle
 {
+    // TC: O(1), constant complexity
+    // SC: O(1), same as time 
     public string TriangleType(int[] nums)
     {
-        return string.Empty;
+        if (IsTriangle(nums))
+        {
+            if (
+                nums[0] == nums[1] &&
+                nums[0] == nums[2]
+            ) return "equilateral";
+            else if (
+                nums[0] == nums[1] ||
+                nums[1] == nums[2] ||
+                nums[2] == nums[0]
+            ) return "isosceles";
+            else return "scalene";
+        }
+        return "none";
     }
+    public bool IsTriangle(int[] input) =>
+    input[0] + input[1] > input[2] &&
+    input[1] + input[2] > input[0] &&
+    input[0] + input[2] > input[1];
+
     public static TheoryData<int[], string> TestData => new()
     {
         {[3,3,3], "equilateral"},

7d176794b85c6847f9c210b10bd9841926287191
Ferry To <ferrywlto@gmail.com>
2025-01-29 13:36:34 +0000

    test: All test cases of Q3033 passed

--- csharp/leetcode/easy/Q3033_ModfiyTheMatrix.cs
+++ csharp/leetcode/easy/Q3033_ModfiyTheMatrix.cs
@@ -1,8 +1,33 @@
 public class Q3033_ModfiyTheMatrix
 {
+    // TC: O(n + m), n scale with total number of items in matrix, m scale with the number of -1 in matrix
+    // SC: O(c + m), c scale with columns of matrix
     public int[][] ModifiedMatrix(int[][] matrix)
     {
-        return [];
+        var colMaxes = new int[matrix[0].Length];
+        var negOneIdxes = new List<(int, int)>();
+        for (var col = 0; col < matrix[0].Length; col++)
+        {
+            var max = matrix[0][col];
+            for (var row = 0; row < matrix.Length; row++)
+            {
+                var current = matrix[row][col];
+                if (current == -1)
+                {
+                    negOneIdxes.Add((row, col));
+                }
+                else if (current > max)
+                {
+                    max = current;
+                }
+            }
+            colMaxes[col] = max;
+        }
+        foreach (var neg in negOneIdxes)
+        {
+            matrix[neg.Item1][neg.Item2] = colMaxes[neg.Item2];
+        }
+        return matrix;
     }
     public static TheoryData<int[][], int[][]> TestData => new()
     {

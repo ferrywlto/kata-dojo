9ae39b68fc4143280ca44b68598acd2833041e0f
Ferry To <ferrywlto@gmail.com>
2025-01-30 22:33:05 +0000

    test: All test cases of Q3042 passed

--- csharp/leetcode/easy/Q3042_CountPrefixAndSuffixPairsI.cs
+++ csharp/leetcode/easy/Q3042_CountPrefixAndSuffixPairsI.cs
@@ -1,8 +1,32 @@
 public class Q3042_CountPrefixAndSuffixPairsI
 {
+    // TC: O(n^2), n scale with length of words, have to iterate in nested loop to test all pairs
+    // SC: O(1), space used does not scale with input 
     public int CountPrefixSuffixPairs(string[] words)
     {
-        return 0;
+        var result = 0;
+        for (var i = 0; i < words.Length - 1; i++)
+        {
+            for (var j = i + 1; j < words.Length; j++)
+            {
+                if (IsPrefixAndSuffix(words[i], words[j]))
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+    private static bool IsPrefixAndSuffix(string str1, string str2)
+    {
+        if (str1.Length > str2.Length) return false;
+
+        var j = str2.Length - str1.Length;
+        for (var i = 0; i < str1.Length; i++)
+        {
+            if (str1[i] != str2[i] || str1[i] != str2[i + j]) return false;
+        }
+        return true;
     }
     public static TheoryData<string[], int> TestData => new()
     {

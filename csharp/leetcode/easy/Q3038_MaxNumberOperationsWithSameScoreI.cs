9ad85a81527d63488aeb3196ec320cc2e5b4e437
Ferry To <ferrywlto@gmail.com>
2025-01-30 22:05:10 +0000

    test: All test cases of Q3038 passed

--- csharp/leetcode/easy/Q3038_MaxNumberOperationsWithSameScoreI.cs
+++ csharp/leetcode/easy/Q3038_MaxNumberOperationsWithSameScoreI.cs
@@ -1,8 +1,21 @@
 public class Q3038_MaxNumberOperationsWithSameScoreI
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int MaxOperations(int[] nums)
     {
-        return 0;
+        var result = 1;
+        var score = nums[0] + nums[1];
+        for (var i = 2; i < nums.Length; i += 2)
+        {
+            if (
+                i == nums.Length - 1 ||
+                nums[i] + nums[i + 1] != score
+            ) return result;
+
+            result++;
+        }
+        return result;
     }
     public static TheoryData<int[], int> TestData => new()
     {

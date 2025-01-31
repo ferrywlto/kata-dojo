c497a38393c6bb3a363fb8b2b18b167997fceb01
Ferry To <ferrywlto@gmail.com>
2025-01-31 22:48:06 +0000

    test: All test cases of Q3046 passed

--- csharp/leetcode/easy/Q3046_SplitTheArray.cs
+++ csharp/leetcode/easy/Q3046_SplitTheArray.cs
@@ -1,11 +1,34 @@
 public class Q3046_SplitTheArray
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(m), m scale with unique numbers in nums
     public bool IsPossibleToSplit(int[] nums)
     {
-        return false;
+        var dict = new Dictionary<int, int>();
+        var singleCount = 0;
+        foreach (var n in nums)
+        {
+            if (dict.TryGetValue(n, out var val))
+            {
+                if (val == 2) return false;
+                else
+                {
+                    dict[n] = ++val;
+                    singleCount--;
+                }
+            }
+            else
+            {
+                dict.Add(n, 1);
+                singleCount++;
+            }
+        }
+
+        return singleCount % 2 == 0;
     }
     public static TheoryData<int[], bool> TestData => new()
     {
+        {[2,10,2,7,8,9,7,6,6,9], true},
         {[1,1,2,2,3,4], true},
         {[1,1,1,1], false},
     };

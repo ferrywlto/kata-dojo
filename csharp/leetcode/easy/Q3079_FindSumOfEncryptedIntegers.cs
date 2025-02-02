c0874b2f9995372bed12f522898797ea3b408750
Ferry To <ferrywlto@gmail.com>
2025-02-02 15:50:06 +0000

    test: All test cases of Q3079 passed

--- csharp/leetcode/easy/Q3079_FindSumOfEncryptedIntegers.cs
+++ csharp/leetcode/easy/Q3079_FindSumOfEncryptedIntegers.cs
@@ -1,8 +1,34 @@
 public class Q3079_FindSumOfEncryptedIntegers
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int SumOfEncryptedInt(int[] nums)
     {
-        return 0;
+        var result = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            result += encrypt(nums[i]);
+        }
+        return result;
+    }
+    private int encrypt(int input)
+    {
+        var largestDigit = 0;
+        var digitCount = 0;
+        while (input > 0)
+        {
+            var d = input % 10;
+            if (d > largestDigit) largestDigit = d;
+            digitCount++;
+            input /= 10;
+        }
+        var result = largestDigit;
+        for (var i = 1; i < digitCount; i++)
+        {
+            result *= 10;
+            result += largestDigit;
+        }
+        return result;
     }
     public static TheoryData<int[], int> TestData => new()
     {

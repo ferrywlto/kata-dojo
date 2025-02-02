bd31dba6059d1c6f00a980bfbd7379994f409f05
Ferry To <ferrywlto@gmail.com>
2025-02-02 16:07:54 +0000

    test: All test cases of Q3083 passed

--- csharp/leetcode/easy/Q3083_ExistenceOfSubstringInStringAndItsReverse.cs
+++ csharp/leetcode/easy/Q3083_ExistenceOfSubstringInStringAndItsReverse.cs
@@ -1,7 +1,20 @@
 public class Q3083_ExistenceOfSubstringInStringAndItsReverse
 {
+    // TC: O(n^2), n scale with length of s
+    // SC: O(1), space used does not scale with input
     public bool IsSubstringPresent(string s)
     {
+        for (var i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] == s[i + 1]) return true;
+            else
+            {
+                for (var j = s.Length - 1; j >= 1; j--)
+                {
+                    if (s[i] == s[j] && s[i + 1] == s[j - 1]) return true;
+                }
+            }
+        }
         return false;
     }
     public static TheoryData<string, bool> TestData => new()

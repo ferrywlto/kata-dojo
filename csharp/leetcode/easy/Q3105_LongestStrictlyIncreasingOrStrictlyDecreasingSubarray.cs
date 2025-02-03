3b66ab19ad2f7956a39156c11490779add7d0375
Ferry To <ferrywlto@gmail.com>
2025-02-03 20:29:22 +0000

    test: All test cases of Q3105 passed

--- csharp/leetcode/easy/Q3105_LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.cs
+++ csharp/leetcode/easy/Q3105_LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.cs
@@ -1,9 +1,31 @@
 public class Q3105_LongestStrictlyIncreasingOrStrictlyDecreasingSubarray
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int LongestMonotonicSubarray(int[] nums)
     {
-        return 0;
+        var longest = 1;
+        var forward = 1;
+        var backward = 1;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                forward++;
+                if (forward > longest) longest = forward;
+            }
+            else forward = 1;
+
+            if (nums[^(i + 1)] > nums[^i])
+            {
+                backward++;
+                if (backward > longest) longest = backward;
+            }
+            else backward = 1;
+        }
+        return longest;
     }
+
     public static TheoryData<int[], int> TestData => new()
     {
         {[1,4,3,3,2], 2},

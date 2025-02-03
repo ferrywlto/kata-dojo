2d4ef3de95edddec85f25951d39522d7d593ea39
Ferry To <ferrywlto@gmail.com>
2025-02-03 22:02:44 +0000

    test: All test cases of Q3099 passed

--- csharp/leetcode/easy/Q3099_HarshadNumber.cs
+++ csharp/leetcode/easy/Q3099_HarshadNumber.cs
@@ -1,10 +1,19 @@
 public class Q3099_HarshadNumber
 {
+    // TC: O(log n), n sink a factor of 10 each iteration
+    // SC: O(1), space used does not scale with input
     public int SumOfTheDigitsOfHarshadNumber(int x)
     {
-        return 0;
+        var n = x;
+        var sum = 0;
+        while (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        return x % sum == 0 ? sum : -1;
     }
-    
+
     public static TheoryData<int, int> TestData => new()
     {
         {18, 9},

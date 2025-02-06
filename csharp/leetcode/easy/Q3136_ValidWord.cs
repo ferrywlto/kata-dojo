8995acd514d7016f73a499552be7c68237c53321
Ferry To <ferrywlto@gmail.com>
2025-02-06 20:13:12 +0000

    test: All test cases of Q3136 passed

--- csharp/leetcode/easy/Q3136_ValidWord.cs
+++ csharp/leetcode/easy/Q3136_ValidWord.cs
@@ -1,14 +1,37 @@
 public class Q3136_ValidWord
 {
+    // TC: O(n), n scale with length of word
+    // SC: O(1), space used does not scale with input
     public bool IsValid(string word)
     {
-        return false;
+        var vowelCount = 0;
+        var nonVowelCount = 0;
+        if (word.Length < 3) return false;
+        foreach (var c in word)
+        {
+            if (!char.IsAsciiLetterOrDigit(c)) return false;
+            else if (!char.IsAsciiDigit(c))
+            {
+                var lower = char.ToLower(c);
+                if (
+                    lower == 'a' ||
+                    lower == 'e' ||
+                    lower == 'i' ||
+                    lower == 'o' ||
+                    lower == 'u'
+                ) vowelCount++;
+                else nonVowelCount++;
+            }
+        }
+
+        return vowelCount > 0 && nonVowelCount > 0;
     }
     public static TheoryData<string, bool> TestData => new()
     {
         {"234Adas", true},
         {"b3", false},
         {"a3$e", false},
+        {"AhI", true},
     };
     [Theory]
     [MemberData(nameof(TestData))]

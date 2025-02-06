020af351c259cf08cc3443f820db4668738c3963
Ferry To <ferrywlto@gmail.com>
2025-02-06 18:04:33 +0000

    test: All test cases of Q3131 passed

--- csharp/leetcode/easy/Q3131_FindIntegerAddedArrayI.cs
+++ csharp/leetcode/easy/Q3131_FindIntegerAddedArrayI.cs
@@ -1,8 +1,18 @@
 public class Q3131_FindIntegerAddedArrayI
 {
+    // TC: O(n), n scale with length of nums1
+    // SC: O(1), space used does not scale with input
     public int AddedInteger(int[] nums1, int[] nums2)
     {
-        return 0;
+        var sum1 = 0;
+        var sum2 = 0;
+        for (var i = 0; i < nums1.Length; i++)
+        {
+            sum1 += nums1[i];
+            sum2 += nums2[i];
+        }
+
+        return (sum2 - sum1) / nums1.Length;
     }
     public static TheoryData<int[], int[], int> TestData => new()
     {

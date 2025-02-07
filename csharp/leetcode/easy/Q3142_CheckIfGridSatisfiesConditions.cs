4f364c88f4e868f76bf698a18864b97bd6a0f42b
Ferry To <ferrywlto@gmail.com>
2025-02-07 22:25:05 +0000

    test: All test cases of Q3142 passed

--- csharp/leetcode/easy/Q3142_CheckIfGridSatisfiesConditions.cs
+++ csharp/leetcode/easy/Q3142_CheckIfGridSatisfiesConditions.cs
@@ -1,14 +1,31 @@
 public class Q3142_CheckIfGridSatisfiesConditions
 {
+    // TC: O(n), n scale with rows x columns in grid
+    // SC: O(1), space used does not scale with input
     public bool SatisfiesConditions(int[][] grid)
     {
-        return false;
+        for (var row = 0; row < grid.Length; row++)
+        {
+            for (var col = 0; col < grid[row].Length; col++)
+            {
+                if (row != grid.Length - 1 &&
+                    grid[row][col] != grid[row + 1][col]) return false;
+
+                if (col != grid[row].Length - 1 &&
+                    grid[row][col] == grid[row][col + 1]) return false;
+            }
+        }
+        return true;
     }
     public static TheoryData<int[][], bool> TestData => new()
     {
         {[[1,0,2],[1,0,2]], true},
         {[[1,1,1],[0,0,0]], false},
         {[[1],[2],[3]], false},
+        {[[1],[1],[1]], true},
+        {[[1,2,3]], true},
+        {[[1,1,3]], false},
+
     };
     [Theory]
     [MemberData(nameof(TestData))]

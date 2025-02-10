b1439dae9530ad47d3cb125522e1e996b7e74d98
Ferry To <ferrywlto@gmail.com>
2025-02-10 21:20:53 +0000

    test: All test cases of Q3174 passed

--- csharp/leetcode/easy/Q3174_ClearDigits.cs
+++ csharp/leetcode/easy/Q3174_ClearDigits.cs
@@ -1,8 +1,18 @@
+using System.Text;
+
 public class Q3174_ClearDigits
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), same as time, worst case the whole s is stored. (no digits)
     public string ClearDigits(string s)
     {
-        return string.Empty;
+        var sb = new StringBuilder();
+        foreach (var c in s)
+        {
+            if (char.IsDigit(c) && sb.Length > 0) sb.Remove(sb.Length - 1, 1);
+            else sb.Append(c);
+        }
+        return sb.ToString();
     }
     public static TheoryData<string, string> TestData => new()
     {

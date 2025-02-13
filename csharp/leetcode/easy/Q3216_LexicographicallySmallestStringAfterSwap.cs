918c3d75f2c999966ae15f00ac8323a828437d8b
Ferry To <ferrywlto@gmail.com>
2025-02-13 18:26:07 +0000

    test: All test cases of Q3216 passed

--- csharp/leetcode/easy/Q3216_LexicographicallySmallestStringAfterSwap.cs
+++ csharp/leetcode/easy/Q3216_LexicographicallySmallestStringAfterSwap.cs
@@ -1,8 +1,24 @@
+using System.Text;
+
 public class Q3216_LexicographicallySmallestStringAfterSwap
 {
+    // TC: O(n), n sacle with length of s
+    // SC: O(n), same as time as the StringBuilder need to hold the whole string to manipulate 
     public string GetSmallestString(string s)
     {
-        return string.Empty;
+        var sb = new StringBuilder(s);
+        for (var i = 0; i < sb.Length - 1; i++)
+        {
+            var valCurr = sb[i] - '0';
+            var valNext = sb[i + 1] - '0';
+            if (valCurr > valNext &&
+            valCurr % 2 == valNext % 2)
+            {
+                (sb[i], sb[i + 1]) = (sb[i + 1], sb[i]);
+                return sb.ToString();
+            }
+        }
+        return s;
     }
     public static TheoryData<string, string> TestData => new()
     {

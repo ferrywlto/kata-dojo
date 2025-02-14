47d44291e99eaee84abf06797de3f2d451ac30c1
Ferry To <ferrywlto@gmail.com>
2025-02-14 22:58:22 +0000

    test: All test cases of Q3222 passed

--- csharp/leetcode/easy/Q3222_FindTheWinningPlayerInCoinGame.cs
+++ csharp/leetcode/easy/Q3222_FindTheWinningPlayerInCoinGame.cs
@@ -1,8 +1,18 @@
 public class Q3222_FindTheWinningPlayerInCoinGame
 {
+    // TC: O(x), x scale with larger between x and y
+    // SC: O(x), same as time  
     public string WinningPlayer(int x, int y)
     {
-        return string.Empty;
+        // the only combination of 115 is 75 + 40
+        var turn = 0;
+        while (x > 0 && y > 3)
+        {
+            turn++;
+            x--;
+            y -= 4;
+        }
+        return turn % 2 == 1 ? "Alice" : "Bob";
     }
     public static TheoryData<int, int, string> TestData => new()
     {

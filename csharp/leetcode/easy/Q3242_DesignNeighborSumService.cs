a525c1b06aba178cada24503967a76f1c20cabac
Ferry To <ferrywlto@gmail.com>
2025-02-16 13:01:00 +0000

    test: All test cases of Q3242 passed

--- csharp/leetcode/easy/Q3242_DesignNeighborSumService.cs
+++ csharp/leetcode/easy/Q3242_DesignNeighborSumService.cs
@@ -1,19 +1,50 @@
 public class NeighborSum
 {
+    private readonly Dictionary<int, int[]> _coords = [];
+    private readonly int[][] _grid;
 
+    // TC: O(n^2), as it have to iterate all items to get the position
+    // SC: O(n^2), same as time
     public NeighborSum(int[][] grid)
     {
-
+        for (var row = 0; row < grid.Length; row++)
+        {
+            for (var col = 0; col < grid[row].Length; col++)
+            {
+                _coords.Add(grid[row][col], [row, col]);
+            }
+        }
+        _grid = grid;
     }
-
+    // TC: O(1)
+    // SC: O(1)
     public int AdjacentSum(int value)
     {
-        return 0;
-    }
+        var coord = _coords[value];
+        var (row, col) = (coord[0], coord[1]);
+        var sum = 0;
 
+        if (row > 0) sum += _grid[row - 1][col];
+        if (row < _grid.Length - 1) sum += _grid[row + 1][col];
+        if (col > 0) sum += _grid[row][col - 1];
+        if (col < _grid[row].Length - 1) sum += _grid[row][col + 1];
+
+        return sum;
+    }
+    // TC: O(1)
+    // SC: O(1)
     public int DiagonalSum(int value)
     {
-        return 0;
+        var coord = _coords[value];
+        var (row, col) = (coord[0], coord[1]);
+        var sum = 0;
+
+        if (row > 0 && col > 0) sum += _grid[row - 1][col - 1];
+        if (row > 0 && col < _grid[row].Length - 1) sum += _grid[row - 1][col + 1];
+        if (row < _grid.Length - 1 && col > 0) sum += _grid[row + 1][col - 1];
+        if (row < _grid.Length - 1 && col < _grid[row].Length - 1) sum += _grid[row + 1][col + 1];
+
+        return sum;
     }
 }
 
@@ -39,10 +70,10 @@ public class Q3242_DesignNeighborSumService
     public void Test(string[] commands, int[][] init, int[] input, int[] expected)
     {
         var sut = new NeighborSum(init);
-        for(var i=0; i<commands.Length; i++)
+        for (var i = 0; i < commands.Length; i++)
         {
             var cmd = commands[i];
-            switch(cmd)
+            switch (cmd)
             {
                 case "adjacentSum":
                     Assert.Equal(expected[i], sut.AdjacentSum(input[i]));

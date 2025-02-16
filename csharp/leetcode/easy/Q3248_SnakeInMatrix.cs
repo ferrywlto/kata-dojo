03498e09203abcfed4bbbda482c513415958bd48
Ferry To <ferrywlto@gmail.com>
2025-02-16 13:23:13 +0000

    test: All test cases of Q3248 passed

--- csharp/leetcode/easy/Q3248_SnakeInMatrix.cs
+++ csharp/leetcode/easy/Q3248_SnakeInMatrix.cs
@@ -1,9 +1,28 @@
 public class Q3248_SnakeInMatrix
 {
+    // TC: O(n), n scale with length of commands
+    // SC: O(1), space used does not scale with input
     public int FinalPositionOfSnake(int n, IList<string> commands)
     {
-        return 0;
+        var result = 0;
+        for (var i = 0; i < commands.Count; i++)
+        {
+            switch (commands[i])
+            {
+                case "UP": result -= n; break;
+                case "DOWN": result += n; break;
+                case "LEFT": result--; break;
+                case "RIGHT": result++; break;
+                default: break;
+            }
+        }
+        return result;
     }
+    public (int row, int col) GetCoord(int input, int n)
+    {
+        return (input / n, input % n);
+    }
+
     public static TheoryData<int, string[], int> TestData => new()
     {
         {2, ["RIGHT","DOWN"], 3},

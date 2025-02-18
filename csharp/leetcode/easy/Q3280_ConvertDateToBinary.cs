7e85fab46b09081ff907e8926a7139e4d6ee1a5a
Ferry To <ferrywlto@gmail.com>
2025-02-18 20:01:08 +0000

    test: All test cases of Q3280 passed

--- csharp/leetcode/easy/Q3280_ConvertDateToBinary.cs
+++ csharp/leetcode/easy/Q3280_ConvertDateToBinary.cs
@@ -1,8 +1,14 @@
 public class Q3280_ConvertDateToBinary
 {
+    // TC: O(1)
+    // SC: O(1)
     public string ConvertDateToBinary(string date)
     {
-        return string.Empty;
+        var year = int.Parse(date.Substring(0, 4));
+        var month = int.Parse(date.Substring(5, 2));
+        var day = int.Parse(date.Substring(8, 2));
+
+        return $"{year:b}-{month:b}-{day:b}";
     }
     public static TheoryData<string, string> TestData => new()
     {

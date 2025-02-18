948248165ea178943e3165111b3c7b3db9b684a0
Ferry To <ferrywlto@gmail.com>
2025-02-18 18:02:05 +0000

    test: All test cases of Q3274 passed

--- csharp/leetcode/easy/Q3274_CheckIfTwoChessboardSquaresHaveTheSameColor.cs
+++ csharp/leetcode/easy/Q3274_CheckIfTwoChessboardSquaresHaveTheSameColor.cs
@@ -1,13 +1,24 @@
 public class Q3274_CheckIfTwoChessboardSquaresHaveTheSameColor
 {
+    // TC: O(1), time used is constant
+    // SC: O(1), space used is constant
     public bool CheckTwoChessboards(string coordinate1, string coordinate2)
     {
-        return false;
+        return color(coordinate1) == color(coordinate2);
+    }
+    public int color(string input)
+    {
+        var x = input[0] - 'a';
+        var y = input[1] - '0';
+        return (x % 2 == y % 2) ? 0 : 1;
     }
     public static TheoryData<string, string, bool> TestData => new()
     {
         {"a1", "c3", true},
         {"a1", "h3", false},
+        {"d4", "e5", true},
+        {"b7", "c6", true},
+        {"a8", "g1", false},
     };
     [Theory]
     [MemberData(nameof(TestData))]

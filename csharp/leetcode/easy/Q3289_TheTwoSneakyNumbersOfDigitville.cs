26e4077bd58c639d307053a4f71ea9806ef12e05
Ferry To <ferrywlto@gmail.com>
2025-02-19 22:20:32 +0000

    test: All test cases of Q3289 passed

--- csharp/leetcode/easy/Q3289_TheTwoSneakyNumbersOfDigitville.cs
+++ csharp/leetcode/easy/Q3289_TheTwoSneakyNumbersOfDigitville.cs
@@ -1,8 +1,22 @@
 public class Q3289_TheTwoSneakyNumbersOfDigitville
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(m), m scale with unique numbers in nums
     public int[] GetSneakyNumbers(int[] nums)
     {
-        return [];
+        var set = new HashSet<int>();
+        var result = new int[2];
+        var resultIdx = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!set.Add(nums[i]))
+            {
+                result[resultIdx] = nums[i];
+                resultIdx++;
+                if (resultIdx == 2) return result;
+            }
+        }
+        return result;
     }
     public static TheoryData<int[], int[]> TestData => new()
     {
@@ -15,6 +29,7 @@ public class Q3289_TheTwoSneakyNumbersOfDigitville
     public void Test(int[] input, int[] expected)
     {
         var actual = GetSneakyNumbers(input);
+        Array.Sort(actual);
         Assert.Equal(expected, actual);
     }
 }

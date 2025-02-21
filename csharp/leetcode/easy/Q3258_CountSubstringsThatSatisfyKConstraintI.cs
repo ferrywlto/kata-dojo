a903d7ab434186255a40b5b5cc758d4fad79919b
Ferry To <ferrywlto@gmail.com>
2025-02-21 20:08:52 +0000

    test: All test cases of Q3258 passed

--- csharp/leetcode/easy/Q3258_CountSubstringsThatSatisfyKConstraintI.cs
+++ csharp/leetcode/easy/Q3258_CountSubstringsThatSatisfyKConstraintI.cs
@@ -1,10 +1,42 @@
 public class Q3258_CountSubstringsThatSatisfyKConstraintI
 {
-    public int CountKConstraintSubstrings(string s, int k) 
+    // TC: O(n^3), n^2 for length of s to find all substrings
+    // SC: O(1), space used does not scale with input
+    public int CountKConstraintSubstrings(string s, int k)
     {
-        return 0;    
-    }    
-    public static TheoryData<string, int, int> TestData => new ()
+        // if size <= k must satisfy
+        // add all combinations from size 1 to k first
+        var result = 0;
+        var size = s.Length;
+        for (var i = 0; i < k; i++)
+        {
+            result += size;
+            size--;
+        }
+        // then from size k to s.Length check each
+        size = k + 1;
+        while (size <= s.Length)
+        {
+            for (var i = 0; i < s.Length - size + 1; i++)
+            {
+                var substring = s.Substring(i, size);
+                if (Satisfy(substring, k)) result++;
+            }
+            size++;
+        }
+        return result;
+    }
+    public bool Satisfy(string input, int k)
+    {
+        var count = new int[2];
+        for (var i = 0; i < input.Length; i++)
+        {
+            var idx = input[i] - '0';
+            count[idx]++;
+        }
+        return count[0] <= k || count[1] <= k;
+    }
+    public static TheoryData<string, int, int> TestData => new()
     {
         {"10101", 1, 12},
         {"1010101", 2, 25},

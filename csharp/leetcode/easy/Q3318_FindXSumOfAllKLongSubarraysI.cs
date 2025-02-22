3f70ccd69fb2dd44ae812a7f23430815464fa47e
Ferry To <ferrywlto@gmail.com>
2025-02-22 13:42:29 +0000

    tes: All test cases of Q3318 passed

--- csharp/leetcode/easy/Q3318_FindXSumOfAllKLongSubarraysI.cs
+++ csharp/leetcode/easy/Q3318_FindXSumOfAllKLongSubarraysI.cs
@@ -1,8 +1,42 @@
 public class Q3318_FindXSumOfAllKLongSubarraysI
 {
+    // TC: O(n^2), n scale with length of nums, for each result it iterates 5n times in XSum
+    // SC: O(n+m), n sacle with the result size, m scale with unique numbers in each subarray 
     public int[] FindXSum(int[] nums, int k, int x)
     {
-        return [];
+        var result = new int[nums.Length - k + 1];
+        for (var i = 0; i < result.Length; i++)
+        {
+            result[i] = XSum(nums, i, i + k - 1, x);
+        }
+        return result;
+    }
+    private int XSum(int[] input, int start, int end, int top)
+    {
+        var dict = new SortedDictionary<int, int>();
+        var sum = 0;
+        for (var i = start; i <= end; i++)
+        {
+            if (dict.TryGetValue(input[i], out var val))
+            {
+                dict[input[i]] = ++val;
+            }
+            else
+            {
+                dict.Add(input[i], 1);
+            }
+            sum += input[i];
+        }
+
+        if (dict.Count < top) return sum;
+
+        sum = dict
+            .OrderByDescending(p => p.Value)
+            .ThenByDescending(p => p.Key)
+            .Take(top)
+            .Sum(p => p.Key * p.Value);
+
+        return sum;
     }
     public static TheoryData<int[], int, int, int[]> TestData => new()
     {

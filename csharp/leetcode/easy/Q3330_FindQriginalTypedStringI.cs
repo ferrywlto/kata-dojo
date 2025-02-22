ed018fb5298f5a9bf3cd3f22854ab022ee0dd3cc
Ferry To <ferrywlto@gmail.com>
2025-02-22 14:19:58 +0000

    test: All test cases of Q3330 passed

--- csharp/leetcode/easy/Q3330_FindQriginalTypedStringI.cs
+++ csharp/leetcode/easy/Q3330_FindQriginalTypedStringI.cs
@@ -1,14 +1,50 @@
 public class Q3330_FindQriginalTypedStringI
 {
+    // TC: O(n), n scale with length of word
+    // SC: O(1), space used does not scale with input
     public int PossibleStringCount(string word)
     {
-        return 0;
+        if (word.Length == 1) return 1;
+
+        var result = 0;
+        var last = word[0];
+        var len = 0;
+        for (var i = 1; i < word.Length; i++)
+        {
+            var ch = word[i];
+            if (ch == last)
+            {
+                if (len == 0) len = 2;
+                else len++;
+            }
+            else
+            {
+                last = ch;
+                if (len > 0)
+                {
+                    result += len - 1;
+                    len = 0;
+                }
+            }
+        }
+        if (len > 0) result += len - 1;
+        // The 1 means the original word, that had no mistakes 
+        return 1 + result;
     }
     public static TheoryData<string, int> TestData => new()
     {
         {"abbcccc", 5},
         {"abcd", 1},
         {"aaaa", 4},
+        {"ere", 1},
+        {"erree", 3},
+        // errreeer
+        // errreeerr
+        // errrerrr
+        // errreerrr
+        // ereeerrr
+        // erreeerrr
+        {"errreeerrr", 7},
     };
     [Theory]
     [MemberData(nameof(TestData))]

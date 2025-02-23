ef60ae4300ebfc951c50581954e4ca8dd5dddf25
Ferry To <ferrywlto@gmail.com>
2025-02-23 13:54:05 +0000

    test: All test cases of Q3345 passed

--- csharp/leetcode/easy/Q3345_SmallestDivisibleDigitProductI.cs
+++ csharp/leetcode/easy/Q3345_SmallestDivisibleDigitProductI.cs
@@ -1,8 +1,26 @@
 public class Q3345_SmallestDivisibleDigitProductI
 {
+    // TC: O(n log 10), n scale inversely, the smaller the n, the more operation required
+    // SC: O(1), space used does not scale with input 
     public int SmallestNumber(int n, int t)
     {
+        for (var i = n; i <= 100; i++)
+        {
+            var product = DigitProduct(i);
+            if (product % t == 0) return i;
+        }
         return 0;
+    }
+    private int DigitProduct(int input)
+    {
+        var product = input % 10;
+        input /= 10;
+        while (input > 0)
+        {
+            product *= input % 10;
+            input /= 10;
+        }
+        return product;
     }
     public static TheoryData<int, int, int> TestData => new()
     {

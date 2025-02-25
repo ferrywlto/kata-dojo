fc5449a4bdba42dc6164ef19169aa3de225548b4
Ferry To <ferrywlto@gmail.com>
2025-02-25 21:23:44 +0000

    test: All test cases of Q3360 passed

--- csharp/leetcode/easy/Q3360_StoneRemovalGame.cs
+++ csharp/leetcode/easy/Q3360_StoneRemovalGame.cs
@@ -1,8 +1,23 @@
 public class Q3360_StoneRemovealGame
 {
+    // TC: O(n), n scale with size of n
+    // SC: O(1), space used does not scale with input
     public bool CanAliceWin(int n)
     {
-        return false;
+        // turn % 2 == 0 => Alice
+        var turn = 0;
+        var stonePickCount = 10;
+        while (stonePickCount > 0)
+        {
+            if (n >= stonePickCount)
+            {
+                n -= stonePickCount;
+                stonePickCount--;
+                turn++;
+            }
+            else return turn % 2 == 1;
+        }
+        return turn % 2 != 0;
     }
     public static TheoryData<int, bool> TestData => new()
     {

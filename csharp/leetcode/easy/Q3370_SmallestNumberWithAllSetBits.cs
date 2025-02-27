843206f589de7e42f95243baa975149b90016ba6
Ferry To <ferrywlto@gmail.com>
2025-02-27 18:13:38 +0000

    test: All test cases of Q3370 passed

--- csharp/leetcode/easy/Q3370_SmallestNumberWithAllSetBits.cs
+++ csharp/leetcode/easy/Q3370_SmallestNumberWithAllSetBits.cs
@@ -1,8 +1,17 @@
 public class Q3370_SmallestNumberWithAllSetBits
 {
+    // TC: O(1), worst case there will only be 10 operations as n <= 1000, 2^10 = 1024
+    // SC: O(1), space used does not scale with input
     public int SmallestNumber(int n)
     {
-        return 0;
+        var x = 1;
+        while (x <= 1000)
+        {
+            if (x >= n) return x;
+            x <<= 1;
+            x++;
+        }
+        return x;
     }
     public static TheoryData<int, int> TestData => new()
     {

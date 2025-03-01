7b6c76709563c6c3d6df381ddd61f1da7fea9f33
Ferry To <ferrywlto@gmail.com>
2025-03-01 14:54:07 +0000

    test: All test cases of Q3392 passed

--- csharp/leetcode/easy/Q3392_CountSubarraysOfLengthThreeWithCondition.cs
+++ csharp/leetcode/easy/Q3392_CountSubarraysOfLengthThreeWithCondition.cs
@@ -1,8 +1,15 @@
 public class Q3392_CountSubarraysOfLengthThreeWithCondition
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int CountSubarrays(int[] nums)
     {
-        return 0;
+        var count = 0;
+        for (var i = 2; i < nums.Length; i++)
+        {
+            if ((nums[i] + nums[i - 2]) * 2 == nums[i - 1]) count++;
+        }
+        return count;
     }
     public static TheoryData<int[], int> TestData => new()
     {

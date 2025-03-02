125ebda66a39bdf4c7c34753017550775201336e
Ferry To <ferrywlto@gmail.com>
2025-03-02 17:42:04 +0000

    test: All test cases of Q3402 passed

--- csharp/leetcode/easy/Q3402_MinOpsToMakeColumnsStrictlyIncreasing.cs
+++ csharp/leetcode/easy/Q3402_MinOpsToMakeColumnsStrictlyIncreasing.cs
@@ -1,13 +1,30 @@
 public class Q3402_MinOpsToMakeColumnsStrictlyIncreasing
 {
+    // TC: O(n^2), it have to iterate all rows except the first
+    // SC: O(1), space used does not scale with input
     public int MinimumOperations(int[][] grid)
     {
-        return 0;
+        var result = 0;
+        for (var row = 1; row < grid.Length; row++)
+        {
+            for (var col = 0; col < grid[row].Length; col++)
+            {
+                if (grid[row][col] <= grid[row - 1][col])
+                {
+                    var diff = grid[row - 1][col] - grid[row][col] + 1;
+                    grid[row][col] += diff;
+                    result += diff;
+                }
+            }
+        }
+        Console.WriteLine(string.Join(Environment.NewLine, grid.Select(row => $"[{string.Join(',', row)}]")));
+        return result;
     }
     public static TheoryData<int[][], int> TestData => new()
     {
         {[[3,2],[1,3],[3,4],[0,1]], 15},
         {[[3,2,1],[2,1,0],[1,2,3]], 12},
+        {[[0,0],[0,0]], 2},
     };
     [Theory]
     [MemberData(nameof(TestData))]

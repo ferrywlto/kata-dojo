06dea3456d73eba5894966be3426630f4d0562f3
Ferry To <ferrywlto@gmail.com>
2025-03-03 22:42:38 +0000

    test: All test cases of Q3417 passed

--- csharp/leetcode/easy/Q3417_ZigzagGridTraversalWithSkip.cs
+++ csharp/leetcode/easy/Q3417_ZigzagGridTraversalWithSkip.cs
@@ -1,8 +1,30 @@
 public class Q3417_ZigzagGridTraversalWithSkip
 {
+    // TC: O(n * m), as each element in grid has to be interated
+    // SC: O(n * m), if counting the result, O(1) otherwise 
     public IList<int> ZigzagTraversal(int[][] grid)
     {
-        return [];
+        var result = new List<int>();
+        for (var i = 0; i < grid.Length; i++)
+        {
+            var rowLen = grid[i].Length;
+            if (i % 2 == 0)
+            {
+                for (var j = 0; j < rowLen; j += 2)
+                {
+                    if (j <= rowLen - 1) result.Add(grid[i][j]);
+                }
+            }
+            else
+            {
+                var start = rowLen % 2 == 0 ? rowLen - 1 : rowLen - 2;
+                for (var j = start; j >= 0; j -= 2)
+                {
+                    if (j >= 0) result.Add(grid[i][j]);
+                }
+            }
+        }
+        return result;
     }
     public static TheoryData<int[][], List<int>> TestData => new()
     {

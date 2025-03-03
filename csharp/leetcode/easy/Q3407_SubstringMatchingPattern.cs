c41eb28ea676f68c259da0863f5588c81ee0a1d4
Ferry To <ferrywlto@gmail.com>
2025-03-03 22:21:18 +0000

    test: All test cases of Q3407 passed

--- csharp/leetcode/easy/Q3407_SubstringMatchingPattern.cs
+++ csharp/leetcode/easy/Q3407_SubstringMatchingPattern.cs
@@ -1,14 +1,28 @@
 public class Q3407_SubstringMatchingPattern
 {
+    // TC: O(n), n scale with length of s due to s.IndexOf() and s.LastIndexOf()
+    // SC: O(1), space used does not scale with input
     public bool HasMatch(string s, string p)
     {
-        return false;
+        if (p.Length == 1) return true;
+
+        var pArr = p.Split('*');
+        var strBeforeAnchor = pArr[0];
+        var strAfterAnchor = pArr[1];
+
+        var sHead = s.IndexOf(strBeforeAnchor);
+        if (sHead == -1) return false;
+        var sTail = s.LastIndexOf(strAfterAnchor);
+        if (sTail == -1) return false;
+
+        return sTail >= sHead + strBeforeAnchor.Length;
     }
     public static TheoryData<string, string, bool> TestData => new()
     {
         {"leetcode", "ee*e", true},
         {"car", "c*v", false},
         {"luck", "u*", true},
+        {"kvb", "k*v", true},
     };
     [Theory]
     [MemberData(nameof(TestData))]

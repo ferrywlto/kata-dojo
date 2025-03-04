986106838a73259fe99d571ffaee8f0d6f45fa50
Ferry To <ferrywlto@gmail.com>
2025-03-04 23:03:00 +0000

    test: All test cases of Q3423 passed

--- csharp/leetcode/easy/Q3423_MaxDiffBetweenAdjacentElementsInCircularArray.cs
+++ csharp/leetcode/easy/Q3423_MaxDiffBetweenAdjacentElementsInCircularArray.cs
@@ -1,13 +1,21 @@
 public class Q3423_MaxDiffBetweenAdjacentElementsInCircularArray
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int MaxAdjacentDistance(int[] nums)
     {
-        return 0;
+        var maxDiff = Math.Abs(nums[0] - nums[^1]);
+        for (var i = 1; i < nums.Length; i++)
+        {
+            var diff = Math.Abs(nums[i] - nums[i - 1]);
+            if (diff > maxDiff) maxDiff = diff;
+        }
+        return maxDiff;
     }
     public static TheoryData<int[], int> TestData => new()
     {
         {[1,2,4], 3},
-        {[-5,-10,-5], 3},
+        {[-5,-10,-5], 5},
     };
     [Theory]
     [MemberData(nameof(TestData))]

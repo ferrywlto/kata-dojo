357f2cb06d16cfa4d9385a09e2f470b842397643
Ferry To <ferrywlto@gmail.com>
2025-03-04 23:51:39 +0000

    test: All test cases of Q3427 passed

--- csharp/leetcode/easy/Q3427_SumOfVariableLengthSubarrays.cs
+++ csharp/leetcode/easy/Q3427_SumOfVariableLengthSubarrays.cs
@@ -1,9 +1,29 @@
 public class Q3427_SumOfVariableLengthSubarrays
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int SubarraySum(int[] nums)
     {
-        return 0;
+        // sum from 0..i
+        var sum = new int[nums.Length];
+        var result = 0;
+        var accumulated = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var start = Math.Max(0, i - nums[i]);
+            accumulated += nums[i];
+            sum[i] = accumulated;
+
+            result += sum[i];
+            if (start != 0)
+            {
+                result -= sum[start - 1];
+            }
+        }
+        return result;
     }
+
     public static TheoryData<int[], int> TestData => new()
     {
         {[2,3,1], 11},

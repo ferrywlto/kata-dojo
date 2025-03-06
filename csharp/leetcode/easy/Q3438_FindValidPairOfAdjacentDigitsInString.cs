59534168747cac2823a896c68c30e3c4694a7f29
Ferry To <ferrywlto@gmail.com>
2025-03-06 21:28:40 +0000

    test: All test cases of Q3438 passed

--- csharp/leetcode/easy/Q3438_FindValidPairOfAdjacentDigitsInString.cs
+++ csharp/leetcode/easy/Q3438_FindValidPairOfAdjacentDigitsInString.cs
@@ -1,7 +1,28 @@
 public class Q3438_FindValidPairOfAdjacentDigitsInString
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(m), m scale with unique characters in s
     public string FindValidPair(string s)
     {
+        var frequencies = new Dictionary<char, int>();
+        for (var i = 0; i < s.Length; i++)
+        {
+            var ch = s[i];
+            if (frequencies.TryGetValue(ch, out var val))
+                frequencies[ch] = ++val;
+            else
+                frequencies.Add(ch, 1);
+        }
+        for (var j = 1; j < s.Length; j++)
+        {
+            var ch1 = s[j - 1];
+            var ch2 = s[j];
+            if (
+                frequencies[ch1] == ch1 - '0'
+                && frequencies[ch2] == ch2 - '0'
+                && ch1 != ch2
+            ) return ch1 + "" + ch2;
+        }
         return string.Empty;
     }
     public static TheoryData<string, string> TestData => new()
@@ -15,6 +36,6 @@ public class Q3438_FindValidPairOfAdjacentDigitsInString
     public void Test(string input, string expected)
     {
         var actual = FindValidPair(input);
-        Assert.Equal(input, expected);
+        Assert.Equal(expected, actual);
     }
 }

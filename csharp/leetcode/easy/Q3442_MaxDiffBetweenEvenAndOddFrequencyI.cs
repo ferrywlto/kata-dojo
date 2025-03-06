e79fcc8a6182c5a79c4b9ccecc10ac6845a23fae
Ferry To <ferrywlto@gmail.com>
2025-03-06 21:57:16 +0000

    test: All test cases of Q3442 passed

--- csharp/leetcode/easy/Q3442_MaxDiffBetweenEvenAndOddFrequencyI.cs
+++ csharp/leetcode/easy/Q3442_MaxDiffBetweenEvenAndOddFrequencyI.cs
@@ -1,13 +1,32 @@
 public class Q3442_MaxDiffBetweenEvenAndOddFrequencyI
 {
+    // TC: O(n + m), n scale with length of s and m scale with unique characters in s
+    // SC: O(m)
     public int MaxDifference(string s)
     {
-        return 0;
+        var freq = new Dictionary<char, int>();
+        var evenMin = int.MaxValue;
+        var oddMax = 1;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var ch = s[i];
+            if (freq.TryGetValue(ch, out var val)) freq[ch] = ++val;
+            else freq.Add(ch, 1);
+        }
+        foreach (var p in freq)
+        {
+            var count = p.Value;
+            if (count % 2 == 0 && count < evenMin) evenMin = count;
+            else if (count % 2 == 1 && count > oddMax) oddMax = count;
+        }
+        return oddMax - evenMin;
     }
     public static TheoryData<string, int> TestData => new()
     {
         {"aaaaabbc", 3},
         {"abcabcab", 1},
+        {"yzyyys", -3},
     };
     [Theory]
     [MemberData(nameof(TestData))]

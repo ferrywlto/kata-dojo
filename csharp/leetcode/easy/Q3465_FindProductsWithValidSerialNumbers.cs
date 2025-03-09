d33b0d6870d9b067b5033d01fcef51564b023f70
Ferry To <ferrywlto@gmail.com>
2025-03-09 14:35:03 +0000

    test: All test cases of Q3465 passed

--- csharp/leetcode/easy/Q3465_FindProductsWithValidSerialNumbers.cs
+++ csharp/leetcode/easy/Q3465_FindProductsWithValidSerialNumbers.cs
@@ -1,9 +1,42 @@
 using Row = (int product_id, string product_name, string description);
 public class Q3465_FindProductsWithValidSerialNumbers(ITestOutputHelper output) : SqlTest(output)
 {
+
     public string Query =>
+    // MSSQL
+    // """
+    // SELECT product_id, product_name, description
+    // FROM products
+    // CROSS APPLY (
+    //     SELECT CHARINDEX('SN', description) AS snIndex
+    // ) ca
+    // WHERE ca.snIndex > 0
+    // AND SUBSTRING(description, ca.snIndex + 6, 1) = '-'
+    // AND SUBSTRING(description, ca.snIndex + 2, 4) LIKE '%[0-9]%'
+    // AND SUBSTRING(description, ca.snIndex + 7, 4) LIKE '%[0-9]%'
+    // AND (
+    //     SUBSTRING(description, ca.snIndex + 11, 1) = ' '
+    //     OR ca.snIndex + 11 > LEN(description)
+    // )
+    // """;
     """
-    Select 1;
+    SELECT product_id, product_name, description
+    FROM (
+        SELECT product_id,
+            product_name,
+            description,
+            instr(description, 'SN') AS snIndex
+        FROM products
+    )
+    WHERE snIndex > 0
+    AND substr(description, snIndex + 6, 1) = '-'
+    AND substr(description, snIndex + 2, 4) NOT GLOB '*[^0-9]*'
+    AND substr(description, snIndex + 7, 4) NOT GLOB '*[^0-9]*'
+    AND (
+            substr(description, snIndex + 11, 1) = ' '
+            OR snIndex + 11 > length(description)
+        )
+    ORDER BY product_id;    
     """;
 
     public static TheoryData<string, Row[]> TestData => new()
@@ -11,10 +44,10 @@ public class Q3465_FindProductsWithValidSerialNumbers(ITestOutputHelper output) 
         {
             """
             insert into products (product_id, product_name, description) values
-            ('1', 'Widget A', 'This is a sample product with SN1234-5678')
-            ('2', 'Widget B', 'A product with serial SN9876-1234 in the description')
-            ('3', 'Widget C', 'Product SN1234-56789 is available now')
-            ('4', 'Widget D', 'No serial number here')
+            ('1', 'Widget A', 'This is a sample product with SN1234-5678'),
+            ('2', 'Widget B', 'A product with serial SN9876-1234 in the description'),
+            ('3', 'Widget C', 'Product SN1234-56789 is available now'),
+            ('4', 'Widget D', 'No serial number here'),
             ('5', 'Widget E', 'Check out SN4321-8765 in this description');
             """,
             new Row[]
@@ -38,14 +71,14 @@ public class Q3465_FindProductsWithValidSerialNumbers(ITestOutputHelper output) 
     public void Test(string testDataSql, Row[] expected)
     {
         ArrangeTestData(testDataSql);
-        var reader = ExecuteQuery(Query);
+        var reader = ExecuteQuery(Query, true);
         AssertResultSchema(reader, ["product_id", "product_name", "description"]);
         foreach (var (product_id, product_name, description) in expected)
         {
             Assert.True(reader.Read());
             Assert.Equal(product_id, reader.GetInt32(0));
             Assert.Equal(product_name, reader.GetString(1));
-            Assert.Equal(description, reader.GetString(1));
+            Assert.Equal(description, reader.GetString(2));
         }
     }
 }

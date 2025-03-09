25bcf17d994f9239ef3b5219ea896b91fb524c96
Ferry To <ferrywlto@gmail.com>
2025-03-09 13:37:47 +0000

    test: All test cases of Q3461 passed

--- csharp/leetcode/easy/Q3461_CheckIfDigitsAreEqualInStringAfterOperationsI.cs
+++ csharp/leetcode/easy/Q3461_CheckIfDigitsAreEqualInStringAfterOperationsI.cs
@@ -1,8 +1,27 @@
+using System.Text;
+
 public class Q3461_CheckIfDigitsAreEqualInStringAfterOperationsI
 {
+    // TC: O(n^2), n scale with length of s, iterates whole string until length is 2
+    // SC: O(n), most space used in the first iteration, n + n - 1
     public bool HasSameDigits(string s)
     {
-        return false;
+        var sb1 = new StringBuilder(s);
+        var sb2 = new StringBuilder();
+        while (sb1.Length > 2)
+        {
+            sb2.Clear();
+            for (var i = 0; i < sb1.Length - 1; i++)
+            {
+                var digit1 = sb1[i] - '0';
+                var digit2 = sb1[i + 1] - '0';
+                var ch = (char)(((digit1 + digit2) % 10) + '0');
+                sb2.Append(ch);
+            }
+            sb1.Clear();
+            sb1.Append(sb2);
+        }
+        return sb1[0] == sb1[1];
     }
     public static TheoryData<string, bool> TestData => new()
     {

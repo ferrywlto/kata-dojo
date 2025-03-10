4b2d875bfa4c1f52d5ef4a7bb3f7a277b875ff6f
Ferry To <ferrywlto@gmail.com>
2025-03-10 23:14:25 +0000

    test: All test cases of Q3467 passed

--- csharp/leetcode/easy/Q3467_TransformArrayByParity.cs
+++ csharp/leetcode/easy/Q3467_TransformArrayByParity.cs
@@ -1,9 +1,22 @@
 public class Q3467_TransformArrayByParity
 {
-    public int[] TransformArray(int[] nums) {
-        return [];
+    // TC: O(n), n scale with length of nums
+    // SC: O(n), same as time
+    public int[] TransformArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+        var idx = nums.Length - 1;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 != 0)
+            {
+                result[idx] = 1;
+                idx--;
+            }
+        }
+        return result;
     }
-    public static TheoryData<int[], int[]> TestData => new ()
+    public static TheoryData<int[], int[]> TestData => new()
     {
         {[4,3,2,1], [0,0,1,1]},
         {[1,5,1,4,2], [0,0,1,1,1]},

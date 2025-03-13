bc8054f0b41dd2ac619344860ef95f6f0f56b6d2
Ferry To <ferrywlto@gmail.com>
2025-03-13 15:00:19 +0000

    doc: Add notes for large input side for reference

--- csharp/leetcode/easy/Q2427_NumberOfCommonFactors.cs
+++ csharp/leetcode/easy/Q2427_NumberOfCommonFactors.cs
@@ -2,7 +2,7 @@ public class Q2427_NumberOfCommonFactors
 {
     // TC: O(n), n scale with minimum between a and b
     // SC: O(1), space used does not scale with input
-    // This use brute force as the constraint is small
+    // This use brute force as the constraint is small, use Euclidean algorithm to get GCD first otherwise
     public int CommonFactors(int a, int b)
     {
         var min = Math.Min(a, b);

84e65a35628d4feccbb64a818a02254f485968c2
Ferry To <ferrywlto@gmail.com>
2025-03-13 14:50:03 +0000

    test: All test cases of Q2389 passed

--- csharp/leetcode/easy/Q2389_LongestSubsequenceWithlimitedSum.cs
+++ csharp/leetcode/easy/Q2389_LongestSubsequenceWithlimitedSum.cs
@@ -1,13 +1,34 @@
 public class Q2389_LongestSubsequenceWithlimitedSum
 {
+    // TC: O(n * m), n scale with length nums, m scale with length of queries
+    // SC: O(n) if counting result, O(1) otherwise
     public int[] AnswerQueries(int[] nums, int[] queries)
     {
-        return [];
+        var answer = new int[queries.Length];
+        Array.Sort(nums);
+        for (var i = 0; i < queries.Length; i++)
+        {
+            var count = 0;
+            var sum = 0;
+            for (var j = 0; j < nums.Length; j++)
+            {
+                sum += nums[j];
+                if (sum <= queries[i]) count++;
+                else
+                {
+                    answer[i] = count;
+                    break;
+                }
+            }
+            if (sum <= queries[i]) answer[i] = count;
+        }
+        return answer;
     }
     public static TheoryData<int[], int[], int[]> TestData => new()
     {
         {[2,3,4,5], [1], [0]},
         {[4,5,2,1], [3,10,21], [2,3,4]},
+        {[469781,45635,628818,324948,343772,713803,452081], [816646,929491], [3,3]},
     };
     [Theory]
     [MemberData(nameof(TestData))]

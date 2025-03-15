5cc206c55059bba058fc6a32280f0c0efe9c4060
Ferry To <ferrywlto@gmail.com>
2025-03-15 12:29:36 +0000

    test: All test cases of Q3090 passed

--- csharp/leetcode/easy/Q3090_MaxLengthSubstringWithTwoOccurences.cs
+++ csharp/leetcode/easy/Q3090_MaxLengthSubstringWithTwoOccurences.cs
@@ -1,11 +1,38 @@
 public class Q3090_MaxLengthSubstringWithTwoOccurences
 {
+    // TC: O(n^2), n scale with length of s with expansion technique
+    // SC: O(1), space used does not scale with input
     public int MaximumLengthSubstring(string s)
     {
-        return 0;
+        var freq = new int[26];
+        var maxLen = 0;
+        var currLen = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i + maxLen >= s.Length) break;
+
+            for (var j = i; j < s.Length; j++)
+            {
+                var ch = s[j];
+                var idx = ch - 'a';
+                if (freq[idx] < 2)
+                {
+                    freq[idx]++;
+                    currLen++;
+                }
+                else break;
+            }
+            if (currLen > maxLen) maxLen = currLen;
+            currLen = 0;
+            freq = new int[26];
+        }
+        if (currLen > maxLen) maxLen = currLen;
+        return maxLen;
     }
     public static TheoryData<string, int> TestData => new()
     {
+        {"abcdddbca", 5},
         {"bcbbbcba", 4},
         {"aaaa", 2},
     };

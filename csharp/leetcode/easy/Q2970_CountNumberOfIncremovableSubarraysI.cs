30509c63795145c4fafe7faedd82deccf0847a5c
Ferry To <ferrywlto@gmail.com>
2025-03-15 14:18:33 +0000

    test: All test cases of Q2970 passed

--- csharp/leetcode/easy/Q2970_CountNumberOfIncremovableSubarraysI.cs
+++ csharp/leetcode/easy/Q2970_CountNumberOfIncremovableSubarraysI.cs
@@ -1,8 +1,43 @@
 public class Q2970_CountNumberOfIncremovableSubarraysI
 {
+    // TC: O(n^3), n scale with length of nums
+    // SC: O(n), temp is the same size as input
     public int IncremovableSubarrayCount(int[] nums)
     {
-        return 0;
+        var result = 0;
+        // expansion
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var temp = new int[nums.Length];
+            Array.Copy(nums, temp, nums.Length);
+            for (var j = i; j < nums.Length; j++)
+            {
+                temp[j] = 0;
+                if (Incresing(temp)) result++;
+            }
+        }
+        return result;
+    }
+    private bool Incresing(int[] input)
+    {
+        var lastNonZeroIdx = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] > 0)
+            {
+                lastNonZeroIdx = i;
+                break;
+            }
+        }
+        for (var i = lastNonZeroIdx + 1; i < input.Length; i++)
+        {
+            if (input[i] > 0 && input[i] <= input[lastNonZeroIdx]) return false;
+            else if (input[i] > 0)
+            {
+                lastNonZeroIdx = i;
+            }
+        }
+        return true;
     }
     public static TheoryData<int[], int> TestData => new()
     {

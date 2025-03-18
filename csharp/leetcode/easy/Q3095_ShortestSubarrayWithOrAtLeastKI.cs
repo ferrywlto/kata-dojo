de17ea15fd08beec60329525dfc3bf889f9e27e3
Ferry To <ferrywlto@gmail.com>
2025-03-18 22:26:28 +0000

    test: All test cases of Q3095 passed

--- csharp/leetcode/easy/Q3095_ShortestSubarrayWithOrAtLeastKI.cs
+++ csharp/leetcode/easy/Q3095_ShortestSubarrayWithOrAtLeastKI.cs
@@ -1,8 +1,30 @@
 public class Q3095_ShortestSubarrayWithOrAtLeastKI
 {
+    // TC: O(n^3), n scale with length of nums
+    // SC: O(1)
     public int MinimumSubarrayLength(int[] nums, int k)
     {
-        return 0;
+        var size = 0;
+        while (size < nums.Length)
+        {
+            for (var i = 0; i < nums.Length - size; i++)
+            {
+                if (Or(nums, i, size) >= k) return size + 1;
+            }
+            size++;
+        }
+        return -1;
+    }
+    private int Or(int[] input, int startIdx, int size)
+    {
+        if (input.Length == 1) return input[startIdx];
+
+        var result = 0;
+        for (var i = startIdx; i <= startIdx + size; i++)
+        {
+            result |= input[i];
+        }
+        return result;
     }
     public static TheoryData<int[], int, int> TestData => new()
     {

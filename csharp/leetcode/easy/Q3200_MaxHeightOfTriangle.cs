9d920d0989641873cca140d83024fb71de4b8594
Ferry To <ferrywlto@gmail.com>
2025-03-19 22:49:24 +0000

    test: All test cases of Q3200 passed

--- csharp/leetcode/easy/Q3200_MaxHeightOfTriangle.cs
+++ csharp/leetcode/easy/Q3200_MaxHeightOfTriangle.cs
@@ -1,8 +1,41 @@
 public class Q3200_MaxHeightOfTriangle
 {
+    // TC: O(n), n scale with larger of red and blue
+    // SC: O(1), space used does not scale with input
     public int MaxHeightOfTriangle(int red, int blue)
     {
-        return 0;
+        return Math.Max(Cal(red, blue), Cal(blue, red));
+    }
+    private int Cal(int r, int b)
+    {
+        var step = 1;
+        while (true)
+        {
+            if (step % 2 == 1)
+            {
+                if (step <= r)
+                {
+                    r -= step;
+                    step++;
+                }
+                else
+                {
+                    return step - 1;
+                }
+            }
+            else
+            {
+                if (step <= b)
+                {
+                    b -= step;
+                    step++;
+                }
+                else
+                {
+                    return step - 1;
+                }
+            }
+        }
     }
     public static TheoryData<int, int, int> TestData => new()
     {

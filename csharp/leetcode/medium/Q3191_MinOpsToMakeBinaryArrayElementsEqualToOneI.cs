8e820964415df72d7bcd79d6ab4fa28ae692b1f7
Ferry To <ferrywlto@gmail.com>
2025-03-20 21:54:31 +0000

    test: All test cases of Q3191 passed

--- csharp/leetcode/medium/Q3191_MinOpsToMakeBinaryArrayElementsEqualToOneI.cs
+++ csharp/leetcode/medium/Q3191_MinOpsToMakeBinaryArrayElementsEqualToOneI.cs
@@ -1,7 +1,28 @@
 public class Q3191_MinOpsToMakeBinaryArrayElementsEqualToOneI
 {
+    // TC: O(n), n sacle with length of nums, worst case 3n
+    // SC: O(1), space used does not sacle with input
     public int MinOperations(int[] nums) {
-        return 0;
+        var oneCount = 0;
+        var ops = 0;
+        for(var i=0; i<nums.Length; i++)
+        {
+            if(nums[i] == 0)
+            {
+                if(i >= nums.Length - 2) return -1;
+                for(var j=i; j<i+3; j++)
+                {
+                    if(nums[j] == 0) {
+                        nums[j] = 1;
+                        oneCount++;
+                    }
+                    else nums[j] = 0;
+                }
+                ops++;
+            }
+            else oneCount++;
+        }
+        return ops;
     }    
     public static TheoryData<int[], int> TestData => new ()
     {

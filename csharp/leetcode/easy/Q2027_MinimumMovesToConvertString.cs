c5c61aec6e4459f3b540b5ae1274b695705935bc
Ferry To <ferrywlto@gmail.com>
2025-03-21 23:15:54 +0000

    test: All test cases of Q2027 passed

--- csharp/leetcode/easy/Q2027_MinimumMovesToConvertString.cs
+++ csharp/leetcode/easy/Q2027_MinimumMovesToConvertString.cs
@@ -1,42 +1,27 @@
 public class Q2027_MinimumMovesToConvertString
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used does not scale with input
     public int MinimumMoves(string s)
     {
-        var minMoves = int.MaxValue;
-
-        for (var k = 0; k < 3; k++)
+        var moves = 0;
+        for (var i = 0; i < s.Length; i++)
         {
-            var moves = 0;
-            for (var i = k; i < s.Length; i += 3)
-            {
-                var targetIdx = Math.Min(i + 3, s.Length);
-
-                for (var j = i; j < targetIdx; j++)
-                {
-                    if (s[j] == 'X')
-                    {
-                        moves++;
-                        break;
-                    }
-                }
-            }
-            if (k == 1 && s[0] == 'X') moves++;
-            else if (k == 2 && (s[0] == 'X' || s[1] == 'X'))
+            if (s[i] == 'X')
             {
                 moves++;
-            }
-            if (moves < minMoves)
-            {
-                minMoves = moves;
+                i += 2;
             }
         }
-        return minMoves;
+
+        return moves;
     }
     public static TheoryData<string, int> TestData => new()
     {
       {"XXX", 1},
       {"XXOX", 2},
       {"OOOO", 0},
+      {"XXXOOXXX", 2},
     };
     [Theory]
     [MemberData(nameof(TestData))]

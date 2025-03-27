f6d1ab720771d080825f93b9792f82ef53c4d3d9
Ferry To <ferrywlto@gmail.com>
2025-03-27 20:40:07 +0000

    test: All test cases of Q2161 passed

--- csharp/leetcode/medium/Q2161_PartitionArrayAccordingToGivenPivot.cs
+++ csharp/leetcode/medium/Q2161_PartitionArrayAccordingToGivenPivot.cs
@@ -1,8 +1,34 @@
 public class Q2161_PartitionArrayAccordingToGivenPivot
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(n), the three queues take at most n space
     public int[] PivotArray(int[] nums, int pivot)
     {
-        return [];
+        var lessThan = new Queue<int>();
+        var largerThan = new Queue<int>();
+        var equals = new Queue<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > pivot)
+                largerThan.Enqueue(nums[i]);
+
+            else if (nums[i] < pivot)
+                lessThan.Enqueue(nums[i]);
+
+            else equals.Enqueue(nums[i]);
+        }
+
+        var idx = 0;
+        while (lessThan.Count > 0)
+            nums[idx++] = lessThan.Dequeue();
+
+        while (equals.Count > 0)
+            nums[idx++] = equals.Dequeue();
+
+        while (largerThan.Count > 0)
+            nums[idx++] = largerThan.Dequeue();
+
+        return nums;
     }
     public static TheoryData<int[], int, int[]> TestData => new()
     {

c50b0e04cbe2c939cae8eff50c1e46d641c2811c
Ferry To <ferrywlto@gmail.com>
2025-03-28 22:32:04 +0000

    perf: Improve time for Q2161 by using one less loop

--- csharp/leetcode/medium/Q2161_PartitionArrayAccordingToGivenPivot.cs
+++ csharp/leetcode/medium/Q2161_PartitionArrayAccordingToGivenPivot.cs
@@ -8,15 +8,12 @@ public class Q2161_PartitionArrayAccordingToGivenPivot
         var ans = new int[len];
         for (var i = 0; i < len; i++) ans[i] = pivot;
 
-        var idx = 0;
+        var startIdx = 0;
+        var endIdx = len - 1;
         for (var i = 0; i < len; i++)
         {
-            if (nums[i] < pivot) ans[idx++] = nums[i];
-        }
-        idx = len - 1;
-        for (var i = len - 1; i >= 0; i--)
-        {
-            if (nums[i] > pivot) ans[idx--] = nums[i];
+            if (nums[i] < pivot) ans[startIdx++] = nums[i];
+            if (nums[len - 1 - i] > pivot) ans[endIdx--] = nums[len - 1 - i];
         }
 
         return ans;

89b2b90d2fefa803fdf32b858b31d2823482dd82
Ferry To <ferrywlto@gmail.com>
2025-03-28 22:02:57 +0000

    test: All test cases of Q2181 passed

--- csharp/leetcode/medium/Q2181_MergeNodesInBetweenZeros.cs
+++ csharp/leetcode/medium/Q2181_MergeNodesInBetweenZeros.cs
@@ -1,8 +1,35 @@
 public class Q2181_MergeNodesInBetweenZeros
 {
+    // TC: O(n), n scale with length of input list
+    // SC: O(m), m scale with length of result;
     public ListNode MergeNodes(ListNode head)
     {
-        return new ListNode();
+        var list = new List<int>();
+        var sum = 0;
+        var curr = head;
+        while (curr != null)
+        {
+            if (curr.Val == 0 && sum > 0)
+            {
+                list.Add(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += curr.Val;
+            }
+            curr = curr.Next;
+        }
+
+        var ans = new ListNode(list[0]);
+        var node = ans;
+        for (var i = 1; i < list.Count; i++)
+        {
+            var next = new ListNode(list[i]);
+            node.Next = next;
+            node = node.Next;
+        }
+        return ans;
     }
     public static TheoryData<ListNode, ListNode> TestData => new()
     {

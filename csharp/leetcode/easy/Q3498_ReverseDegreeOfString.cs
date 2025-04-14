2cf456f4fe2ed04dfaa7100a990da2d3b7a04620
Ferry To <ferrywlto@gmail.com>
2025-04-14 22:48:28 +0100

    test: All test cases of Q3498 passed

--- csharp/leetcode/easy/Q3498_ReverseDegreeOfString.cs
+++ csharp/leetcode/easy/Q3498_ReverseDegreeOfString.cs
@@ -1,8 +1,24 @@
 public class Q3498_ReverseDegreeOfString
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used does not scale with input
     public int ReverseDegree(string s)
     {
-        return 0;
+        var map = new int[26];
+        var start = 26;
+        for (var i = 0; i < map.Length; i++)
+        {
+            map[i] = start--;
+        }
+
+        var result = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            var idx = s[i] - 'a';
+            var revIdx = map[idx];
+            result += (i + 1) * revIdx;
+        }
+        return result;
     }
     public static TheoryData<string, int> TestData => new()
     {

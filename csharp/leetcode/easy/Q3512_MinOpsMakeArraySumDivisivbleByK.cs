de7bc671da971b20b506d306090fee865cc396e4
Ferry To <ferrywlto@gmail.com>
2025-04-16 23:31:29 +0100

    test: All test cases of Q3512 passed

--- csharp/leetcode/easy/Q3512_MinOpsMakeArraySumDivisivbleByK.cs
+++ csharp/leetcode/easy/Q3512_MinOpsMakeArraySumDivisivbleByK.cs
@@ -1,10 +1,18 @@
 public class Q3512_MinOpsMakeArraySumDivisivbleByK
 {
-    public int MinOperations(int[] nums, int k) 
+    // TC: O(n), n scale with lenght of nums
+    // SC: O(1), space used does not scale with input
+    public int MinOperations(int[] nums, int k)
     {
-        return 0;    
+        var sum = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        var remainer = sum % k;
+        return remainer;
     }
-    public static TheoryData<int[], int, int> TestData = new () 
+    public static TheoryData<int[], int, int> TestData = new()
     {
         {[3,9,7], 5, 4},
         {[4,1,3], 4, 0},

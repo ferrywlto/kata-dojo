872c72369df5f8fe6193dbee2b524b53f90095d0
Ferry To <ferrywlto@gmail.com>
2025-04-20 14:28:30 +0100

    test: All test cases of Q1038 passed

--- csharp/leetcode/medium/Q1038_BinarySearchTreeToGreaterSumTree.cs
+++ csharp/leetcode/medium/Q1038_BinarySearchTreeToGreaterSumTree.cs
@@ -1,22 +1,50 @@
 
 public class Q1038_BinarySearchTreeToGreaterSumTree(ITestOutputHelper output) : TreeNodeTest(output)
 {
-    public TreeNode BstToGst(TreeNode root) {
-        // 0,1,2,3,4,5,6,7,8
-        // 0,  1, 3, 6,10,15,21,28,36
-        // 0+36, 1+35, 2+33, 3+30, 4+25, 5+21, 6+15, 7+8, 8+0 
-        // 36. , 36,     35,   33,   30,   26,   21,  15, 8
+    // TC: O(n), n scale with number of nodes in tree
+    // SC: O(d), d scale with the max depth of the tree
+    // This solution solve in single pass
+    public TreeNode BstToGst(TreeNode root)
+    {
+        var sum = 0;
+        var stack = new Stack<TreeNode>();
+        var curr = root;
+        stack.Push(curr);
+
+        while (stack.Count > 0)
+        {
+            // curr = stack.Peek();
+            while (curr!.right != null)
+            {
+                stack.Push(curr.right);
+                curr = curr.right;
+            }
+
+            // process node
+            var node = stack.Pop();
+            var temp = node.val;
+            node.val += sum;
+            sum += temp;
+
+            Output.WriteLine(node.val.ToString());
+
+            if (node.left != null)
+            {
+                stack.Push(node.left);
+                curr = node.left;
+            }
+        }
         return root;
     }
-    public static TheoryData<TreeNode, TreeNode> TestData => new ()
+    public static TheoryData<TreeNode, TreeNode> TestData => new()
     {
         {
-            TreeNode.FromLevelOrderingIntArray([4,1,6,0,2,5,7,null,null,null,3,null,null,null,8])!, 
+            TreeNode.FromLevelOrderingIntArray([4,1,6,0,2,5,7,null,null,null,3,null,null,null,8])!,
             TreeNode.FromLevelOrderingIntArray([30,36,21,36,35,26,15,null,null,null,33,null,null,null,8])!
         },
         {
-            TreeNode.FromLevelOrderingIntArray([0,null,1])!, 
-            TreeNode.FromLevelOrderingIntArray([1,null,1])!            
+            TreeNode.FromLevelOrderingIntArray([0,null,1])!,
+            TreeNode.FromLevelOrderingIntArray([1,null,1])!
         }
     };
     [Theory]

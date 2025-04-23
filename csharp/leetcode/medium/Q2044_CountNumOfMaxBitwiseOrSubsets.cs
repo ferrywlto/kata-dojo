f40238da3f99937e30e72735dfa76ae4e262107e
Ferry To <ferrywlto@gmail.com>
2025-04-23 23:20:56 +0100

    test: All test cases of Q2044 passed

--- csharp/leetcode/medium/Q2044_CountNumOfMaxBitwiseOrSubsets.cs
+++ csharp/leetcode/medium/Q2044_CountNumOfMaxBitwiseOrSubsets.cs
@@ -1,9 +1,34 @@
 public class Q2044_CountNumOfMaxBitwiseOrSubsets
 {
+    // TC: O(2^n), used brute force for small constraints
+    // SC: O(1), space used does not scale with input
     public int CountMaxOrSubsets(int[] nums)
     {
-        return 0;
+        var max = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            max |= nums[i];
+        }
+
+        var result = 0; // the whole array must be one of them
+
+        // Loop through all possible subsets using bit manipulation
+        var n = nums.Length;
+        for (var i = 0; i < (1 << n); i++)
+        {
+            var temp = 0;
+            for (var j = 0; j < n; j++)
+            {
+                if ((i & (1 << j)) != 0)
+                {
+                    temp |= nums[j];
+                }
+            }
+            if (temp == max) result++;
+        }
+        return result;
     }
+
     public static TheoryData<int[], int> TestData => new()
     {
         {[3,1], 2},

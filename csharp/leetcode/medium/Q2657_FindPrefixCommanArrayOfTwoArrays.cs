d7e6322212f2baa814477f271c36421b55d2160e
Ferry To <ferrywlto@gmail.com>
2025-04-26 00:33:04 +0100

    test: All test cases of Q2657 passed

--- csharp/leetcode/medium/Q2657_FindPrefixCommanArrayOfTwoArrays.cs
+++ csharp/leetcode/medium/Q2657_FindPrefixCommanArrayOfTwoArrays.cs
@@ -2,11 +2,32 @@ public class Q2657_FindPrefixCommanArrayOfTwoArrays
 {
     public int[] FindThePrefixCommonArray(int[] A, int[] B)
     {
-        return [];
+        var setA = new HashSet<int>();
+        var setB = new HashSet<int>();
+        var result = new int[A.Length];
+        for (var i = 0; i < A.Length; i++)
+        {
+            setA.Add(A[i]);
+            setB.Add(B[i]);
+            var commonCount = 0;
+            foreach (var n in setB)
+            {
+                if (setA.Contains(n))
+                {
+                    commonCount++;
+                }
+            }
+            result[i] = commonCount;
+        }
+        return result;
     }
     public static TheoryData<int[], int[], int[]> TestData => new()
     {
-        {[1,3,2,4], [3,1,2,4], [0,2,3,4]},
+        {
+            [1,3,2,4],
+            [3,1,2,4],
+            [0,2,3,4]
+        },
         {[2,3,1], [3,1,2], [0,1,3]},
     };
     [Theory]

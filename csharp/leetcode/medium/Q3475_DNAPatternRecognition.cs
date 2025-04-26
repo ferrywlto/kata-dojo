c9ce815f1163e9713a827fa29656ac53dd2e63ba
Ferry To <ferrywlto@gmail.com>
2025-04-26 14:28:05 +0100

    test: All test cases of Q3475 passed

--- csharp/leetcode/medium/Q3475_DNAPatternRecognition.cs
+++ csharp/leetcode/medium/Q3475_DNAPatternRecognition.cs
@@ -1,10 +1,17 @@
-using Row = (int sample_id, string dna_sequence, string species);
+using Row = (int sample_id, string dna_sequence, string species, int has_start, int has_stop, int has_atat, int has_ggg);
 
 public class Q3475_DNAPatternRecognition(ITestOutputHelper output) : SqlTest(output)
 {
     public string Query =>
     """
-    SELECT 1;
+    SELECT sample_id, dna_sequence, species, 
+    dna_sequence like 'ATG%' as has_start,
+    dna_sequence like '%TAA' or 
+    dna_sequence like '%TAG' or
+    dna_sequence like '%TGA' as has_stop,
+    dna_sequence like '%ATAT%' as has_atat,
+    dna_sequence like '%GGG%' as has_ggg
+    from Samples;
     """;
 
     public static TheoryData<string, Row[]> TestData => new()
@@ -20,13 +27,13 @@ public class Q3475_DNAPatternRecognition(ITestOutputHelper output) : SqlTest(out
             insert into Samples (sample_id, dna_sequence, species) values ('7', 'CGTATGCGTCGTA', 'Zebrafish');
             """,
             [
-                (1, "ATGCTAGCTAGCTAA", "Human"),
-                (2, "GGGTCAATCATC", "Human"),
-                (3, "ATATATCGTAGCTA", "Human"),
-                (4, "ATGGGGTCATCATAA", "Mouse"),
-                (5, "TCAGTCAGTCAG", "Mouse"),
-                (6, "ATATCGCGCTAG", "Zebrafish"),
-                (7, "CGTATGCGTCGTA", "Zebrafish"),
+                (1, "ATGCTAGCTAGCTAA", "Human", 1, 1, 0, 0),
+                (2, "GGGTCAATCATC", "Human", 0, 0, 0, 1),
+                (3, "ATATATCGTAGCTA", "Human", 0, 0, 1, 0),
+                (4, "ATGGGGTCATCATAA", "Mouse", 1, 1, 0, 1),
+                (5, "TCAGTCAGTCAG", "Mouse", 0, 0, 0, 0),
+                (6, "ATATCGCGCTAG", "Zebrafish", 0, 1, 1, 0),
+                (7, "CGTATGCGTCGTA", "Zebrafish", 0, 0, 0, 0),
             ]
         }
     };
@@ -45,14 +52,18 @@ public class Q3475_DNAPatternRecognition(ITestOutputHelper output) : SqlTest(out
     public void Test(string testDataSql, Row[] expected)
     {
         ArrangeTestData(testDataSql);
-        var reader = ExecuteQuery(Query);
-        AssertResultSchema(reader, ["sample_id", "dna_sequence", "species"]);
-        foreach (var (sample_id, dna_sequence, species) in expected)
+        var reader = ExecuteQuery(Query, true);
+        AssertResultSchema(reader, ["sample_id", "dna_sequence", "species", "has_start", "has_stop", "has_atat", "has_egg"]);
+        foreach (var (sample_id, dna_sequence, species, has_start, has_stop, has_atat, has_egg) in expected)
         {
             Assert.True(reader.Read());
             Assert.Equal(sample_id, reader.GetInt32(0));
             Assert.Equal(dna_sequence, reader.GetString(1));
             Assert.Equal(species, reader.GetString(2));
+            Assert.Equal(has_start, reader.GetInt32(3));
+            Assert.Equal(has_stop, reader.GetInt32(4));
+            Assert.Equal(has_atat, reader.GetInt32(5));
+            Assert.Equal(has_egg, reader.GetInt32(6));
         }
     }
 }

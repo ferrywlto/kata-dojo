9c660a62a0c9d68e14ce7a71a1680675afa1a778
Ferry To <ferrywlto@gmail.com>
2025-04-27 14:56:42 +0100

    test: All test cases of Q3211 passed

--- csharp/leetcode/medium/Q3211_GenerateBinaryStringsWithoutAdjacentZeros.cs
+++ csharp/leetcode/medium/Q3211_GenerateBinaryStringsWithoutAdjacentZeros.cs
@@ -1,8 +1,33 @@
+using System.Text;
+
 public class Q3211_GenerateBinaryStringsWithoutAdjacentZeros
 {
+    // TC: O(n^2), n scale with size of n
+    // SC: O(2^n)
     public IList<string> ValidStrings(int n)
     {
-        return [];
+        var set = new HashSet<string> { "0", "1" };
+        var len = 1;
+        while (len < n)
+        {
+            var newSet = new HashSet<string>();
+            foreach (var str in set)
+            {
+                if (str[^1] == '0')
+                {
+                    newSet.Add(str + '1');
+                }
+                else
+                {
+                    newSet.Add(str + '0');
+                    newSet.Add(str + '1');
+                }
+            }
+            set = newSet;
+            len++;
+        }
+
+        return set.ToList();
     }
     public static TheoryData<int, string[]> TestData => new()
     {

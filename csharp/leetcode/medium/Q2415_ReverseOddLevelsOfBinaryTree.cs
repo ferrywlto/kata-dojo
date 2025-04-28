706040e3e56bb2b5c7887b339fcc0abcd127b407
Ferry To <ferrywlto@gmail.com>
2025-04-28 21:44:12 +0100

    test: All test cases of Q2415 passed

--- csharp/leetcode/medium/Q2415_ReverseOddLevelsOfBinaryTree.cs
+++ csharp/leetcode/medium/Q2415_ReverseOddLevelsOfBinaryTree.cs
@@ -1,10 +1,61 @@
-
 public class Q2415_ReverseOddLevelOfBinaryTree(ITestOutputHelper output) : TreeNodeTest(output)
 {
-    public TreeNode ReverseOddLevels(TreeNode root) {
-     return new TreeNode();   
-    }   
-    public static TheoryData<TreeNode, TreeNode> TestData => new () 
+    Dictionary<int, Stack<int>> OddLevels = new();
+    public TreeNode ReverseOddLevels(TreeNode root)
+    {
+        var curr = root;
+        var stack = new Stack<(int, TreeNode)>();
+        var level = 0;
+        stack.Push((level, curr));
+
+        while (stack.Count > 0)
+        {
+            var (lv, node) = stack.Pop();
+            if (lv % 2 == 1)
+            {
+                if (!OddLevels.TryGetValue(lv, out var levelStack))
+                {
+                    var stk = new Stack<int>();
+                    stk.Push(node.val);
+                    OddLevels.Add(lv, stk);
+                }
+                else
+                {
+                    levelStack.Push(node.val);
+                }
+            }
+            lv++;
+            if (node.right != null)
+            {
+                stack.Push((lv, node.right));
+            }
+            if (node.left != null)
+            {
+                stack.Push((lv, node.left));
+            }
+        }
+        level = 0;
+        stack.Push((level, curr));
+        while (stack.Count > 0)
+        {
+            var (lv, node) = stack.Pop();
+            if (lv % 2 == 1)
+            {
+                node.val = OddLevels[lv].Pop();
+            }
+            lv++;
+            if (node.right != null)
+            {
+                stack.Push((lv, node.right));
+            }
+            if (node.left != null)
+            {
+                stack.Push((lv, node.left));
+            }
+        }
+        return root;
+    }
+    public static TheoryData<TreeNode, TreeNode> TestData => new()
     {
         {TreeNode.FromLevelOrderingIntArray([2,3,5,8,13,21,34])!, TreeNode.FromLevelOrderingIntArray([2,5,3,8,13,21,34])!},
         {TreeNode.FromLevelOrderingIntArray([7,13,11])!, TreeNode.FromLevelOrderingIntArray([7,11,13])!},

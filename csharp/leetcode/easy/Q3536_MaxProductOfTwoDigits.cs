169d293f0b12f232dfbe3d8c115ae84bb56b0e63
Ferry To <ferrywlto@gmail.com>
2025-05-04 21:23:55 +0100

    test: All test cases of Q3536 passed

--- csharp/leetcode/easy/Q3536_MaxProductOfTwoDigits.cs
+++ csharp/leetcode/easy/Q3536_MaxProductOfTwoDigits.cs
@@ -1,8 +1,25 @@
 public class Q3536_MaxProductOfTwoDigits
 {
+    // TC: O(n), n scale with number of digits in n
+    // SC: O(1), space used does not scale with input
     public int MaxProduct(int n)
     {
-        return 0;
+        var digits = new int[2];
+        while (n > 0)
+        {
+            var digit = n % 10;
+            if (digit >= digits[0])
+            {
+                digits[1] = digits[0];
+                digits[0] = digit;
+            }
+            else if (digit >= digits[1])
+            {
+                digits[1] = digit;
+            }
+            n /= 10;
+        }
+        return digits[0] * digits[1];
     }
     public static TheoryData<int, int> TestData => new()
     {

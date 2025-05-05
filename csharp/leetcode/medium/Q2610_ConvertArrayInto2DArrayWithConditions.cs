6affae2084b22b06c6601b52dcdd1761aea15dcd
Ferry To <ferrywlto@gmail.com>
2025-05-05 21:51:27 +0100

    test: All test cases of Q2610 passed

--- csharp/leetcode/medium/Q2610_ConvertArrayInto2DArrayWithConditions.cs
+++ csharp/leetcode/medium/Q2610_ConvertArrayInto2DArrayWithConditions.cs
@@ -2,8 +2,42 @@ public class Q2610_ConvertArrayInto2DArrayWithConditions
 {
     public IList<IList<int>> FindMatrix(int[] nums)
     {
-        
-        return [];
+        var dict = new Dictionary<int, int>();
+        for(var i=0; i<nums.Length; i++)
+        {
+            var n = nums[i];
+            if(dict.TryGetValue(n, out var val))
+            {
+                dict[n] = ++val;
+            }
+            else 
+            {
+                dict.Add(n, 1);
+            }
+        }
+
+        var result = new List<IList<int>>();
+        while(dict.Count > 0)
+        {
+            var listAdd = new List<int>();
+            var listRemove = new List<int>();
+            foreach(var p in dict) 
+            {
+                var k = p.Key;
+                listAdd.Add(k);
+                dict[k]--;
+                if(dict[k] == 0) 
+                {
+                    listRemove.Add(k);
+                }
+            }
+            for(var i=0; i<listRemove.Count; i++)
+            {
+                dict.Remove(listRemove[i]);
+            }
+            result.Add(listAdd);
+        }
+        return result;
     }
     public static TheoryData<int[], List<List<int>>> TestData => new()
     {
@@ -13,7 +47,7 @@ public class Q2610_ConvertArrayInto2DArrayWithConditions
         },
         {
             [1,2,3,4],
-            [[4,3,2,1]]
+            [[1,2,3,4]]
         }
     };
     [Theory]

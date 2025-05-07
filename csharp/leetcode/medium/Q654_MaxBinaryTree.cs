1ca2c393da0a60bef68767f85253cb067130823e
Ferry To <ferrywlto@gmail.com>
2025-05-07 21:54:45 +0100

    test: All test cases of Q654 passed

--- csharp/leetcode/medium/Q654_MaxBinaryTree.cs
+++ csharp/leetcode/medium/Q654_MaxBinaryTree.cs
@@ -1,8 +1,41 @@
-public class Q654_MaxBinaryTree
+
+public class Q654_MaxBinaryTree(ITestOutputHelper output) : TreeNodeTest(output)
 {
+    // TC: O(n^2), n scale with length nums, worst case if sorted always run to end to get the max
+    // SC: O(n), for the recursion stack
     public TreeNode ConstructMaximumBinaryTree(int[] nums)
     {
-        return new();
+        return FindMaxNode(nums, 0, nums.Length - 1)!;
+    }
+    private TreeNode? FindMaxNode(int[] nums, int startIdx, int endIdx)
+    {
+        if (
+            startIdx > endIdx ||
+            startIdx < 0 || endIdx < 0 ||
+            startIdx > nums.Length || endIdx > nums.Length
+        )
+        {
+            return null;
+        }
+        if (startIdx == endIdx) return new TreeNode(nums[startIdx]);
+        var maxIdx = -1;
+        var max = -1;
+        for (var i = startIdx; i <= endIdx; i++)
+        {
+            if (nums[i] > max)
+            {
+                max = nums[i];
+                maxIdx = i;
+            }
+        }
+
+        var node = new TreeNode(max)
+        {
+            left = FindMaxNode(nums, startIdx, maxIdx - 1),
+            right = FindMaxNode(nums, maxIdx + 1, endIdx)
+        };
+
+        return node;
     }
     public static TheoryData<int[], TreeNode> TestData => new()
     {
@@ -14,6 +47,8 @@ public class Q654_MaxBinaryTree
     public void Test(int[] input, TreeNode expected)
     {
         var actual = ConstructMaximumBinaryTree(input);
-        Assert.Equal(expected, actual);
+        DebugTree(actual);
+        AssertTreeNodeEqual(expected, actual);
+        // Assert.Equal(expected, actual);
     }
 }

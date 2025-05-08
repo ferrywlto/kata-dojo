db12590f259c5944cbafd511f756348315569897
Ferry To <ferrywlto@gmail.com>
2025-05-08 22:36:24 +0100

    test: All test cases of Q1315 passed

--- csharp/leetcode/medium/Q1315_SumNodesWithEvenValueGrandParent.cs
+++ csharp/leetcode/medium/Q1315_SumNodesWithEvenValueGrandParent.cs
@@ -1,9 +1,36 @@
 
 public class Q1315_SumNodesWithEvenValueGrandParent(ITestOutputHelper output) : TreeNodeTest(output)
 {
+    // TC: O(n), n scale with number of nodes in tree
+    // SC: O(d), d scale with depth of tree for recursion stack
     public int SumEvenGrandparent(TreeNode root)
     {
-        return 0;
+        return Cal(root);
+    }
+    private int Cal(TreeNode? node)
+    {
+        if (node == null) return 0;
+        var temp = 0;
+        if (node.val % 2 == 0)
+        {
+            if (node.left?.left != null)
+            {
+                temp += node.left.left.val;
+            }
+            if (node.left?.right != null)
+            {
+                temp += node.left.right.val;
+            }
+            if (node.right?.left != null)
+            {
+                temp += node.right.left.val;
+            }
+            if (node.right?.right != null)
+            {
+                temp += node.right.right.val;
+            }
+        }
+        return temp + Cal(node.left) + Cal(node.right);
     }
     public static TheoryData<TreeNode, int> TestData => new()
     {

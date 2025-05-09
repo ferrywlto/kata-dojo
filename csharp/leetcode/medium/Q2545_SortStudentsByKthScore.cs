829ac7b24168af34eb54afb9cf88575fd28019c1
Ferry To <ferrywlto@gmail.com>
2025-05-09 23:27:15 +0100

    test: All test cases of Q2545 passed

--- csharp/leetcode/medium/Q2545_SortStudentsByKthScore.cs
+++ csharp/leetcode/medium/Q2545_SortStudentsByKthScore.cs
@@ -1,8 +1,28 @@
 public class Q2545_SortStudentsByKthScore
 {
+    // TC: O(n log n), n scale with length of score
+    // SC: O(n)
     public int[][] SortTheStudents(int[][] score, int k)
     {
-        return [];
+        var len = score.Length;
+        var originalIdx = new Dictionary<int, int>();
+        var toSort = new int[len];
+        for (var row = 0; row < len; row++)
+        {
+            var item = score[row][k];
+            originalIdx.Add(item, row);
+            toSort[row] = item;
+        }
+
+        Array.Sort(toSort);
+        Array.Reverse(toSort);
+
+        var result = new int[len][];
+        for (var newIdx = 0; newIdx < toSort.Length; newIdx++)
+        {
+            result[newIdx] = score[originalIdx[toSort[newIdx]]];
+        }
+        return result;
     }
     public static TheoryData<int[][], int, int[][]> TestData => new(){
         {[[10,6,9,1],[7,5,11,2],[4,8,3,15]], 2, [[7,5,11,2],[10,6,9,1],[4,8,3,15]]},

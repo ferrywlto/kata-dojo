cd13eeb39577d092464a0107129c6f6c8aa1f4e8
Ferry To <ferrywlto@gmail.com>
2025-05-10 12:32:51 +0100

    test: All test cases of Q2125 passed

--- csharp/leetcode/medium/Q2125_NumLaserBeamsinBank.cs
+++ csharp/leetcode/medium/Q2125_NumLaserBeamsinBank.cs
@@ -2,7 +2,27 @@ public class Q2125_NumLaserBeamsinBank
 {
     public int NumberOfBeams(string[] bank)
     {
-        return 0;
+        var result = 0;
+        var prevDevices = CountDevices(bank[0]);
+        for (var i = 1; i < bank.Length; i++)
+        {
+            var nextDevices = CountDevices(bank[i]);
+            if (nextDevices != 0)
+            {
+                result += prevDevices * nextDevices;
+                prevDevices = nextDevices;
+            }
+        }
+        return result;
+    }
+    private int CountDevices(string input)
+    {
+        var devices = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '1') devices++;
+        }
+        return devices;
     }
     public static TheoryData<string[], int> TestData => new()
     {

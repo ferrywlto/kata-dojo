402fd73fe4262c45c9d1207b4aabfb26854e9c79
Ferry To <ferrywlto@gmail.com>
2025-05-29 00:06:51 +0100

    test: All test cases of Q885 passed

--- csharp/leetcode/medium/Q885_SpiralMatrixIII.cs
+++ csharp/leetcode/medium/Q885_SpiralMatrixIII.cs
@@ -1,8 +1,88 @@
-public class Q885_SpiralMatrixIII
+public class Q885_SpiralMatrixIII(ITestOutputHelper output)
 {
+    // TC: O(n), n scale with rows * cols
+    // SC: O(n), the result list must equals to rows * cols
     public int[][] SpiralMatrixIII(int rows, int cols, int rStart, int cStart)
     {
-        return [];
+        // var step = 0;
+        // steps: 0, 1, 2, 3, 4, 5, 6, 7, 
+        // dis:   1, 1, 2, 2, 3, 3, 4, 4
+        var totalSteps = rows * cols;
+
+        var result = new List<int[]>
+        {
+            ([rStart, cStart])
+        };
+
+        var directionCount = 0;
+        while (result.Count < totalSteps)
+        {
+            var direction = directionCount % 4;
+            var distance = (directionCount / 2) + 1;
+
+            if (direction == 0)
+            {
+                // output.WriteLine($"right, d:{distance}");
+                for (var i = 0; i < distance; i++)
+                {
+                    cStart++;
+                    // output.WriteLine($"[{rStart},{cStart}]");
+                    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols)
+                    {
+                        // output.WriteLine($"add [{rStart},{cStart}]");
+                        result.Add([rStart, cStart]);
+                    }
+                    // step++;
+                }
+            }
+            else if (direction == 1)
+            {
+                // output.WriteLine($"down, d:{distance}");
+                for (var i = 0; i < distance; i++)
+                {
+                    rStart++;
+                    // output.WriteLine($"[{rStart},{cStart}]");
+                    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols)
+                    {
+                        // output.WriteLine($"add [{rStart},{cStart}], rows: {rows}");
+                        result.Add([rStart, cStart]);
+                    }
+                    // step++;
+                }
+            }
+            else if (direction == 2)
+            {
+                // output.WriteLine($"left, d:{distance}");
+                for (var i = 0; i < distance; i++)
+                {
+                    cStart--;
+                    // output.WriteLine($"[{rStart},{cStart}]");
+                    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols)
+                    {
+                        // output.WriteLine($"add [{rStart},{cStart}]");
+                        result.Add([rStart, cStart]);
+                    }
+                    // step++;
+                }
+            }
+            else
+            {
+                output.WriteLine($"up, d:{distance}");
+                for (var i = 0; i < distance; i++)
+                {
+                    rStart--;
+                    // output.WriteLine($"[{rStart},{cStart}]");
+                    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols)
+                    {
+                        // output.WriteLine($"add [{rStart},{cStart}]");
+                        result.Add([rStart, cStart]);
+                    }
+                    // step++;
+                }
+            }
+            directionCount++;
+        }
+        return result.ToArray();
     }
     public static TheoryData<int, int, int, int, int[][]> TestData => new()
     {

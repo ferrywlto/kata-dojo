3e638d2a08a51fe8f15adb9eb52133cf08d759f5
Ferry To <ferrywlto@gmail.com>
2025-05-29 22:50:22 +0100

    test: All test cases of Q1561 passed

--- csharp/leetcode/medium/Q1561_MaxNumOfCoinsYouCanGet.cs
+++ csharp/leetcode/medium/Q1561_MaxNumOfCoinsYouCanGet.cs
@@ -1,14 +1,27 @@
 public class Q1561_MaxNumOfCoinsYouCanGet
 {
+    // TC: O(n log n), n scale with length of piles
+    // SC: O(1), spaces used does not scale with input
     public int MaxCoins(int[] piles)
     {
-        return 0;
+        Array.Sort(piles);
+        var groups = piles.Length / 3;
+        var idx = piles.Length - 2;
+        var result = 0;
+
+        for (var i = 0; i < groups; i++)
+        {
+            result += piles[idx];
+            idx -= 2;
+        }
+
+        return result;
     }
     public static TheoryData<int[], int> TestData => new()
     {
         {[2,4,1,2,7,8], 9},
         {[2,4,5], 4},
-        {[9,8,7,6,5,1,2,3,4], 4},
+        {[9,8,7,6,5,1,2,3,4], 18},
     };
     [Theory]
     [MemberData(nameof(TestData))]

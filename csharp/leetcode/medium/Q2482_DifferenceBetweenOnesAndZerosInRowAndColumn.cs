e31c3c22b461cfaba3f63048c7e21270178d58e7
Ferry To <ferrywlto@gmail.com>
2025-06-05 18:54:51 +0100

    test: All test cases of Q2482 passed

--- csharp/leetcode/medium/Q2482_DifferenceBetweenOnesAndZerosInRowAndColumn.cs
+++ csharp/leetcode/medium/Q2482_DifferenceBetweenOnesAndZerosInRowAndColumn.cs
@@ -2,7 +2,38 @@ public class Q2482_DifferenceBetweenOnesAndZerosInRowAndColumn
 {
     public int[][] OnesMinusZeros(int[][] grid)
     {
-        return [];
+        var m = grid.Length;
+        var n = grid[0].Length;
+        var rowOnes = new int[m];
+        var colOnes = new int[n];
+        var rowZeros = new int[m];
+        var colZeros = new int[n];
+
+        for(var row=0; row<m; row++)
+        {
+            for(var col=0; col<grid[row].Length; col++) 
+            {
+                if(grid[row][col] == 1) {
+                    rowOnes[row]++;
+                    colOnes[col]++;
+                }
+                else {
+                    rowZeros[row]++;
+                    colZeros[col]++;
+                };
+            }
+        }
+        var result = new int[m][];
+        for(var i=0; i<m; i++) {result[i] = new int[n];}
+                
+        for(var row=0; row<m; row++)
+        {
+            for(var col=0; col<grid[row].Length; col++) 
+            {
+                result[row][col] = rowOnes[row] + colOnes[col] - rowZeros[row] - colZeros[col];
+            }
+        }        
+        return result;
     }
     public static TheoryData<int[][], int[][]> TestData => new()
     {

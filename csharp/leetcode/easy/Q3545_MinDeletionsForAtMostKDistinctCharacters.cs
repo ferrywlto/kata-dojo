300d75a0b313d444f63ca5e2b312adcb1f3a782d
Ferry To <ferrywlto@gmail.com>
2025-06-06 12:05:14 +0100

    test: All test cases of Q3545 passed

--- csharp/leetcode/easy/Q3545_MinDeletionsForAtMostKDistinctCharacters.cs
+++ csharp/leetcode/easy/Q3545_MinDeletionsForAtMostKDistinctCharacters.cs
@@ -1,8 +1,23 @@
 public class Q3545_MinDeletionsForAtMostKDistinctCharacters
 {
+    // TC: O(n log n), n scale with length of s, due to Array.Sort(), but the constraints is small s.Length <= 16
+    // Non sorting approach can be faster, but not worth the effort
+    // SC: O(1), space used does not scale with input
     public int MinDeletion(string s, int k)
     {
-        return 0;
+        var freq = new int[26];
+        for (var i = 0; i < s.Length; i++)
+        {
+            freq[s[i] - 'a']++;
+        }
+        Array.Sort(freq);
+
+        var result = 0;
+        for (var j = freq.Length - k - 1; j >= 0; j--)
+        {
+            result += freq[j];
+        }
+        return result;
     }
     public static TheoryData<string, int, int> TestData => new()
     {

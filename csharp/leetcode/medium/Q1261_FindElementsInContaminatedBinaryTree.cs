1ece8c59a57c3da798489752e9da826f9db2cc72
Ferry To <ferrywlto@gmail.com>
2025-06-06 12:43:26 +0100

    test: All test cases of Q1261 passed

--- csharp/leetcode/medium/Q1261_FindElementsInContaminatedBinaryTree.cs
+++ csharp/leetcode/medium/Q1261_FindElementsInContaminatedBinaryTree.cs
@@ -1,14 +1,32 @@
 public class FindElements
 {
+    // Fastest solution make use of bitArray and the constaints 10^6, not bother here as current solution is good enough
+    private HashSet<int> _vals = [];
     public FindElements(TreeNode root)
     {
-        
+        root.val = 0;
+        _vals.Add(0);
+        Recover(root);
     }
-
-    public bool Find(int target)
+    private void Recover(TreeNode? node)
     {
-        return false;
+        if (node == null) return;
+        if (node.left != null)
+        {
+            var val = (node.val * 2) + 1;
+            node.left.val = val;
+            _vals.Add(val);
+            Recover(node.left);
+        }
+        if (node.right != null)
+        {
+            var val = (node.val * 2) + 2;
+            node.right.val = val;
+            _vals.Add(val);
+            Recover(node.right);
+        }
     }
+    public bool Find(int target) => _vals.Contains(target);
 }
 public class Q1261_FindElementsInContaminatedBinaryTree
 {

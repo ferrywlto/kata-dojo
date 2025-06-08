51e8c8249c85344edf2f3e79150a099a0d6d761c
Ferry To <ferrywlto@gmail.com>
2025-06-08 11:35:25 +0100

    test: All test cases of Q3560 passed

--- csharp/leetcode/easy/Q3560_MinLogTransportationCost.cs
+++ csharp/leetcode/easy/Q3560_MinLogTransportationCost.cs
@@ -1,17 +1,21 @@
 public class Q3560_MinLogTransportationCost
 {
+    // TC: O(1)
+    // SC: O(1)
     public long MinCuttingCost(int n, int m, int k)
     {
-        return 0;
+        if (n <= k && m <= k) return 0;
+        return (long)k * (Math.Max(n, m) - k);
     }
-    public static TheoryData<int, int, int, int> TestData => new()
+    public static TheoryData<int, int, int, long> TestData => new()
     {
         {6,5,5,5},
         {4,4,6,0},
+        {49898,109372,62703,2926286307},
     };
     [Theory]
     [MemberData(nameof(TestData))]
-    public void Test(int n, int m, int k, int expected)
+    public void Test(int n, int m, int k, long expected)
     {
         var actual = MinCuttingCost(n, m, k);
         Assert.Equal(expected, actual);

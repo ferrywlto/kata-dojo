e67f63d3a8b9fb0cfc07ed734ed8cda2e594e408
Ferry To <ferrywlto@gmail.com>
2025-06-13 17:58:30 +0100

    test: All test cases of Q797 passed

--- csharp/leetcode/medium/Q797_AllPathsFromSourceToTarget.cs
+++ csharp/leetcode/medium/Q797_AllPathsFromSourceToTarget.cs
@@ -1,8 +1,35 @@
 public class Q797_AllPathsFromSourceToTarget
 {
+    private Dictionary<int, int[]> _paths = [];
+    private List<IList<int>> _result = [];
+    private int n = -1;
     public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
     {
-        return [];
+        n = graph.Length - 1;
+        for (var i = 0; i < n; i++)
+        {
+            _paths.Add(i, graph[i]);
+        }
+        foreach (var r in _paths[0])
+        {
+            Route([0], r);
+        }
+        return _result;
+    }
+    private void Route(List<int> list, int option)
+    {
+        list.Add(option);
+        if (option == n)
+        {
+            _result.Add(list);
+            return;
+        }
+
+        var options = _paths[option];
+        foreach (var opt in options)
+        {
+            Route([.. list], opt);
+        }
     }
     public static TheoryData<int[][], List<IList<int>>> TestData => new()
     {

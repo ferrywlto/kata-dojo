c36ca98eb95715cf99b43b0599261a7048896ef4
Ferry To <ferrywlto@gmail.com>
2025-06-14 13:53:09 +0100

    test: All test cases of Q1329 passed

--- csharp/leetcode/medium/Q1329_SortMatrixDiagonally.cs
+++ csharp/leetcode/medium/Q1329_SortMatrixDiagonally.cs
@@ -1,8 +1,40 @@
 public class Q1329_SortMatrixDiagonally
 {
+    // TC: O(n ^ 2)
+    // SC: O(1)
     public int[][] DiagonalSort(int[][] mat)
     {
-        return [];
+        var col = 0;
+        int row;
+        for(row=0; row<mat.Length; row++)
+        {
+            SortDiagonal(mat, row, col);
+        }
+        row = 0;
+        for(col=1; col<mat[0].Length; col++)
+        {
+            SortDiagonal(mat, row, col);
+        }
+        return mat;
+    }
+    // TC: O(r or c => n)
+    // SC: O(n)
+    // Sort inplace
+    private void SortDiagonal(int[][] input, int row, int col)
+    {
+        var list = new List<int>();
+        int r = row, c = col;
+        while(r < input.Length && c < input[0].Length) {
+            list.Add(input[r++][c++]);
+        }
+        list.Sort();
+
+        var listIdx = 0;
+        r = row;
+        c = col;
+        while(r < input.Length && c < input[0].Length) {
+            input[r++][c++] = list[listIdx++];
+        }
     }
     public static TheoryData<int[][], int[][]> TestData => new()
     {

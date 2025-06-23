5ff5c089b422b11c5f85b0c24bf02688e636c1af
Ferry To <ferrywlto@gmail.com>
2025-06-23 22:33:38 +0100

    test: All test cases of Q1551 passed

--- csharp/leetcode/medium/Q1551_MinOpsToMakeArrayEqual.cs
+++ csharp/leetcode/medium/Q1551_MinOpsToMakeArrayEqual.cs
@@ -1,9 +1,22 @@
-public class Q1551_MinOpsToMakeArrayEqual
+public class Q1551_MinOpsToMakeArrayEqual//(ITestOutputHelper output)
 {
+    // TC: O(n)
+    // SC: O(1)
     public int MinOperations(int n)
     {
         var result = 0;
+        // all elements should be n at last,
+        // cal diff from last
+        // one ops will +1/-1 thus only need to calculate the first half diff.
+        var len = n / 2;
+        for (var i = 0; i < len; i++)
+        {
+            var expected = (2 * i) + 1;
+            // since expected before n/2 must smaller than n, no need to Math.Abs()
+            var diff = n - expected;
+            result += diff;
 
+        }
         return result;
     }
     public static TheoryData<int, int> TestData => new()

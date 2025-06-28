f1a33aa4fe8305912ccc207a3f04d29937a6fbf2
Ferry To <ferrywlto@gmail.com>
2025-06-28 14:17:53 +0100

    test: All test cases of Q1347 passed

--- csharp/leetcode/medium/Q1347_MinNumOfStepsToMakeTwoStringsAnagram.cs
+++ csharp/leetcode/medium/Q1347_MinNumOfStepsToMakeTwoStringsAnagram.cs
@@ -2,16 +2,31 @@ public class Q1347_MinNumOfStepsToMakeTwoStringsAnagram
 {
     public int MinSteps(string s, string t)
     {
-        return 0;
+        var freq = new int[26];
+        for (var i = 0; i < s.Length; i++)
+        {
+            freq[s[i] - 'a']++;
+        }
+        for (var j = 0; j < t.Length; j++)
+        {
+            freq[t[j] - 'a']--;
+        }
+
+        var result = 0;
+        for (var k = 0; k < freq.Length; k++)
+        {
+            result += freq[k] < 0 ? -freq[k] : freq[k];
+        }
+        return result / 2;
     }
     public static TheoryData<string, string, int> TestData => new()
     {
         {"bab", "aba", 1},
         {"leetcode", "practice", 5},
-        {"anagram", "mangaar", 5},
+        {"anagram", "mangaar", 0},
     };
     [Theory]
-    [MemberData(nameof(TestData)]
+    [MemberData(nameof(TestData))]
     public void Test(string input1, string input2, int expected)
     {
         var actual = MinSteps(input1, input2);

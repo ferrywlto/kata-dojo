0a3608981e2563466192e81e81d960bc4d8ecd04
Ferry To <ferrywlto@gmail.com>
2025-06-30 21:52:17 +0100

    test: All test cases of Q1823 passed

--- csharp/leetcode/medium/Q1823_FindWinnerOfCircularGame.cs
+++ csharp/leetcode/medium/Q1823_FindWinnerOfCircularGame.cs
@@ -1,8 +1,20 @@
-public class Q1823_FindWinnerOfCircularGame
+public class Q1823_FindWinnerOfCircularGame(ITestOutputHelper output) : ListNodeTest(output)
 {
+    // TC: O(n)
+    // SC: O(n)
     public int FindTheWinner(int n, int k)
     {
-        return 0;
+        var list = new List<int>();
+        for (var i = 1; i <= n; i++) list.Add(i);
+
+        var currentIdx = 0;
+        while (list.Count > 1)
+        {
+            currentIdx = (currentIdx + k - 1) % list.Count;
+            list.RemoveAt(currentIdx);
+            currentIdx %= list.Count;
+        }
+        return list[0];
     }
     public static TheoryData<int, int, int> TestData => new()
     {

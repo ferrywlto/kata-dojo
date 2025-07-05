e4abcdc1c46596fb3deb42d8a0df8662f5d2f68d
Ferry To <ferrywlto@gmail.com>
2025-07-05 18:33:00 +0100

    test: Add correct partition enumeration algorithm

--- csharp/leetcode/medium/Q2698_FindPunishmentNumberOfInteger.cs
+++ csharp/leetcode/medium/Q2698_FindPunishmentNumberOfInteger.cs
@@ -1,8 +1,53 @@
-public class Q2698_FindPunishmentNumberOfInteger
+using System.Text;
+
+public class Q2698_FindPunishmentNumberOfInteger(ITestOutputHelper output)
 {
+    private List<List<string>> combinations = [];
+    private string numStr = "";
+    private string nStr = "";
     public int PunishmentNumber(int n)
     {
+        // var p = 0;
+        // for (var i = 0; i < 1001; i++)
+        // {
+        //     if(Possible(i))p++;
+        // }
+        // output.WriteLine("p: {0}", p);
+        // n = 36;
+        var sq = n * n;
+        nStr = n.ToString();
+        numStr = "1296";
+
+        t(new List<string>(), 0, 0, 36);
+        output.WriteLine("list: {0}", string.Join(Environment.NewLine, combinations.Select(c => $"[{string.Join(',', c)}]")));
         return 0;
+    }
+    private void t(List<string> list, int idx, int sum, int n)
+    {
+        if (idx >= numStr.Length)
+        {
+            if (sum == n)
+            {
+                output.WriteLine("found: [{0}]", string.Join(',', list));
+                combinations.Add(list);
+            }
+            else
+            {
+                output.WriteLine("not-found: [{0}], sum: {1}", string.Join(',', list), sum);
+            }
+            return;
+        }
+
+        var sb = new StringBuilder();
+        for (var i = idx; i < numStr.Length; i++)
+        {
+            var l = new List<string>(list);
+            sb.Append(numStr[i]);
+            var val = int.Parse(sb.ToString());
+
+            l.Add(sb.ToString());
+            t(l, i + 1, sum + val, n);
+        }
     }
     public static TheoryData<int, int> TestData => new()
     {

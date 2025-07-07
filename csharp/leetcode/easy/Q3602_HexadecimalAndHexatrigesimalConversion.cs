44d3cd6adc20c930450382051a72c1aedcd8694f
Ferry To <ferrywlto@gmail.com>
2025-07-07 21:38:39 +0100

    test: All test cases of Q3602 passed

--- csharp/leetcode/easy/Q3602_HexadecimalAndHexatrigesimalConversion.cs
+++ csharp/leetcode/easy/Q3602_HexadecimalAndHexatrigesimalConversion.cs
@@ -1,8 +1,33 @@
+using System.Text;
+
 public class Q3602_HexadecimalAndHexatrigesimalConversion
 {
+    // TC: O(n)
+    // SC: O(n), for the string builders
+    const string x = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public string ConcatHex36(int n)
     {
-        return "";
+        var sq = n * n;
+        var cube = sq * n;
+        var sb1 = new StringBuilder();
+        while (sq > 0)
+        {
+            var reminder = sq % 16;
+            var ch = x[reminder];
+
+            sb1.Insert(0, ch);
+            sq /= 16;
+        }
+        var sb2 = new StringBuilder();
+        while (cube > 0)
+        {
+            var reminder = cube % 36;
+            var ch = x[reminder];
+
+            sb2.Insert(0, ch);
+            cube /= 36;
+        }
+        return sb1.ToString() + sb2.ToString();
     }
     public static TheoryData<int, string> TestData => new()
     {

62d857198c4975f1e3aa20108ae071430d2c602c
Ferry To <ferrywlto@gmail.com>
2025-07-10 22:08:43 +0100

    test: All test cases of Q2120 passed

--- csharp/leetcode/medium/Q2120_ExecutionOfAllSuffixInstructionsStayingInGrid.cs
+++ csharp/leetcode/medium/Q2120_ExecutionOfAllSuffixInstructionsStayingInGrid.cs
@@ -2,7 +2,59 @@ public class Q2120_ExecutionOfAllSuffixInstructionsStayingInGrid
 {
     public int[] ExecuteInstructions(int n, int[] startPos, string s)
     {
-        return [];
+        var result = new int[s.Length];
+        for (var i = 0; i < s.Length; i++)
+        {
+            result[i] = CountExecutedCommands(n, startPos, s, i);
+        }
+        return result;
+    }
+    private int CountExecutedCommands(int n, int[] startPos, string s, int startIdx)
+    {
+        var row = startPos[0];
+        var col = startPos[1];
+        var executed = 0;
+        for (var i = startIdx; i < s.Length; i++)
+        {
+            var cmd = s[i];
+            if (cmd == 'R')
+            {
+                if (col < n - 1)
+                {
+                    col++;
+                    executed++;
+                }
+                else return executed;
+            }
+            else if (cmd == 'L')
+            {
+                if (col > 0)
+                {
+                    col--;
+                    executed++;
+                }
+                else return executed;
+            }
+            else if (cmd == 'U')
+            {
+                if (row > 0)
+                {
+                    row--;
+                    executed++;
+                }
+                else return executed;
+            }
+            else if (cmd == 'D')
+            {
+                if (row < n - 1)
+                {
+                    row++;
+                    executed++;
+                }
+                else return executed;
+            }
+        }
+        return executed;
     }
     public static TheoryData<int, int[], string, int[]> TestData => new()
     {

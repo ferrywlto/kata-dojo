9b45ae67c352e552a9aca639e61283935bccda32
Ferry To <ferrywlto@gmail.com>
2025-07-18 21:42:13 +0100

    test: All test cases of Q2130 passed

--- csharp/leetcode/medium/Q2130_MaxTwinSumOfLinkedList.cs
+++ csharp/leetcode/medium/Q2130_MaxTwinSumOfLinkedList.cs
@@ -1,13 +1,59 @@
 
-public class Q2130_MaxTwinSumOfLinkedList : ListNodeTest
+public class Q2130_MaxTwinSumOfLinkedList(ITestOutputHelper output) : ListNodeTest(output)
 {
-    public Q2130_MaxTwinSumOfLinkedList(ITestOutputHelper output) : base(output)
-    {
-    }
-
+    // TC: O(n), n scale with the length of the linked list
+    // SC: O(n) for the list
     public int PairSum(ListNode head)
     {
-        return 0;
+        var count = 0;
+        var node = head;
+        var list = new List<int>();
+        while (node != null)
+        {
+            count++;
+            list.Add(node.Val);
+            node = node.Next;
+        }
+
+        var max = int.MinValue;
+        for (var i = 0; i < count / 2; i++)
+        {
+            var twinSum = list[i] + list[count - i - 1];
+            if (twinSum > max)
+                max = twinSum;
+        }
+        return max;
+    }
+
+    // TC: O(n), n scale with the length of the linked list
+    // SC: O(1), no extra space used
+    public int PairSumOptimized(ListNode head)
+    {
+        // This technique use a fast pointer to find the middle of the linked list
+        // while reverse the first half of the linked list, after running the loop it will have:
+        // slow tail-> 4 3 2 1 0 
+        // slow:    -> 5 6 7 8 9
+        var slow = head;
+        var fast = head;
+        var slowReverse = head;
+        while (fast != null)
+        {
+            fast = fast.Next!.Next;
+            var temp = slowReverse!.Next;
+            slowReverse.Next = slow;
+            slow = slowReverse;
+            slowReverse = temp;
+        }
+
+        var max = int.MinValue;
+        while (slowReverse != null)
+        {
+            max = Math.Max(max, slow!.Val + slowReverse!.Val);
+            slow = slow.Next;
+            slowReverse = slowReverse.Next;
+        }
+
+        return max;
     }
     public static TheoryData<ListNode, int> TestData => new()
     {
@@ -19,7 +65,7 @@ public class Q2130_MaxTwinSumOfLinkedList : ListNodeTest
     [MemberData(nameof(TestData))]
     public void Test(ListNode input, int expected)
     {
-        var result = PairSum(input);
+        var result = PairSumOptimized(input);
         Assert.Equal(expected, result);
     }
 }

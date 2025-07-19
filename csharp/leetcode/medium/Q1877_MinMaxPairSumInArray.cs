c08c06dd0c8908f195620dcaea962f67f194e1b0
Ferry To <ferrywlto@gmail.com>
2025-07-19 20:23:20 +0100

    test: All test cases of Q1877 passed

--- csharp/leetcode/medium/Q1877_MinMaxPairSumInArray.cs
+++ csharp/leetcode/medium/Q1877_MinMaxPairSumInArray.cs
@@ -1,15 +1,27 @@
 public class Q1877_MinMaxPairSumInArray
 {
+    // TC: O(n log n) due to sort
+    // SC: O(1), space used does not scale with input
     public int MinPairSum(int[] nums)
     {
-        return 0;
+        Array.Sort(nums);
+        var max = int.MinValue;
+        var len = nums.Length;
+        var half = nums.Length / 2;
+
+        for (var i = 0; i < half; i++)
+        {
+            max = Math.Max(max, nums[i] + nums[len - i - 1]);
+        }
+
+        return max;
     }
+    // 2,3,3,5
+    // Onces sorted, adding largest with smallest can keep the sum as minimized
     public static TheoryData<int[], int> TestData => new()
     {
         { [3, 5, 2, 3], 7 },
-        { [1, 2, 3, 4, 5], 6 },
-        { [1, 100000], 100001 },
-        { [-1, -2, -3, -4], -5 },
+        { [3, 5, 4, 2, 4, 6], 8 },
     };
     [Theory]
     [MemberData(nameof(TestData))]

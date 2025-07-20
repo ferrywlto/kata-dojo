d41a8d78dc3afac549a41bff45c194c6f3acdc4b
Ferry To <ferrywlto@gmail.com>
2025-07-20 16:33:34 +0100

    test: All test cases of Q1557 passed

--- csharp/leetcode/medium/Q1557_MinNumOfVerticesToReachAllNodes.cs
+++ csharp/leetcode/medium/Q1557_MinNumOfVerticesToReachAllNodes.cs
@@ -1,9 +1,30 @@
 
 public class Q1557_MinNumOfVerticesToReachAllNodes
 {
+
+    // TC: O(edges + n)
+    // SC: O(edges)
     public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
     {
-        return [];
+        var fromSet = new HashSet<int>();
+        var toSet = new HashSet<int>();
+
+        foreach (var edge in edges)
+        {
+            fromSet.Add(edge[0]);
+            toSet.Add(edge[1]);
+        }
+
+        var result = new List<int>();
+        for (var i = 0; i < n; i++)
+        {
+            // The technique here is that if a vertex that no one can reach, they are the top vertexes in a directed graph, which means they can reach the rest of the graph from their destinations.
+            if (fromSet.Contains(i) && !toSet.Contains(i))
+            {
+                result.Add(i);
+            }
+        }
+        return result;
     }
     public static TheoryData<int, IList<IList<int>>, List<int>> TestData => new()
     {

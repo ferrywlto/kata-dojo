ba3c2eea24a0d23ddad5e79eb9a7b46020565e34
Ferry To <ferrywlto@gmail.com>
2025-07-20 13:10:54 +0100

    test: All test cases of Q3622 passed

--- csharp/leetcode/easy/Q3622_CheckDivisibilityByDigitSumAndProduct.cs
+++ csharp/leetcode/easy/Q3622_CheckDivisibilityByDigitSumAndProduct.cs
@@ -1,8 +1,22 @@
 public class Q3622_CheckDivisibilityByDigitSumAndProduct
 {
+    // TC: O(d), where d is the number of digits in n.
+    // SC: O(1), since we are using a constant amount of space.
     public bool CheckDivisibility(int n)
     {
-        return false;
+        var original = n;
+        var firstDigit = n % 10;
+        var sum = firstDigit;
+        var product = firstDigit;
+        n /= 10;
+        while (n > 0)
+        {
+            var digit = n % 10;
+            sum += digit;
+            product *= digit;
+            n /= 10;
+        }
+        return original % (sum + product) == 0;
     }
     public static TheoryData<int, bool> TestData => new()
     {

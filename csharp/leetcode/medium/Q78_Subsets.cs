5a6b7c8681ecaa10162085abba6309c6c21ad115
Ferry To <ferrywlto@gmail.com>
2025-07-25 21:49:47 +0100

    test: All test cases of Q78 passed

--- csharp/leetcode/medium/Q78_Subsets.cs
+++ csharp/leetcode/medium/Q78_Subsets.cs
@@ -1,8 +1,34 @@
 public class Q78_Subsets
 {
+    // TC: O(n * 2^n), n scale with length of nums, 2^n for all combinations times number of bits from 2^n.
+    // SC: O(2^n), for storing result
+
     public IList<IList<int>> Subsets(int[] nums)
     {
-        return [];
+        var result = new List<IList<int>>();
+        var len = nums.Length;
+        var max = 1;
+
+        for (var i = 0; i < len; i++) max *= 2;
+
+        for (var j = 0; j < max; j++)
+        {
+            var bitIdx = len - 1;
+            var tmp = j;
+            var list = new List<int>();
+            while (bitIdx >= 0)
+            {
+                if ((tmp & 1) == 1)
+                {
+                    list.Insert(0, nums[bitIdx]);
+                }
+                tmp >>= 1;
+                bitIdx--;
+            }
+            result.Add(list);
+        }
+
+        return result;
     }
     public static TheoryData<int[], IList<IList<int>>> TestData => new()
     {

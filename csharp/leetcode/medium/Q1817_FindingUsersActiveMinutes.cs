466d57ef20a4edbc33738527cd1bc30ae8a63b01
Ferry To <ferrywlto@gmail.com>
2025-07-31 23:39:44 +0100

    test: All test cases of Q1817 passed

--- csharp/leetcode/medium/Q1817_FindingUsersActiveMinutes.cs
+++ csharp/leetcode/medium/Q1817_FindingUsersActiveMinutes.cs
@@ -1,8 +1,37 @@
 public class Q1817_FindingUsersActiveMinutes
 {
+    // TC: O(n + p + m), n scale with length of logs, p scale with number of user, m scale with unique UAM
+    // SC: O(p + m) 
     public int[] FindingUsersActiveMinutes(int[][] logs, int k)
     {
-        return [];
+        var len = logs.Length;
+        var dict = new Dictionary<int, HashSet<int>>();
+        for (var i = 0; i < len; i++)
+        {
+            (var id, var time) = (logs[i][0], logs[i][1]);
+            if (dict.TryGetValue(id, out var set))
+            {
+                set.Add(time);
+            }
+            else dict.Add(id, [time]);
+        }
+        var uams = new Dictionary<int, int>();
+        foreach (var p in dict)
+        {
+            var uamCount = p.Value.Count;
+            if (uams.TryGetValue(uamCount, out var val))
+            {
+                uams[uamCount] = ++val;
+            }
+            else uams.Add(uamCount, 1);
+        }
+
+        var result = new int[k];
+        foreach (var p in uams)
+        {
+            result[p.Key - 1] = p.Value;
+        }
+        return result;
     }
     public static TheoryData<int[][], int, int[]> TestData => new()
     {

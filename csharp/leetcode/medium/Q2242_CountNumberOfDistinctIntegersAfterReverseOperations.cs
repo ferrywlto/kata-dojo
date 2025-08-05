82986234da63a9ca63505df0d66313b547762add
Ferry To <ferrywlto@gmail.com>
2025-08-05 18:39:30 +0100

    test: All test cases of Q2242 passed

--- csharp/leetcode/medium/Q2242_CountNumberOfDistinctIntegersAfterReverseOperations.cs
+++ csharp/leetcode/medium/Q2242_CountNumberOfDistinctIntegersAfterReverseOperations.cs
@@ -1,13 +1,31 @@
+using System.Text;
+
 public class Q2242_CountNumberOfDistinctIntegersAfterReverseOperations
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(n) for storing the result
     public int CountDistinctIntegers(int[] nums)
     {
-        return 0;
+        var distinct = new HashSet<string>();
+        var sb = new StringBuilder();
+        var len = nums.Length;
+        for (var i = 0; i < len; i++)
+        {
+            distinct.Add(nums[i].ToString());
+            while (nums[i] > 0)
+            {
+                sb.Append(nums[i] % 10);
+                nums[i] /= 10;
+            }
+            distinct.Add(int.Parse(sb.ToString()).ToString());
+            sb.Clear();
+        }
+        return distinct.Count;
     }
     public static TheoryData<int[], int> TestData => new()
     {
         {[1,13,10,12,31], 6},
-        {[2,2,2], 6},
+        {[2,2,2], 1},
     };
     [Theory]
     [MemberData(nameof(TestData))]
@@ -15,5 +33,5 @@ public class Q2242_CountNumberOfDistinctIntegersAfterReverseOperations
     {
         var actual = CountDistinctIntegers(input);
         Assert.Equal(expected, actual);
-    }    
+    }
 }

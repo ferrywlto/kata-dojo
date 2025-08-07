70a3e6947d1d0745bf58b456833f6574c6804415
Ferry To <ferrywlto@gmail.com>
2025-08-07 22:25:04 +0100

    test: All test cases of Q861 passed

--- csharp/leetcode/medium/Q861_ScoreAfterFlippingMatrix.cs
+++ csharp/leetcode/medium/Q861_ScoreAfterFlippingMatrix.cs
@@ -1,13 +1,87 @@
 public class Q861_ScoreAfterFlippingMatrix
 {
+    // TC: O(row + col)
+    // SC: O(1)
     public int MatrixScore(int[][] grid)
     {
-        return 0;
+        var rowCount = grid.Length;
+        var colCount = grid[0].Length;
+
+        // var result = 0;
+        for (var row = 0; row < rowCount; row++)
+        {
+            // only need to check first col
+            if (grid[row][0] == 0)
+            {
+                // flip row
+                for (var col = 0; col < grid[row].Length; col++)
+                {
+                    grid[row][col] = grid[row][col] == 0 ? 1 : 0;
+                }
+            }
+        }
+
+        for (var col = 1; col < colCount; col++)
+        {
+            var zeroCount = 0;
+            var oneCount = 0;
+
+            for (var row = 0; row < grid.Length; row++)
+            {
+                if (grid[row][col] == 0) zeroCount++;
+                else oneCount++;
+            }
+
+            if (zeroCount > oneCount)
+            {
+                // flip col
+                for (var row = 0; row < grid.Length; row++)
+                {
+                    grid[row][col] = grid[row][col] == 0 ? 1 : 0;
+                }
+            }
+        }
+
+        return Sum(grid);
+    }
+    private int Sum(int[][] input)
+    {
+        var result = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            result += FromBinary(input[i]);
+        }
+        return result;
+    }
+    private int FromBinary(int[] input)
+    {
+        var result = input[0];
+        for (var i = 1; i < input.Length; i++)
+        {
+            result <<= 1;
+            result += input[i];
+        }
+        return result;
     }
     public static TheoryData<int[][], int> TestData => new()
     {
-        {[[0,0,1,1],[1,0,1,0],[1,1,0,0]], 39},
         {[[0]], 1},
+        {[
+            [0,0,1,1],
+            [1,0,1,0],
+            [1,1,0,0]
+        ], 39},
+        {[
+            [0,1,1],
+            [1,1,1],
+            [0,1,0]
+        ], 18},
+        {[
+            [1,1,1],
+            [0,1,0],
+            [1,0,0],
+            [1,0,1]
+        ], 25}
     };
     [Theory]
     [MemberData(nameof(TestData))]

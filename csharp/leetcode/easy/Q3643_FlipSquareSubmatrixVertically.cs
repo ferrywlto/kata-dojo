402c0088caeb846fbfd84055afd00d3cdd963093
Ferry To <ferrywlto@gmail.com>
2025-08-12 22:06:45 +0100

    test: All test cases of Q3643 passed

--- csharp/leetcode/easy/Q3643_FlipSquareSubmatrixVertically.cs
+++ csharp/leetcode/easy/Q3643_FlipSquareSubmatrixVertically.cs
@@ -1,8 +1,22 @@
 public class Q3643_FlipSquareSubmatrixVertically
 {
+    // TC: O(k ^ 2), k rows * k cols
+    // SC: O(1)
     public int[][] ReverseSubmatrix(int[][] grid, int x, int y, int k)
     {
-        return [];
+        // only need to flip k / 2 times (folding)
+        for (var rowOffset = 0; rowOffset < (k / 2); rowOffset++)
+        {
+            var top = x + rowOffset;
+            var bottom = x + k - rowOffset - 1;
+
+            for (var colOffset = 0; colOffset < k; colOffset++)
+            {
+                var col = y + colOffset;
+                (grid[bottom][col], grid[top][col]) = (grid[top][col], grid[bottom][col]);
+            }
+        }
+        return grid;
     }
     public static TheoryData<int[][], int, int, int, int[][]> TestData => new()
     {

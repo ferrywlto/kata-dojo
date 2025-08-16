7b56c905a4b38adaa53a7d40f9339e65aee5679e
Ferry To <ferrywlto@gmail.com>
2025-08-16 15:01:37 +0100

    test: All test cases of Q1305 passed

--- csharp/leetcode/medium/Q1305_AllElementsInTwoBST.cs
+++ csharp/leetcode/medium/Q1305_AllElementsInTwoBST.cs
@@ -1,13 +1,58 @@
 
-public class Q1305_AllElementsInTwoBST : TreeNodeTest
+public class Q1305_AllElementsInTwoBST(ITestOutputHelper output) : TreeNodeTest(output)
 {
-    public Q1305_AllElementsInTwoBST(ITestOutputHelper output) : base(output)
-    {
-    }
-
+    // TC: O(n + m) where n and m are the number of nodes in the two BSTs
+    // SC: O(n + m) for the result list
+    // The function returns a sorted list of all elements from both BSTs.
+    // It uses in-order traversal to collect elements from both trees and merges them.
     public IList<int> GetAllElements(TreeNode root1, TreeNode root2)
     {
-        return [];
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        var result = new List<int>();
+
+        InorderTraversal_Recursion(root1, list1);
+
+        InorderTraversal_Recursion(root2, list2);
+
+        var list1Idx = 0;
+        var list2Idx = 0;
+
+        while (list1Idx < list1.Count || list2Idx < list2.Count)
+        {
+            if (list1Idx == list1.Count && list2Idx < list2.Count)
+            {
+                result.Add(list2[list2Idx++]);
+                continue;
+            }
+
+            if (list2Idx == list2.Count && list1Idx < list1.Count)
+            {
+                result.Add(list1[list1Idx++]);
+                continue;
+            }
+
+            if (list1[list1Idx] < list2[list2Idx])
+            {
+                result.Add(list1[list1Idx++]);
+            }
+            else
+            {
+                result.Add(list2[list2Idx++]);
+            }
+        }
+
+        return result;
+    }
+
+    private void InorderTraversal_Recursion(TreeNode? node, IList<int> result)
+    {
+        if (node != null)
+        {
+            InorderTraversal_Recursion(node.left, result);
+            result.Add(node.val);
+            InorderTraversal_Recursion(node.right, result);
+        }
     }
 
     public static TheoryData<TreeNode, TreeNode, IList<int>> TestData => new()
@@ -23,7 +68,7 @@ public class Q1305_AllElementsInTwoBST : TreeNodeTest
             [1, 1, 8, 8]
         },
     };
-    
+
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(TreeNode root1, TreeNode root2, IList<int> expected)

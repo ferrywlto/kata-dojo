d4f45707198690cf4f645b4712a1bf6c717186ed
Ferry To <ferrywlto@gmail.com>
2025-08-19 22:27:17 +0100

    test: All test cases of Q2079 passed

--- csharp/leetcode/medium/Q2079_WateringPlants.cs
+++ csharp/leetcode/medium/Q2079_WateringPlants.cs
@@ -1,8 +1,26 @@
 public class Q2079_WateringPlants
 {
+    // TC: O(n), n scale with length of plants
+    // SC: O(1), space used does not scale with input.
     public int WateringPlants(int[] plants, int capacity)
     {
+        var currentWater = capacity;
+        var result = 0;
 
+        for (var i = 0; i < plants.Length; i++)
+        {
+            if (currentWater >= plants[i])
+            {
+                result++;
+                currentWater -= plants[i];
+            }
+            else
+            {
+                result += i + i + 1;
+                currentWater = capacity - plants[i];
+            }
+        }
+        return result;
     }
     public static TheoryData<int[], int, int> TestData => new()
     {

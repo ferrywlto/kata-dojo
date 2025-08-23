b5c4d41f743326ba763b0150941370a04da0664f
Ferry To <ferrywlto@gmail.com>
2025-08-23 21:13:10 +0100

    test: All test cases of Q3016 passed

--- csharp/leetcode/medium/Q3016_MinNumberOfPushesToTypeWordII.cs
+++ csharp/leetcode/medium/Q3016_MinNumberOfPushesToTypeWordII.cs
@@ -1,13 +1,43 @@
-public class Q3016_MinNumberOfPushesToTypeWordII
+public class Q3016_MinNumberOfPushesToTypeWordII(ITestOutputHelper output)
 {
+    // TC: O(n), n scale with length of word
+    // SC: O(1), space used does not scale with input
     public int MinimumPushes(string word)
     {
-        return 0;
+        var count = new int[26];
+        for (var i = 0; i < word.Length; i++)
+        {
+            count[word[i] - 'a']++;
+        }
+
+        Array.Sort(count);
+        output.WriteLine(string.Join(",", count));
+        var result = 0;
+        for (var i = 25; i >= 0; i--)
+        {
+            if (i > 17)
+            {
+                result += count[i];
+            }
+            else if (i > 9)
+            {
+                result += count[i] * 2;
+            }
+            else if (i > 1)
+            {
+                result += count[i] * 3;
+            }
+            else
+            {
+                result += count[i] * 4;
+            }
+        }
+        return result;
     }
     public static TheoryData<string, int> TestData => new()
     {
         {"abcde", 5},
-        {"xyzxyzxyzxyz", 13},
+        {"xyzxyzxyzxyz", 12},
         {"aabbccddeeffgghhiiiiii", 24},
     };
     [Theory]

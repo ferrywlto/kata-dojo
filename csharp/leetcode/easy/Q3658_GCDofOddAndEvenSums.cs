1617c9aa67eb1d5d0c5cf2cc6ce16c54a96049d8
Ferry To <ferrywlto@gmail.com>
2025-08-25 18:14:30 +0100

    test: All test cases of Q3658 passed

--- csharp/leetcode/easy/Q3658_GCDofOddAndEvenSums.cs
+++ csharp/leetcode/easy/Q3658_GCDofOddAndEvenSums.cs
@@ -1,9 +1,29 @@
 public class Q3658_GCDofOddAndEvenSums
 {
+    // Actually it just need to return n, as the different of sum is always n
+    // TC: O(1)
+    // SC: O(1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
     public int GcdOfOddEvenSums(int n)
     {
-        return 0;
+        var oddSum = 0;
+        var evenSum = 0;
+        for (var i = 0; i < n; i++)
+        {
+            var odd = (2 * i) + 1;
+            var even = 2 * (i + 1);
+
+            oddSum += odd;
+            evenSum += even;
+        }
+        return GCD(oddSum, evenSum);
     }
+
+    private int GCD(int a, int b)
+    {
+        if (b == 0) return a;
+        return GCD(b, a % b);
+    }
+
     public static TheoryData<int, int> TestData => new()
     {
         {4, 4},

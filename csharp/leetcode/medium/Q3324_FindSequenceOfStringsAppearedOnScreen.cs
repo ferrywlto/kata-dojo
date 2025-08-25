457ac67ef94ce2df488539fd31dc64fe66e52139
Ferry To <ferrywlto@gmail.com>
2025-08-25 18:33:46 +0100

    test: All test cases of Q3324 passed

--- csharp/leetcode/medium/Q3324_FindSequenceOfStringsAppearedOnScreen.cs
+++ csharp/leetcode/medium/Q3324_FindSequenceOfStringsAppearedOnScreen.cs
@@ -1,8 +1,27 @@
+using System.Text;
+
 public class Q3324_FindSequenceOfStringsAppearedOnScreen
 {
+    // TC: O(n), n scale with length of target
+    // SC: O(n), to store the intermediate string
     public IList<string> StringSequence(string target)
     {
-        return [];
+        var sb = new StringBuilder();
+
+        var result = new List<string>();
+
+        for (var i = 0; i < target.Length; i++)
+        {
+            sb.Append('a');
+            result.Add(sb.ToString());
+            while (sb[i] != target[i])
+            {
+                sb[i]++;
+                result.Add(sb.ToString());
+            }
+        }
+
+        return result;
     }
     public static TheoryData<string, IList<string>> TestData => new()
     {

d1b8f54ea69273f95b42db4daf529e3b719a7a38
Ferry To <ferrywlto@gmail.com>
2025-08-26 22:29:12 +0100

    test: All test cases of Q2317 passed

--- csharp/leetcode/medium/Q2317_MaxXorAfterOperations.cs
+++ csharp/leetcode/medium/Q2317_MaxXorAfterOperations.cs
@@ -1,13 +1,47 @@
 public class Q2317_MaxXorAfterOperations
 {
+    // The trick is if a bit is set to 1 at least once, take it.
+    // The result is the number created by the bit positions that at least 1
+    // If the bit position never set to 1, it remains 0
+    // Which means, the OR of all numbers. 
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int MaximumXOR(int[] nums)
     {
-        return 0;
+        var result = nums[0];
+        for (var i = 1; i < nums.Length; i++)
+        {
+            result |= nums[i];
+        }
+        return result;
     }
+    // This is to keep reference, the ops is just a confusion from the question. 
+    // The Op is to turn bits off.
+    // e.g. Op(3, 0) => 3 nothing change
+    // Op(3, 1) => 2
+    // Op(3, 2) => 1
+    // Op(3, 3) => 0 
+    // private int Op(int input, int x)
+    // {
+    //     return (input ^ x) & input;
+    // }
     public static TheoryData<int[], int> TestData => new()
     {
-        {[3,2,4,6], 7},
-        {[1,2,3,9,2], 11},
+        // 011
+        // 010
+        // 100
+        // 110
+        // --
+        // 111
+        { [3,2,4,6], 7},
+        // 0001 
+        // 0010
+        // 0011
+        // 1001
+        // 0010
+        // ---
+        // 1011
+        { [1,2,3,9,2], 11},
     };
     [Theory]
     [MemberData(nameof(TestData))]

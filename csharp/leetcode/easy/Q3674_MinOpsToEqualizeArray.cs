966af5ce2716d7c367037c2e04474b3fdd35b6d5
Ferry To <ferry@verdantsparks.io>
2025-10-17 17:42:06 +0100

    test: All test cases of Q3674 passed

--- csharp/leetcode/easy/Q3674_MinOpsToEqualizeArray.cs
+++ csharp/leetcode/easy/Q3674_MinOpsToEqualizeArray.cs
@@ -1,11 +1,18 @@
 public class Q3674_MinOpsToEqualizeArray
 {
+    // TC: O(n), n scale with length of nums. Worst case all items are equal, n = nums.length
+    // SC: O(1), space used does not scale with input.
     public int MinOperations(int[] nums)
     {
+        var start = nums[0];
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != start) return 1;
+        }
         return 0;
     }
 
-    public static TheoryData<int[], int> TestData => new() { { [1, 2], 1 }, { [5, 5, 5], 1 }, };
+    public static TheoryData<int[], int> TestData => new() { { [1, 2], 1 }, { [5, 5, 5], 0 }, };
 
     [Theory]
     [MemberData(nameof(TestData))]

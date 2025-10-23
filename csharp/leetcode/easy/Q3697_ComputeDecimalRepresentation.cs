33bb124f9e5c8a900d6cebfc0648dd7d3141181e
Ferry To <ferry@verdantsparks.io>
2025-10-23 23:24:59 +0100

    test: All test cases of Q3697 passed

--- csharp/leetcode/easy/Q3697_ComputeDecimalRepresentation.cs
+++ csharp/leetcode/easy/Q3697_ComputeDecimalRepresentation.cs
@@ -1,8 +1,22 @@
 public class Q3697_ComputeDecimalRepresentation
 {
+    // TC: O(log n), n scale with the size of the number
+    // SC: O(d), d is the number of digits in n
     public int[] DecimalRepresentation(int n)
     {
-        return [];
+        var result = new List<int>();
+        var radix = 1;
+        while (n > 0)
+        {
+            var digit = n % 10;
+            if (digit != 0)
+            {
+                result.Insert(0, digit * radix);
+            }
+            n /= 10;
+            radix *= 10;
+        }
+        return result.ToArray();
     }
     public static TheoryData<int, int[]> TestData => new TheoryData<int, int[]>
     {

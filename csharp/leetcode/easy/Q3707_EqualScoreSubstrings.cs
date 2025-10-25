42f1aa17e3e353e42beee34c39d6b907c7e2ba30
Ferry To <ferry@verdantsparks.io>
2025-10-25 16:47:01 +0100

    test: All test cases of Q3707 passed

--- csharp/leetcode/easy/Q3707_EqualScoreSubstrings.cs
+++ csharp/leetcode/easy/Q3707_EqualScoreSubstrings.cs
@@ -1,7 +1,22 @@
 public class Q3707_EqualScoreSubstrings
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used is constant
     public bool ScoreBalance(string s)
     {
+        var total = 0;
+        foreach (var ch in s) total += ch - 'a' + 1;
+
+        var left = s[0] - 'a' + 1;
+        var right = total - left;
+        if (left == right) return true;
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            left += s[i] - 'a' + 1;
+            right -= s[i] - 'a' + 1;
+            if (left == right) return true;
+        }
         return false;
     }
     public static TheoryData<string, bool> TestData => new()

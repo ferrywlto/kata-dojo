340c68b698fb3fe48a4511b69b1ba50c98c2f3d4
Ferry To <ferry@verdantsparks.io>
2025-11-03 14:05:44 +0000

    test: All test cases of Q3731 passed

--- csharp/leetcode/easy/Q3731_FindMissingElements.cs
+++ csharp/leetcode/easy/Q3731_FindMissingElements.cs
@@ -1,8 +1,27 @@
 public class Q3731_FindMissingElements
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), counts array size is fixed
     public IList<int> FindMissingElements(int[] nums)
     {
-        return [];
+        var min = 100;
+        var max = 1;
+        var counts = new int[101];
+        foreach (var num in nums)
+        {
+            counts[num]++;
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+        var result = new List<int>();
+        for (var i = min; i <= max; i++)
+        {
+            if (counts[i] == 0)
+            {
+                result.Add(i);
+            }
+        }
+        return result;
     }
     public static TheoryData<int[], IList<int>> TestData => new()
     {

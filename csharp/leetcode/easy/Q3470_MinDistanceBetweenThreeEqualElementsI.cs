a195a9b0b3927080780caebdf5a5a6efe9734be5
Ferry To <ferry@verdantsparks.io>
2025-11-09 23:37:43 +0000

    test: All test cases of Q3470 passed

--- csharp/leetcode/easy/Q3470_MinDistanceBetweenThreeEqualElementsI.cs
+++ csharp/leetcode/easy/Q3470_MinDistanceBetweenThreeEqualElementsI.cs
@@ -1,13 +1,41 @@
 public class Q3470_MinDistanceBetweenThreeEqualElementsI
 {
+    // TC: O(n * f * k) where n is the length of nums, f is the frequency of the elements count > 3, k is the number of such elements
+    // SC: O(d), where d is the number of distinct elements in nums
     public int MinimumDistance(int[] nums)
     {
-        return -1;
+        var freq = new int[101];
+        var bucket = new Dictionary<int, List<int>>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            freq[nums[i]]++;
+            if (!bucket.ContainsKey(nums[i]))
+                bucket[nums[i]] = [];
+            bucket[nums[i]].Add(i);
+        }
+
+        var minDistance = int.MaxValue;
+        for (var j = 1; j < freq.Length; j++)
+        {
+            if (freq[j] < 3) continue;
+
+            var indices = bucket[j];
+
+            for (var k = 0; k < indices.Count - 2; k++)
+            {
+                var dist = indices[k + 2] - indices[k] + indices[k + 2] - indices[k + 1] + indices[k + 1] - indices[k];
+                minDistance = Math.Min(minDistance, dist);
+            }
+        }
+        return minDistance == int.MaxValue ? -1 : minDistance;
     }
     public static TheoryData<int[], int> TestData => new()
     {
-      {[1,2,1,1,3], 6},
-      {[1,1,2,3,2,1,2], 8},
+        {[1,2,1,1,3], 6},
+        {[1,1,2,3,2,1,2], 8},
+        {[1], -1},
+        {[1,1,1], 4},
+        {[5,3,5,5,5], 4}
     };
     [Theory]
     [MemberData(nameof(TestData))]

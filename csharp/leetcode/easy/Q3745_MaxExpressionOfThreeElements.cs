6164054afab1ffb3ec8443c0917b790735afbc8e
Ferry To <ferry@verdantsparks.io>
2025-11-16 20:14:36 +0000

    test: All test cases of Q3745 passed

--- csharp/leetcode/easy/Q3745_MaxExpressionOfThreeElements.cs
+++ csharp/leetcode/easy/Q3745_MaxExpressionOfThreeElements.cs
@@ -1,8 +1,31 @@
 public class Q3745_MaxExpressionOfThreeElements
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input size
+
     public int MaximizeExpressionOfThree(int[] nums)
     {
-        return 0;
+        var max = int.MinValue;
+        var secondMax = int.MinValue;
+        var min = int.MaxValue;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > max)
+            {
+                secondMax = max;
+                max = nums[i];
+            }
+            else if (nums[i] > secondMax)
+            {
+                secondMax = nums[i];
+            }
+            if (nums[i] < min)
+            {
+                min = nums[i];
+            }
+        }
+        return max + secondMax - min;
     }
     public static TheoryData<int[], int> TestData => new()
     {

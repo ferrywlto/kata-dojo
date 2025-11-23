da68fbe133eb09742010f1c4cc61e8b0339c68f2
Ferry To <ferry@verdantsparks.io>
2025-11-23 14:35:41 +0000

    test: All test cases of Q3754 passed

--- csharp/leetcode/easy/Q3754_ConcatenateNonZeroDigitsAndMultiplyBySumI.cs
+++ csharp/leetcode/easy/Q3754_ConcatenateNonZeroDigitsAndMultiplyBySumI.cs
@@ -1,13 +1,31 @@
 public class Q3754_ConcatenateNonZeroDigitsAndMultiplyBySumI
 {
+    // TC: O(d) where d is the number of digits in n
+    // SC: O(1)
     public long SumAndMultiply(int n)
     {
-        return 0;
+        var x = n;
+        var nonZeroConcat = 0L;
+        var nonZeroSum = 0;
+        var rad = 1;
+        while (x > 0)
+        {
+            var digit = x % 10;
+            if (digit != 0)
+            {
+                var actual = digit * rad;
+                nonZeroConcat += actual;
+                nonZeroSum += digit;
+                rad *= 10;
+            }
+            x /= 10;
+        }
+        return nonZeroConcat * nonZeroSum;
     }
     public static TheoryData<int, long> TestData => new()
     {
-        { 10203004, 12340 }, // Non-zero digits: 1,2,3,4,5 -> "12345" = 12345 * (1+2+3+4+5)=15 -> 12345*15=185175
-        { 1000, 1 },    // Non-zero digits: 1 -> "1" = 1 * (1)=1 -> 1*1=1
+        { 10203004, 12340 },
+        { 1000, 1 },
     };
     [Theory]
     [MemberData(nameof(TestData))]

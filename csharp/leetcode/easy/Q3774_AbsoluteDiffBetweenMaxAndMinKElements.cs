0542f8b96ffc2adf8259b26a3e5a912d1267fa00
Ferry To <ferry@verdantsparks.io>
2025-12-14 23:10:11 +0000

    test: All test cases of Q3774 passed

--- csharp/leetcode/easy/Q3774_AbsoluteDiffBetweenMaxAndMinKElements.cs
+++ csharp/leetcode/easy/Q3774_AbsoluteDiffBetweenMaxAndMinKElements.cs
@@ -1,10 +1,25 @@
 public class Q3774_AbsoluteDiffBetweenMaxAndMinKElements
 {
+    // TC: O(n log n), dominated by Array.Sort()
+    // SC: O(1), space used does not scale with input.
     public int AbsDifference(int[] nums, int k)
     {
-        return 0;
+        Array.Sort(nums);
+        var min = 0;
+        var max = 0;
+        for (var i = 0; i < k; i++)
+        {
+            min += nums[i];
+            max += nums[nums.Length - i - 1];
+        }
+        return Math.Abs(max - min);
     }
 
+    public static TheoryData<int[], int, int> TestData = new TheoryData<int[], int, int>() {
+        {[5,2,2,4], 2, 5},
+        {[100], 1, 0},
+    };
+    
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(int[] input, int k, int expected)
@@ -13,8 +28,4 @@ public class Q3774_AbsoluteDiffBetweenMaxAndMinKElements
         Assert.Equal(expected, actual);
     }
 
-    public static TheoryData<int[], int, int> TestData = new TheoryData<int[], int, int>() {
-        {[5,2,2,4], 2, 5},
-        {[100], 1, 0},
-    };
 }

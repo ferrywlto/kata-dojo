dea5b26dc7124967b010df2f4407563a15cf194f
Ferry To <ferry@verdantsparks.io>
2025-12-17 22:52:54 +0000

    test: All test cases of Q2966 passed

---
+++ csharp/leetcode/medium/Q2966_DivideArrayIntoArraysWithMaxDiff.cs
@@ -0,0 +1,36 @@
+public class Q2966_DivideArrayIntoArraysWithMaxDiff
+{
+    public int[][] DivideArray(int[] nums, int k)
+    {
+        // nums.length = 3*k
+        Array.Sort(nums);
+        var result = new List<int[]>();
+        for(var i = 0; i<nums.Length-2; i++)
+        {
+            if(
+                (nums[i+1] - nums[i] <= k) &&
+                (nums[i+2] - nums[i+1] <= k) &&
+                (nums[i+2] - nums[i] <= k)
+            )
+            {
+                result.Add([nums[i], nums[i+1], nums[i+2]]);
+                i+=2;
+            }
+            else return [];
+        }
+        return result.ToArray();
+    }
+    public static TheoryData<int[], int, int[][]> TestData => new()
+    {
+        {[1,3,4,8,7,9,3,5,1], 2, [[1,1,3],[3,4,5],[7,8,9]]},
+        {[2,4,2,2,5,2], 2, []},
+        {[4,2,9,8,2,12,7,12,10,5,8,5,5,7,9,2,5,11], 14, [[2,2,2],[4,5,5],[5,5,7],[7,8,8],[9,9,10],[11,12,12]]}
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int k, int[][] expected)
+    {
+        var actual = DivideArray(input, k);
+        Assert.Equal(expected, actual);
+    }
+}

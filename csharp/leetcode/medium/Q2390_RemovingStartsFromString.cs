5799f987cc94810d2eac96304f8e8642f500135b
Ferry To <ferry@verdantsparks.io>
2025-12-18 22:56:18 +0000

    test: All test cases of Q2390 passed

--- csharp/leetcode/medium/Q2390_RemovingStartsFromString.cs
+++ csharp/leetcode/medium/Q2390_RemovingStartsFromString.cs
@@ -1,14 +1,28 @@
+using System.Text;
+
 public class Q2390_RemovingStartsFromString
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), for string builder
     public string RemoveStars(string s)
     {
-        return string.Empty;
+        var sb = new StringBuilder();
+        foreach (var c in s)
+        {
+            if (c == '*')
+                sb.Remove(sb.Length - 1, 1);
+            else
+                sb.Append(c);
+        }
+        return sb.ToString();
     }
+
     public static TheoryData<string, string> TestData => new()
     {
         {"leet**cod*e", "lecoe"},
         {"erase*****", ""},
     };
+
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(string input, string expected)

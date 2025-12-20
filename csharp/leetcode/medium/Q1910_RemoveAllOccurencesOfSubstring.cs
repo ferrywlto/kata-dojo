76daeda295d8be0be30176c9cb0dd056093bf773
Ferry To <ferry@verdantsparks.io>
2025-12-20 16:03:17 +0000

    test: All test cases of Q1910 passed

--- csharp/leetcode/medium/Q1910_RemoveAllOccurencesOfSubstring.cs
+++ csharp/leetcode/medium/Q1910_RemoveAllOccurencesOfSubstring.cs
@@ -1,8 +1,25 @@
-public class Q1910_RemoveAllOccurencesOfSubstring
+public class Q1910_RemoveAllOccurencesOfSubstring(ITestOutputHelper output)
 {
     public string RemoveOccurrences(string s, string part)
     {
-        return string.Empty;
+        while(true)
+        {
+            var idx = s.IndexOf(part);
+            if(idx == -1) return s;
+            else if(idx == 0)
+            {
+                s = s.Substring(part.Length);
+            }
+            else if(idx == s.Length - part.Length)
+            {
+                s = s.Substring(0, s.Length- part.Length);
+            }
+            else
+            {
+                s = s.Substring(0, idx) + s.Substring(idx + part.Length);
+            }
+            output.WriteLine($"current: {s}");
+        }
     }
     public static TheoryData<string, string, string> TestData => new()
     {

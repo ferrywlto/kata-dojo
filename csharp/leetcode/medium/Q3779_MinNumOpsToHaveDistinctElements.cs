20b5f8e5f89c38cef07a7eef149dc0f4eea62eee
Ferry To <ferry@verdantsparks.io>
2025-12-25 16:56:26 +0000

    test: All test cases of Q3779 passed

--- csharp/leetcode/medium/Q3779_MinNumOpsToHaveDistinctElements.cs
+++ csharp/leetcode/medium/Q3779_MinNumOpsToHaveDistinctElements.cs
@@ -1,14 +1,33 @@
 public class Q3779_MinNumOpsToHaveDistinctElements
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(m), m scale with unique numbers in nums
     public int MinOperations(int[] nums)
     {
-        return 0;
+        var set = new HashSet<int>();
+        var lastDupIdx = -1;
+
+        for (var i = nums.Length - 1; i >= 0; i--)
+        {
+            var key = nums[i];
+            if (!set.Add(key))
+            {
+                lastDupIdx = i;
+                break;
+            }
+        }
+
+        if (lastDupIdx == -1) return 0;
+
+        return (lastDupIdx / 3) + 1;
     }
+
     public static TheoryData<int[], int> TestData => new()
     {
         {[3,8,3,6,5,8], 1},
         {[2,2], 1},
         {[4,3,5,1,2], 0},
+        {[87,15,26,32,32,18], 2},
     };
     [Theory]
     [MemberData(nameof(TestData))]

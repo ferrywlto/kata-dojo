9f54773aed4bf04ef51872c63659625417d47179
Ferry To <ferry@verdantsparks.io>
2025-12-27 17:11:34 +0000

    test: Before fixing the timeout test cases of Q3771

--- csharp/leetcode/medium/Q3771_TotalScoreOfDungeonRuns.cs
+++ csharp/leetcode/medium/Q3771_TotalScoreOfDungeonRuns.cs
@@ -1,13 +1,29 @@
-public class Q3771_TotalScoreOfDungeonRuns
+public class Q3771_TotalScoreOfDungeonRuns(ITestOutputHelper output)
 {
     public long TotalScore(int hp, int[] damage, int[] requirement)
     {
-        return 0;
+        var score = 0L;
+        for(var i=0; i<damage.Length; i++)
+        {
+            score += Score(i, hp, damage, requirement);
+        }
+        return score;
+    }
+    private long Score(int startIdx, int hp, int[] damage, int[] requirement)
+    {
+        var score = 0L;
+        for(var i=startIdx; i<damage.Length; i++)
+        {
+            hp -= damage[i];
+            if(hp >= requirement[i]) score++;
+        }
+        output.WriteLine($"score: {score}");
+        return score;
     }
     public static TheoryData<int, int[], int[], int> TestData => new()
     {
         {11, [3,6,7], [4,2,5], 3},
-        {2, [10000,1], [1,1], 1}
+        {2, [10000,1], [1,1], 1},
     };
     [Theory]
     [MemberData(nameof(TestData))]

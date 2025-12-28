a71bd10f5b3f49989ad1f7cead6c4e0cef65d927
Ferry To <ferry@verdantsparks.io>
2025-12-28 17:57:25 +0000

    test: All test cases of Q3788 passed

--- csharp/leetcode/medium/Q3788_MaxScoreOfSplit.cs
+++ csharp/leetcode/medium/Q3788_MaxScoreOfSplit.cs
@@ -1,8 +1,31 @@
 public class Q3788_MaxScoreOfSplit
 {
+    // TC: O(n), n scale with nums.Length
+    // SC: O(n)
     public long MaximumScore(int[] nums)
     {
-        return 0;
+        var prefixSum = new long[nums.Length];
+        var suffixMin = new int[nums.Length];
+
+        prefixSum[0] = nums[0];
+        for (var i = 1; i < nums.Length; i++)
+        {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        var min = int.MaxValue;
+        for (var j = nums.Length - 1; j >= 1; j--)
+        {
+            min = Math.Min(min, nums[j]);
+            suffixMin[j - 1] = min;
+        }
+
+        var maxScore = long.MinValue;
+        for (var k = 0; k < nums.Length - 1; k++)
+        {
+            maxScore = Math.Max(maxScore, prefixSum[k] - suffixMin[k]);
+        }
+        return maxScore;
     }
     public static TheoryData<int[], int> TestData => new()
     {

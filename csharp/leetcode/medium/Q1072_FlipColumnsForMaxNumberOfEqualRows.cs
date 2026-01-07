206650018c453d923ef5359dc7516cd59a34e2ca
Ferry To <ferry@verdantsparks.io>
2026-01-07 21:52:49 +0000

    test: Add test cases of Q1072

---
+++ csharp/leetcode/medium/Q1072_FlipColumnsForMaxNumberOfEqualRows.cs
@@ -0,0 +1,26 @@
+public class Q1072_FlipColumnsForMaxNumberOfEqualRows
+{
+    public int MaxEqualRowsAfterFlips(int[][] matrix)
+    {
+        return 0;
+    }
+    public static TheoryData<int[][], int> TestData => new()
+    {
+        {
+            [[0,1],[1,1]], 1
+        },
+        {
+            [[0,1],[1,0]], 2
+        },
+        {
+            [[0,0,0],[0,0,1],[1,1,0]], 2
+        }
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[][] input, int expected)
+    {
+        var actual = MaxEqualRowsAfterFlips(input);
+        Assert.Equal(expected, actual);
+    }
+}

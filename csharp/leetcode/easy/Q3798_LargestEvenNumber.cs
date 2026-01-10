57d0caeecbfdca4ea7434d289623172d79fd8d23
Ferry To <ferry@verdantsparks.io>
2026-01-10 16:48:22 +0000

    test: All test cases of Q3798 passed

--- csharp/leetcode/easy/Q3798_LargestEvenNumber.cs
+++ csharp/leetcode/easy/Q3798_LargestEvenNumber.cs
@@ -1,8 +1,14 @@
+using System.Text;
+
 public class Q3798_LargestEvenNumber
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), to hold the result, worst case n = s.Length for already even number
     public string LargestEven(string s)
     {
-        return string.Empty;
+        var sb = new StringBuilder(s);
+        while (sb.Length > 0 && sb[^1] == '1') sb.Length--;
+        return sb.ToString();
     }
     public static TheoryData<string, string> TestData => new()
     {

27bf309f0d03eb29cc34597fc19c5dc2e0df99d2
Ferry To <ferry@verdantsparks.io>
2026-01-11 21:31:01 +0000

    test: All test cases of Q1780 passed

--- csharp/leetcode/medium/Q1780_CheckIfNumIsSumOfPowersOfThree.cs
+++ csharp/leetcode/medium/Q1780_CheckIfNumIsSumOfPowersOfThree.cs
@@ -1,8 +1,36 @@
 public class Q1780_CheckIfNumIsSumOfPowersOfThree
 {
+    int[] powers = [
+        3,
+        9,
+        27,
+        81,
+        243,
+        729,
+        2187,
+        6561,
+        19683,
+        59049,
+        177147,
+        531441,
+        1594323,
+        4782969,
+        14348907,
+        43046721,
+    ];
+
+    // TC: O(1), it run at most length of powers times
+    // SC: O(1)
     public bool CheckPowersOfThree(int n)
     {
-        return false;
+        var idx = powers.Length - 1;
+        while (idx >= 0)
+        {
+            while (idx >= 0 && powers[idx] > n) idx--;
+            if (idx >= 0)
+                n -= powers[idx--];
+        }
+        return n == 0 || n == 1;
     }
     public static TheoryData<int, bool> TestData => new()
     {

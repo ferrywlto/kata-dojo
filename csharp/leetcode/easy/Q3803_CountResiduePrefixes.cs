d392d42dd280e327817c5695b7f0614e946b7be8
Ferry To <ferry@verdantsparks.io>
2026-01-11 19:12:45 +0000

    test: All test cases of Q3803 passed

--- csharp/leetcode/easy/Q3803_CountResiduePrefixes.cs
+++ csharp/leetcode/easy/Q3803_CountResiduePrefixes.cs
@@ -1,13 +1,35 @@
 public class Q3803_CountResiduePrefixes
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1)
     public int ResiduePrefixes(string s)
     {
-        return 0;
+        Span<bool> seen = stackalloc bool[255];
+        var distinctCount = 0;
+
+        var set = new HashSet<char>();
+        var result = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (!seen[s[i]])
+            {
+                distinctCount++;
+                seen[s[i]] = true;
+            }
+
+            if (distinctCount > 3)
+                return result;
+            else if (distinctCount == (i + 1) % 3)
+                result++;
+        }
+        return result;
     }
     public static TheoryData<string, int> TestData => new()
     {
         {"abc", 2},
         {"dd", 1},
+        {"bob", 2},
+        {"bbbb", 2},
     };
     [Theory]
     [MemberData(nameof(TestData))]

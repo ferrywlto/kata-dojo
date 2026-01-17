1f80bb2e9c3795d65efaf525ba3e0135c6efde71
Ferry To <ferry@verdantsparks.io>
2026-01-17 23:25:40 +0000

    test: All test cases of Q1963 passed

--- csharp/leetcode/medium/Q1963_MinNumberOfSwapsToMakeStringBalanced.cs
+++ csharp/leetcode/medium/Q1963_MinNumberOfSwapsToMakeStringBalanced.cs
@@ -1,8 +1,34 @@
 public class Q1963_MinNumberOfSwapsToMakeStringBalanced
 {
+    // Poor description and name, the question didn't need the answer to be symmetric.
+    // TC: O(n)
+    // SC: O(1)
     public int MinSwaps(string s)
     {
-        return 0;
+        var swapCount = 0;
+        var imbalance = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '[')
+            {
+                imbalance++;
+            }
+            else
+            {
+                if (imbalance == 0)
+                {
+                    // It means ] > [, that means must need a swap from the right of current position. 
+                    swapCount++;
+                    // Open bracket swapped to the front therefore imbalance need to increment. 
+                    imbalance++;
+                }
+                else
+                {
+                    imbalance--;
+                }
+            }
+        }
+        return swapCount;
     }
     public static TheoryData<string, int> TestData => new()
     {

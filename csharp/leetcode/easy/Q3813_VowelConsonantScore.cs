6b9b26aa04d6eb6036e0495a6e9d7c8e7695b1e3
Ferry To <ferry@verdantsparks.io>
2026-01-19 00:26:33 +0000

    test: All test cases of Q3813 passed

--- csharp/leetcode/easy/Q3813_VowelConsonantScore.cs
+++ csharp/leetcode/easy/Q3813_VowelConsonantScore.cs
@@ -1,19 +1,21 @@
 public class Q3813_VowelConsonantScore
 {
-    private readonly HashSet<char> Vowels = ['a','e','i','o','u'];
+    private readonly HashSet<char> Vowels = ['a', 'e', 'i', 'o', 'u'];
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used does not scale with input
     public int VowelConsonantScore(string s)
     {
         var cCount = 0;
         var vCount = 0;
-        foreach(var c in s)
+        foreach (var c in s)
         {
-            if(char.IsAsciiLetter(c))
+            if (char.IsAsciiLetter(c))
             {
-                if(Vowels.Contains(char.ToLower(c))) vCount++;
-                else cCount++;   
+                if (Vowels.Contains(char.ToLower(c))) vCount++;
+                else cCount++;
             }
         }
-        return (int)Math.Floor((double)vCount/cCount);
+        return cCount == 0 ? 0 : (int)Math.Floor((double)vCount / cCount);
     }
     public static TheoryData<string, int> TestData => new()
     {

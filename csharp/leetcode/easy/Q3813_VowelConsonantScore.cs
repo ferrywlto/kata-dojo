5a5d61581335ae8677fb847e7632da2384dc8663
Ferry To <ferry@verdantsparks.io>
2026-01-19 00:25:17 +0000

    test: Add test cases of Q3813

---
+++ csharp/leetcode/easy/Q3813_VowelConsonantScore.cs
@@ -0,0 +1,31 @@
+public class Q3813_VowelConsonantScore
+{
+    private readonly HashSet<char> Vowels = ['a','e','i','o','u'];
+    public int VowelConsonantScore(string s)
+    {
+        var cCount = 0;
+        var vCount = 0;
+        foreach(var c in s)
+        {
+            if(char.IsAsciiLetter(c))
+            {
+                if(Vowels.Contains(char.ToLower(c))) vCount++;
+                else cCount++;   
+            }
+        }
+        return (int)Math.Floor((double)vCount/cCount);
+    }
+    public static TheoryData<string, int> TestData => new()
+    {
+        {"cooear", 2},
+        {"axeyizou", 1},
+        {"au 123", 0},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(string input, int expected)
+    {
+        var actual = VowelConsonantScore(input);
+        Assert.Equal(expected, actual);
+    }
+}

0a16a2fcfb88570867e5ace33d623be0092c888b
Ferry To <ferry@verdantsparks.io>
2026-01-25 23:56:40 +0000

    test: All test cases of Q3818 passed

--- csharp/leetcode/medium/Q3818_MinPrefixRemovalToMakeArrayStrictlyIncreasing.cs
+++ csharp/leetcode/medium/Q3818_MinPrefixRemovalToMakeArrayStrictlyIncreasing.cs
@@ -1,16 +1,23 @@
 ﻿public class Q3818_MinPrefixRemovalToMakeArrayStrictlyIncreasing
 {
-    public int MinimumPrefixLength(int[] nums) {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
+    public int MinimumPrefixLength(int[] nums)
+    {
+        for (var i = nums.Length - 1; i >= 1; i--)
+        {
+            if (nums[i - 1] >= nums[i]) return i;
+        }
         return 0;
     }
 
-    public static TheoryData<int[], int> TestData => new ()
+    public static TheoryData<int[], int> TestData => new()
     {
         {[1,-1,2,3,3,4,5], 4},
         {[4,3,-2,-5], 3},
         {[1,2,3,4], 0}
     };
-    
+
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(int[] input, int expected)

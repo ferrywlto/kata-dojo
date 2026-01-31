bcdbf29819041ea87750a81d7f089ede429afe00
Ferry To <ferry@verdantsparks.io>
2026-01-31 21:07:59 +0000

    test: All test cases of Q3810 passed

--- csharp/leetcode/medium/Q3810_MinOpsToReachTargetArray.cs
+++ csharp/leetcode/medium/Q3810_MinOpsToReachTargetArray.cs
@@ -1,8 +1,27 @@
 ﻿public class Q3810_MinOpsToReachTargetArray
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1)
+    // The technique is the confusing description, the maximum contiguous sequence is just for any subsequence of the same value, fill the sequence with values from target at the same positions.
+    // Then the description become pick a number in nums, replace all values with target at the same position.
+    // Therefore the answer is number of distinct values in nums that is different from target. (distinct because all of the same values will become target in single operation.)
     public int MinOperations(int[] nums, int[] target)
     {
-        return 0;
+        // Constraint max length is 100000, thus size need to be 100001
+        Span<int> freq = stackalloc int[100_001];
+
+        var result = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != target[i])
+            {
+                if(++freq[nums[i]] == 1)
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
     public static TheoryData<int[], int[], int> TestData => new()
     {

4b06d7ab1a2da81f34a4d49aaf560070191499dd
Ferry To <ferry@verdantsparks.io>
2026-02-01 23:07:52 +0000

    test: Add test cases of Q3804

---
+++ csharp/leetcode/medium/Q3804_NumberOfCenteredArray.cs
@@ -0,0 +1,19 @@
+﻿public class Q3804_NumberOfCanteredArray
+{
+    public int CenteredSubarrays(int[] nums)
+    {
+        return 0;
+    }
+    public static TheoryData<int[], int> TestData => new()
+    {
+        {[-1,1,0], 5},
+        {[2,-3], 1},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int expected)
+    {
+        var actual = CenteredSubarrays(input);
+        Assert.Equal(expected, actual);
+    }
+}

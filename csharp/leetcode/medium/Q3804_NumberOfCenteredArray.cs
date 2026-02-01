f56a1cf482c770db683df8d27ca7314b4ffed519
Ferry To <ferry@verdantsparks.io>
2026-02-01 23:20:32 +0000

    test: All test cases of Q3804 passed

--- csharp/leetcode/medium/Q3804_NumberOfCenteredArray.cs
+++ csharp/leetcode/medium/Q3804_NumberOfCenteredArray.cs
@@ -1,13 +1,32 @@
 ﻿public class Q3804_NumberOfCanteredArray
 {
+    // TC: O(n^2)
+    // SC: O(n), worst case set contains all elements (first run)
     public int CenteredSubarrays(int[] nums)
     {
-        return 0;
+        var result = 0;
+        var set = new HashSet<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            // single element must include
+            result++;
+            set.Clear();
+            set.Add(nums[i]);
+            var sum = nums[i];
+
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                set.Add(nums[j]);
+                sum += nums[j];
+                if (set.Contains(sum)) result++;
+            }
+        }
+        return result;
     }
     public static TheoryData<int[], int> TestData => new()
     {
         {[-1,1,0], 5},
-        {[2,-3], 1},
+        {[2,-3], 2},
     };
     [Theory]
     [MemberData(nameof(TestData))]

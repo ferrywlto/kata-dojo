2a9c4c744e44decc91b7ac3bdd12eaeccb25a2fb
Ferry To <ferry@verdantsparks.io>
2026-02-08 18:49:04 +0000

    pref: Improve speed of Q3823 from 2n to n

--- csharp/leetcode/easy/Q3823_ReverseLettersThenSpecialCharactersInString.cs
+++ csharp/leetcode/easy/Q3823_ReverseLettersThenSpecialCharactersInString.cs
@@ -1,20 +1,89 @@
-﻿public class Q3823_ReverseLettersThenSpecialCharactersInString
+﻿using System.Text;
+
+public class Q3823_ReverseLettersThenSpecialCharactersInString
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n)
     public string ReverseByType(string s)
     {
-        return "";
+        var charPos = new List<int>();
+        var nonCharPos = new List<int>();
+
+        for(var i=s.Length-1; i>=0; i--)
+        {
+            if(char.IsAsciiLetterLower(s[i]))
+            {
+                charPos.Add(i);
+            }
+            else
+            {
+                nonCharPos.Add(i);
+            }
+        }
+
+        var sb = new StringBuilder(s);
+        var charIdx = 0;
+        var nonCharIdx = 0;
+        for(var i=0; i<s.Length; i++)
+        {
+            if(char.IsAsciiLetterLower(s[i]))
+            {
+                sb[charPos[charIdx++]] = s[i];
+            }
+            else
+            {
+                sb[nonCharPos[nonCharIdx++]] = s[i];
+            }            
+        }
+        return sb.ToString();
     }
+
+    public string ReverseByTypeFast(string s)
+    {
+        var sb = new StringBuilder(s);
+        var charIdx = sb.Length - 1;
+        var nonCharIdx = sb.Length - 1;
+
+        for(var i=0; i<s.Length; i++)
+        {
+            if(char.IsAsciiLetterLower(s[i]))
+            {
+                // swap with first encounter char
+                while(!char.IsAsciiLetterLower(sb[charIdx])) { charIdx--; }
+                if(charIdx > i)
+                {                    
+                    (sb[charIdx], sb[i]) = (sb[i], sb[charIdx]);
+                    charIdx--;
+                }
+            }
+            else
+            {
+                // swap with first encounter non-char
+                while(char.IsAsciiLetterLower(sb[nonCharIdx])) { nonCharIdx--; }
+                if(nonCharIdx > i)
+                {                    
+                    (sb[nonCharIdx], sb[i]) = (sb[i], sb[nonCharIdx]);
+                    nonCharIdx--;
+                }
+            }            
+        }
+        return sb.ToString();
+    }
+    
     public static TheoryData<string, string> TestData => new()
     {
         {")ebc#da@f(", "(fad@cb#e)"},
         {"z", "z"},
         {"!@#$%^&*()", ")(*&^%$#@!"},
+        {"#zq", "#qz"},
+        {"fk#", "kf#"},
+        {"$!lv", "!$vl"},
     };
     [Theory]
     [MemberData(nameof(TestData))]
     public void Test(string input, string expected)
     {
-        var actual = ReverseByType(input);
+        var actual = ReverseByTypeFast(input);
         Assert.Equal(expected, actual);
     }
 }

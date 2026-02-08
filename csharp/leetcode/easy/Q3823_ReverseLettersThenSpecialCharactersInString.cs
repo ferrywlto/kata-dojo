851f3b15932312834c32dc183abdf2f49effb680
Ferry To <ferry@verdantsparks.io>
2026-02-08 18:16:08 +0000

    test: Add test cases of Q3823

---
+++ csharp/leetcode/easy/Q3823_ReverseLettersThenSpecialCharactersInString.cs
@@ -0,0 +1,20 @@
+﻿public class Q3823_ReverseLettersThenSpecialCharactersInString
+{
+    public string ReverseByType(string s)
+    {
+        return "";
+    }
+    public static TheoryData<string, string> TestData => new()
+    {
+        {")ebc#da@f(", "(fad@cb#e)"},
+        {"z", "z"},
+        {"!@#$%^&*()", ")(*&^%$#@!"},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(string input, string expected)
+    {
+        var actual = ReverseByType(input);
+        Assert.Equal(expected, actual);
+    }
+}

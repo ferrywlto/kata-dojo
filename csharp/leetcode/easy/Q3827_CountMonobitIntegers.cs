f51557e21940fe7b41e708545eabdaba2a723167
Ferry To <ferry@verdantsparks.io>
2026-02-08 17:47:08 +0000

    test: Add test cases of Q3827

---
+++ csharp/leetcode/easy/Q3827_CountMonobitIntegers.cs
@@ -0,0 +1,19 @@
+﻿public class Q3827_CountMonobitIntegers
+{
+    public int CountMonobit(int n)
+    {
+        return 0;
+    }
+    public static TheoryData<int, int> TestData => new()
+    {
+        {1, 2},
+        {4, 3},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int input, int expected)
+    {
+        var actual = CountMonobit(input);
+        Assert.Equal(expected, actual);
+    }
+}

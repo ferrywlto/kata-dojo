8b8d660a8a76222fe02023d6f5fa76b24bb6fbd9
Ferry To <ferry@verdantsparks.io>
2026-02-08 17:44:26 +0000

    test: All test cases of Q3828 passed

--- csharp/leetcode/medium/Q3828_FinalElementAfterSubarrayDeletions.cs
+++ csharp/leetcode/medium/Q3828_FinalElementAfterSubarrayDeletions.cs
@@ -1,8 +1,14 @@
 ﻿public class Q3828_FinalElementAfterSubarrayDeletions
 {
+    // TC: O(1)
+    // SC: O(1)
+    // This is because by observation, Alice must win if the largest element is at the head or tail on her move.
+    // Same as Bob as they will eliminate as much elements as they could for optimal play. 
+    // Whoever can push the largest/smallest element to the edge wins.
+    // Thus only the head and tail elements matters.
     public int FinalElement(int[] nums)
     {
-        return 0;
+        return Math.Max(nums[0], nums[nums.Length - 1]);
     }
 
     public static TheoryData<int[], int> TestData => new()

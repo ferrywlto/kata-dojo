1ee854e28fe33323ad7b567bd1cea0401129e0bc
Ferry To <ferry@verdantsparks.io>
2026-02-08 17:21:12 +0000

    test: Add test cases of Q3828

---
+++ csharp/leetcode/medium/Q3828_FinalElementAfterSubarrayDeletions.cs
@@ -0,0 +1,21 @@
+﻿public class Q3828_FinalElementAfterSubarrayDeletions
+{
+    public int FinalElement(int[] nums)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int> TestData => new()
+    {
+        {[1,5,2], 2},
+        {[3,7], 7},
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int expected)
+    {
+        var actual = FinalElement(input);
+        Assert.Equal(expected, actual);
+    }
+}

27ed6ec3964941042c05d7224d0499f2b2176551
Ferry To <ferry@verdantsparks.io>
2026-02-14 20:25:50 +0000

    test: Add test cases of Q3839

---
+++ csharp/leetcode/medium/Q3839_NumPrefixConnectedGroups.cs
@@ -0,0 +1,22 @@
+﻿public class Q3839_NumPrefixConnectedGroups
+{
+    public int PrefixConnected(string[] words, int k) {
+        return 0;
+    }   
+
+    public static TheoryData<string[], int, int> TestData => new ()
+    {
+        {["apple","apply","banana","bandit"], 2, 2},
+        {["car","cat","cartoon"], 3 , 1},
+        {["bat","dog","dog","doggy","bat"], 3, 2}
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(string[] input, int k, int expected)
+    {
+        var actual = PrefixConnected(input, k);
+        Assert.Equal(expected, actual);
+    }
+     
+}

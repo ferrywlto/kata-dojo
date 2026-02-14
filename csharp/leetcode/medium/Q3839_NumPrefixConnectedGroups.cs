880f22219315e34d4b34aa5730a45943387cf597
Ferry To <ferry@verdantsparks.io>
2026-02-14 20:41:14 +0000

    test: All test cases of Q3839 passed

--- csharp/leetcode/medium/Q3839_NumPrefixConnectedGroups.cs
+++ csharp/leetcode/medium/Q3839_NumPrefixConnectedGroups.cs
@@ -1,10 +1,23 @@
 ﻿public class Q3839_NumPrefixConnectedGroups
 {
-    public int PrefixConnected(string[] words, int k) {
-        return 0;
-    }   
+    // TC: O(n), n scale with length of words
+    // SC: O(n), worst case all prefix are unique
+    public int PrefixConnected(string[] words, int k)
+    {
+        var prefixSetAll = new HashSet<string>();
+        var prefixSetAtLeastTwo = new HashSet<string>();
+        foreach (var w in words)
+        {
+            if (w.Length < k) continue;
 
-    public static TheoryData<string[], int, int> TestData => new ()
+            var prefix = w[0..k];
+            if (!prefixSetAll.Add(prefix))
+                prefixSetAtLeastTwo.Add(prefix);
+        }
+        return prefixSetAtLeastTwo.Count;
+    }
+
+    public static TheoryData<string[], int, int> TestData => new()
     {
         {["apple","apply","banana","bandit"], 2, 2},
         {["car","cat","cartoon"], 3 , 1},
@@ -18,5 +31,5 @@
         var actual = PrefixConnected(input, k);
         Assert.Equal(expected, actual);
     }
-     
+
 }

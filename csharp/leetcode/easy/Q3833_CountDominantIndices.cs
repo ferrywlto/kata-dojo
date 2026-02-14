3238f84962c4a389bb3f47122190dc83a24a36bd
Ferry To <ferry@verdantsparks.io>
2026-02-14 19:54:38 +0000

    test: Add test cases of Q3833

---
+++ csharp/leetcode/easy/Q3833_CountDominantIndices.cs
@@ -0,0 +1,19 @@
+﻿public class Q3833_CountDominantIndices
+{
+    public int DominantIndices(int[] nums)
+    {
+        return 0;
+    }
+    public static TheoryData<int[], int> TestData => new()
+    {
+        {[5,4,3], 2},
+        {[4,1,2], 1},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int expected)
+    {
+        var actual = DominantIndices(input);
+        Assert.Equal(expected, actual);
+    }
+}

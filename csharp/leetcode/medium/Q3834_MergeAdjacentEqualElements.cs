cedf272e00886425d37a28f1fcabf89101a91478
Ferry To <ferry@verdantsparks.io>
2026-02-15 16:36:16 +0000

    test: Add test cases of Q3834

---
+++ csharp/leetcode/medium/Q3834_MergeAdjacentEqualElements.cs
@@ -0,0 +1,20 @@
+﻿public class Q3834_MergeAdjacentEqualElements
+{
+    public IList<long> MergeAdjacent(int[] nums)
+    {
+        return [];
+    }
+    public static TheoryData<int[], List<long>> TestData => new()
+    {
+        {[3,1,1,2], [3,4]},
+        {[2,2,4], [8]},
+        {[3,7,5], [3,7,5]}
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, List<long> expected)
+    {
+        var actual = MergeAdjacent(input);
+        Assert.Equal(expected, actual);
+    }
+}

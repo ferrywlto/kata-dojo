478ebdd9d8b0bfe491711665b6a4771e6bfba42a
Ferry To <ferry@verdantsparks.io>
2026-02-21 17:45:17 +0000

    test: All test cases of Q3843 passed

--- csharp/leetcode/medium/Q3843_FirstElementWithUniqueFrequency.cs
+++ csharp/leetcode/medium/Q3843_FirstElementWithUniqueFrequency.cs
@@ -1,8 +1,55 @@
 ﻿public class Q3843_FirstElementWithUniqueFrequency
 {
+    // TC: O(n), n scale with length of num
+    // SC: O(n + f + d), n scale with unique numbers in num, f scale with unique frequencies, d scale with number of frequency count is 1
     public int FirstUniqueFreq(int[] nums)
     {
-        return 0;
+        var freq = new Dictionary<int, int>();
+        var bucket = new Dictionary<int, List<int>>();
+
+        foreach (var n in nums)
+        {
+            if (freq.TryGetValue(n, out var val))
+            {
+                freq[n] = ++val;
+            }
+            else
+            {
+                freq.Add(n, 1);
+            }
+        }
+
+        foreach (var (key, value) in freq)
+        {
+            if (bucket.TryGetValue(value, out var list))
+            {
+                list.Add(key);
+            }
+            else
+            {
+                bucket.Add(value, [key]);
+            }
+        }
+
+        var uniqueFreqSet = new HashSet<int>();
+
+        foreach (var b in bucket)
+        {
+            if (b.Value.Count == 1)
+            {
+                uniqueFreqSet.Add(b.Value[0]);
+            }
+        }
+
+        if (uniqueFreqSet.Count == 0) return -1;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (uniqueFreqSet.Contains(nums[i]))
+                return nums[i];
+        }
+
+        return -1;
     }
     public static TheoryData<int[], int> TestData => new()
     {

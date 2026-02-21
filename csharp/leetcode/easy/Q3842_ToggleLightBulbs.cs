7fbb8e545cb93a3b78b4df68b2ededa87ae68b0f
Ferry To <ferry@verdantsparks.io>
2026-02-21 17:50:20 +0000

    test: Add test cases of Q3842

---
+++ csharp/leetcode/easy/Q3842_ToggleLightBulbs.cs
@@ -0,0 +1,19 @@
+﻿public class Q3842_ToggleLightBulbs
+{
+    public IList<int> ToggleLightBulbs(IList<int> bulbs)
+    {
+        return [];
+    }
+    public static TheoryData<List<int>, List<int>> TestData => new()
+    {
+        {[10,30,20,10], [20,30]},
+        {[100,100], []},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(List<int> input, List<int> expected)
+    {
+        var actual = ToggleLightBulbs(input);
+        Assert.Equal(expected, actual);
+    }
+}

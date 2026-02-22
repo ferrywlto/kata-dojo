27e1a540d8082033059ddb9ef5f497b36670d95e
Ferry To <ferry@verdantsparks.io>
2026-02-22 22:00:51 +0000

    perf: Improve speed of Q3439 by running one N less

--- csharp/leetcode/medium/Q3849_MaxBitwiseXorAfterRearrangement.cs
+++ csharp/leetcode/medium/Q3849_MaxBitwiseXorAfterRearrangement.cs
@@ -1,43 +1,42 @@
 ﻿using System.Text;
 
-public class Q3849_MaxBitwiseXorAfterRearrangement//(ITestOutputHelper output)
+public class Q3849_MaxBitwiseXorAfterRearrangement
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), for storing the result
     public string MaximumXor(string s, string t)
     {
         var oneCount = 0;
-        foreach(var c in t) if (c == '1') oneCount++;
+        foreach (var c in t) if (c == '1') oneCount++;
 
         // For max, flip all 0 to 1
         // then if still have one left in t, flip the 1 from right to left to minimize loss
-        var flipIdx = new List<int>();
+        var zeroIdx = new List<int>();
+        var oneIdx = new List<int>();
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '0')
+                zeroIdx.Add(i);
+
+            var reverseIdx = s.Length - i - 1;
+            if (s[reverseIdx] == '1')
+                oneIdx.Add(reverseIdx);
+        }
 
         var sb = new StringBuilder(s);
-        for(var i =0; i<sb.Length; i++)
+        var startIdx = 0;
+        while(oneCount > 0 && startIdx < zeroIdx.Count)
         {
-            if(sb[i] == '0' && oneCount > 0)
-            {
-                flipIdx.Add(i);
-                oneCount--;
-            }
+            sb[zeroIdx[startIdx++]] = '1';
+            oneCount--;
         }
 
-        var tailIdx = sb.Length -1;
-        while(oneCount>0 && tailIdx >=0)
+        var tailIdx = 0;
+        while (oneCount > 0 && tailIdx < oneIdx.Count)
         {
-            if(sb[tailIdx] == '1')
-            {
-                flipIdx.Add(tailIdx);
-                oneCount--;
-            }
-            tailIdx--;
-        }
-
-        // output.WriteLine(string.Join(',', flipIdx));
-        // Perform the actual flip
-        foreach(var idx in flipIdx)
-        {
-            if(sb[idx] == '1') sb[idx] = '0';
-            else sb[idx] = '1';
+            sb[oneIdx[tailIdx++]] = '0';
+            oneCount--;
         }
 
         return sb.ToString();

0f34a88c302fc39199185128af032b3ba114c21f
Ferry To <ferry@verdantsparks.io>
2026-02-22 13:54:01 +0000

    test: Add test cases of Q3849

---
+++ csharp/leetcode/medium/Q3849_MaxBitwiseXorAfterRearrangement.cs
@@ -0,0 +1,20 @@
+﻿public class Q3849_MaxBitwiseXorAfterRearrangement
+{
+    public string MaximumXor(string s, string t)
+    {
+        return "";
+    }
+    public static TheoryData<string, string, string> TestData => new()
+    {
+        {"101", "011", "110"},
+        {"0110", "1110", "1101"},
+        {"0101", "1001", "1111"},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(string s, string t, string expected)
+    {
+        var actual = MaximumXor(s, t);
+        Assert.Equal(expected, actual);
+    }
+}

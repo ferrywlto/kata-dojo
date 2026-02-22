4cc7ca9131b0b615e099c48d39eafbfef17e57d1
Ferry To <ferry@verdantsparks.io>
2026-02-22 14:12:28 +0000

    test: All test cases of Q3849 passed

--- csharp/leetcode/medium/Q3849_MaxBitwiseXorAfterRearrangement.cs
+++ csharp/leetcode/medium/Q3849_MaxBitwiseXorAfterRearrangement.cs
@@ -1,8 +1,46 @@
-﻿public class Q3849_MaxBitwiseXorAfterRearrangement
+﻿using System.Text;
+
+public class Q3849_MaxBitwiseXorAfterRearrangement//(ITestOutputHelper output)
 {
     public string MaximumXor(string s, string t)
     {
-        return "";
+        var oneCount = 0;
+        foreach(var c in t) if (c == '1') oneCount++;
+
+        // For max, flip all 0 to 1
+        // then if still have one left in t, flip the 1 from right to left to minimize loss
+        var flipIdx = new List<int>();
+
+        var sb = new StringBuilder(s);
+        for(var i =0; i<sb.Length; i++)
+        {
+            if(sb[i] == '0' && oneCount > 0)
+            {
+                flipIdx.Add(i);
+                oneCount--;
+            }
+        }
+
+        var tailIdx = sb.Length -1;
+        while(oneCount>0 && tailIdx >=0)
+        {
+            if(sb[tailIdx] == '1')
+            {
+                flipIdx.Add(tailIdx);
+                oneCount--;
+            }
+            tailIdx--;
+        }
+
+        // output.WriteLine(string.Join(',', flipIdx));
+        // Perform the actual flip
+        foreach(var idx in flipIdx)
+        {
+            if(sb[idx] == '1') sb[idx] = '0';
+            else sb[idx] = '1';
+        }
+
+        return sb.ToString();
     }
     public static TheoryData<string, string, string> TestData => new()
     {

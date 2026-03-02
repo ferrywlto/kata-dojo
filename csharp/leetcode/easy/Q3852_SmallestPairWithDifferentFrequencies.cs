48ae8053b267b04ae9311c702803e8c5b428048f
Ferry To <ferry@verdantsparks.io>
2026-03-02 23:18:46 +0000

    test: All test cases of Q3852 passed

--- csharp/leetcode/easy/Q3852_SmallestPairWithDifferentFrequencies.cs
+++ csharp/leetcode/easy/Q3852_SmallestPairWithDifferentFrequencies.cs
@@ -1,8 +1,31 @@
 ﻿public class Q3852_SmallestPairWithDifferentFrequencies
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input.
     public int[] MinDistinctFreqPair(int[] nums)
     {
-        return [];
+        Span<int> freq = stackalloc int[101];
+        foreach (var n in nums)
+            freq[n]++;
+
+        var first = -1;
+        var firstFreq = 0;
+
+        for (var i = 1; i < freq.Length; i++)
+        {
+            if (freq[i] == 0) continue;
+
+            if (first == -1)
+            {
+                first = i;
+                firstFreq = freq[i];
+            }
+            else if (freq[i] != firstFreq)
+            {
+                return [first, i];
+            }
+        }
+        return [-1, -1];
     }
     public static TheoryData<int[], int[]> TestData => new()
     {

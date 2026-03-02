821a1d83ee54d0d404b84cabb299d51bf9dafd37
Ferry To <ferry@verdantsparks.io>
2026-03-02 16:14:17 +0000

    test: All test cases of Q3856 passed

--- csharp/leetcode/easy/Q3856_TrimTrailingVowels.cs
+++ csharp/leetcode/easy/Q3856_TrimTrailingVowels.cs
@@ -1,14 +1,27 @@
-﻿public class Q3856_TrimTrailingVowels
+﻿using System.Text;
+
+public class Q3856_TrimTrailingVowels
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(n), for storing result, worst case no vowel found.
     public string TrimTrailingVowels(string s)
     {
-        return "";
+        var sb = new StringBuilder(s);
+        while(sb.Length > 0 && IsVowel(sb[^1]))
+        {
+            sb.Remove(sb.Length - 1 , 1);
+        }
+        return sb.ToString();
+    }
+    private bool IsVowel(char input)
+    {
+        return input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u';
     }
     public static TheoryData<string, string> TestData => new()
     {
         {"idea", "id"},
         {"day", "day"},
-        {"aeiou", "aeiou"},
+        {"aeiou", ""},
     };
     [Theory]
     [MemberData(nameof(TestData))]

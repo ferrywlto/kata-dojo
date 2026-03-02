1573e3a7634f247ba6f3a6797461a8b3de97f4b6
Ferry To <ferry@verdantsparks.io>
2026-03-02 16:08:33 +0000

    test: Add test cases of Q3856

---
+++ csharp/leetcode/easy/Q3856_TrimTrailingVowels.cs
@@ -0,0 +1,20 @@
+﻿public class Q3856_TrimTrailingVowels
+{
+    public string TrimTrailingVowels(string s)
+    {
+        return "";
+    }
+    public static TheoryData<string, string> TestData => new()
+    {
+        {"idea", "id"},
+        {"day", "day"},
+        {"aeiou", "aeiou"},
+    };
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(string input, string expected)
+    {
+        var actual = TrimTrailingVowels(input);
+        Assert.Equal(expected, actual);
+    }
+}

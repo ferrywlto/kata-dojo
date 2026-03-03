e1d041422fd74f019fe78817b043f9f94aebc4a8
Ferry To <ferry@verdantsparks.io>
2026-03-03 21:02:08 +0000

    test: Add test cases of Q3853

---
+++ csharp/leetcode/medium/Q3853_MergeCloseCharacters.cs
@@ -0,0 +1,20 @@
+﻿public class Q3853_MergeCloseCharacters
+{
+    public string MergeCharacters(string s, int k)
+    {
+        return "";
+    }
+
+    public static TheoryData<string, int, string> TestData => new()
+    {
+        { "abca", 3, "abc" }, { "aabca", 2, "abca" }, { "yybyzybz", 2, "ybzybz" },
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(string input, int k, string expected)
+    {
+        var actual = MergeCharacters(input, k);
+        Assert.Equal(expected, actual);
+    }
+}

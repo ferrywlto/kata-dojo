afa54522b2191b2306e595994cc3e5012504a3f7
Ferry To <ferry@verdantsparks.io>
2026-03-04 21:23:37 +0000

    test: All test cases of Q3857 passed

--- csharp/leetcode/medium/Q3857_MinCostToSplitIntoOnes.cs
+++ csharp/leetcode/medium/Q3857_MinCostToSplitIntoOnes.cs
@@ -1,8 +1,20 @@
 ﻿public class Q3857_MinCostToSplitIntoOnes
 {
+    // TC: O(n), n scale with input
+    // SC: O(1), space used does not scale with input
     public int MinCost(int n)
     {
-        return 0;
+        return Cost(n);
+    }
+
+    private int Cost(int n)
+    {
+        if (n == 2) return 1;
+        if (n == 1) return 0;
+        if (n % 2 == 0)
+            return 2 * (n - 2) + Cost(2) + Cost(n - 2);
+
+        return (n - 1) + Cost(1) + Cost(n - 1);
     }
 
     public static TheoryData<int, int> TestData => new() { { 3, 3 }, { 4, 6 }, };

a45bd277915e73b79484cc1bd40b66f73596a27f
Ferry To <ferry@verdantsparks.io>
2026-03-04 21:06:02 +0000

    test: Add test cases of Q3857

---
+++ csharp/leetcode/medium/Q3857_MinCostToSplitIntoOnes.cs
@@ -0,0 +1,17 @@
+﻿public class Q3857_MinCostToSplitIntoOnes
+{
+    public int MinCost(int n)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int, int> TestData => new() { { 3, 3 }, { 4, 6 }, };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int input, int expected)
+    {
+        var actual = MinCost(input);
+        Assert.Equal(expected, actual);
+    }
+}

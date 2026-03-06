82b394a421022ca8b68217db8a6945f75ffdc824
Ferry To <ferry@verdantsparks.io>
2026-03-06 19:29:25 +0000

    test: All test cases of Q3847 passed

--- csharp/leetcode/medium/Q3847_FindScoreDiffInGame.cs
+++ csharp/leetcode/medium/Q3847_FindScoreDiffInGame.cs
@@ -1,16 +1,26 @@
 ﻿public class Q3847_FindScoreDiffInGame
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int ScoreDifference(int[] nums)
     {
-        return 0;
+        Span<int> scores = stackalloc int[2];
+        var idx = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (i % 6 == 5)
+                idx = idx == 0 ? 1 : 0;
+
+            if (nums[i] % 2 == 1)
+                idx = idx == 0 ? 1 : 0;
+
+            scores[idx] += nums[i];
+        }
+
+        return scores[0] - scores[1];
     }
 
-    public static TheoryData<int[], int> TestData => new()
-    {
-        { [1, 2, 3], 0 },
-        { [2, 4, 2, 1, 2, 1], 4 },
-        { [1], -1 }
-    };
+    public static TheoryData<int[], int> TestData => new() { { [1, 2, 3], 0 }, { [2, 4, 2, 1, 2, 1], 4 }, { [1], -1 } };
 
     [Theory]
     [MemberData(nameof(TestData))]

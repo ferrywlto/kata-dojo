08fc9a106d82f61add2278c41ce7cdb559b9fc0f
Ferry To <ferry@verdantsparks.io>
2026-03-06 19:24:40 +0000

    test: Add test cases of Q3847

---
+++ csharp/leetcode/medium/Q3847_FindScoreDiffInGame.cs
@@ -0,0 +1,22 @@
+﻿public class Q3847_FindScoreDiffInGame
+{
+    public int ScoreDifference(int[] nums)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int> TestData => new()
+    {
+        { [1, 2, 3], 0 },
+        { [2, 4, 2, 1, 2, 1], 4 },
+        { [1], -1 }
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int expected)
+    {
+        var actual = ScoreDifference(input);
+        Assert.Equal(expected, actual);
+    }
+}

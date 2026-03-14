abc3808795e632e6a9305cfdd32bd453950cb581
Ferry To <ferry@verdantsparks.io>
2026-03-14 21:54:27 +0000

    test: Add test cases of Q3866

---
+++ csharp/leetcode/easy/Q3866_FirstUniqueEvenElement.cs
@@ -0,0 +1,17 @@
+﻿public class Q3866_FirstUniqueEvenElement
+{
+    public int FirstUniqueEven(int[] nums)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int> TestData => new() { { [3, 4, 2, 5, 4, 6], 2 }, { [4, 4], -1 }, };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int expected)
+    {
+        var actual = FirstUniqueEven(input);
+        Assert.Equal(expected, actual);
+    }
+}

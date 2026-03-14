f03d2dfffca9fee8e28c81afe420fe97a98f7513
Ferry To <ferry@verdantsparks.io>
2026-03-14 22:14:41 +0000

    test: All test cases of Q3866 passed

--- csharp/leetcode/easy/Q3866_FirstUniqueEvenElement.cs
+++ csharp/leetcode/easy/Q3866_FirstUniqueEvenElement.cs
@@ -1,11 +1,36 @@
 ﻿public class Q3866_FirstUniqueEvenElement
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1), space used does not scale with input
     public int FirstUniqueEven(int[] nums)
     {
-        return 0;
+        Span<int> freq = stackalloc int[101];
+        Span<int> pos = stackalloc int[101];
+        for (var i = 0; i < pos.Length; i++) pos[i] = -1;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            freq[nums[i]]++;
+
+            if (pos[nums[i]] == -1)
+                pos[nums[i]] = i;
+        }
+
+        var result = -1;
+        var resultIdx = int.MaxValue;
+        for (var i = 2; i < pos.Length; i += 2)
+        {
+            if (freq[i] == 1 && pos[i] < resultIdx)
+            {
+                resultIdx = pos[i];
+                result = i;
+            }
+        }
+
+        return result;
     }
 
-    public static TheoryData<int[], int> TestData => new() { { [3, 4, 2, 5, 4, 6], 2 }, { [4, 4], -1 }, };
+    public static TheoryData<int[], int> TestData => new() { { [3, 4, 2, 5, 4, 6], 2 }, { [4, 4], -1 }, { [2], 2 } };
 
     [Theory]
     [MemberData(nameof(TestData))]

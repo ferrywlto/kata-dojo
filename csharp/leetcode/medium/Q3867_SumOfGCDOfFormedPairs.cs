4ed894edb7eb9d756aeaa7a33cbe315239d98b8f
Ferry To <ferry@verdantsparks.io>
2026-03-14 19:03:54 +0000

    test: Add test cases of Q3867

---
+++ csharp/leetcode/medium/Q3867_SumOfGCDOfFormedPairs.cs
@@ -0,0 +1,17 @@
+﻿public class Q3867_SumOfGCDOfFormedPairs
+{
+    public long GcdSum(int[] nums)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int> TestData => new() { { [2, 6, 4], 2 }, { [3, 6, 2, 8], 5 }, };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int expected)
+    {
+        var actual = GcdSum(input);
+        Assert.Equal(expected, actual);
+    }
+}

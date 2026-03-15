c343880fa49f06b31be52c844ef94eb073710787
Ferry To <ferry@verdantsparks.io>
2026-03-15 17:46:39 +0000

    test: Add test cases of Q3668

---
+++ csharp/leetcode/medium/Q3668_MinCostToEqualizeArraysUsingSwaps.cs
@@ -0,0 +1,20 @@
+﻿public class Q3668_MinCostToEqualizeArraysUsingSwaps
+{
+    public int MinCost(int[] nums1, int[] nums2)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int[], int> TestData => new()
+    {
+        { [10, 20], [20, 10], 0 }, { [10, 10], [20, 20], 1 }, { [10, 20], [30, 40], -1 },
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] nums1, int[] nums2, int expected)
+    {
+        var actual = MinCost(nums1, nums2);
+        Assert.Equal(expected, actual);
+    }
+}

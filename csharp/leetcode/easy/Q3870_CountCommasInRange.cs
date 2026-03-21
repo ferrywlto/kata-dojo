149f7398cb0f71586dac9837f068ee692d2e4212
Ferry To <ferry@verdantsparks.io>
2026-03-21 22:58:14 +0000

    test: Add test cases of Q3870

---
+++ csharp/leetcode/easy/Q3870_CountCommasInRange.cs
@@ -0,0 +1,20 @@
+﻿public class Q3870_CountCommasInRange
+{
+    public int CountCommas(int n)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int, int> TestData => new()
+    {
+        { 1002, 3 }, { 998, 0 }, { 1000, 1 }, { 100000, 1 },
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int input, int expected)
+    {
+        var actual = CountCommas(input);
+        Assert.Equal(expected, actual);
+    }
+}

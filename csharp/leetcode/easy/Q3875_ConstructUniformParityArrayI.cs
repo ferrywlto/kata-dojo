c6af4cc70e5b082d7cc46ab9f4e8540c9358bf70
Ferry To <ferry@verdantsparks.io>
2026-03-22 19:15:14 +0000

    test: Add test cases of Q3875

---
+++ csharp/leetcode/easy/Q3875_ConstructUniformParityArrayI.cs
@@ -0,0 +1,17 @@
+﻿public class Q3875_ConstructUniformParityArrayI
+{
+    public bool UniformArray(int[] nums1)
+    {
+        return false;
+    }
+
+    public static TheoryData<int[], bool> TestData => new() { { [2, 3], true }, { [4, 6], false }, };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, bool expected)
+    {
+        var actual = UniformArray(input);
+        Assert.Equal(expected, actual);
+    }
+}

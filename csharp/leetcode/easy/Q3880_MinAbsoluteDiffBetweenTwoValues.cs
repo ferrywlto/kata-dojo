d944dd4f65850216515372b0df48e562cae222d9
Ferry To <ferry@verdantsparks.io>
2026-03-28 17:23:51 +0000

    test: Add test cases of Q3880

---
+++ csharp/leetcode/easy/Q3880_MinAbsoluteDiffBetweenTwoValues.cs
@@ -0,0 +1,21 @@
+﻿public class Q3880_MinAbsoluteDiffBetweenTwoValues
+{
+    public int MinAbsoluteDifference(int[] nums)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int> TestData => new()
+    {
+        { [1, 0, 0, 2, 0, 1], 2 },
+        {[1, 0, 1, 0], -1}
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int expected)
+    {
+        var actual = MinAbsoluteDifference(input);
+        Assert.Equal(expected, actual);
+    }
+}

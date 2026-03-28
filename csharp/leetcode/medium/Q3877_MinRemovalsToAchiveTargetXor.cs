88c2e37dccc74e8720bdfd21afede51ffbb6a0d6
Ferry To <ferry@verdantsparks.io>
2026-03-28 21:34:15 +0000

    test: Add test cases of Q3877

---
+++ csharp/leetcode/medium/Q3877_MinRemovalsToAchiveTargetXor.cs
@@ -0,0 +1,20 @@
+﻿public class Q3877_MinRemovalsToAchiveTargetXor
+{
+    public int MinRemovals(int[] nums, int target)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int, int> TestData => new()
+    {
+        { [1, 2, 3], 2, 1 }, { [2, 4], 2, 1 }, { [7], 1, -1 },
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int target, int expected)
+    {
+        var actual = MinRemovals(input, target);
+        Assert.Equal(expected, actual);
+    }
+}

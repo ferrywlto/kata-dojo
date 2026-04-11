da07e478b653c632d459b3c18cadcd6648378d66
Ferry To <ferry@verdantsparks.io>
2026-04-11 23:54:39 +0100

    test: All test cases of Q3894 passed

--- csharp/leetcode/easy/Q3894_TrafficSingalColor.cs
+++ csharp/leetcode/easy/Q3894_TrafficSingalColor.cs
@@ -1,8 +1,16 @@
 ﻿public class Q3894_TrafficSingalColor
 {
+    // TC: O(1)
+    // SC: O(1)
     public string TrafficSignal(int timer)
     {
-        return "";
+        return timer switch
+        {
+            0 => "Green",
+            30 => "Orange",
+            > 30 and <= 90 => "Red",
+            _ => "Invalid"
+        };
     }
 
     public static TheoryData<int, string> TestData => new() { { 60, "Red" }, { 5, "Invalid" }, };

66b3dfb98009c895289d750bc6cf6c70e47d2ef3
Ferry To <ferry@verdantsparks.io>
2026-04-12 00:00:50 +0100

    test: All test cases of Q3895 passed

--- csharp/leetcode/medium/Q3895_CountDigitAppeasrances.cs
+++ csharp/leetcode/medium/Q3895_CountDigitAppeasrances.cs
@@ -1,8 +1,21 @@
 ﻿public class Q3895_CountDigitAppeasrances
 {
+    // TC: O(log n)
+    // SC: O(1)
     public int CountDigitOccurrences(int[] nums, int digit)
     {
-        return 0;
+        var result = 0;
+        foreach (var n in nums)
+        {
+            var curr = n;
+            while(curr > 0)
+            {
+                if (curr % 10 == digit)
+                    result++;
+                curr /= 10;
+            }
+        }
+        return result;
     }
 
     public static TheoryData<int[], int, int> TestData => new() { { [12, 54, 32, 22], 2, 4 }, { [1, 34, 7], 9, 0 }, };

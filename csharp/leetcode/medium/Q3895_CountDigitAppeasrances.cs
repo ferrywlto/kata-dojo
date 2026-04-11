f25d5f67176ee9231e1df5b469deb8f80b7e277b
Ferry To <ferry@verdantsparks.io>
2026-04-11 23:57:57 +0100

    test: Add test cases of Q3895

---
+++ csharp/leetcode/medium/Q3895_CountDigitAppeasrances.cs
@@ -0,0 +1,17 @@
+﻿public class Q3895_CountDigitAppeasrances
+{
+    public int CountDigitOccurrences(int[] nums, int digit)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int, int> TestData => new() { { [12, 54, 32, 22], 2, 4 }, { [1, 34, 7], 9, 0 }, };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int digit, int expected)
+    {
+        var actual = CountDigitOccurrences(input, digit);
+        Assert.Equal(expected, actual);
+    }
+}

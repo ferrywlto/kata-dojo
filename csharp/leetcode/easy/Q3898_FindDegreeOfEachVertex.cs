4679f1d92eab58de48d5fa83958dd10b61f6169c
Ferry To <ferry@verdantsparks.io>
2026-04-12 16:54:46 +0100

    test: Add test cases of Q3898

---
+++ csharp/leetcode/easy/Q3898_FindDegreeOfEachVertex.cs
@@ -0,0 +1,22 @@
+﻿public class Q3898_FindDegreeOfEachVertex
+{
+    public int[] FindDegrees(int[][] matrix)
+    {
+        return [];
+    }
+
+    public static TheoryData<int[][], int[]> TestData => new()
+    {
+        { [[0, 1, 1], [1, 0, 1], [1, 1, 0]], [2, 2, 2] },
+        { [[0, 1, 0], [1, 0, 0], [0, 0, 0]], [1, 1, 0] },
+        { [[0]], [0] }
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[][] input, int[] expected)
+    {
+        var actual = FindDegrees(input);
+        Assert.Equal(expected, actual);
+    }
+}

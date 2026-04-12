cf7365036587a226bf3d76ece6629caad6b1952c
Ferry To <ferry@verdantsparks.io>
2026-04-12 16:49:15 +0100

    test: All test cases of Q3899 passed

--- csharp/leetcode/medium/Q3899_AnglesOfTriangle.cs
+++ csharp/leetcode/medium/Q3899_AnglesOfTriangle.cs
@@ -2,11 +2,45 @@
 {
     public double[] InternalAngles(int[] sides)
     {
-        return [];
+        var sideA = sides[0];
+        var sideB = sides[1];
+        var sideC = sides[2];
+
+        if (sideA + sideB <= sideC ||
+            sideB + sideC <= sideA ||
+            sideC + sideA <= sideB) return [];
+
+        // set sideA on vertical axis, sideB on horizontal axis
+        // var coordA = (0, sideA);
+        // var coordB = (sideB, 0);
+        // Cater if not right angle -> Use law of cosine
+        // a² = b² + c² - 2bc cos(A)
+        // if A is 90 degree -> cos(90) = 0 thus a² = b² + c²
+        // 2bc cos(A) is the correction term for angle not being 90 degree.
+        // It comes from how much one side projects onto the other.
+
+        var angleA = RadianToDegree(FindAngle(sideA, sideB, sideC));
+        var angleB = RadianToDegree(FindAngle(sideB, sideC, sideA));
+        var angleC = 180 - angleA - angleB;
+
+        Span<double> result = [Format(angleA), Format(angleB), Format(angleC)];
+        result.Sort();
+
+        return result.ToArray();
     }
 
+    private static double FindAngle(int sideA, int sideB, int sideC) => Math.Acos((sideB * sideB + sideC * sideC - sideA * sideA) / ((double)2 * sideB * sideC));
+    private static double Format(double input) => Math.Round(input, 5);
+    private static double RadianToDegree(double input) => input * 180 / Math.PI;
+
     public static TheoryData<int[], double[]> TestData =>
-        new() { { [3, 4, 5], [36.86990, 53.13010, 90.00000] }, { [2, 4, 2], [] } };
+        new()
+        {
+            { [3, 4, 5], [36.86990, 53.13010, 90.00000] },
+            { [2, 4, 2], [] },
+            { [1, 1, 1], [60.0, 60.0, 60.0] },
+            { [1, 4, 4], [14.36151, 82.81924, 82.81924] }
+        };
 
     [Theory]
     [MemberData(nameof(TestData))]

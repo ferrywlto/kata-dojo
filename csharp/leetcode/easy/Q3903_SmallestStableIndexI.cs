ca07c9025de4568bca4cfe6af8f76a68f037d8ef
Ferry To <ferry@verdantsparks.io>
2026-04-19 17:17:31 +0100

    test: Add test cases of Q3903

---
+++ csharp/leetcode/easy/Q3903_SmallestStableIndexI.cs
@@ -0,0 +1,22 @@
+﻿public class Q3903_SmallestStableIndexI
+{
+    public int FirstStableIndex(int[] nums, int k)
+    {
+        return 0;
+    }
+
+    public static TheoryData<int[], int, int> TestData => new()
+    {
+        { [5, 0, 1, 4], 3, 3 }, { [3, 2, 1], 1, -1 }, { [0], 0, 0 },
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] input, int k, int expected)
+    {
+        var actual = FirstStableIndex(input, k);
+        Assert.Equal(expected, actual);
+    }
+
+
+}
--- csharp/leetcode/medium/Q3896_MinOperationsToTransformArrayIntoAlternatingPrime.cs
+++ csharp/leetcode/medium/Q3896_MinOperationsToTransformArrayIntoAlternatingPrime.cs
@@ -4,13 +4,13 @@ public class Q3896_MinOperationsToTransformArrayIntoAlternatingPrime
 
     // TC: O(n * g * sqrt(M + g)), n scales with length of nums, g scale with max distance from even-index value to next prime, which sqrt(M + g) is from IsPrime()
     // SC: O(p), p scales with number of prime found in the process
-    private static HashSet<int> primes = [2,3];
+    private static HashSet<int> primes = [2, 3];
     public int MinOperations(int[] nums)
     {
         var result = 0;
 
         // for primes
-        for(var i=0; i<nums.Length; i+=2)
+        for (var i = 0; i < nums.Length; i += 2)
         {
             var n = nums[i];
             if (n == 1)
@@ -27,7 +27,7 @@ public class Q3896_MinOperationsToTransformArrayIntoAlternatingPrime
                     primes.Add(n);
                     break;
                 }
-                    
+
                 result++;
                 n++;
             }
@@ -39,14 +39,14 @@ public class Q3896_MinOperationsToTransformArrayIntoAlternatingPrime
             var item = nums[i];
             if (item == 2) { result += 2; continue; }
             if (item % 2 == 0) continue;
-            
+
             if (primes.Contains(item))
             {
                 // Prime must be odd, except 2. So any prime plus one is non-prime.
                 result++;
                 continue;
             }
-            
+
             // is odd and not sure if is prime    
             if (IsPrime(item))
             {

0d93dc8d2f87332c275140eaadddea36e1a52238
Ferry To <ferry@verdantsparks.io>
2026-04-26 17:21:44 +0100

    test: All test cases of Q3909 passed

--- csharp/leetcode/medium/Q3909_CompareSumsOfBitonicParts.cs
+++ csharp/leetcode/medium/Q3909_CompareSumsOfBitonicParts.cs
@@ -1,8 +1,35 @@
 public class Q3909_CompareSumsOfBitonicParts
 {
+    // TC: O(n)
+    // SC: O(1)
     public int CompareBitonicSums(int[] nums)
     {
-        return 0;
+        long ascendingSum = nums[0];
+        long descendingSum = 0;
+        var peakIdx = -1;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                ascendingSum += nums[i];
+            }
+            else
+            {
+                peakIdx = i - 1;
+                descendingSum = nums[peakIdx];
+                break;
+            }
+        }
+
+        for (var i = peakIdx + 1; i < nums.Length; i++)
+        {
+            descendingSum += nums[i];
+        }
+
+        if (ascendingSum > descendingSum) return 0;
+        if (descendingSum > ascendingSum) return 1;
+        return -1;
     }
 
     public static TheoryData<int[], int> TestData => new()
@@ -10,6 +37,7 @@ public class Q3909_CompareSumsOfBitonicParts
         { [1, 3, 2, 1], 1 },
         { [2, 4, 5, 2], 0 },
         { [1, 2, 4, 3], -1 },
+        { [30494606,875031872,850559628,844602130], 1 },
     };
 
     [Theory]

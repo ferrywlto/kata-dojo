586cfd22480588c0659a594d4c2fb1b89b0929ea
Ferry To <ferry@verdantsparks.io>
2026-05-07 22:16:56 +0100

    test: All test cases of Q3917 passed

--- csharp/leetcode/easy/Q3917_CountIndicesWithOppositeParity.cs
+++ csharp/leetcode/easy/Q3917_CountIndicesWithOppositeParity.cs
@@ -1,8 +1,30 @@
 public class Q3917_CountIndicesWithOppositeParity
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(n) for storing the result, otherwise O(1)
     public int[] CountOppositeParity(int[] nums)
     {
-        return [];
+        var len = nums.Length;
+        Span<int> result = stackalloc int[len];
+        var oddCount = 0;
+        var evenCount = 0;
+
+        for (var i = 0; i < len; i++)
+        {
+            var idx = len - 1 - i;
+
+            if (nums[idx] % 2 == 0)
+            {
+                result[idx] = oddCount;
+                evenCount++;
+            }
+            else
+            {
+                result[idx] = evenCount;
+                oddCount++;
+            }
+        }
+        return result.ToArray();
     }
 
     public static TheoryData<int[], int[]> TestData => new()

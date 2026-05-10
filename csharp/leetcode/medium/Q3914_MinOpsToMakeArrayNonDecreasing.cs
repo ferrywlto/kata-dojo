26050dce242d65245401d7d3fe892e33f73e1f09
Ferry To <ferry@verdantsparks.io>
2026-05-10 17:49:09 +0100

    test: All test cases of Q3914 passed

--- csharp/leetcode/medium/Q3914_MinOpsToMakeArrayNonDecreasing.cs
+++ csharp/leetcode/medium/Q3914_MinOpsToMakeArrayNonDecreasing.cs
@@ -1,14 +1,25 @@
 public class Q3914_MinOpsToMakeArrayNonDecreasing
 {
+    // TC: O(n), n scale with length of nums
+    // SC: O(1) space used does not scale with input
     public long MinOperations(int[] nums)
     {
-        return 0;
+        long result = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < nums[i - 1])
+            {
+                result += nums[i - 1] - nums[i];
+            }
+        }
+        return result;
     }
 
     public static TheoryData<int[], int> TestData = new()
     {
         { [3, 3, 2, 1], 2 },
         { [5, 1, 2, 3], 4 },
+        { [12, 1, 14], 11 },
     };
 
     [Theory]

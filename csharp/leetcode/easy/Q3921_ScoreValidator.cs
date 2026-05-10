1525862f587a46cce37cc7c4609ee0ad15aaf575
Ferry To <ferry@verdantsparks.io>
2026-05-10 16:29:12 +0100

    test: All test cases of Q3821 passed

--- csharp/leetcode/easy/Q3921_ScoreValidator.cs
+++ csharp/leetcode/easy/Q3921_ScoreValidator.cs
@@ -1,14 +1,38 @@
 public class Q3921_ScoreValidator
 {
+    // TC: O(n), n scale with length of events
+    // SC: O(1)
     public int[] ScoreValidator(string[] events)
     {
-        return [];
+        var result = 0;
+        var counter = 0;
+        for (var i = 0; i < events.Length; i++)
+        {
+            var evt= events[i];
+            switch (evt)
+            {
+                case "W":
+                {
+                    if(++counter == 10)
+                        return [result, counter];
+                    break;
+                }
+                case "WD":
+                case "NB":
+                    result += 1;
+                    break;
+                default:
+                    result += int.Parse(evt);
+                    break;
+            }
+        }
+        return [result, counter];
     }
 
     public static TheoryData<string[], int[]> TestData => new()
     {
         { ["1", "4", "W", "6", "WD"], [12, 1] },
-        { ["WD", "NB", "0", "4", "4"], [10, 1] },
+        { ["WD", "NB", "0", "4", "4"], [10, 0] },
         { ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W"], [0, 10] }
     };
 

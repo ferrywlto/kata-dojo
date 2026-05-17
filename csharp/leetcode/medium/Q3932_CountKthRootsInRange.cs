fbda271c60ed907da526c8d1993e61d174a921d3
Ferry To <ferry@verdantsparks.io>
2026-05-17 23:47:10 +0100

    test: All test cases of Q3932 passed

--- csharp/leetcode/medium/Q3932_CountKthRootsInRange.cs
+++ csharp/leetcode/medium/Q3932_CountKthRootsInRange.cs
@@ -1,11 +1,38 @@
 public class Q3932_CountKthRootsInRange
 {
+    // TC: O(r-l * power)
+    // SC: O(1)
     public int CountKthRoots(int l, int r, int k)
     {
-        return 0;
+        var result = 0;
+        for (var i = 0; i <= r; i++)
+        {
+            var pow = PowK(i, k);
+            if (pow >= l && pow <= r) result++;
+            else if (pow > r) return result;
+        }
+        return result;
     }
 
-    public static TheoryData<int, int, int, int> TestData => new() { { 1, 9, 3, 2 }, { 8, 30, 2, 3 } };
+    private long PowK(int input, int power)
+    {
+        var i = 1;
+        var n = input;
+        while (i < power)
+        {
+            n *= input;
+            i++;
+        }
+        return n;
+    }
+
+    public static TheoryData<int, int, int, int> TestData => new()
+    {
+        { 1, 9, 3, 2 },
+        { 8, 30, 2, 3 },
+        { 19, 22, 1, 4},
+        { 0, 26, 2, 6},
+    };
 
     [Theory]
     [MemberData(nameof(TestData))]

215cb769fe56c128c30022bdc1d4725565502cf5
Ferry To <ferry@verdantsparks.io>
2026-05-17 22:52:22 +0100

    test: All test cases of Q3931 passed

--- csharp/leetcode/easy/Q3931_CheckAdjacentDigitDifferences.cs
+++ csharp/leetcode/easy/Q3931_CheckAdjacentDigitDifferences.cs
@@ -1,8 +1,14 @@
 public class Q3931_CheckAdjacentDigitDifferences
 {
+    // TC: O(n), n scale with length of s
+    // SC: O(1), space used does not scale with input
     public bool IsAdjacentDiffAtMostTwo(string s)
     {
-        return false;
+        for (var i = 0; i < s.Length - 1; i++)
+        {
+            if (Math.Abs(s[i] - s[i + 1]) > 2) return false;
+        }
+        return true;
     }
 
     public static TheoryData<string, bool> TestData => new() { { "132", true }, { "129", false } };

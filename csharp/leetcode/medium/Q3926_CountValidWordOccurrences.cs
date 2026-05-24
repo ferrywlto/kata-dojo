d50e3e8763b80bc68f8d813a4af32c9fc8c7325e
Ferry To <ferry@verdantsparks.io>
2026-05-24 21:06:50 +0100

    test: All test cases of Q3926 passed

--- csharp/leetcode/medium/Q3926_CountValidWordOccurrences.cs
+++ csharp/leetcode/medium/Q3926_CountValidWordOccurrences.cs
@@ -1,8 +1,43 @@
+using System.Text;
+
 public class Q3926_CountValidWordOccurrences
 {
+    // TC: O(n), n is the total length of chunks
+    // SC: O(n + q), where n is the total length in chunks and q is the number of query
     public int[] CountWordOccurrences(string[] chunks, string[] queries)
     {
-        return [];
+        var qDict = new Dictionary<string, int>(queries.Length);
+
+        for (var i = 0; i < queries.Length; i++)
+        {
+            qDict.TryAdd(queries[i], 0);
+        }
+
+        var sb = new StringBuilder(string.Concat(chunks));
+
+        for (var i = 0; i < sb.Length; i++)
+        {
+            if (sb[i] != '-') continue;
+            if (i == 0 || i == sb.Length - 1 || !char.IsAsciiLetterLower(sb[i - 1]) || !char.IsAsciiLetterLower(sb[i + 1]))
+            {
+                sb[i] = ' ';
+            }
+        }
+        // Faster version can be achieved by instead of building the full word array with Split, process each word as soon as you find its boundary in the loop above.
+        var words = sb.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var word in words)
+        {
+            if (qDict.TryGetValue(word, out int value)) qDict[word] = ++value;
+        }
+
+        var result = new int[queries.Length];
+        for (var i = 0; i < queries.Length; i++)
+        {
+            result[i] = qDict[queries[i]];
+        }
+
+        return result;
     }
 
     public static TheoryData<string[], string[], int[]> TestData => new TheoryData<string[], string[], int[]>

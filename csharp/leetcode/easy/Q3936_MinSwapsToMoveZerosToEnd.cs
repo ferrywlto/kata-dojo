9352fcbf2ffe85b9b33485781a624b7ec3c904ac
Ferry To <ferry@verdantsparks.io>
2026-05-24 18:08:17 +0100

    test: All test cases of Q3936 passed

--- csharp/leetcode/easy/Q3936_MinSwapsToMoveZerosToEnd.cs
+++ csharp/leetcode/easy/Q3936_MinSwapsToMoveZerosToEnd.cs
@@ -1,8 +1,25 @@
 public class Q3936_MinSwapsToMoveZerosToEnd
 {
+    // TC: O(n), run at most nums.Length times
+    // SC: O(1), space used does not scale with input
     public int MinimumSwaps(int[] nums)
     {
-        return 0;
+        var result = 0;
+        var head = 0;
+        var tail = nums.Length - 1;
+        while (head < tail)
+        {
+            if (nums[tail] != 0 && nums[head] == 0)
+            {
+                result++;
+                head++;
+                tail--;
+            }
+            else if (nums[tail] == 0) tail--;
+            else if (nums[head] != 0) head++;
+        }
+
+        return result;
     }
 
     public static TheoryData<int[], int> TestData => new()

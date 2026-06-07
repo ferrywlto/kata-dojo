68e653f48103d784192bdb1aafbf3ca8f956c4dc
Ferry To <ferry@verdantsparks.io>
2026-06-08 00:03:07 +0100

    test: All test cases of Q3954 passed

--- csharp/leetcode/easy/Q3954_SumOfCompatibleNumbersInRangeI.cs
+++ csharp/leetcode/easy/Q3954_SumOfCompatibleNumbersInRangeI.cs
@@ -1,14 +1,25 @@
 public class Q3954_SumOfCompatibleNumbersInRangeI
 {
+    // TC: O(n + k)
+    // SC: O(1)
     public int SumOfGoodIntegers(int n, int k)
     {
-        return 0;
+        var result = 0;
+        var start = Math.Max(0, n - k);
+        var end = n + k;
+        for (var i = start; i <= end; i++)
+        {
+            if ((n & i) == 0) result += i;
+        }
+
+        return result;
     }
 
     public static TheoryData<int, int, int> TestData => new()
     {
         { 2, 3, 10 },
         { 5, 1, 0 },
+        { 6, 2, 8 },
     };
 
     [Theory]

b577dd955f6006603fadce55d31b49bf2907c94d
Ferry To <ferry@verdantsparks.io>
2026-06-08 23:23:26 +0100

    test: All test cases of Q3955 passed

--- csharp/leetcode/medium/Q3955_ValidBinaryStringsWithCostLimit.cs
+++ csharp/leetcode/medium/Q3955_ValidBinaryStringsWithCostLimit.cs
@@ -1,8 +1,33 @@
 public class Q3955_ValidBinaryStringsWithCostLimit
 {
+    // TC: O(2^n)
+    // SC: O(n)
     public IList<string> GenerateValidStrings(int n, int k)
     {
-        return [];
+        var result = new List<string>();
+        Span<char> sb = stackalloc char[n];
+        Print(result, sb, n, k, 0, 0);
+        return result;
+    }
+
+    private void Print(List<string> list, Span<char> sb, int n, int k, int cost, int curr)
+    {
+        if (curr >= n)
+        {
+            if (cost <= k)
+            {
+                list.Add(new string(sb));
+            }
+            return;
+        }
+
+        sb[curr] = '0';
+        Print(list, sb, n, k, cost, curr + 1);
+
+        if (curr != 0 && sb[curr - 1] == '1') return;
+
+        sb[curr] = '1';
+        Print(list, sb, n, k, cost + curr, curr + 1);
     }
 
     public static TheoryData<int, int, List<string>> TestData => new()

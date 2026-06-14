7a51ba9e244b87d3752b9a291246766aa3bca176
Ferry To <ferry@verdantsparks.io>
2026-06-14 16:53:51 +0100

    test: All test cases of Q3961 passed

--- csharp/leetcode/medium/Q3961_MaxSumOfDeviceRatings.cs
+++ csharp/leetcode/medium/Q3961_MaxSumOfDeviceRatings.cs
@@ -1,8 +1,57 @@
-public class Q3961_MaxSumOfDeviceRatings
+public class Q3961_MaxSumOfDeviceRatings(ITestOutputHelper output)
 {
+    // TC: O(n log n) because of many Array.Sort()
+    // SC: O(1) all sorting are in-place
     public long MaxRatings(int[][] units)
     {
-        return 0;
+        // get max sum of min from each array
+        // each array can only move 0 to 1 item to other array once
+        foreach (var t in units)
+            Array.Sort(t);
+
+        if (units.Length == 1) return units[0][0];
+
+        // find the global smallest device, then each other device throw the smallest (1st item after sort) unit to that device if the device has more than 1 item
+        // This makes all other device rating increase while doesn't change the rating of global smallest rating device
+        var globalSmallest = int.MaxValue;
+        for (var i = 0; i < units.Length; i++)
+        {
+            for (var j = 0; j < units[i].Length; j++)
+            {
+                if (units[i][j] < globalSmallest)
+                    globalSmallest = units[i][j];
+            }
+        }
+
+        // Since every array can move the smallest item out, thus the 2nd item matters than the 1st. Therefore sort by second item.
+        Array.Sort(units, (a, b) =>
+        {
+            var c = a.Length > 1 ? a[1] : a[0];
+            var d = b.Length > 1 ? b[1] : b[0];
+            if (c > d) return 1;
+            if (c < d) return -1;
+            return 0;
+        });
+
+        // now global smallest rating device is units[0]
+        output.WriteLine($"sort: {string.Join(Environment.NewLine, units.Select(t => string.Join(',', t)))}");
+
+        // Handle the case that after all transfer the smallest rating device should have to global smallest rating
+        long result = globalSmallest;
+
+        for (var i = 1; i < units.Length; i++)
+        {
+            if(units[i].Length > 1)
+            {
+                result += units[i][1];
+            }
+            else
+            {
+                result += units[i][0];
+            }
+        }
+
+        return result;
     }
 
     public static TheoryData<int[][], long> TestData => new()

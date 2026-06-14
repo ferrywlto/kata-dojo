5f371c2c7a499abf4cf7aa8ce2039090b4016584
Ferry To <ferry@verdantsparks.io>
2026-06-14 17:22:36 +0100

    perf: Improve time from n log n to log n for Q3961

--- csharp/leetcode/medium/Q3961_MaxSumOfDeviceRatings.cs
+++ csharp/leetcode/medium/Q3961_MaxSumOfDeviceRatings.cs
@@ -1,4 +1,4 @@
-public class Q3961_MaxSumOfDeviceRatings
+public class Q3961_MaxSumOfDeviceRatings(ITestOutputHelper output)
 {
     // TC: O(n log n) because of many Array.Sort()
     // SC: O(1) all sorting are in-place
@@ -18,16 +18,19 @@ public class Q3961_MaxSumOfDeviceRatings
         // Since every array can move the smallest item out, thus the 2nd item matters than the 1st. Therefore sort by second item.
         foreach (var t in units)
         {
-            Array.Sort(t);
-            if (t[0] < globalSmallest) globalSmallest = t[0];
-            if (t.Length > 1)
+            // The improvement is here, from O(n log n) to O(n), we don't need to sort as we only want to know the min two numbers.
+            var mins = Min2(t);
+
+            output.WriteLine($"[{string.Join(',', mins)}]");
+            if (mins[0] < globalSmallest) globalSmallest = mins[0];
+            if (mins[1] != int.MaxValue)
             {
-                if (t[1] < smallestSecond) smallestSecond = t[1];
-                result += t[1];
+                if (mins[1] < smallestSecond) smallestSecond = mins[1];
+                result += mins[1];
             }
             else
             {
-                result += t[0];
+                result += mins[0];
             }
         }
 
@@ -39,12 +42,33 @@ public class Q3961_MaxSumOfDeviceRatings
         return result;
     }
 
+    private int[] Min2(int[] input)
+    {
+        var min1 = int.MaxValue;
+        var min2 = int.MaxValue;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] < min1)
+            {
+                min2 = min1;
+                min1 = input[i];
+            }
+            else if (input[i] < min2)
+            {
+                min2 = input[i];
+            }
+        }
+
+        return [min1, min2];
+    }
     public static TheoryData<int[][], long> TestData => new()
     {
         { [[1, 3], [2, 2]], 4 },
         { [[1, 2, 3], [4, 5, 6]], 6 },
         { [[5, 5, 5], [1, 1, 1]], 6 },
         { [[5],[5],[1],[4],[4]], 19 },
+        { [[2,3],[4,1],[4,1],[2,1]], 12},
     };
 
     [Theory]

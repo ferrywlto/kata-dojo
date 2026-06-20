5adfd3edff74c535c21129a2b969092d0e6840cf
Ferry To <ferry@verdantsparks.io>
2026-06-20 22:22:48 +0100

    test: All test cases of Q3965 passed

--- csharp/leetcode/medium/Q3965_FinishTimeOfTaskI.cs
+++ csharp/leetcode/medium/Q3965_FinishTimeOfTaskI.cs
@@ -1,8 +1,38 @@
 public class Q3965_FinishTimeOfTaskI
 {
+    // TC: O(n), n scales with number of edges for generating the graph. Then another n for DFS calculation for each node.
+    // SC: O(n)
     public long FinishTime(int n, int[][] edges, int[] baseTime)
     {
-        return 0;
+        Dictionary<int, HashSet<int>> graph = [];
+
+        // generate graph
+        for (var i = 0; i < edges.Length; i++)
+        {
+            if (graph.TryGetValue(edges[i][0], out var children))
+                children.Add(edges[i][1]);
+            else
+                graph.Add(edges[i][0], [edges[i][1]]);
+        }
+
+        return Calc(0, graph, baseTime);
+    }
+
+    private long Calc(int node, Dictionary<int, HashSet<int>> graph, int[] baseTime)
+    {
+        if (!graph.TryGetValue(node, out HashSet<int>? children))
+            return baseTime[node];
+
+        long min = long.MaxValue;
+        long max = long.MinValue;
+        foreach (var child in children)
+        {
+            var t = Calc(child, graph, baseTime);
+            if (t > max) max = t;
+            if (t < min) min = t;
+        }
+
+        return max + max - min + baseTime[node];
     }
 
     public static TheoryData<int, int[][], int[], long> TestData => new()

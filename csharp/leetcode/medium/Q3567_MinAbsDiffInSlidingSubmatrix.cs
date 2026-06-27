5144c09d763d8b54d1af799a44d794217fb6f648
Ferry To <ferry@verdantsparks.io>
2026-06-28 00:01:38 +0100

    test: All test cases of Q3567 passed

--- csharp/leetcode/medium/Q3567_MinAbsDiffInSlidingSubmatrix.cs
+++ csharp/leetcode/medium/Q3567_MinAbsDiffInSlidingSubmatrix.cs
@@ -1,8 +1,50 @@
 public class Q3567_MinAbsDiffInSlidingSubmatrix
 {
+    // TC: O(n^2), each possible grid run through each cell in the cell
+    // SC: O((m - k) * (n - k))
     public int[][] MinAbsDiff(int[][] grid, int k)
     {
-        return [];
+        var m = grid.Length - k + 1;
+        var n = grid[0].Length - k + 1;
+
+        var result = new int[m][];
+
+        for (var row = 0; row < m; row++)
+        {
+            result[row] = new int[n];
+            for (var col = 0; col < n; col++)
+            {
+                result[row][col] = SubMatrix(grid, row, col, k);
+            }
+        }
+        return result;
+    }
+
+    private int SubMatrix(int[][] input, int startX, int startY, int size)
+    {
+        Span<int> arr = stackalloc int[size * size];
+
+        int minDiff = int.MaxValue;
+        for (var row = startX; row < startX + size; row++)
+        {
+            for (var col = startY; col < startY + size; col++)
+            {
+                var cell = input[row][col];
+                arr[(size * (row - startX)) + (col - startY)] = cell;
+            }
+        }
+        arr.Sort();
+
+        for (var i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i + 1] == arr[i]) continue;
+
+            var diff = Math.Abs(arr[i + 1] - arr[i]);
+            if (diff < minDiff) minDiff = diff;
+        }
+
+        if (minDiff == int.MaxValue) return 0;
+        return minDiff;
     }
 
     public static TheoryData<int[][], int, int[][]> TestData => new()

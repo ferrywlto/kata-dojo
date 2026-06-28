52273e5857ae98af50daa2d0b59cc8b039c5e718
Ferry To <ferry@verdantsparks.io>
2026-06-29 00:43:41 +0100

    test: All test cases of Q3968 passed

--- csharp/leetcode/medium/Q3968_MaxManhattanDistanceAfterAllMoves.cs
+++ csharp/leetcode/medium/Q3968_MaxManhattanDistanceAfterAllMoves.cs
@@ -1,8 +1,28 @@
 public class Q3968_MaxManhattanDistanceAfterAllMoves
 {
+    // TC: O(n), n scales with length of moves
+    // SC: O(1)
     public int MaxDistance(string moves)
     {
-        return 0;
+        // 0 - UP
+        // 1 - DOWN
+        // 2 - LEFT
+        // 3 - RIGHT
+        // 4 - _
+        Span<int> count = stackalloc int[5];
+
+        foreach (var c in moves)
+        {
+            switch (c)
+            {
+                case 'U': count[0]++; break;
+                case 'D': count[1]++; break;
+                case 'L': count[2]++; break;
+                case 'R': count[3]++; break;
+                default: count[4]++; break;
+            }
+        }
+        return Math.Abs(count[0] - count[1]) + Math.Abs(count[2] - count[3]) + count[4];
     }
 
     public static TheoryData<string, int> TestData => new()

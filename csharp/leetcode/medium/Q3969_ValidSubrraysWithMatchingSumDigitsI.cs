3e8cca7c04d1f1ab46511f1889ee4af1d5c0a826
Ferry To <ferry@verdantsparks.io>
2026-07-05 17:53:52 +0100

    test: All test cases of Q3969 passed

--- csharp/leetcode/medium/Q3969_ValidSubrraysWithMatchingSumDigitsI.cs
+++ csharp/leetcode/medium/Q3969_ValidSubrraysWithMatchingSumDigitsI.cs
@@ -1,14 +1,39 @@
 public class Q3969_ValidSubarraysWithMatchingSumDigitsI
 {
+    // TC: O(n^2)
+    // SC: O(1)
+    // Since the interim result is not used after calculation, using rolling sum is faster than triangular prefix sum.
     public int CountValidSubarrays(int[] nums, int x)
     {
-        return 0;
+        var result = 0;
+        var len = nums.Length;
+        for (var start = 0; start < len; start++)
+        {
+            var sum = 0L;
+            for (var end = start; end < len; end++)
+            {
+                sum += nums[end];
+                if (IsValidSum(sum, x)) result++;
+            }
+        }
+
+        return result;
+    }
+
+    private bool IsValidSum(long input, int x)
+    {
+        var lastDigit = input % 10;
+        if (lastDigit != x) return false;
+
+        while (input >= 10) input /= 10;
+        return x == input;
     }
 
     public static TheoryData<int[], int, int> TestData => new()
     {
         { [1, 100, 1], 1, 4 },
         { [1], 2, 0 },
+        { [1000000000,1,1000000000,1,1000000000,1,1000000000], 3, 2},
     };
 
     [Theory]

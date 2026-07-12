c09296c56e9fafdda170eff5312b9970a88bf192
Ferry To <ferry@verdantsparks.io>
2026-07-12 21:22:40 +0100

    test: All test cases of Q3982 passed

--- csharp/leetcode/easy/Q3982_SumOfIntegersWithMaxDigitRange.cs
+++ csharp/leetcode/easy/Q3982_SumOfIntegersWithMaxDigitRange.cs
@@ -2,7 +2,31 @@ public class Q3982_SumOfIntegersWithMaxDigitRange
 {
     public int MaxDigitRange(int[] nums)
     {
-        return 0;
+        Span<int> buckets = stackalloc int[10];
+        // range must be 0 - 9
+        var maxDiff = int.MinValue;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var digitMax = int.MinValue;
+            var digitMin = int.MaxValue;
+
+            var num = nums[i];
+            while (num > 0)
+            {
+                var digit = num % 10;
+                if (digit > digitMax) digitMax = digit;
+                if (digit < digitMin) digitMin = digit;
+                num /= 10;
+            }
+
+            var diff = digitMax - digitMin;
+            buckets[diff] += nums[i];
+
+            if (diff > maxDiff) maxDiff = diff;
+        }
+
+        return buckets[maxDiff];
     }
     public static TheoryData<int[], int> TestData => new()
     {

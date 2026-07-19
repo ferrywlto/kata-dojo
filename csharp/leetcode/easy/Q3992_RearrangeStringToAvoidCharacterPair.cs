ff9cfca837144700d4e313fc0cf45f5b0a3a29e5
Ferry To <ferry@verdantsparks.io>
2026-07-19 19:59:16 +0100

    test: All test cases of Q3992 passed

--- csharp/leetcode/easy/Q3992_RearrangeStringToAvoidCharacterPair.cs
+++ csharp/leetcode/easy/Q3992_RearrangeStringToAvoidCharacterPair.cs
@@ -1,15 +1,28 @@
+using System.Text;
+
 public class Q3992_RearrangeStringToAvoidCharacterPair
 {
     public string RearrangeString(string s, char x, char y)
     {
-        return string.Empty;
+        var sb_t = new StringBuilder();
+        var sb_x = new StringBuilder();
+        var sb_s = new StringBuilder();
+
+        foreach (var ch in s)
+        {
+            if (ch == x) sb_x.Append(ch);
+            else if (ch == y) sb_t.Append(ch);
+            else sb_s.Append(ch);
+        }
+
+        return sb_t.Append(sb_s).Append(sb_x).ToString();
     }
 
     public static TheoryData<string, char, char, string> TestData => new()
     {
         { "aabc", 'a', 'c', "cbaa" },
-        { "dcab", 'd', 'b', "cabd" },
-        { "axe", 'o', 'x', "axe" }
+        { "dcab", 'd', 'b', "bcad" },
+        { "axe", 'o', 'x', "xae" }
     };
 
     [Theory]

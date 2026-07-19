79586cbcde10ba9b404496402f31d8e859ff45f5
Ferry To <ferry@verdantsparks.io>
2026-07-19 23:47:34 +0100

    test: All test cases of Q3997 passed

--- csharp/leetcode/medium/Q3997_CountDominantNodesInBinaryTree.cs
+++ csharp/leetcode/medium/Q3997_CountDominantNodesInBinaryTree.cs
@@ -1,14 +1,36 @@
 public class Q3997_CountDominantNodesInBinaryTree
 {
+    // TC: O(n)
+    // SC: O(1)
     public int CountDominantNodes(TreeNode root)
     {
-        return 0;
+        var (_, count) = GetMax(root);
+        return count;
+    }
+
+    private (int max, int dominantCount) GetMax(TreeNode? node)
+    {
+        if (node is null) return (0, 0);
+
+        // IsLeaf
+        if (node.left is null && node.right is null)
+            return (node.val, 1);
+
+        var (leftMax, leftDominantCount) = GetMax(node.left);
+        var (rightMax, rightDominantCount) = GetMax(node.right);
+        var subtreeMax = Math.Max(leftMax, rightMax);
+
+        if (node.val >= subtreeMax)
+            return (node.val, 1 + leftDominantCount + rightDominantCount);
+
+        return (subtreeMax, leftDominantCount + rightDominantCount);
     }
 
     public static TheoryData<TreeNode, int> TestData => new()
     {
         { TreeNode.FromLevelOrderingIntArray([5, 3, 8, 2, 4, 7, 1])!, 5 },
         { TreeNode.FromLevelOrderingIntArray([1, 2, 3, 1, 2])!, 4 },
+        { TreeNode.FromLevelOrderingIntArray([7, 1])!, 2 },
     };
 
     [Theory]

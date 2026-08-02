8e4b3b021f472e7a3f09b0dfd0017666cdf62064
Ferry To <ferry@verdantsparks.io>
2026-08-02 14:12:16 +0100

    test: All test cases of Q4001 passed

--- csharp/leetcode/medium/Q4001_AggregateTwoTimeSeries.cs
+++ csharp/leetcode/medium/Q4001_AggregateTwoTimeSeries.cs
@@ -1,8 +1,47 @@
 public class Q4001_AggregateTwoTimeSeries
 {
+    // TC: O(n)
+    // SC: O(n) for the result, O(1) otherwise
     public IList<IList<int>> AggregateTimeSeries(int[][] series1, int[][] series2)
     {
-        return [];
+        int idx1 = 0, idx2 = 0;
+        var result = new List<IList<int>>();
+
+        while (idx1 < series1.Length || idx2 < series2.Length)
+        {
+            if (idx1 == series1.Length)
+            {
+                result.Add([series2[idx2][0], series2[idx2][1]]);
+                idx2++;
+            }
+            else if (idx2 == series2.Length)
+            {
+                result.Add([series1[idx1][0], series1[idx1][1]]);
+                idx1++;
+            }
+            // ideal case, time is the same, then add both
+            else if (series1[idx1][0] == series2[idx2][0])
+            {
+                result.Add([series1[idx1][0], series1[idx1][1] + series2[idx2][1]]);
+                idx1++;
+                idx2++;
+            }
+            // move the lower first
+            else if (series1[idx1][0] < series2[idx2][0])
+            {
+                // take series1 time and value
+                result.Add([series1[idx1][0], series1[idx1][1] + series2[idx2][1]]);
+                idx1++;
+            }
+            // series1 > series2
+            else
+            {
+                result.Add([series2[idx2][0], series2[idx2][1] + series1[idx1][1]]);
+                idx2++;
+            }
+        }
+
+        return result;
     }
 
     public static TheoryData<int[][], int[][], IList<IList<int>>> TestData => new()

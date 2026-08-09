d3597495d0b6664e707435696cda4210488cf3de
Ferry To <ferry@verdantsparks.io>
2026-08-09 14:51:13 +0100

    test: All test cases of Q4014 passed

--- csharp/leetcode/medium/Q4014_MinTotalPriceAfterApplyingDiscounts.cs
+++ csharp/leetcode/medium/Q4014_MinTotalPriceAfterApplyingDiscounts.cs
@@ -1,8 +1,34 @@
 public class Q4014_MinTotalPriceAfterApplyingDiscounts
 {
+    // TC: O(p log p + d)
+    // SC: O(d)
     public double MinPrice(int[] prices, int[] discounts)
     {
-        return 0;
+        Array.Sort(prices);
+
+        // Since discounts can only be 0 - 100, using a histogram avoid sort.
+        Span<int> d = stackalloc int[101];
+        foreach (var discount in discounts) d[discount]++;
+
+        var pIdx = prices.Length - 1;
+        var highestDiscountIdx = 100;
+
+        long result = 0;
+        while (pIdx >= 0)
+        {
+            while (highestDiscountIdx >= 0 && d[highestDiscountIdx] == 0) highestDiscountIdx--;
+
+            // trick: multiply 100 first for integer arithmetic, then only do floating point division once at the end.
+            var percent = 100;
+            if (highestDiscountIdx >= 0)
+            {
+                d[highestDiscountIdx]--;
+                percent -= highestDiscountIdx;
+            }
+
+            result += prices[pIdx--] * percent;
+        }
+        return result / 100.0;
     }
 
     public static TheoryData<int[], int[], double> TestData => new()

f297ba307c315fcf61d5d0197f35198a3c913b46
Ferry To <ferry@verdantsparks.io>
2026-08-16 23:03:03 +0100

    test: All test cases of Q4025 passed

--- csharp/leetcode/medium/Q4025_MinimizeMaxWaitingTimeAtSynchronizedTrafficLights.cs
+++ csharp/leetcode/medium/Q4025_MinimizeMaxWaitingTimeAtSynchronizedTrafficLights.cs
@@ -1,8 +1,27 @@
 public class Q4025_MinimizeMaxWaitingTimeAtSynchronizedTrafficLights
 {
+    /*
+     * Each car must be assigned to exactly one traffic light.
+     * Multiple cars may be assigned to the same traffic light.
+     * Any number of cars may cross the same traffic light simultaneously while it is green. Cars do not block or delay one another.
+     *
+     * Due to the above fact, we can only consider the light that have the longest duration of green light, thus minimal wait time.
+     */
+    // lights means green light duration
+    // TC: O(n)
+    // SC: O(1)
     public int MinPenalty(int period, int[] lights, int[] arrivalTime)
     {
-        return 0;
+        var result = int.MinValue;
+        var longestGreenDuration = lights.Max();
+        foreach (var time in arrivalTime)
+        {
+            var timeInPeriodArrival = time % period;
+            var penalty = period - timeInPeriodArrival;
+            if (timeInPeriodArrival >= longestGreenDuration && penalty >= result)
+                result = penalty;
+        }
+        return result == int.MinValue ? 0 : result;
     }
 
     public static TheoryData<int, int[], int[], int> TestData => new()
@@ -10,6 +29,7 @@ public class Q4025_MinimizeMaxWaitingTimeAtSynchronizedTrafficLights
         { 8, [2, 3], [2, 5, 8, 11], 5 },
         { 10, [3, 6, 8], [4, 9, 15], 1 },
         { 5, [2], [2, 3, 4, 5, 6], 3 },
+        { 2, [1, 1, 1, 1, 1], [30], 0}
     };
 
     [Theory]

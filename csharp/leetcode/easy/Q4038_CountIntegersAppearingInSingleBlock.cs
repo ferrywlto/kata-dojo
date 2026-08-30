3eff27fd45df0fea6306ab583a0f2e363911a80e
Ferry To <ferry@verdantsparks.io>
2026-08-30 17:58:04 +0100

    test: All test cases of Q4038

--- csharp/leetcode/easy/Q4038_CountIntegersAppearingInSingleBlock.cs
+++ csharp/leetcode/easy/Q4038_CountIntegersAppearingInSingleBlock.cs
@@ -1,8 +1,42 @@
 public class Q4038_CountIntegersAppearingInSingleBlock
 {
+    // TC: O(n)
+    // SC: O(1)
     public int CountSpecialIntegers(int[] nums)
     {
-        return 0;
+        Span<int> seen = stackalloc int[101];
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            // record a new truck
+            if (nums[i] != nums[i - 1])
+            {
+                seen[nums[i - 1]] = seen[nums[i - 1]] switch
+                {
+                    // 0 means unseen
+                    // 1 means seen
+                    // -1 means seen more than once
+                    0 => 1,
+                    1 => -1,
+                    _ => seen[nums[i - 1]]
+                };
+            }
+        }
+
+        seen[nums[^1]] = seen[nums[^1]] switch
+        {
+            // Handle the last element
+            0 => 1,
+            1 => -1,
+            _ => seen[nums[^1]]
+        };
+
+        var result = 0;
+        foreach (var t in seen)
+            if (t == 1)
+                result++;
+
+        return result;
     }
 
     public static TheoryData<int[], int> TestData => new()

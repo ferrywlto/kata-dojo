85ca62462c8e81c227a8cdfa12be1a785ce740eb
Ferry To <ferry@verdantsparks.io>
2026-09-06 15:37:14 +0100

    test: All test cases of Q4043 passed

--- csharp/leetcode/easy/Q4043_CountRotationsWithExactlyKEqualAdjacentPairs.cs
+++ csharp/leetcode/easy/Q4043_CountRotationsWithExactlyKEqualAdjacentPairs.cs
@@ -1,8 +1,32 @@
 public class Q4043_CountRotationsWithExactlyKEqualAdjacentPairs
 {
+    // TC: O(n)
+    // SC: O(1)
     public int CountRotations(string s, int k)
     {
-        return 0;
+        var len = s.Length - 1;
+        var result = 0;
+        var score = 0;
+
+        // handle the score of no rotation
+        for (var i = 0; i < len; i++)
+        {
+            if (s[i] == s[i + 1]) score++;
+        }
+
+        if (score == k) result++;
+
+        for (var i = 0; i < len; i++)
+        {
+            if (s[i] == s[i + 1]) score--;
+
+            // Using modulus two-pointers can save time from accessing string builder. Save also space.
+            if (s[(i + len) % s.Length] == s[(i + len + 1) % s.Length]) score++;
+
+            if (score == k) result++;
+        }
+
+        return result;
     }
 
     public static TheoryData<string, int, int> TestData => new()

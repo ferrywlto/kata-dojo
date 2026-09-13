902c3f9180e8ff1ac3fff071dd4270cd3801bb63
Ferry To <ferrywlto@gmail.com>
2026-09-13 23:25:18 +0100

    test: All test cases of Q4052 passed

--- csharp/leetcode/easy/Q4052_CyclicallyShiftRowsAndColumns.cs
+++ csharp/leetcode/easy/Q4052_CyclicallyShiftRowsAndColumns.cs
@@ -1,8 +1,72 @@
 public class Q4052_CyclicallyShiftRowsAndColumns
 {
+    // TC: O(n^2)
+    // SC: O(n), because the grid is always a square, the temp space used is either row length or col length
     public int[][] CyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift)
     {
-        return [];
+        for (var r = 0; r < rowShift.Length; r++)
+        {
+            RowShiftLeft(grid, r, rowShift[r]);
+        }
+
+        for (var c = 0; c < colShift.Length; c++)
+        {
+            ColShiftUp(grid, c, colShift[c]);
+        }
+
+        return grid;
+    }
+
+    private void RowShiftLeft(int[][] grid, int rowIdx, int n)
+    {
+        var row = grid[rowIdx];
+        var result = new int[row.Length];
+        for (var i = 0; i < row.Length; i++)
+        {
+            var newColIdx = (i - n + row.Length) % row.Length;
+            result[newColIdx] = row[i];
+        }
+
+        grid[rowIdx] = result;
+    }
+
+    private void ColShiftUp(int[][] grid, int colIdx, int n)
+    {
+        var rowCount = grid.Length;
+        // clone the column
+        var cloned = new int[rowCount];
+        for (var rowIdx = 0; rowIdx < rowCount; rowIdx++)
+        {
+            cloned[rowIdx] = grid[rowIdx][colIdx];
+        }
+
+        // replace by shifted idx
+        for (var rowIdx = 0; rowIdx < rowCount; rowIdx++)
+        {
+            var newRowIdx = (rowIdx - n + rowCount) % rowCount;
+
+            grid[newRowIdx][colIdx] = cloned[rowIdx];
+        }
+    }
+
+    [Fact]
+    public void TestColShiftUp()
+    {
+        int[][] arr = [[1], [2], [3], [4], [5]];
+
+        ColShiftUp(arr, 0, 3);
+
+        Assert.Equal([[4], [5], [1], [2], [3]], arr);
+    }
+
+    [Fact]
+    public void TestRowShiftLeft()
+    {
+        int[][] arr = [[1, 2, 3, 4, 5]];
+
+        RowShiftLeft(arr, 0, 3);
+
+        Assert.Equal([4, 5, 1, 2, 3], arr[0]);
     }
 
     public static TheoryData<int, int[][], int[], int[], int[][]> TestData => new()
